8e53545f7d06c53816e08d144944f6852f549fe3
mariohamann <mario.hamann@virtual-identity.com>
2023-04-24 17:23:58 +0200

    chore: let storybook build

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,10 +1,19 @@
-const path = require('path');
+const tsconfigPaths = require("vite-tsconfig-paths");
 module.exports = {
   "stories": ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-mdx-gfm"],
   "framework": {
     "name": "@storybook/web-components-vite",
     "options": {}
+  },
+  core: {
+    builder: '@storybook/builder-vite', // 👈 The builder enabled here.
+  },
+  async viteFinal(config) {
+    return {
+      ...config,
+      plugins: [...config.plugins, tsconfigPaths.default()],
+    };
   },
   "docs": {
     docs: true,
