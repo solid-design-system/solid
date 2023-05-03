395e4158d41c36439adce7f9cac7ec16e50ce6df
mariohamann <mario.hamann@virtual-identity.com>
2023-05-03 09:12:28 +0200

    ci(close #53): wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
-import { terser } from 'rollup-plugin-terser';
 import path from 'path';
+import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 import pkgMinifyHTML from 'rollup-plugin-minify-html-literals';
+import summary from 'rollup-plugin-summary';
 
 process.env.VITE_BUILD = 'lib';
 
@@ -10,27 +12,42 @@ const minifyHTML = (pkgMinifyHTML as any).default;
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    minify: 'terser',
+    // minify: 'terser',
     outDir: 'dist',
-    // lib: {
-    //   entry: path.resolve(__dirname, 'src/index.ts'),
-    //   name: 'Solid Components',
-    //   fileName: (format) => `solid-components.${format}.js`,
-    // },
+    lib: {
+      entry: path.resolve(__dirname, 'src/solid-components.ts'),
+      name: 'Solid Components',
+      fileName: (format) => `${format}/solid-components.js`,
+    },
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'src/solid-components.ts'),
-      },
+      // input: {
+      //   main: path.resolve(__dirname, 'src/solid-components.ts'),
+      // },
       // external: ['@floating-ui/dom', '@shoelace-style/animations', 'lit', 'qr-creator'],
-      output: {
-        entryFileNames: 'solid-components.js',
-        inlineDynamicImports: true,
-        assetFileNames: 'solid-components[extname]',
+      output:
+      {
+        // Modern JS bundles (no JS compilation, ES module output)
+        format: 'esm',
+        chunkFileNames: 'es/[name].js',
+        // entryFileNames: 'solid-components.js',
+        dir: 'dist/components',
+        // inlineDynamicImports: true,
+        // plugins: [htmlPlugin.api.addOutput('modern')],
       },
-      plugins: [
-        process.env['npm_lifecycle_event'] === 'build' ? minifyHTML() as any : console.log('ℹ️ Minification skipped'),
-        process.env['npm_lifecycle_event'] === 'build' ? terser({ compress: { defaults: true, passes: 2 }, mangle: true }) as any : console.log('ℹ️ Terser skipped'),
-      ],
+      plugins: process.env['npm_lifecycle_event'] === 'build' ? [
+        // Resolve bare module specifiers to relative paths
+        resolve(),
+        // Minify HTML template literals
+        minifyHTML(),
+        terser({
+          ecma: 2020,
+          module: true,
+          compress: { defaults: true, passes: 2 },
+          mangle: true
+        }),
+        // Print bundle summary
+        summary({ showGzippedSize: true }),
+      ] : [],
     },
   },
 });
