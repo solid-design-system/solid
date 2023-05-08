12ec8688e905cb265373094da0528bec580d42bf
mariohamann <mario.hamann@virtual-identity.com>
2023-05-08 21:09:41 +0200

    build: fix storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,24 @@
 import { setCustomElementsManifest } from '@storybook/web-components';
-// import '../dist/themes/final.css';
 import 'normalize.css';
 
-fetch('/custom-elements.json')
-  .then(res => res.json())
-  .then(customElements => { setCustomElementsManifest(customElements); console.log('Custom elements manifest set', customElements) })
+async function loadCustomElements() {
+  let customElements;
+
+  try {
+    const response = await fetch('/custom-elements.json');
+
+    if (!response.ok) {
+      throw new Error(`Fetch failed with status ${response.status}`);
+    }
+
+    customElements = await response.json();
+  } catch (error) {
+    console.log('Failed to fetch custom-elements.json, importing from dist:', error.message);
+    customElements = await import('../dist/custom-elements.json');
+  }
+
+  setCustomElementsManifest(customElements);
+  console.log('Custom elements manifest set', customElements);
+}
+
+loadCustomElements();
