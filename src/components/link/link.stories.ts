610148b9a9fb18b0aea2c355ad07eebd1d0f793b
mariohamann <mario.hamann@virtual-identity.com>
2023-06-20 12:27:59 +0200

    docs: improve stories + add bold story

--- src/components/link/link.stories.ts
+++ src/components/link/link.stories.ts
@@ -27,11 +27,63 @@ export const Default = {
 };
 
 /**
+ * Unset `href` to make the link disabled.
+ */
+
+export const InvertedAndDisabled = {
+  name: 'Disabled × Inverted',
+  parameters: { controls: { exclude: ['size', 'default', 'href', 'inverted'] } },
+  render: (args: any) =>
+    generateTemplate({
+      axis: {
+        x: {
+          type: 'attribute',
+          name: 'href',
+          values: ['#', '']
+        },
+        y: {
+          type: 'attribute',
+          name: 'inverted',
+          values: [false, true]
+        }
+      },
+      args,
+      options: {
+        templateBackgrounds: { alternate: 'y', colors: ['white', '#00358E'] }
+      }
+    })
+};
+
+/**
+ * You can make links bold by setting `<b>` tags around the text in the main slot.
+ */
+
+export const BoldInMainSlot = {
+  name: 'Bold',
+  parameters: { controls: { exclude: ['default'] } },
+  render: (args: any) =>
+    generateTemplate({
+      axis: {
+        x: {
+          type: 'slot',
+          name: 'default',
+          title: 'main-slot',
+          values: ['Link', '<b>Link</b>']
+        }
+      },
+      args,
+      options: {
+        templateBackgrounds: { alternate: 'y', colors: ['white', '#00358E'] }
+      }
+    })
+};
+
+/**
  * Use the `icon-left` and `icon-right` slots to add icons. They automatically adapt the size.
  */
 
-export const IconsAndSize = {
-  name: 'Icons × Size',
+export const SizeAndIconSlots = {
+  name: 'Size × Icon Slots',
   parameters: { controls: { exclude: ['size', 'default', 'icon-left', 'icon-right'] } },
   render: (args: any) =>
     generateTemplate({
@@ -62,39 +114,11 @@ export const IconsAndSize = {
 };
 
 /**
- * Unset `href` to make the link disabled.
- */
-
-export const IconsAndDisabled = {
-  name: 'Icons × Disabled',
-  parameters: { controls: { exclude: ['size', 'default', 'href', 'inverted'] } },
-  render: (args: any) =>
-    generateTemplate({
-      axis: {
-        x: {
-          type: 'attribute',
-          name: 'href',
-          values: ['#', '']
-        },
-        y: {
-          type: 'attribute',
-          name: 'inverted',
-          values: [false, true]
-        }
-      },
-      args,
-      options: {
-        templateBackgrounds: { alternate: 'y', colors: ['white', '#00358E'] }
-      }
-    })
-};
-
-/**
  * Icons automatically adapt to the link's invertedness.
  */
 
-export const IconsAndInverted = {
-  name: 'Icons × Inverted',
+export const InvertedAndIconSlots = {
+  name: 'Inverted × Icon Slots',
   parameters: { controls: { exclude: ['inverted', 'default', 'icon-left', 'icon-right'] } },
   render: (args: any) =>
     generateTemplate({
@@ -134,7 +158,8 @@ export const IconsAndInverted = {
  * - If false or not provided, the icon will be displayed inline within the text.
  */
 
-export const Standalone = {
+export const StandaloneAndIconSlots = {
+  name: 'Standalone × Icon Slots',
   parameters: { controls: { exclude: ['standalone', 'default', 'icon-left', 'icon-right'] } },
   render: (args: any) => {
     return html`
