c7142af7f6b69c34f148a7ccfe51e6a54590315e
Azra Efendic <azra.efendic@embo.localdomain>
2023-06-29 00:56:53 +0200

    feat: accordion

---
+++ src/components/accordion/accordion.stories.ts
@@ -0,0 +1,71 @@
+import '../../solid-components';
+import { storybookDefaults, storybookTemplate } from '../../../scripts/storybook/helper';
+
+const { argTypes, args } = storybookDefaults('sd-accordion');
+const { generateTemplate } = storybookTemplate('sd-accordion');
+
+export default {
+  title: 'Components/sd-accordion',
+  component: 'sd-accordion',
+  args: {
+    ...args,
+    'default-slot': '<slot-comp></slot-comp>',
+    'summary-slot': '<span slot="summary">Accordion</span>'
+  },
+  argTypes
+};
+
+/**
+ * Use as vertical stack of interactive headlines that can be used to toggle the display of additional information; each element can be collapsed with only a brief caption visible, or 'expanded to display the entire content.
+ */
+
+export const Default = {
+  render: (args: any) => {
+    return generateTemplate({
+      args,
+      constants: { type: 'template', name: 'width', value: '<div style="width: 300px">%TEMPLATE%</div>' }
+    });
+  }
+};
+
+/**
+ * An accordion item can either be collapsed or open.
+ */
+export const States = {
+  parameters: { controls: { exclude: 'open' } },
+  render: (args: any) => {
+    return generateTemplate({
+      axis: {
+        y: { type: 'attribute', name: 'open' }
+      },
+      args,
+      constants: { type: 'template', name: 'width', value: '<div style="width: 300px">%TEMPLATE%</div>' }
+    });
+  }
+};
+
+/**
+ * Use the expand-icon and collapse-icon slots to change the expand and collapse icons, respectively.
+ * To disable the details, override the rotate property on the summary-icon part as shown below:
+ * ```
+ * sl-details.custom-icons::part(summary-icon) {
+ *   rotate: none;
+ * }
+ * ```
+ */
+
+export const Slots = {
+  parameters: {
+    controls: { exclude: ['expand-icon', 'collapse-icon'] }
+  },
+  render: (args: any) => {
+    return generateTemplate({
+      args: {
+        ...args,
+        'expand-icon-slot': '<span slot="expand-icon">✅</span>',
+        'collapse-icon-slot': '<span slot="collapse-icon">❎</span>'
+      },
+      constants: { type: 'template', name: 'width', value: '<div style="width: 300px">%TEMPLATE%</div>' }
+    });
+  }
+};
---
+++ src/components/accordion/accordion.ts
@@ -0,0 +1,209 @@
+import '../icon/icon';
+import { animateTo, shimKeyframesHeightAuto, stopAnimations } from '../../internal/animate';
+import { css, html } from 'lit';
+import { customElement, property, query } from 'lit/decorators.js';
+import { getAnimation, setDefaultAnimation } from '../../utilities/animation-registry';
+import { LocalizeController } from '../../utilities/localize';
+import { waitForEvent } from '../../internal/event';
+import { watch } from '../../internal/watch';
+import cx from 'classix';
+import SolidElement from '../../internal/solid-element';
+
+/**
+ * @summary Accordion show a brief summary and expand to show additional content.
+ * @documentation https://solid.union-investment.com/[storybook-link]/accordion
+ * @status stable
+ * @since 2.0
+ *
+ * @dependency sd-icon
+ *
+ * @slot - The accordion' main content.
+ * @slot summary - The accordion' summary. Alternatively, you can use the `summary` attribute.
+ * @slot expand-icon - Optional expand icon to use instead of the default. Works best with `<sd-icon>`.
+ * @slot collapse-icon - Optional collapse icon to use instead of the default. Works best with `<sd-icon>`.
+ *
+ * @event sd-show - Emitted when the accordion opens.
+ * @event sd-after-show - Emitted after the accordion opens and all animations are complete.
+ * @event sd-hide - Emitted when the accordion closes.
+ * @event sd-after-hide - Emitted after the accordion closes and all animations are complete.
+ *
+ * @csspart base - The component's base wrapper.
+ * @csspart header - The header that wraps both the summary and the expand/collapse icon.
+ * @csspart summary - The container that wraps the summary.
+ * @csspart summary-icon - The container that wraps the expand/collapse icons.
+ * @csspart content - The accordion content.
+ *
+ * @animation accordion.show - The animation to use when showing accordion. You can use `height: auto` with this animation.
+ * @animation accordion.hide - The animation to use when hiding accordion. You can use `height: auto` with this animation.
+ */
+@customElement('sd-accordion')
+export default class SdAccordion extends SolidElement {
+  // static styles: CSSResultGroup = styles;
+
+  private readonly localize = new LocalizeController(this);
+
+  @query('[part="base"]') accordion: HTMLElement;
+  @query('[part="header"]') header: HTMLElement;
+  @query('[part="body"]') body: HTMLElement;
+
+  /**
+   * Indicates whether or not the accordion is open. You can toggle this attribute to show and hide the accordion, or you
+   * can use the `show()` and `hide()` methods and this attribute will reflect the accordion' open state.
+   */
+  @property({ type: Boolean, reflect: true }) open = false;
+
+  /** The summary to show in the header. If you need to display HTML, use the `summary` slot instead. */
+  @property() summary: string;
+
+  firstUpdated() {
+    this.body.hidden = !this.open;
+    this.body.style.height = this.open ? 'auto' : '0';
+  }
+
+  private handleSummaryClick() {
+    if (this.open) {
+      this.hide();
+    } else {
+      this.show();
+    }
+  }
+
+  private handleSummaryKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+
+      if (this.open) {
+        this.hide();
+      } else {
+        this.show();
+      }
+    }
+
+    if (event.key === 'ArrowUp' || event.key === 'ArrowLeft') {
+      event.preventDefault();
+      this.hide();
+    }
+
+    if (event.key === 'ArrowDown' || event.key === 'ArrowRight') {
+      event.preventDefault();
+      this.show();
+    }
+  }
+
+  @watch('open', { waitUntilFirstUpdate: true })
+  async handleOpenChange() {
+    if (this.open) {
+      // Show
+      const slShow = this.emit('sd-show', { cancelable: true });
+      if (slShow.defaultPrevented) {
+        this.open = false;
+        return;
+      }
+
+      await stopAnimations(this.body);
+      this.body.hidden = false;
+
+      const { keyframes, options } = getAnimation(this, 'accordion.show', { dir: this.localize.dir() });
+      await animateTo(this.body, shimKeyframesHeightAuto(keyframes, this.body.scrollHeight), options);
+      this.body.style.height = 'auto';
+
+      this.emit('sd-after-show');
+    } else {
+      // Hide
+      const slHide = this.emit('sd-hide', { cancelable: true });
+      if (slHide.defaultPrevented) {
+        this.open = true;
+        return;
+      }
+
+      await stopAnimations(this.body);
+
+      const { keyframes, options } = getAnimation(this, 'accordion.hide', { dir: this.localize.dir() });
+      await animateTo(this.body, shimKeyframesHeightAuto(keyframes, this.body.scrollHeight), options);
+      this.body.hidden = true;
+      this.body.style.height = 'auto';
+
+      this.emit('sd-after-hide');
+    }
+  }
+
+  /** Shows the accordion. */
+  async show() {
+    this.open = true;
+    return waitForEvent(this, 'sd-after-show');
+  }
+
+  /** Hides the accordion */
+  async hide() {
+    this.open = false;
+    return waitForEvent(this, 'sd-after-hide');
+  }
+
+  render() {
+    return html`
+      <div part="base" class=${cx('border border-neutral-400')}>
+        <header
+          part="header"
+          id="header"
+          class=${cx(
+            'flex text-base gap-4 font-bold items-center cursor-pointer select-none px-4 py-3 focus-visible:focus-outline',
+            this.open ? 'bg-white text-accent hover:bg-neutral-200' : 'text-primary bg-neutral-100 hover:bg-neutral-200'
+          )}
+          role="button"
+          aria-expanded=${this.open ? 'true' : 'false'}
+          aria-controls="content"
+          @click=${this.handleSummaryClick}
+          @keydown=${this.handleSummaryKeyDown}
+        >
+          <slot name="summary" part="summary" class="flex flex-auto items-center ">${this.summary}</slot>
+
+          <span
+            part="summary-icon"
+            class=${cx('flex flex-grow-0 flex-shrink-0 flex-auto items-center transition ease-in-out')}
+          >
+            <slot name="expand-icon" class=${cx(this.open && 'hidden')}>
+              <sd-icon library="system" name="chevron-down"></sd-icon>
+            </slot>
+            <slot name="collapse-icon" class=${cx(!this.open && 'hidden')}>
+              <sd-icon library="system" name="chevron-up"></sd-icon>
+            </slot>
+          </span>
+        </header>
+        <div part="body" class="overflow-hidden">
+          <slot part="content" id="content" class="block px-4 py-6" role="region" aria-labelledby="header"></slot>
+        </div>
+      </div>
+    `;
+  }
+
+  static styles = [
+    SolidElement.styles,
+    css`
+      :host {
+        display: block;
+      }
+    `
+  ];
+}
+
+setDefaultAnimation('accordion.show', {
+  keyframes: [
+    { height: '0', opacity: '0' },
+    { height: 'auto', opacity: '1' }
+  ],
+  options: { duration: 250, easing: 'linear' }
+});
+
+setDefaultAnimation('accordion.hide', {
+  keyframes: [
+    { height: 'auto', opacity: '1' },
+    { height: '0', opacity: '0' }
+  ],
+  options: { duration: 250, easing: 'linear' }
+});
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'sd-accordion': SdAccordion;
+  }
+}
