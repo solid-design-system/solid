a458a72cf30f9d958ba3ca576c147f76cbf4f842
Azra Efendic <azra.efendic@azravi.localdomain>
2023-07-03 10:27:29 +0200

    fix: update test

--- src/components/accordion-group/accordion-group.test.ts
+++ src/components/accordion-group/accordion-group.test.ts
@@ -1,16 +1,31 @@
-import { expect, fixture, html } from '@open-wc/testing';
+import { expect, fixture, html, waitUntil } from '@open-wc/testing';
 import type SdAccordionGroup from './accordion-group';
 
 describe('<sd-accordion-group>', () => {
   it('should close other accordions when closeOthers is true', async () => {
     const el = await fixture<SdAccordionGroup>(
       html`
-        <sd-accordion-group closeOthers
-          ><sd-accordion></sd-accordion><sd-accordion></sd-accordion><sd-accordion></sd-accordion
-          ><sd-accordion></sd-accordion><sd-accordion></sd-accordion
-        ></sd-accordion-group>
+        <sd-accordion-group closeOthers>
+          <sd-accordion>Accordion</sd-accordion>
+          <sd-accordion>Accordion</sd-accordion>
+          <sd-accordion>Accordion</sd-accordion>
+          <sd-accordion>Accordion</sd-accordion>
+        </sd-accordion-group>
       `
     );
-    expect(el).to.exist;
+
+    // Get all the accordions
+    const accordions = el.querySelectorAll('sd-accordion');
+
+    // Open the first accordion
+    accordions[0].open = true;
+
+    // Wait for the animation to finish
+    await waitUntil(() => !accordions[1].open && !accordions[2].open && !accordions[3].open);
+
+    // Check if other accordions are closed
+    expect(accordions[1].open).to.be.false;
+    expect(accordions[2].open).to.be.false;
+    expect(accordions[3].open).to.be.false;
   });
 });
