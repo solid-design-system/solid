93476e6e78b9479dc4a9eee73706150c0e31969b
mariohamann <mario.hamann@virtual-identity.com>
2023-07-04 16:44:08 +0200

    feat: setup release & prettier

--- packages/tokens/src/create-theme.cjs
+++ packages/tokens/src/create-theme.cjs
@@ -1,74 +1,69 @@
-const tokens = require("./tokens.json");
+const tokens = require('./tokens.json');
 
-const hexToRgb = (hex) => {
-	const hexCharacters = "a-f\\d";
-	const match3or4Hex = `#?[${hexCharacters}]{3}[${hexCharacters}]?`;
-	const match6or8Hex = `#?[${hexCharacters}]{6}([${hexCharacters}]{2})?`;
-	const nonHexChars = new RegExp(`[^#${hexCharacters}]`, "gi");
-	const validHexSize = new RegExp(`^${match3or4Hex}$|^${match6or8Hex}$`, "i");
+const hexToRgb = hex => {
+  const hexCharacters = 'a-f\\d';
+  const match3or4Hex = `#?[${hexCharacters}]{3}[${hexCharacters}]?`;
+  const match6or8Hex = `#?[${hexCharacters}]{6}([${hexCharacters}]{2})?`;
+  const nonHexChars = new RegExp(`[^#${hexCharacters}]`, 'gi');
+  const validHexSize = new RegExp(`^${match3or4Hex}$|^${match6or8Hex}$`, 'i');
 
-	if (
-		typeof hex !== "string" ||
-		nonHexChars.test(hex) ||
-		!validHexSize.test(hex)
-	) {
-		throw new TypeError("Expected a valid hex string");
-	}
+  if (typeof hex !== 'string' || nonHexChars.test(hex) || !validHexSize.test(hex)) {
+    throw new TypeError('Expected a valid hex string');
+  }
 
-	hex = hex.replace(/^#/, "");
-	let alphaFromHex = 1;
+  hex = hex.replace(/^#/, '');
+  let alphaFromHex = 1;
 
-	if (hex.length === 8) {
-		alphaFromHex = Number.parseInt(hex.slice(6, 8), 16) / 255;
-		hex = hex.slice(0, 6);
-	}
+  if (hex.length === 8) {
+    alphaFromHex = Number.parseInt(hex.slice(6, 8), 16) / 255;
+    hex = hex.slice(0, 6);
+  }
 
-	if (hex.length === 4) {
-		alphaFromHex = Number.parseInt(hex.slice(3, 4).repeat(2), 16) / 255;
-		hex = hex.slice(0, 3);
-	}
+  if (hex.length === 4) {
+    alphaFromHex = Number.parseInt(hex.slice(3, 4).repeat(2), 16) / 255;
+    hex = hex.slice(0, 3);
+  }
 
-	if (hex.length === 3) {
-		hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-	}
+  if (hex.length === 3) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+  }
 
-	const number = Number.parseInt(hex, 16);
-	const red = number >> 16;
-	const green = (number >> 8) & 255;
-	const blue = number & 255;
+  const number = Number.parseInt(hex, 16);
+  const red = number >> 16;
+  const green = (number >> 8) & 255;
+  const blue = number & 255;
 
-	return [red, green, blue];
+  return [red, green, blue];
 };
 
-const resolveValue = (value) => {
-	// if value starts with "{" it's a reference.
-	if (value.startsWith("{")) {
-		const path = value.replace("{", "").replace("}", "").split(".");
-		const resolvePath = (path, set) => {
-			let resolvedValue = tokens[set];
-			path.forEach((p) => {
-				resolvedValue = resolvedValue[p];
-			});
-			return resolvedValue;
-		};
-		const resolvedValue =
-			resolvePath(path, "UI Core") || resolvePath(path, "UI Semantic");
-		return resolveValue(resolvedValue.value);
-	} else {
-		return sanitizeValue(value);
-	}
+const resolveValue = value => {
+  // if value starts with "{" it's a reference.
+  if (value.startsWith('{')) {
+    const path = value.replace('{', '').replace('}', '').split('.');
+    const resolvePath = (path, set) => {
+      let resolvedValue = tokens[set];
+      path.forEach(p => {
+        resolvedValue = resolvedValue[p];
+      });
+      return resolvedValue;
+    };
+    const resolvedValue = resolvePath(path, 'UI Core') || resolvePath(path, 'UI Semantic');
+    return resolveValue(resolvedValue.value);
+  } else {
+    return sanitizeValue(value);
+  }
 };
 
-const sanitizeValue = (value) => {
-	value = value.replaceAll("\b", "");
-	return value;
+const sanitizeValue = value => {
+  value = value.replaceAll('\b', '');
+  return value;
 };
 
 const sanitizeKey = (value, cssVariable = true) => {
-	value = value.replaceAll("\b", "");
-	value = value.replaceAll(",", ".");
-	if (cssVariable) value = value.replaceAll(".", "-");
-	return value;
+  value = value.replaceAll('\b', '');
+  value = value.replaceAll(',', '.');
+  if (cssVariable) value = value.replaceAll('.', '-');
+  return value;
 };
 
 /**
@@ -79,182 +74,166 @@ const sanitizeKey = (value, cssVariable = true) => {
  * @returns {Object} result - The reformatted color object with grouped colors and default values set.
  *
  */
-const reformatColors = (obj) => {
-	let result = {};
+const reformatColors = obj => {
+  let result = {};
 
-	for (const key in obj) {
-		if (obj.hasOwnProperty(key)) {
-			const colorValue = obj[key];
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      const colorValue = obj[key];
 
-			if (key.includes("-")) {
-				const [colorName, shade] = key.split("-");
-				if (!result[colorName]) {
-					result[colorName] = {};
-				} else if (typeof result[colorName] !== "object") {
-					// Set the default value when the first shade is found
-					if (result[colorName]) {
-						const defaultColor = result[colorName];
-						result[colorName] = {};
-						result[colorName]["DEFAULT"] = defaultColor;
-					}
-				}
+      if (key.includes('-')) {
+        const [colorName, shade] = key.split('-');
+        if (!result[colorName]) {
+          result[colorName] = {};
+        } else if (typeof result[colorName] !== 'object') {
+          // Set the default value when the first shade is found
+          if (result[colorName]) {
+            const defaultColor = result[colorName];
+            result[colorName] = {};
+            result[colorName]['DEFAULT'] = defaultColor;
+          }
+        }
 
-				result[colorName][shade] = colorValue;
-			} else if (!result[key]) {
-				result[key] = colorValue;
-			}
-		}
-	}
+        result[colorName][shade] = colorValue;
+      } else if (!result[key]) {
+        result[key] = colorValue;
+      }
+    }
+  }
 
-	result["transparent"] = "transparent";
+  result['transparent'] = 'transparent';
 
-	return result;
+  return result;
 };
 
 // Go through the opacity object that has the format object: {50: { value: string, comment: string }, 60: {value: string, comment: string}}
 // convert it to an interable array first in the format [ {name: '50', value: string, comment: string}, {name: '60', value: string, comment: string}]
 const getOpacities = () => {
-	let result = {};
-	Object.entries(tokens["UI Semantic"].opacity)
-		.map(([name, value]) => ({ name, ...value }))
-		.forEach(({ name, value, description }) => {
-			// add the opacity to the theme
-			// convert e.g. "90%" to "0.9"
-			const convertedValue = resolveValue(value).replace("%", "") / 100;
-			result[sanitizeValue(name)] = `var(--sd-opacity-${sanitizeValue(
-				name
-			)}, ${convertedValue})${
-				description ? ` /* ${description} */` : ""
-			}`;
-		});
-	return result;
+  let result = {};
+  Object.entries(tokens['UI Semantic'].opacity)
+    .map(([name, value]) => ({ name, ...value }))
+    .forEach(({ name, value, description }) => {
+      // add the opacity to the theme
+      // convert e.g. "90%" to "0.9"
+      const convertedValue = resolveValue(value).replace('%', '') / 100;
+      result[sanitizeValue(name)] = `var(--sd-opacity-${sanitizeValue(name)}, ${convertedValue})${
+        description ? ` /* ${description} */` : ''
+      }`;
+    });
+  return result;
 };
 
 // Go through the opacity object that has the format object: {50: { value: string, comment: string }, 60: {value: string, comment: string}}
 // convert it to an interable array first in the format [ {name: '50', value: string, comment: string}, {name: '60', value: string, comment: string}]
 const getBorderRadius = () => {
-	let result = {};
-	Object.entries(tokens["UI Semantic"].rounded)
-		.map(([name, value]) => ({ name, ...value }))
-		.forEach(({ name, value, description }) => {
-			result[
-				sanitizeValue(name)
-			] = `var(--sd-border-radius-${name}, ${resolveValue(value)})${
-				description ? ` /* ${description} */` : ""
-			}`;
-		});
-	return result;
+  let result = {};
+  Object.entries(tokens['UI Semantic'].rounded)
+    .map(([name, value]) => ({ name, ...value }))
+    .forEach(({ name, value, description }) => {
+      result[sanitizeValue(name)] = `var(--sd-border-radius-${name}, ${resolveValue(value)})${
+        description ? ` /* ${description} */` : ''
+      }`;
+    });
+  return result;
 };
 
 // Get all Object.entries(tokens['UI Semantic'].spacing and Object.entries(tokens['UI Semantic'].sizing values
 // Merge them and create spacing tokens for TailwindCSS
 const getSpacings = () => {
-	let result = {};
-	Object.entries({
-		...tokens["UI Semantic"].spacing,
-		0: { value: "0px", description: "No spacing (manually added)" },
-		auto: {
-			value: "auto",
-			description: "Automatic spacing (manually added)",
-		},
-	})
-		.map(([name, value]) => ({ name, ...value }))
-		.concat(
-			Object.entries(tokens["UI Semantic"].sizing).map(
-				([name, value]) => ({ name, ...value })
-			)
-		)
-		.forEach(({ name, value, description }) => {
-			// add the spacing to the theme
-			result[sanitizeKey(name, false)] = `var(--sd-spacing-${sanitizeKey(
-				name
-			)}, ${resolveValue(value)})${
-				description ? ` /* ${description} */` : ""
-			}`;
-		});
-	return result;
+  let result = {};
+  Object.entries({
+    ...tokens['UI Semantic'].spacing,
+    0: { value: '0px', description: 'No spacing (manually added)' },
+    auto: {
+      value: 'auto',
+      description: 'Automatic spacing (manually added)'
+    }
+  })
+    .map(([name, value]) => ({ name, ...value }))
+    .concat(Object.entries(tokens['UI Semantic'].sizing).map(([name, value]) => ({ name, ...value })))
+    .forEach(({ name, value, description }) => {
+      // add the spacing to the theme
+      result[sanitizeKey(name, false)] = `var(--sd-spacing-${sanitizeKey(name)}, ${resolveValue(value)})${
+        description ? ` /* ${description} */` : ''
+      }`;
+    });
+  return result;
 };
 
 // Get all Object.entries(tokens['UI Semantic'].background and set them as background colors
 const getColors = (name, cssVariableScope) => {
-	let result = {};
-	Object.entries(tokens["UI Semantic"][name])
-		.map(([name, value]) => ({ name, ...value }))
-		.forEach(({ name, value, description }) => {
-			if (name === "transparent") return;
-			const color = hexToRgb(resolveValue(value)).join(" ");
-			// add the background color to the theme
-			result[sanitizeValue(name)] = `rgb(var(--sd-color-${sanitizeKey(
-				sanitizeValue(name)
-			)}, ${color}) / <alpha-value>)${
-				description ? ` /* ${description} */` : ""
-			}`;
-		});
+  let result = {};
+  Object.entries(tokens['UI Semantic'][name])
+    .map(([name, value]) => ({ name, ...value }))
+    .forEach(({ name, value, description }) => {
+      if (name === 'transparent') return;
+      const color = hexToRgb(resolveValue(value)).join(' ');
+      // add the background color to the theme
+      result[sanitizeValue(name)] = `rgb(var(--sd-color-${sanitizeKey(
+        sanitizeValue(name)
+      )}, ${color}) / <alpha-value>)${description ? ` /* ${description} */` : ''}`;
+    });
 
-	result = reformatColors(result);
-	return {
-		...result,
-		inherit: "inherit",
-		current: "currentColor",
-		transparent: "transparent",
-	};
+  result = reformatColors(result);
+  return {
+    ...result,
+    inherit: 'inherit',
+    current: 'currentColor',
+    transparent: 'transparent'
+  };
 };
 
 const getCoreTokensByType = (tokenType, cssVariableScope) => {
-	let result = {};
-	const obj = tokens["UI Core"];
-	const keys = Object.keys(obj);
+  let result = {};
+  const obj = tokens['UI Core'];
+  const keys = Object.keys(obj);
 
-	keys.filter((key) => {
-		const { type } = obj[key];
+  keys.filter(key => {
+    const { type } = obj[key];
 
-		if (type === tokenType) {
-			const keyWithoutUtility = key.split("-").slice(1).join("-");
-			const description = obj[key]["description"];
-			result[
-				keyWithoutUtility
-			] = `var(--sd-${cssVariableScope}-${sanitizeKey(
-				keyWithoutUtility
-			)}, ${obj[key]["value"]})${
-				description ? ` /* ${description} */` : ""
-			}`;
-		}
-	});
+    if (type === tokenType) {
+      const keyWithoutUtility = key.split('-').slice(1).join('-');
+      const description = obj[key]['description'];
+      result[keyWithoutUtility] = `var(--sd-${cssVariableScope}-${sanitizeKey(keyWithoutUtility)}, ${
+        obj[key]['value']
+      })${description ? ` /* ${description} */` : ''}`;
+    }
+  });
 
-	return result;
+  return result;
 };
 
 /** @type {import('tailwindcss').Config} */
 const config = {
-	content: [], // make sure to override this later
-	theme: {
-		// Checkout https://tailwindcss.com/docs/configuration#core-plugins for a list of core plugins
-		accentColor: { ...getColors("text", "text-color") },
-		backgroundColor: { ...getColors("background", "background-color") },
-		borderColor: { ...getColors("border", "border-color") },
-		borderRadius: { ...getBorderRadius() },
-		boxShadowColor: { ...getColors("background", "background-color") },
-		caretColor: { ...getColors("text", "text-color") },
-		fill: { ...getColors("icon-fill", "fill-color") },
-		fontFamily: {},
-		fontSize: { ...getCoreTokensByType("fontSizes", "font-size") },
-		fontStyle: {},
-		fontWeight: { normal: "400", bold: "700" }, // Tokens currently provide "Bk" and "Bold" which doesn't help anything
-		gradientColorStops: { ...getColors("background", "background-color") },
-		lineHeight: { ...getCoreTokensByType("lineHeights", "line-height") },
-		opacity: { ...getOpacities() },
-		outlineColor: { ...getColors("border", "border-color") },
-		placeholderColor: { ...getColors("text", "text-color") },
-		ringColor: { ...getColors("border", "border-color") },
-		ringOffsetColor: { ...getColors("background", "background-color") },
-		space: { ...getSpacings() },
-		spacing: { ...getSpacings() },
-		stroke: { ...getColors("border", "border-color") },
-		tracking: {},
-		textColor: { ...getColors("text", "text-color") },
-		textDecorationColor: { ...getColors("text", "text-color") },
-	},
+  content: [], // make sure to override this later
+  theme: {
+    // Checkout https://tailwindcss.com/docs/configuration#core-plugins for a list of core plugins
+    accentColor: { ...getColors('text', 'text-color') },
+    backgroundColor: { ...getColors('background', 'background-color') },
+    borderColor: { ...getColors('border', 'border-color') },
+    borderRadius: { ...getBorderRadius() },
+    boxShadowColor: { ...getColors('background', 'background-color') },
+    caretColor: { ...getColors('text', 'text-color') },
+    fill: { ...getColors('icon-fill', 'fill-color') },
+    fontFamily: {},
+    fontSize: { ...getCoreTokensByType('fontSizes', 'font-size') },
+    fontStyle: {},
+    fontWeight: { normal: '400', bold: '700' }, // Tokens currently provide "Bk" and "Bold" which doesn't help anything
+    gradientColorStops: { ...getColors('background', 'background-color') },
+    lineHeight: { ...getCoreTokensByType('lineHeights', 'line-height') },
+    opacity: { ...getOpacities() },
+    outlineColor: { ...getColors('border', 'border-color') },
+    placeholderColor: { ...getColors('text', 'text-color') },
+    ringColor: { ...getColors('border', 'border-color') },
+    ringOffsetColor: { ...getColors('background', 'background-color') },
+    space: { ...getSpacings() },
+    spacing: { ...getSpacings() },
+    stroke: { ...getColors('border', 'border-color') },
+    tracking: {},
+    textColor: { ...getColors('text', 'text-color') },
+    textDecorationColor: { ...getColors('text', 'text-color') }
+  }
 };
 
-const theme = config["theme"];
+const theme = config['theme'];
 module.exports = theme;
--- packages/tokens/tailwind.config.cjs
+++ packages/tokens/tailwind.config.cjs
@@ -1,6 +1,6 @@
-const theme = require("./src/create-theme.cjs");
+const theme = require('./src/create-theme.cjs');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	theme,
+  theme
 };
