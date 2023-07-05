04ed8ec1bbc2a0479eda2193f0df5110d4e358d0
mariohamann <mario.hamann@virtual-identity.com>
2023-07-05 13:20:04 +0200

    format: fix style

--- packages/components/scripts/node-get-sizes.mjs
+++ packages/components/scripts/node-get-sizes.mjs
@@ -36,4 +36,4 @@ export const getSizes = () => {
     uncompressed: newRealSize,
     gzip: newGzipSize
   };
-}
+};
--- packages/components/scripts/node-write-sizes.mjs
+++ packages/components/scripts/node-write-sizes.mjs
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import {getOutputs, getSizes} from './node-get-sizes.mjs';
+import { getOutputs, getSizes } from './node-get-sizes.mjs';
 
 /**
  * 1. Update CHANGELOG.md with new bundle sizes
@@ -19,12 +19,12 @@ fs.readFile('./src/docs/General/Changelog.mdx', 'utf8', (err, data) => {
   const result = additionalContent + '\n\n' + data;
 
   // Write the updated changelog back to CHANGELOG.md
-  fs.writeFile('./src/docs/General/Changelog.mdx', result, 'utf8', (err) => {
+  fs.writeFile('./src/docs/General/Changelog.mdx', result, 'utf8', err => {
     if (err) return console.log(err);
   });
 });
 
-console.log('📝 Updated CHANGELOG.md with new bundle sizes.')
+console.log('📝 Updated CHANGELOG.md with new bundle sizes.');
 
 /**
  * 2. Update package.json with new bundle sizes
@@ -34,8 +34,8 @@ let packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
 packageJson.meta.bundleSizeInKb = getSizes();
 
-fs.writeFile('./package.json', JSON.stringify(packageJson, null, 2) + '\n', 'utf8', (err) => {
+fs.writeFile('./package.json', JSON.stringify(packageJson, null, 2) + '\n', 'utf8', err => {
   if (err) return console.log(err);
 });
 
-console.log('📦 Updated package.json with new bundle sizes.')
+console.log('📦 Updated package.json with new bundle sizes.');
