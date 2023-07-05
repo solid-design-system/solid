9c765955090a7888790c1ec513ae25cb6661276f
mariohamann <mario.hamann@virtual-identity.com>
2023-07-05 18:01:27 +0200

    docs: improve and fix stats in changelog

--- packages/components/scripts/node-write-sizes.mjs
+++ packages/components/scripts/node-write-sizes.mjs
@@ -5,18 +5,32 @@ import { getOutputs, getSizes } from './node-get-sizes.mjs';
  * 1. Update CHANGELOG.md with new bundle sizes
  */
 
-const additionalContent = `### Stats
-* ${getOutputs().uncompressed}
-* ${getOutputs().gzip}`;
+const pkg = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+const newVersion = pkg.version;
 
-// Read the generated CHANGELOG.md
+const additionalContent = `### 📈 Stats\n* ${getOutputs().uncompressed}\n* ${getOutputs().gzip}\n\n`;
+
 fs.readFile('./src/docs/General/Changelog.mdx', 'utf8', (err, data) => {
   if (err) {
     return console.log(err);
   }
 
-  // Append additional content to the start of the changelog
-  const result = additionalContent + '\n\n' + data;
+  // Check if the new version header exists
+  const newVersionHeader = `# [@solid-design-system/components-v${newVersion}]`;
+  const nextVersionHeaderRegex = /# @solid-design-system\/components-v\d+\.\d+\.\d+/g;
+
+  let result;
+
+  if (data.startsWith(newVersionHeader)) {
+    // If the new version header exists, find the next version header and insert the content before it
+    result = data.replace(nextVersionHeaderRegex, (match, offset) => {
+      if (offset === 0) return match;  // Ignore the first match (new version header)
+      return additionalContent + match;
+    });
+  } else {
+    // If the new version header doesn't exist, insert the content at the start of the file
+    result = additionalContent + data;
+  }
 
   // Write the updated changelog back to CHANGELOG.md
   fs.writeFile('./src/docs/General/Changelog.mdx', result, 'utf8', err => {
@@ -38,4 +52,4 @@ fs.writeFile('./package.json', JSON.stringify(packageJson, null, 2) + '\n', 'utf
   if (err) return console.log(err);
 });
 
-console.log('📦 Updated package.json with new bundle sizes.');
+console.log('📝 Updated package.json with new bundle sizes.');
