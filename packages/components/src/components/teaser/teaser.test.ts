bab8486a75464dda7f26fc0f936945a8cabff0fc
Azra Efendic <azra.efendic@azravi.localdomain>
2023-07-27 09:24:22 +0200

    fix: test file

--- packages/components/src/components/teaser/teaser.test.ts
+++ packages/components/src/components/teaser/teaser.test.ts
@@ -8,16 +8,5 @@ describe('<sd-teaser>', () => {
       expect(teaser).to.exist;
     });
   });
-
-  describe('when provided orientation, variant, distribution ratio, and breakpoints', () => {
-    it('should render with the correct orientation', async () => {
-      const teaser = await fixture<SdTeaser>(html` <sd-teaser orientation="horizontal"></sd-teaser> `);
-      expect(teaser.orientation).to.equal('horizontal');
-    });
-
-    it('should render with the correct variant', async () => {
-      const teaser = await fixture<SdTeaser>(html` <sd-teaser variant="primary"></sd-teaser> `);
-      expect(teaser.variant).to.equal('primary');
-    });
-  });
+  // Tests will follow...
 });
