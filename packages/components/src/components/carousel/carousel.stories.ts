3821942c89c91e618b17b3328aefb2a5ff8bbf42
Vahid Menu Nesro <vahid.nesro@virtual-identity.com>
2023-08-03 10:45:37 +0200

    feat: adding carousel buttons

--- packages/components/src/components/carousel/carousel.stories.ts
+++ packages/components/src/components/carousel/carousel.stories.ts
@@ -1,14 +1,12 @@
 import '../../solid-components';
-import { storybookDefaults, storybookHelpers, storybookTemplate } from '../../../scripts/storybook/helper';
-
-const { argTypes, parameters } = storybookDefaults('sd-carousel');
-const { overrideArgs } = storybookHelpers('sd-carousel');
+import { storybookDefaults, storybookTemplate } from '../../../scripts/storybook/helper';
+const { args, argTypes, parameters } = storybookDefaults('sd-carousel');
 const { generateTemplate } = storybookTemplate('sd-carousel');
 
 export default {
   title: 'Components/sd-carousel',
   component: 'sd-carousel',
-  args: overrideArgs([{ type: 'slot', name: 'default', value: '<slot-comp></slot-comp>' }]),
+  args,
   argTypes,
   parameters: { ...parameters }
 };
--- packages/components/src/components/carousel/carousel.ts
+++ packages/components/src/components/carousel/carousel.ts
@@ -1,12 +1,32 @@
 import '../icon/icon';
-import { customElement } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 import { html } from 'lit';
 import SolidElement from '../../internal/solid-element';
 
 @customElement('sd-carousel')
 export default class SdCarousel extends SolidElement {
+  /** Determines the counting system for the carousel. */
+  @property({ attribute: 'variant', reflect: true }) variant: 'dot' | 'numeral' = 'numeral';
+  /** Inverts the carousel */
+  @property({ type: Boolean, reflect: true }) inverted = false;
+
+  /** Specifies how many slides should be shown at a given time.  */
+  @property({ type: Number, attribute: 'slides-per-page' }) slidesPerPage = 1;
+
   render() {
-    return html` <slot></slot> `;
+    return html`
+      <span class="flex flex-row">
+        <button class="text-primary rotate-90 hover:text-neutral-500">
+          <sd-icon library="system" name="chevron-down"></sd-icon>
+        </button>
+        <span>
+          <span class="h-3 w-3 inline-block bg-accent rounded-full"></span>
+        </span>
+        <button class="text-primary rotate-90 hover:text-neutral-500">
+          <sd-icon library="system" name="chevron-up"></sd-icon>
+        </button>
+      </span>
+    `;
   }
 }
 
