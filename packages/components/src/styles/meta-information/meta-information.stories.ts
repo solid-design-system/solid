95832323a3f877c357b85118a0c8fcccc504be1e
Yigit, Özlem (UIT) <Oezlem.Yigit@union-investment.de>
2023-08-14 18:45:10 +0200

    feat: added meta-information styles

--- packages/components/src/styles/meta-information/meta-information.stories.ts
+++ packages/components/src/styles/meta-information/meta-information.stories.ts
@@ -1,152 +1,124 @@
 import '../../solid-components';
-import { classMap } from 'lit/directives/class-map.js';
-import { html } from 'lit-html';
+
+import { storybookDefaults, storybookHelpers, storybookTemplate } from '../../../scripts/storybook/helper';
+
+const { argTypes, parameters } = storybookDefaults('sd-meta-information');
+const { overrideArgs } = storybookHelpers('sd-meta-information');
+const { generateTemplate } = storybookTemplate('sd-meta-information');
 
 /**
  * List of meta information like file size, date or whatever needed.
  */
 
 export default {
-  title: 'Styles/sd-meta-information-01',
-  args: {
-    // boolean for "modifier" arg
-    inverted: false,
-    size: 'lg',
-    color: 'default',
-    pipe: false,
-    highlight: false
-  },
-  argTypes: {
-    inverted: {
-      name: 'inverted',
-      control: 'boolean',
-      description: 'Inverts the headline. <br><code>boolean</code>'
-    },
-    // slot: {
-    //   name: 'slot',
-    //   control: 'text',
-    //   description:
-    //     'this argType is only to show the slot in storybook. You will not need it in your code. <br><code>string</code>'
-    // },
-    size: {
-      name: 'size',
-      control: 'radio',
-      options: ['lg', 'sm'],
-      description:
-        "The display's size. Small can be used as an alternative in tight spaces. <br><code>'lg'</code> &nbsp; <code>'sm'</code>"
-    },
-    color: {
-      name: 'color',
-      control: 'radio',
-      options: ['default', 'additional'],
-      description: "The color of the meta information. <br><code>'default'</code> &nbsp; <code>'additional'</code>"
-    },
-    pipe: {
-      name: 'pipe',
-      control: 'boolean',
-      description: 'Adds a pipe between the meta information and the date. <br><code>boolean</code>'
-    },
-    highlight: {
-      name: 'highlight',
-      control: 'boolean',
-      description: 'The green accent color can be used to highlight parts of the text. <br><code>boolean</code>'
+  title: 'Styles/sd-meta-information',
+  component: 'sd-meta-information',
+  parameters: {
+    ...parameters,
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/Um1B3fI5fvdFVJv6LO7kZG/Meta-Information?type=design&node-id=0-1&mode=design&t=I2fDQn7HjSc75K1V-0'
     }
-  }
-};
-
-const getClasses = args => {
-  return {
-    'sd-meta-information': true,
-    'sd-meta-information--inverted': args.inverted,
-    'sd-meta-information--highlight': args.highlight,
-
-    'sd-meta-information--size-sm': args.metaInformation === 'sm',
-    'sd-meta-information--size-lg': args.metaInformation === 'lg',
-
-    'sd-meta-information--color-default': args.color === 'default',
-    'sd-meta-information--color-additional': args.color === 'additional',
-
-    'sd-meta-information--pipe': args.pipe
-  };
+  },
+  args: overrideArgs({ type: 'slot', name: 'default', value: '11. August 2023' }),
+  argTypes
 };
 
 /**
- * This is the the typography section for all kind of styles and sizes of the Headline.
+ * Default: This shows sd-meta-information in its default state.
  */
+
 export const Default = {
-  // args: {
-  //   slot: 'Lorem ipsum'
-  // },
-  render: (args: { size: string; highlight: boolean }) => {
-    return html`<div class=${classMap(getClasses({ ...args, metaInformation: `${args.size}` }))}>
-      ${args.highlight ? html`Lorem <mark>Ipsum</mark>` : 'Lorem Ipsum'}
-    </div>`;
-  }
-};
-
-export const SizesAndInverted = {
-  name: 'Sizes x Inverted',
-  parameters: { controls: { exclude: ['size', 'inverted'] } },
-  args: {},
-  render: (args: { size: string }) => {
-    return html`
-      <div class=${classMap(getClasses({ ...args, metaInformation: 'lg' }))}>text-lg</div>
-      <div class=${classMap(getClasses({ ...args, metaInformation: 'sm' }))}>text-sm</div>
-    `;
-    // axis: {
-    //   x: { type: 'attribute', name: 'size' },
-    //   y: { type: 'attribute', name: 'inverted', values: [false, true] }
-    // },
-    // options: {
-    //   templateBackgrounds: { alternate: 'y', colors: ['white', '#00358E'] }
-    // }
-  }
-};
-
-export const SizesAndColor = {
-  name: 'Sizes x Color',
-  parameters: { controls: { exclude: ['size', 'inverted'] } },
-  args: {},
-  render: (args: { size: string }) => {
-    return html`
-      <div class=${classMap(getClasses({ ...args, metaInformation: 'lg' }))}>text-lg</div>
-      <div class=${classMap(getClasses({ ...args, metaInformation: 'sm' }))}>text-sm</div>
-    `;
-    // axis: {
-    //   x: { type: 'attribute', name: 'size' },
-    //   y: { type: 'attribute', name: 'inverted', values: [false, true] }
-    // },
-    // options: {
-    //   templateBackgrounds: { alternate: 'y', colors: ['white', '#00358E'] }
-    // }
+  render: (args: any) => {
+    return generateTemplate({
+      options: { templateContent: '<time class="%CLASSES%" datetime="2023-08-11">%SLOT%</time>' },
+      args
+    });
   }
 };
 
 /**
- * This is the the typography section for all kind of styles and sizes of the Display.
+ * The sd-meta-information in all possible combinations of `variant` and `size`.
  */
-export const Highlight = {
-  name: 'Highlight x Inverted',
-  parameters: { controls: { exclude: ['size', 'highlight', 'inverted'] } },
-  render: (args: { size: string; highlight: boolean }) => {
-    return html`
-      <div class=${classMap(getClasses({ ...args, metaInformation: 'lg', highlight: true }))}>
-        Icon left lorem ipsum <mark>dolor</mark> sit amet
-      </div>
-      <div class=${classMap(getClasses({ ...args, metaInformation: 'sm', highlight: true }))}>
-        Icon left lorem ipsum <mark>dolor</mark> sit amet
-      </div>
-    `;
+
+export const VariantAndSize = {
+  name: 'Variant × Size',
+  parameters: { controls: { exclude: ['sd-meta-information--color-additional', 'sd-meta-information--size-...'] } },
+  render: (args: any) => {
+    return generateTemplate({
+      axis: {
+        x: { type: 'attribute', name: 'sd-meta-information--color-additional' },
+        y: { type: 'attribute', name: 'sd-meta-information--size-...' }
+      },
+      args
+    });
   }
 };
+
+/**
+ * The sd-meta-information in all possible combinations of `variant` and `inverted`.
+ */
+
+export const VariantAndInverted = {
+  name: 'Variant × Inverted',
+  parameters: { controls: { exclude: ['sd-meta-information--color-additional', 'sd-meta-information--inverted'] } },
+  render: (args: any) => {
+    return generateTemplate({
+      axis: {
+        x: { type: 'attribute', name: 'sd-meta-information--color-additional' },
+        y: { type: 'attribute', name: 'sd-meta-information--inverted', values: [false, true] }
+      },
+      args,
+      options: {
+        templateBackgrounds: { alternate: 'y', colors: ['white', '#00358E'] }
+      }
+    });
+  }
+};
+
+/**
+ * Use the `inverted` class to make the sd-meta-information with inverted colors.
+ */
+
+export const Inverted = {
+  parameters: {
+    controls: {
+      exclude: [
+        'sd-meta-information--size-...',
+        'sd-meta-information--inverted',
+        'sd-meta-information--color-additional'
+      ]
+    }
+  },
+  render: (args: any) => {
+    return generateTemplate({
+      axis: {
+        x: [
+          { type: 'attribute', name: 'sd-meta-information--size-...' },
+          { type: 'attribute', name: 'sd-meta-information--color-additional' },
+          { type: 'attribute', name: 'sd-meta-information--pipe' }
+        ]
+      },
+      constants: { type: 'attribute', name: 'sd-meta-information--inverted', value: true },
+      options: { templateBackground: '#00358E' },
+      args
+    });
+  }
+};
+
+/**
+ * The sd-meta-information with pipe.
+ */
 
 export const Pipe = {
-  // args: {
-  //   slot: 'Lorem ipsum'
-  // },
-  render: (args: { size: string; pipe: boolean }) => {
-    return html`<div class=${classMap(getClasses({ ...args, metaInformation: `${args.size}`, pipe: true }))}>
-      Lorem Ipsum
-    </div>`;
+  parameters: { controls: { exclude: ['sd-meta-information--pipe', 'sd-meta-information--size-...'] } },
+  render: (args: any) => {
+    return generateTemplate({
+      axis: {
+        x: { type: 'attribute', name: 'sd-meta-information--pipe' },
+        y: { type: 'attribute', name: 'sd-meta-information--size-...' }
+      },
+      args
+    });
   }
 };
