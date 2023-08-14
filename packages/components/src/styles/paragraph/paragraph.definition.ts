3b4a0af18e544e62b872c7de9085eab2a1be4e31
Yigit, Özlem (UIT) <Oezlem.Yigit@union-investment.de>
2023-08-14 16:18:21 +0200

    feat: added paragraph as styles

---
+++ packages/components/src/styles/paragraph/paragraph.definition.ts
@@ -0,0 +1,20 @@
+import type { Style } from '../definitions';
+
+export default {
+  styleName: 'sd-paragraph',
+  summary:
+    'A paragraph is used to display blocks of text. It uses the base font size and can contain bold and/or link styles.',
+  status: 'experimental',
+  since: '1.1',
+  attributes: [
+    {
+      name: 'sd-paragraph--size-...',
+      options: ['-', 'sm'],
+      description: "The paragraph's size."
+    },
+    {
+      name: 'sd-paragraph--inverted',
+      description: 'Inverts the paragraph text.'
+    }
+  ]
+} satisfies Style;
--- packages/components/src/styles/paragraph/paragraph.stories.ts
+++ packages/components/src/styles/paragraph/paragraph.stories.ts
@@ -1,103 +1,81 @@
 import '../../solid-components';
-import { classMap } from 'lit/directives/class-map.js';
-import { html } from 'lit-html';
 
-/**
- * A paragraph is used to display blocks of text.
- * It uses the base font size and can contain bold and/or link styles.
- */
+import { storybookDefaults, storybookHelpers, storybookTemplate } from '../../../scripts/storybook/helper';
+
+const { argTypes, parameters } = storybookDefaults('sd-paragraph');
+const { overrideArgs } = storybookHelpers('sd-paragraph');
+const { generateTemplate } = storybookTemplate('sd-paragraph');
 
 export default {
   title: 'Styles/sd-paragraph',
-  args: {
-    // boolean for "modifier" arg
-    inverted: false,
-    size: 'lg',
-    highlight: false
-  },
-  argTypes: {
-    inverted: {
-      name: 'inverted',
-      control: 'boolean',
-      description: 'Inverts the headline. <br><code>boolean</code>'
-    },
-    size: {
-      name: 'size',
-      control: 'radio',
-      options: ['xl', 'lg'],
-      description: "The paragraph's size.<br><code>'lg'</code> &nbsp; <code>'sm'</code>"
-    },
-    highlight: {
-      name: 'highlight',
-      control: 'boolean',
-      description: 'The green accent color can be used to highlight parts of the text. <br><code>boolean</code>'
+  component: 'sd-paragraph',
+  parameters: {
+    ...parameters,
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/SkTc8tXPNPjZlxvXXJ6vTt/Paragraph?type=design&node-id=1701-741&mode=design&t=FsmhHop5U1y6FbYg-0'
     }
-  }
-};
-
-const getClasses = args => {
-  return {
-    'sd-paragraph': true,
-    'sd-paragraph--inverted': args.inverted,
-    'sd-mark': args.highlight,
-
-    'sd-paragraph--size-lg': args.paragraph === 'lg',
-    'sd-paragraph--size-xl': args.paragraph === 'xl'
-  };
+  },
+  args: overrideArgs({ type: 'slot', name: 'default', value: 'Lorem Ipsum' }),
+  argTypes
 };
 
 /**
- * This is the the typography section for all kind of styles and sizes of the Headline.
+ * Default: This shows sd-paragraph in its default state.
  */
+
 export const Default = {
-  render: (args: { size: string; highlight: string }) => {
-    // need to remove highlight from args to avoid adding the class twice
-    const { highlight, ...restOfArgs } = args;
-
-    return html`<div class=${classMap(getClasses({ ...restOfArgs, paragraph: `${args.size}` }))}>
-      ${args.highlight
-        ? html`Lorem <mark class=${classMap(getClasses({ highlight: `${args.highlight}` }))}></div>Ipsum</mark>`
-        : 'Lorem Ipsum'}
-    </div>`;
-  }
-};
-
-export const SizesAndInverted = {
-  name: 'Sizes x Inverted',
-  parameters: { controls: { exclude: ['size', 'inverted'] } },
-  args: {},
-  render: (args: { size: string }) => {
-    return html`
-      <div class=${classMap(getClasses({ ...args, paragraph: 'lg' }))}>text-lg</div>
-      <div class=${classMap(getClasses({ ...args, paragraph: 'sm' }))}>text-sm</div>
-    `;
-    // axis: {
-    //   x: { type: 'attribute', name: 'size' },
-    //   y: { type: 'attribute', name: 'inverted', values: [false, true] }
-    // },
-    // options: {
-    //   templateBackgrounds: { alternate: 'y', colors: ['white', '#00358E'] }
-    // }
+  render: (args: any) => {
+    return generateTemplate({
+      options: { templateContent: '<div class="%CLASSES%">%SLOT%</time>' },
+      args
+    });
   }
 };
 
 /**
- * This is the the typography section for all kind of styles and sizes of the Display.
+ * Use the `inverted` class to make a leadtext with inverted colors.
  */
-export const Highlight = {
-  name: 'Highlight x Inverted',
-  parameters: { controls: { exclude: ['size', 'highlight', 'inverted'] } },
-  render: (args: { size: string; highlight: boolean }) => {
-    const { highlight, ...restOfArgs } = args;
-    return html`
-      <div class=${classMap(getClasses({ ...restOfArgs, display: 'lg', highlight: true }))}>
-        Icon left lorem ipsum <mark class=${classMap(getClasses({ highlight: `${args.highlight}` }))}>dolor</mark> sit
-        amet
-      </div>
-      <div class=${classMap(getClasses({ ...restOfArgs, display: 'sm', highlight: true }))}>
-        Icon left lorem ipsum <mark class=${classMap(getClasses({ highlight: `${args.highlight}` }))}>dolor</mark> sit
-        amet
-      </div>
-    `;
+
+export const Inverted = {
+  parameters: { controls: { exclude: ['sd-paragraph--size-...', 'sd-paragraph--inverted'] } },
+  render: (args: any) => {
+    return generateTemplate({
+      axis: {
+        x: [{ type: 'attribute', name: 'sd-paragraph--size-...' }]
+      },
+      constants: { type: 'attribute', name: 'sd-paragraph--inverted', value: true },
+      options: { templateBackground: '#00358E' },
+      args
+    });
+  }
+};
+
+/**
+ * Use the `mark` class to highlight parts of the text inside leadtext with the green accent color.
+ */
+
+export const MarkAndInverted = {
+  name: 'Mark × Inverted',
+  parameters: { controls: { exclude: ['sd-paragraph--size-...', 'sd-paragraph--inverted', 'default'] } },
+  render: (args: any) => {
+    return generateTemplate({
+      axis: {
+        x: {
+          type: 'attribute',
+          name: 'sd-paragraph--size-...'
+        },
+        y: {
+          type: 'attribute',
+          name: 'sd-paragraph--inverted',
+          values: [false, true]
+        }
+      },
+      constants: { type: 'slot', name: 'default', value: 'Lorem <mark class="sd-mark">Ipsum</mark>' },
+      args,
+      options: {
+        templateBackgrounds: { alternate: 'y', colors: ['white', '#00358E'] }
+      }
+    });
   }
 };
