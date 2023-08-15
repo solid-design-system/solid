21d28a19e8f511ae2da92a5d278ec824293f978b
Karl Baumhauer <48211526+karlbaumhauer@users.noreply.github.com>
2023-08-15 12:47:45 +0200

    ci: use env variable from azure pipeline

--- packages/components/scripts/storybook/node-modify-page-title.cjs
+++ packages/components/scripts/storybook/node-modify-page-title.cjs
@@ -6,23 +6,22 @@ const path = require('path');
  */
 try {
   console.log('Rewriting index.html document title.');
-  const packageJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../package.json'), 'utf8'));
-  const version = packageJson.version;
+  const version = process.env.VERSION;
   const filePath = '../../dist/storybook/index.html';
   const document = fs.readFileSync(path.resolve(__dirname, filePath), 'utf8');
   const output = document
-    .replace(/<title>.*<\/title>/, `<title>Solid Design System by Union Investment | Version: ${version}</title>`)
+    .replace(/<title>.*<\/title>/, `<title>Solid Design System by Union Investment | ${version}</title>`)
     .replace(
       /<meta property="og:title" content="Solid Design System by Union Investment" \/>/,
-      `<meta property="og:title" content="Solid Design System by Union Investment | Version: ${version}" />`
+      `<meta property="og:title" content="Solid Design System by Union Investment | ${version}" />`
     )
     .replace(
       /<meta name="twitter:title" content="Solid Design System by Union Investment" \/>/,
-      `<meta name="twitter:title" content="Solid Design System by Union Investment | Version: ${version}" />`
+      `<meta name="twitter:title" content="Solid Design System by Union Investment | ${version}" />`
     )
     .replace(
       /<meta itemprop="name" content="Solid Design System by Union Investment" \/>/,
-      `<meta itemprop="name" content="Solid Design System by Union Investment | Version: ${version}" />`
+      `<meta itemprop="name" content="Solid Design System by Union Investment | ${version}" />`
     );
   fs.writeFileSync(path.resolve(__dirname, filePath), output);
   console.log('Title rewrite complete.');
