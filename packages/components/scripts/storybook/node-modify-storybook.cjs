96a617c119097ea4fce73f6e34c9412ff4e3bb34
Karl Baumhauer <48211526+karlbaumhauer@users.noreply.github.com>
2023-08-23 16:13:04 +0200

    refactor: cleanup and combine script functions

--- packages/components/scripts/storybook/node-modify-storybook.cjs
+++ packages/components/scripts/storybook/node-modify-storybook.cjs
@@ -2,31 +2,25 @@ const fs = require('fs');
 const path = require('path');
 
 /**
- * Change the generated document title to "Solid Design System by Union Investment".
+ * Change the generated browser title to "Solid Design System by Union Investment".
  */
-
 const searchDir = 'dist/storybook/sb-manager';
 const searchText = 'u22C5 Storybook';
 const replaceText = 'u22C5 Solid Design System by Union Investment (Storybook)';
-
 function replaceTextInFiles(directory) {
   fs.readdir(directory, (err, files) => {
     if (err) {
       console.error('Error reading directory:', err);
       return;
     }
-
     files.forEach(file => {
       const filePath = path.join(directory, file);
-
       fs.readFile(filePath, 'utf8', (err, content) => {
         if (err) {
           console.error('Error reading file:', err);
           return;
         }
-
         const updatedContent = content.replace(new RegExp(searchText, 'g'), replaceText);
-
         if (updatedContent !== content) {
           fs.writeFile(filePath, updatedContent, 'utf8', err => {
             if (err) {
@@ -40,23 +34,21 @@ function replaceTextInFiles(directory) {
     });
   });
 }
-
 replaceTextInFiles(searchDir);
 
 /**
- * Remove the generated index.html favicon link to use custom tag from manager-header.html.
+ * Change the generated index.html document title to "Solid Design System by Union Investment" and remove generated favicon link to use custom tag from manager-header.html.
  */
 try {
   const filePath = '../../dist/storybook/index.html';
   const document = fs.readFileSync(path.resolve(__dirname, filePath), 'utf8');
-  console.log('Removing generated favicon link.');
-  // find the link tag with type="image/svg+xml" and remove it
-  const faviconLink = document.match(/<link rel="icon" type="image\/svg\+xml" href=".*" \/>/)[0];
-  const output = document.replace(faviconLink, '');
+  const output = document
+    .replace(/<title>@storybook\/cli - Storybook<\/title>/, `<title>Solid Design System by Union Investment</title>`)
+    .replace(/<link rel="icon" type="image\/svg\+xml" href=".*" \/>/, '');
   fs.writeFileSync(path.resolve(__dirname, filePath), output);
-  console.log('Removing favicon link complete.');
+  console.log('index.html document rewrite complete.');
 } catch (error) {
-  console.log('Removing favicon link failed.');
+  console.log('Document rewrite failed.');
   console.error(error);
   process.exit(1);
 }
