5676ff5527aa6298d38707a0d95b92e99fcdedd7
Azra Efendic <azra.efendic@azravi.localdomain>
2023-08-25 09:48:29 +0200

    doc: add migration guide for ui-offcanvas-panel

--- packages/components/src/components/drawer/drawer.test.ts
+++ packages/components/src/components/drawer/drawer.test.ts
@@ -1,5 +1,4 @@
 // cspell:dictionaries lorem-ipsum
-// in progress...
 import { expect, fixture, html, waitUntil } from '@open-wc/testing';
 import { sendKeys } from '@web/test-runner-commands';
 import sinon from 'sinon';
