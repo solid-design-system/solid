d82c198c45c2b75ff6b3d3c1ca35c85b72d45732
mariohamann <mario.hamann@virtual-identity.com>
2023-08-29 17:29:38 +0200

    fix popup story

--- packages/components/src/components/popup/popup.stories.ts
+++ packages/components/src/components/popup/popup.stories.ts
@@ -1,18 +1,37 @@
 import '../../solid-components';
-import { storybookDefaults, storybookTemplate } from '../../../scripts/storybook/helper';
+import { storybookDefaults, storybookHelpers, storybookTemplate } from '../../../scripts/storybook/helper';
 import { withActions } from '@storybook/addon-actions/decorator';
 
-const { argTypes, args, parameters } = storybookDefaults('sd-popup');
+const { argTypes, parameters } = storybookDefaults('sd-popup');
 const { generateTemplate } = storybookTemplate('sd-popup');
+const { overrideArgs } = storybookHelpers('sd-popup');
 
 /**
  * Popup is a utility that lets you declaratively anchor "popup" containers to another element.
+ *
+ * This component’s name is inspired by <popup>. It uses Floating UI under the hood to provide a well-tested, lightweight, and fully declarative positioning utility for tooltips, dropdowns, and more.
+ *
+ * Popup doesn’t provide any styles — just positioning! The popup’s preferred placement, distance, and skidding (offset) can be configured using attributes.
+ *
+ * > Popup is a low-level utility built specifically for positioning elements. Do not mistake it for a tooltip or similar because it does not facilitate an accessible experience! Almost every correct usage of <sl-popup> will involve building other components. It should rarely, if ever, occur directly in your HTML.
  */
 
 export default {
-  title: 'Components/sd-popup',
+  title: 'Utilities/sd-popup',
   component: 'sd-popup',
-  args,
+  args: overrideArgs([
+    { type: 'slot', name: 'anchor', value: '<span style="background: gray" slot="anchor">Anchor</span>' },
+    {
+      type: 'slot',
+      name: 'default',
+      value: '<span style="background: red">Popup content</span>'
+    },
+    {
+      type: 'attribute',
+      name: 'active',
+      value: true
+    }
+  ]),
   argTypes,
   parameters: { ...parameters },
   decorators: [withActions] as any
