59162101bbaa810d7402ed8ed0d12f1826fdabeb
mariohamann <mario.hamann@virtual-identity.com>
2023-09-01 15:14:04 +0200

    feat: sd-interactive

---
+++ packages/components/src/styles/interactive/interactive.declaration.ts
@@ -0,0 +1,22 @@
+import type { Style } from '../../declaration';
+
+export default {
+  styleName: 'sd-interactive',
+  summary: 'Generates basic styles for interactive elements.',
+  status: 'experimental',
+  since: '1.9',
+  attributes: [
+    {
+      name: 'sd-interactive--disabled',
+      description: 'Makes an element look disabled.'
+    },
+    {
+      name: 'sd-interactive--selected',
+      description: 'Makes an element look active.'
+    },
+    {
+      name: 'sd-interactive--reset',
+      description: 'Resets the default browser styles of e. g. a button.'
+    }
+  ]
+} satisfies Style;
