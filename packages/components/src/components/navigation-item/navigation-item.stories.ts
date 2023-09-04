38db41b0bf08d7a6a6c714cec2aa8145fad2f334
Alec Budd <alec.budd@prodyna.com>
2023-09-04 16:00:35 +0200

    sd-navigation-item configuring details padding

--- packages/components/src/components/navigation-item/navigation-item.stories.ts
+++ packages/components/src/components/navigation-item/navigation-item.stories.ts
@@ -1,4 +1,5 @@
 import '../../solid-components';
+import { html } from 'lit-html';
 import { storybookDefaults, storybookTemplate } from '../../../scripts/storybook/helper';
 import { withActions } from '@storybook/addon-actions/decorator';
 
@@ -8,7 +9,7 @@ const { generateTemplate } = storybookTemplate('sd-navigation-item');
 export default {
   title: 'Components/sd-navigation-item',
   component: 'sd-navigation-item',
-  args,
+  args: args,
   argTypes,
   parameters: { ...parameters },
   decorators: [withActions] as any
@@ -21,5 +22,101 @@ export default {
 export const Default = {
   render: (args: any) => {
     return generateTemplate({ args });
+  }
+};
+
+/**
+ * Default: This shows sd-navigation-item in its default state.
+ */
+
+export const Test = {
+  render: (args: any) => {
+    return generateTemplate({
+      constants: [
+        {
+          type: 'slot',
+          name: 'icon-left',
+          value: '<span slot="icon-left">Left</span>'
+        },
+        {
+          type: 'slot',
+          name: 'default',
+          value: '<span slot="default">Middle</span>'
+        },
+        {
+          type: 'slot',
+          name: 'icon-right',
+          value: '<span slot="icon-right">Right</span>'
+        },
+        {
+          type: 'slot',
+          name: 'main',
+          value: '<span slot="main">Main</span>'
+        }
+      ],
+      args
+    });
+  }
+};
+
+/**
+ * Use the `icon-left` and `icon-right` slots to add icons.
+ */
+
+export const IconSlots = {
+  parameters: { controls: { exclude: ['size', 'icon-left', 'icon-right'] } },
+  render: (args: any) => {
+    return html`
+      ${['sm', 'md', 'lg'].map(size =>
+        // We have to compare different types of icons: "square", "wide" and "tall" ones.
+        generateTemplate({
+          axis: {
+            x: {
+              type: 'slot',
+              name: 'icon-right',
+              values: [
+                { value: '', title: '–' },
+                {
+                  value: '<sd-icon library="global-resources" name="system/picture" slot="icon-right"></sd-icon>',
+                  title: 'system/picture'
+                },
+                {
+                  value:
+                    '<sd-icon library="global-resources" name="system/multi-functions" slot="icon-right"></sd-icon>',
+                  title: 'system/multi-functions'
+                },
+                {
+                  value: '<sd-icon library="global-resources" name="system/minus" slot="icon-right"></sd-icon>',
+                  title: 'system/minus'
+                }
+              ]
+            },
+            y: {
+              type: 'slot',
+              name: 'icon-left',
+              values: [
+                { value: '', title: '–' },
+                {
+                  value: '<sd-icon library="global-resources" name="system/picture" slot="icon-left"></sd-icon>',
+                  title: 'system/picture'
+                },
+                {
+                  value:
+                    '<sd-icon library="global-resources" name="system/multi-functions" slot="icon-left"></sd-icon>',
+                  title: 'system/multi-functions'
+                },
+                {
+                  value: '<sd-icon library="global-resources" name="system/minus" slot="icon-left"></sd-icon>',
+                  title: 'system/minus'
+                }
+              ]
+            }
+          },
+          constants: [{ type: 'attribute', name: 'size', value: size }],
+          args,
+          options: { title: `size="${size}"` }
+        })
+      )}
+    `;
   }
 };
--- packages/components/src/components/navigation-item/navigation-item.ts
+++ packages/components/src/components/navigation-item/navigation-item.ts
@@ -1,12 +1,14 @@
-import { css, html } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
-import { LocalizeController } from '../../utilities/localize';
-import { watch } from '../../internal/watch';
+import '../spinner/spinner';
+import { css } from 'lit';
+import { customElement, property, query, queryAssignedElements, state } from 'lit/decorators.js';
+import { HasSlotController } from '../../internal/slot';
+import { html, literal } from 'lit/static-html.js';
+import componentStyles from '../../styles/component.styles';
 import cx from 'classix';
 import SolidElement from '../../internal/solid-element';
 
 /**
- * @summary Short summary of the component's intended use.
+ * @summary Flexible button / link component that can be used to quickly build navigations.
  * @status experimental
  * @since 1.0
  *
@@ -15,17 +17,36 @@ import SolidElement from '../../internal/solid-element';
  * @event sd-event-name - Emitted as an example.
  *
  * @slot - The navigation item's label.
+ * @slot icon-left - A prefix icon or similar element.
+ * @slot icon-right - A suffix icon or similar element.
  * @slot main - Main slot used to set additional content like badges. Available for horizontal and vertical orientations.
- * @slot description - Slot used to provide a description for the navigation item. Only available for horizontal orientation.
- * @slot children - Slot used to provide nested child navigation elements. If present, the chevron property will be set to true.
+ * @slot description - *Vertical only: Slot used to provide a description for the navigation item.
+ * @slot children - Slot used to provide nested child navigation elements. If present, a chevron will be shown on the right side.
  *
  * @csspart base - The component's base wrapper.
+ * @csspart icon-left - The container that wraps the left icon area.
+ * @csspart label - The button's label.
+ * @csspart icon-right - The container that wraps the right icon area.
  *
  * @cssproperty --example - An example CSS custom property.
  */
 @customElement('sd-navigation-item')
 export default class SdNavigationItem extends SolidElement {
-  private readonly localize = new LocalizeController(this);
+  private readonly hasSlotController = new HasSlotController(
+    this,
+    '[default]',
+    'icon-left',
+    'icon-right',
+    'main',
+    'description',
+    'children'
+  );
+
+  @query('a, button') button: HTMLButtonElement | HTMLLinkElement;
+  @queryAssignedElements({ selector: 'sd-icon' }) _iconsInDefaultSlot!: HTMLElement[];
+
+  @state()
+  detailsOpen = false;
 
   /** The navigation item's href target. If provided, the navigation item will use an anchor tag, otherwise it will use a button tag. */
   @property({ reflect: true }) href = '';
@@ -54,53 +75,204 @@ export default class SdNavigationItem extends SolidElement {
   /** *Vertical Only: Adds additional padding to navigation item's left and right sides. */
   @property({ type: Boolean, reflect: true }) divider = false;
 
-  @watch('someProperty')
-  doSomething() {
-    // Example event
-    this.emit('sd-event-name');
+  private handleBlur() {
+    this.emit('sd-blur');
+  }
+
+  private handleFocus() {
+    this.emit('sd-focus');
+  }
+
+  private isButton() {
+    return this.href ? false : true;
+  }
+
+  private isLink() {
+    return this.href ? true : false;
+  }
+
+  /** Simulates a click on the button. */
+  click() {
+    this.button.click();
+  }
+
+  /** Sets focus on the button. */
+  focus(options?: FocusOptions) {
+    this.button.focus(options);
+  }
+
+  /** Removes focus from the button. */
+  blur() {
+    this.button.blur();
+  }
+
+  private handleClick(event: MouseEvent) {
+    console.log('handleClick');
+    const detailsElementOpen: boolean = this.shadowRoot?.querySelector('details')?.open || false;
+
+    console.log(detailsElementOpen);
+
+    this.emit('sd-toggle-details', { detail: { isOpen: detailsElementOpen } });
+
+    this.detailsOpen = !this.detailsOpen;
   }
 
   render() {
-    const rootClasses = cx(
-      'relative min-h-[48px] px-4 pt-3 pb-2 hover:bg-neutral-200 border-b-4 transition-all',
-      this.current ? 'border-accent' : 'border-transparent',
-      { base: 'text-base', larger: 'text-lg', smaller: 'text-[14px]' }[this.size]
-    );
+    const slots = {
+      label: this.hasSlotController.test('[default]'),
+      'icon-left': this.hasSlotController.test('icon-left'),
+      'icon-right': this.hasSlotController.test('icon-right'),
+      'icon-only': this._iconsInDefaultSlot.length > 0,
+      main: this.hasSlotController.test('main'),
+      description: this.hasSlotController.test('description'),
+      children: this.hasSlotController.test('children')
+    };
 
-    // Slot Elements
-    const iconLeftSlot = html`<slot name="icon-left" part="icon-left">L</slot>`; // icon left of label
-    const defaultSlot = html`<slot part="label">label</slot>`; // navigation item label
-    const iconRightSlot = html`<slot name="icon-right" part="icon-right">R</slot>`; // icon right of label
-    const mainSlot = html`<slot name="main" part="main">main</slot>`; // used for adornments like badges
-    const childrenSlot = html`<slot name="children" part="children">Children</slot>`; // if childrenSlot is used, chevron should be displayed
+    const isLink = this.isLink();
+    const tag = isLink ? literal`a` : slots['children'] ? literal`summary` : literal`button`;
+    const includeChildren = !this.href && slots['children'];
+    const horizontalPaddingBottom = this.horizontal ? 'pb-2' : 'pb-3';
 
-    // Additional Elements
-    const chevron = html`<sd-icon name="chevron-down" library="system" color="currentColor"></sd-icon>`;
-
-    // Composed Elements
-    const body = html`${iconLeftSlot}${defaultSlot}${iconRightSlot}${mainSlot}${chevron}`;
-    const root = this.href
-      ? html`<a class=${rootClasses} href=${this.href}>${body}</a>`
-      : html`<button class=${rootClasses}>${body}</button>`;
-
-    // TODO: render details and summary ONLY if children slot is used
-    return html`<details>
-      <summary>${root}</summary>
-      ${childrenSlot}
-    </details> `;
-  }
-
-  /** Inherits Tailwindclasses and includes additional styling. */
-  static styles = [
-    SolidElement.styles,
-    css`
-      :host {
-        display: inline-block;
-        box-sizing: border-box;
+    const calculatePaddingX = (): string => {
+      if (this.relaxed && this.indented) {
+        return 'pl-8 pr-4';
       }
 
-      summary::marker {
-        content: none;
+      if (this.relaxed) {
+        return 'px-4';
+      }
+
+      if (this.indented) {
+        return 'pl-4';
+      }
+
+      return '';
+    };
+
+    const horizontalStyle = this.horizontal
+      ? html`<style>
+          :host {
+            display: inline-block;
+          }
+        </style>`
+      : html`<style>
+          :host {
+            display: block;
+          }
+        </style>`;
+
+    // Conditionally Style Root
+    const rootClasses = cx(
+      'hover:bg-neutral-200 transition-all relative',
+      this.horizontal ? 'border-b-4 px-4' : 'border-l-4 pl-[28px] pr-8',
+      this.current ? 'border-accent' : 'border-transparent',
+      this.disabled && 'text-neutral-500 border-neutral-500',
+      { base: 'text-base', larger: 'text-lg', smaller: 'text-[14px]' }[this.size],
+      includeChildren ? '' : 'inline-block w-full'
+    );
+    const chevronClasses = cx(
+      'h-6 w-6',
+      includeChildren ? (this.detailsOpen ? 'rotate-180' : 'rotate-0') : 'rotate-[270deg]'
+    );
+
+    // Additional Elements
+    const chevron = html`<sd-icon
+      name="chevron-down"
+      library="system"
+      color="currentColor"
+      class=${chevronClasses}
+    ></sd-icon>`;
+
+    const divider = this.divider
+      ? html`<sd-divider
+          class=${cx(
+            'w-[calc(100%-64px)] absolute top-[-1px] left-0',
+            this.horizontal ? 'mx-4' : 'mx-8',
+            calculatePaddingX()
+          )}
+        ></sd-divider>`
+      : null;
+
+    /* eslint-disable lit/no-invalid-html */
+    /* eslint-disable lit/binding-positions */
+    const root = html`
+      ${horizontalStyle}
+
+      <${tag}
+      part="base"
+      class=${rootClasses}
+      @click=${this.handleClick}
+      >
+        <span class=${cx(
+          'relative pt-3 inline-flex justify-between w-full',
+          slots['description'] ? horizontalPaddingBottom : 'pb-1',
+          calculatePaddingX()
+        )}>
+          <span>
+            <slot name="icon-left" part="icon-left" class=${cx('inline mr-2')}>L</slot>
+            <span class=${cx(
+              slots['icon-left'] && 'ml-2',
+              slots['icon-right'] && 'mr-2'
+            )}><slot part="label">Label</slot></span>
+            <slot name="icon-right" part="icon-right">R</slot>
+          </span>
+          <span class="inline-flex items-center">
+            <slot name="main" part="main" class=${cx('inline mr-4')}>main</slot>
+            ${chevron}
+          </span>
+        </span>
+        <slot name="description" part="description" class=${cx(
+          'inline-block w-full bg-primary-100',
+          horizontalPaddingBottom,
+          calculatePaddingX()
+        )}>description</slot>
+      </${tag}>
+    `;
+    /* eslint-enable lit/no-invalid-html */
+    /* eslint-enable lit/binding-positions */
+
+    return !this.href && slots['children']
+      ? html`${divider}
+          <details class="w-full relative flex">${root}<slot name="children"></slot></details>`
+      : html`${divider}${root}`;
+  }
+
+  /**
+   * Inherits Tailwindclasses and includes additional styling.
+   */
+  static styles = [
+    componentStyles,
+    SolidElement.styles,
+
+    css`
+      :host {
+        background-color: lime;
+        box-sizing: border-box;
+        position: relative;
+      }
+
+      :host > * {
+        text-align: left;
+      }
+
+      summary {
+        display: inline;
+        list-style: none;
+        cursor: pointer;
+      }
+
+      sd-spinner {
+        --indicator-color: currentColor;
+        --track-color: var(--tw-varcolor-200);
+      }
+
+      /**
+       * sd-icons should automatically resize correctly based on the button size.
+       */
+
+      ::slotted(sd-icon),
+      sd-spinner {
+        font-size: calc(var(--tw-varspacing) / 2);
       }
     `
   ];
