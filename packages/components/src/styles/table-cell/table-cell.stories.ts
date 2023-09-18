74ae143715bed4f2bb572ee2fc97d17b1e6daf7a
MarcMatthiae <marc.matthiae@union-investment.de>
2023-09-18 11:14:38 +0200

    Remove sortable sample, fix findings from PR

--- packages/components/src/styles/table-cell/table-cell.stories.ts
+++ packages/components/src/styles/table-cell/table-cell.stories.ts
@@ -18,7 +18,10 @@ export default {
   parameters: {
     ...parameters
   },
-  args: overrideArgs({ type: 'slot', name: 'default', value: 'Lorem ipsum dolor sit amet.' }),
+  args: overrideArgs([
+    { type: 'slot', name: 'default', value: 'Lorem ipsum dolor sit amet.' },
+    { type: 'attribute', name: 'sd-table-cell--bg-...', value: 'transparent' }
+  ]),
   argTypes
 };
 
@@ -37,9 +40,9 @@ export const Default = {
 
 /**
  * These examples demonstrate the usage of sd-table-cell in various contexts.
- *
- * __Note:__ These examples are intended solely for illustrating how sd-table-cell can be used to style tables.
+ * The examples are intended solely for illustrating how sd-table-cell can be used to style tables.
  * The data generation and table sorting logic should not be used in production environments.
+ *
  */
 
 export const Samples = {
@@ -52,56 +55,12 @@ export const Samples = {
     // Initalize table data
     const tableRowCount = 7;
     const tableColumnCount = 5;
-    const sortData: ('ascending' | 'descending' | 'none')[] = Array.from({ length: tableColumnCount }, (v, i) =>
-      i === 0 ? 'ascending' : 'none'
-    );
     const headerData = Array.from({ length: tableColumnCount }, () => 'Header');
     const tableData = Array.from({ length: tableRowCount }, () =>
       Array.from({ length: tableColumnCount }, () => {
         return args['default-slot'] ?? 'Lorem ipsum dolor sit amet.';
       })
     );
-
-    // Function to exchange the sort icons and set the aria-sort attribute in the sortable table after a click on a table header
-    const sortTable = (column: number) => {
-      const sortingOptions: {
-        [key: string]: {
-          nextSort: 'ascending' | 'descending' | 'none';
-          iconName: string;
-          ariaSort: string | undefined;
-        };
-      } = {
-        none: { nextSort: 'ascending', iconName: 'system/sort-up', ariaSort: undefined },
-        ascending: { nextSort: 'descending', iconName: 'system/sort-up-filled', ariaSort: 'ascending' },
-        descending: { nextSort: 'none', iconName: 'system/sort-down-filled', ariaSort: 'descending' }
-      };
-      const icons = document.querySelectorAll('[id*="sortIcon"]');
-      const headerCells = document.querySelectorAll('[id*="sortableHeader"]');
-
-      if (icons && headerCells) {
-        headerCells.forEach((headerCell, index) => {
-          //Change the sort icon and aria-sort attribute for the clicked column
-          if (index === column) {
-            const nextSort = sortingOptions[sortData[column]].nextSort;
-            const { iconName, ariaSort } = sortingOptions[nextSort];
-
-            sortData[index] = nextSort;
-            icons[index].setAttribute('name', iconName);
-            ariaSort !== undefined
-              ? headerCell.setAttribute('aria-sort', ariaSort)
-              : headerCell.removeAttribute('aria-sort');
-          }
-          //Reset the sort icon and remove the aria-sort attribute for all other columns
-          else {
-            const { iconName } = sortingOptions['none'];
-
-            sortData[index] = 'none';
-            icons[index].setAttribute('name', iconName);
-            headerCell.removeAttribute('aria-sort');
-          }
-        });
-      }
-    };
 
     return html`
       <style>
@@ -158,6 +117,27 @@ export const Samples = {
         }
       </style>
       <div class="story-wrapper">
+        <div class="headline">Simple Table</div>
+        <table class="sd-table sample-table">
+          <thead>
+            ${(() => {
+              return html`<tr>
+                ${headerData.map(cellData => {
+                  return html`<th class="sd-table-cell sd-table-cell--bg-transparent">${cellData}</th>`;
+                })}
+              </tr>`;
+            })()}
+          </thead>
+          <tbody>
+            ${tableData.map(rowData => {
+              return html`<tr>
+                ${rowData.map(cellData => {
+                  return html`<td class="sd-table-cell sd-table-cell--bg-transparent">${cellData}</td>`;
+                })}
+              </tr>`;
+            })}
+          </tbody>
+        </table>
         <div class="headline">Simple Table With Vertical Lines</div>
         <table class="sd-table sample-table">
           <thead>
@@ -191,61 +171,13 @@ export const Samples = {
             })}
           </tbody>
         </table>
-
-        <div class="headline">Sortable Table</div>
-        <table class="sd-table sample-table" id="sortableTable" .sortData=${sortData}>
-          <thead>
-            ${(() => {
-              return html`<tr>
-                ${headerData.map((cellData, columnIndex) => {
-                  return html`<th
-                    class="sd-table-cell sd-table-cell--bg-transparent sortable ${columnIndex + 1 < headerData.length
-                      ? 'sd-table-cell--divider'
-                      : ''}"
-                    id="sortableHeader-${columnIndex}"
-                    aria-sort=${ifDefined(sortData[columnIndex] === 'none' ? undefined : 'ascending')}
-                  >
-                    <button class="sd-interactive sd-interactive--reset" @click="${() => sortTable(columnIndex)}">
-                      ${cellData}<sd-icon
-                        id="sortIcon-${columnIndex}"
-                        library="global-resources"
-                        name=${sortData[columnIndex] === 'none' ? 'system/sort-up' : 'system/sort-up-filled'}
-                      ></sd-icon>
-                    </button>
-                  </th>`;
-                })}
-              </tr>`;
-            })()}
-          </thead>
-          <tbody>
-            ${tableData.map(rowData => {
-              return html`<tr>
-                ${rowData.map((cellData, columnIndex) => {
-                  return html`<td
-                    class="sd-table-cell sd-table-cell--bg-transparent ${columnIndex + 1 < headerData.length
-                      ? 'sd-table-cell--divider'
-                      : ''}"
-                  >
-                    ${cellData}
-                  </td>`;
-                })}
-              </tr>`;
-            })}
-          </tbody>
-        </table>
         <div class="headline">Simple Table With Alternating Colors</div>
         <table class="sd-table sample-table">
           <thead>
             ${(() => {
               return html`<tr>
-                ${headerData.map((cellData, columnIndex) => {
-                  return html`<th
-                    class="sd-table-cell sd-table-cell--bg-primary-100 ${columnIndex + 1 < headerData.length
-                      ? 'sd-table-cell--divider'
-                      : ''}"
-                  >
-                    ${cellData}
-                  </th>`;
+                ${headerData.map(cellData => {
+                  return html`<th class="sd-table-cell sd-table-cell--bg-primary-100">${cellData}</th>`;
                 })}
               </tr>`;
             })()}
@@ -253,12 +185,11 @@ export const Samples = {
           <tbody>
             ${tableData.map((rowData, rowIndex) => {
               return html`<tr>
-                ${rowData.map((cellData, columnIndex) => {
+                ${rowData.map(cellData => {
                   return html`<td
                     class="sd-table-cell ${rowIndex % 2 === 0
                       ? 'sd-table-cell--bg-white'
-                      : 'sd-table-cell--bg-primary-100'} ${columnIndex + 1 < headerData.length &&
-                    'sd-table-cell--divider'}"
+                      : 'sd-table-cell--bg-primary-100'}"
                   >
                     ${cellData}
                   </td>`;
@@ -267,7 +198,6 @@ export const Samples = {
             })}
           </tbody>
         </table>
-
         <div class="headline">Simple Table, First Column Fixed</div>
         <div class="disclaimer">This sample will be provided soon.</div>
 
