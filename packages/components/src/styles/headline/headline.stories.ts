fd933cb4c966de3cdb38890fb9b8ead39ad433a7
Vahid Menu Nesro <vahid.nesro@virtual-identity.com>
2023-09-25 09:23:44 +0200

    fix: fix headline icon shrink

--- packages/components/src/styles/headline/headline.stories.ts
+++ packages/components/src/styles/headline/headline.stories.ts
@@ -1,5 +1,5 @@
-import '../../solid-components'
-
+import '../../solid-components';
+import { html } from 'lit-html';
 import { storybookDefaults, storybookHelpers, storybookTemplate } from '../../../scripts/storybook/helper';
 
 const { argTypes, parameters } = storybookDefaults('sd-headline');
@@ -9,45 +9,46 @@ const { generateTemplate } = storybookTemplate('sd-headline');
 /**
  * Headlines are vital for displaying content hierarchy and to improve accessibility. <br>
  * A headline can be additionally accompanied by an icon. The icon can be displayed on the left side or inline. <br>
- * <br>
- * <b>Sizes</b><br>
- * <li>4xl is the default size.</li>
- * <li>3xl</li>
- * <li>xl</li>
- * <li>lg</li>
- * <li>base</li>
- * 
+ *
  */
 
 export default {
-    title: 'Styles/sd-headline',
-    component: 'sd-headline',
-    parameters: {
-        ...parameters,
-        design: {
-            type: 'figma',
-            url: 'https://www.figma.com/file/1Dc7fiQU12U6f7SFgsHjQE/Headline?type=design&node-id=0-1&mode=design&t=lkfrp1PXc280seHQ-0'
-
+  title: 'Styles/sd-headline',
+  component: 'sd-headline',
+  parameters: {
+    ...parameters,
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/1Dc7fiQU12U6f7SFgsHjQE/Headline?type=design&node-id=0-1&mode=design&t=lkfrp1PXc280seHQ-0'
+    }
+  },
+  args: overrideArgs({ type: 'slot', name: 'default', value: 'Nisi eu excepteur anim esse' }),
+  argTypes,
+  decorators: [
+    (story: () => typeof html) => html`
+      <style>
+        td.template{
+          text-align: left !important;
         }
-    },
-    args: overrideArgs({ type: 'slot', name: 'default', value: 'Lorem Ipsum' }),
-    argTypes
+      </style>
+      ${story()}
+    `
+  ]
 };
-
 
 /**
  * Default: This shows sd-headline in its default state.
  */
 
 export const Default = {
-    render: (args: any) => {
-      return generateTemplate({
-        options: { templateContent: '<div class="%CLASSES%">%SLOT%</div>' },
-        args
-      });
-    }
+  render: (args: any) => {
+    return generateTemplate({
+      options: { templateContent: '<p class="%CLASSES%">%SLOT%</p>' },
+      args
+    });
+  }
 };
-  
+
 /**
  * Use the `inverted` class to make a headline with inverted colors.
  */
@@ -61,6 +62,29 @@ export const Inverted = {
       },
       constants: { type: 'attribute', name: 'sd-headline--inverted', value: true },
       options: { templateBackgrounds: { alternate: 'y', colors: ['transparent', '#00358E'] } },
+      args
+    });
+  }
+};
+
+/**
+ * Use the `inline` class to determine the positioning of an (optional) icon.
+ */
+
+export const Inline = {
+  parameters: { controls: { exclude: ['sd-headline--inline'] } },
+  render: (args: any) => {
+    return generateTemplate({
+      axis: {
+        y: [{ type: 'attribute', name: 'sd-headline--inline', values: [true, false] }]
+      },
+      constants: {
+        type: 'slot',
+        name: 'default',
+        value: ` 
+        <sd-icon name="content/picture" library="global-resources" color="primary"></sd-icon>
+        Icon left lorem ipsum dolor sit amet.`
+      },
       args
     });
   }
