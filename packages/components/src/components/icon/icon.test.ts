82010ca87d967bb20ebf5766dbc6ee48ab10e254
Mario Hamann <mario.hamann@virtual-identity.com>
2023-09-27 12:55:08 +0200

    fix: 🐛 provide docs how to use registerIconLibrary with UMD bundle (#433)
    ## Description:
    *PR notes: Please describe the changes in this PR.*
    
    1. I updated the docs for `sd-icon`.
    2. I fixed the tests using the documented approach.
    
    ## Definition of Reviewable:
    *PR notes: Irrelevant elements should be removed.*
    - [x] Documentation is created/updated
    - [x] relevant tickets are linked
    - [x] PR is assigned to project board

--- packages/components/src/components/icon/icon.test.ts
+++ packages/components/src/components/icon/icon.test.ts
@@ -1,41 +1,51 @@
 import { elementUpdated, expect, fixture, html, oneEvent } from '@open-wc/testing';
-// TODO: registerIconLibrary is currently not working: https://github.com/solid-design-system/solid/issues/296
-// import { registerIconLibrary } from './library';
+import { registerIconLibrary } from './library';
 import type SdIcon from './icon';
 
-// TODO: registerIconLibrary is currently not working: https://github.com/solid-design-system/solid/issues/296
-// const testLibraryIcons = {
-//   'test-icon1': `
-//     <svg id="test-icon1">
-//       <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"></path>
-//     </svg>
-//   `,
-//   'test-icon2': `
-//     <svg id="test-icon2">
-//     <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"></path>
-//     </svg>
-//   `,
-//   'bad-icon': `<div></div>`
-// };
+const testLibraryIcons = {
+  'test-icon1': `
+    <svg id="test-icon1">
+      <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"></path>
+    </svg>
+  `,
+  'test-icon2': `
+    <svg id="test-icon2">
+    <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"></path>
+    </svg>
+  `,
+  'bad-icon': `<div></div>`
+};
 
 describe('<sd-icon>', () => {
-  // TODO: registerIconLibrary is currently not working: https://github.com/solid-design-system/solid/issues/296
-  // before(() => {
-  //   registerIconLibrary('test-library', {
-  //     resolver: (name: keyof typeof testLibraryIcons) => {
-  //       // only for testing a bad request
-  //       if (name === ('bad-request' as keyof typeof testLibraryIcons)) {
-  //         return `data:image/svg+xml`;
-  //       }
+  before(() => {
+    // Tests run differently when served as ESM vs. bundled
+    // Here we are checking for the existence of the Solid Components global
+    // to determine which mode we are in.
 
-  //       if (name in testLibraryIcons) {
-  //         return `data:image/svg+xml,${encodeURIComponent(testLibraryIcons[name])}`;
-  //       }
-  //       return '';
-  //     },
-  //     mutator: (svg: SVGElement) => svg.setAttribute('fill', 'currentColor')
-  //   });
-  // });
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    let registerIconLibraryForTest;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+    if ((window as any)['Solid Components']) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+      registerIconLibraryForTest = (window as any)['Solid Components']['registerIconLibrary']; // Bundle Mode
+    } else {
+      registerIconLibraryForTest = registerIconLibrary; // ES Module Mode
+    }
+    registerIconLibraryForTest('test-library', {
+      resolver: (name: keyof typeof testLibraryIcons) => {
+        // only for testing a bad request
+        if (name === ('bad-request' as keyof typeof testLibraryIcons)) {
+          return `data:image/svg+xml`;
+        }
+
+        if (name in testLibraryIcons) {
+          return `data:image/svg+xml,${encodeURIComponent(testLibraryIcons[name])}`;
+        }
+        return '';
+      },
+      mutator: (svg: SVGElement) => svg.setAttribute('fill', 'currentColor')
+    });
+  });
 
   describe('defaults ', () => {
     it('default properties', async () => {
@@ -100,59 +110,58 @@ describe('<sd-icon>', () => {
     });
   });
 
-  // TODO: registerIconLibrary is currently not working: https://github.com/solid-design-system/solid/issues/296
-  // describe('new library', () => {
-  //   it('renders icons from the new library and emits sd-load event', async () => {
-  //     const el = await fixture<SdIcon>(html` <sd-icon library="test-library"></sd-icon> `);
-  //     const listener = oneEvent(el, 'sd-load') as Promise<CustomEvent>;
+  describe('new library', () => {
+    it('renders icons from the new library and emits sd-load event', async () => {
+      const el = await fixture<SdIcon>(html` <sd-icon library="test-library"></sd-icon> `);
+      const listener = oneEvent(el, 'sd-load') as Promise<CustomEvent>;
 
-  //     el.name = 'test-icon1';
-  //     const ev = await listener;
-  //     await elementUpdated(el);
+      el.name = 'test-icon1';
+      const ev = await listener;
+      await elementUpdated(el);
 
-  //     expect(el.shadowRoot?.querySelector('svg')).to.exist;
-  //     expect(ev.isTrusted).to.exist;
-  //   });
+      expect(el.shadowRoot?.querySelector('svg')).to.exist;
+      expect(ev.isTrusted).to.exist;
+    });
 
-  //   it('runs mutator from new library', async () => {
-  //     const el = await fixture<SdIcon>(html` <sd-icon library="test-library" name="test-icon1"></sd-icon> `);
-  //     await elementUpdated(el);
+    it('runs mutator from new library', async () => {
+      const el = await fixture<SdIcon>(html` <sd-icon library="test-library" name="test-icon1"></sd-icon> `);
+      await elementUpdated(el);
 
-  //     const svg = el.shadowRoot?.querySelector('svg');
-  //     expect(svg?.getAttribute('fill')).to.equal('currentColor');
-  //   });
-  // });
+      const svg = el.shadowRoot?.querySelector('svg');
+      expect(svg?.getAttribute('fill')).to.equal('currentColor');
+    });
+  });
 
-  // describe('negative cases', () => {
-  //   // using new library so we can test for malformed icons when registered
-  //   it("svg not rendered with an icon that doesn't exist in the library", async () => {
-  //     const el = await fixture<SdIcon>(html` <sd-icon library="test-library" name="does-not-exist"></sd-icon> `);
+  describe('negative cases', () => {
+    // using new library so we can test for malformed icons when registered
+    it("svg not rendered with an icon that doesn't exist in the library", async () => {
+      const el = await fixture<SdIcon>(html` <sd-icon library="test-library" name="does-not-exist"></sd-icon> `);
 
-  //     expect(el.shadowRoot?.querySelector('svg')).to.be.null;
-  //   });
+      expect(el.shadowRoot?.querySelector('svg')).to.be.null;
+    });
 
-  //   it('emits sd-error when the file cant be retrieved', async () => {
-  //     const el = await fixture<SdIcon>(html` <sd-icon library="test-library"></sd-icon> `);
-  //     const listener = oneEvent(el, 'sd-error') as Promise<CustomEvent>;
+    it('emits sd-error when the file cant be retrieved', async () => {
+      const el = await fixture<SdIcon>(html` <sd-icon library="test-library"></sd-icon> `);
+      const listener = oneEvent(el, 'sd-error') as Promise<CustomEvent>;
 
-  //     el.name = 'bad-request';
-  //     const ev = await listener;
-  //     await elementUpdated(el);
+      el.name = 'bad-request';
+      const ev = await listener;
+      await elementUpdated(el);
 
-  //     expect(el.shadowRoot?.querySelector('svg')).to.be.null;
-  //     expect(ev).to.exist;
-  //   });
+      expect(el.shadowRoot?.querySelector('svg')).to.be.null;
+      expect(ev).to.exist;
+    });
 
-  //   it("emits sd-error when there isn't an svg element in the registered icon", async () => {
-  //     const el = await fixture<SdIcon>(html` <sd-icon library="test-library"></sd-icon> `);
-  //     const listener = oneEvent(el, 'sd-error') as Promise<CustomEvent>;
+    it("emits sd-error when there isn't an svg element in the registered icon", async () => {
+      const el = await fixture<SdIcon>(html` <sd-icon library="test-library"></sd-icon> `);
+      const listener = oneEvent(el, 'sd-error') as Promise<CustomEvent>;
 
-  //     el.name = 'bad-icon';
-  //     const ev = await listener;
-  //     await elementUpdated(el);
+      el.name = 'bad-icon';
+      const ev = await listener;
+      await elementUpdated(el);
 
-  //     expect(el.shadowRoot?.querySelector('svg')).to.be.null;
-  //     expect(ev).to.exist;
-  //   });
-  // });
+      expect(el.shadowRoot?.querySelector('svg')).to.be.null;
+      expect(ev).to.exist;
+    });
+  });
 });
