c556f9144aea865e83d9508d3af3c34ed029e4e8
mariohamann <mario.hamann@virtual-identity.com>
2023-10-06 13:44:08 +0200

    fix linting

--- packages/components/scripts/vite-plugin-get-tailwind-theme.ts
+++ packages/components/scripts/vite-plugin-get-tailwind-theme.ts
@@ -1,15 +1,17 @@
+// @ts-expect-error - no types
 import theme from '../../tokens/src/create-theme.cjs';
 
 export default () => {
   return {
     name: 'inject-theme',
-    resolveId(id) {
+    resolveId(id: unknown) {
       if (id === 'tailwind-theme') {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
         return id;
       }
       return null;
     },
-    load(id) {
+    load(id: unknown) {
       if (id === 'tailwind-theme') {
         return `export default ${JSON.stringify(theme).replace(/<alpha-value>/g, '1')};`;
       }
