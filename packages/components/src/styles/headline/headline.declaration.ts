91efaf8ca62eb3b052a05c893a57ce0ee6d450fa
Vahid Menu Nesro <vahid.nesro@virtual-identity.com>
2023-10-06 14:59:55 +0200

    docs: update headline status

--- packages/components/src/styles/headline/headline.declaration.ts
+++ packages/components/src/styles/headline/headline.declaration.ts
@@ -4,8 +4,8 @@ export default {
   styleName: 'sd-headline',
   summary:
     'Headlines are vital for displaying content hierarchy and to improve accessibility. A headline can be additionally accompanied by an icon. The icon can be displayed on the left side or inline.',
-  status: 'experimental',
-  since: '1.7',
+  status: 'stable',
+  since: '1.16',
   attributes: [
     {
       name: 'sd-headline--size-...',
