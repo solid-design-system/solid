5d0218674d89e4687db466f9051480017528d11e
Yigit, Özlem (UIT) <Oezlem.Yigit@union-investment.de>
2023-10-09 17:04:49 +0200

    feat: add test

--- packages/components/src/components/radio/radio.test.ts
+++ packages/components/src/components/radio/radio.test.ts
@@ -1,22 +1,23 @@
-// import { expect, fixture, html } from '@open-wc/testing';
-// import type SdRadio from './radio';
-// import type SdRadioGroup from '../radio-group/radio-group';
+import '../../../dist/shoelace.js';
+import { expect, fixture, html } from '@open-wc/testing';
+import type SdRadio from './radio';
+import type SdRadioGroup from '../radio-group/radio-group';
 
-// describe('<sd-radio>', () => {
-//   it('should not get checked when disabled', async () => {
-//     const radioGroup = await fixture<SdRadioGroup>(html`
-//       <sd-radio-group value="1">
-//         <sd-radio id="radio-1" value="1"></sd-radio>
-//         <sd-radio id="radio-2" value="2" disabled></sd-radio>
-//       </sd-radio-group>
-//     `);
-//     const radio1 = radioGroup.querySelector<SdRadio>('#radio-1')!;
-//     const radio2 = radioGroup.querySelector<SdRadio>('#radio-2')!;
+describe('<sd-radio>', () => {
+  it('should not get checked when disabled', async () => {
+    const radioGroup = await fixture<SdRadioGroup>(html`
+      <sd-radio-group value="1">
+        <sd-radio id="radio-1" value="1"></sd-radio>
+        <sd-radio id="radio-2" value="2" disabled></sd-radio>
+      </sd-radio-group>
+    `);
+    const radio1 = radioGroup.querySelector<SdRadio>('#radio-1')!;
+    const radio2 = radioGroup.querySelector<SdRadio>('#radio-2')!;
 
-//     radio2.click();
-//     await Promise.all([radio1.updateComplete, radio2.updateComplete]);
+    radio2.click();
+    await Promise.all([radio1.updateComplete, radio2.updateComplete]);
 
-//     expect(radio1.checked).to.be.true;
-//     expect(radio2.checked).to.be.false;
-//   });
-// });
+    expect(radio1.checked).to.be.true;
+    expect(radio2.checked).to.be.false;
+  });
+});
