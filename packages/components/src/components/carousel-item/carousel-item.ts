aa50f56bc6ef6df0bc5ff7c1344e14ce329c66e5
Vahid Nesro <63849626+Vahid1919@users.noreply.github.com>
2023-10-10 16:09:32 +0200

    feat: ✨sd-carousel (#324)

---
+++ packages/components/src/components/carousel-item/carousel-item.ts
@@ -0,0 +1,61 @@
+import { css, html } from 'lit';
+import { customElement } from '../../../src/internal/register-custom-element';
+import componentStyles from '../../styles/component.styles';
+import SolidElement from '../../internal/solid-element.js';
+
+/**
+ * @summary A carousel item represent a slide within a [carousel](/components/carousel).
+ * @documentation https://solid.union-investment.com/[storybook-link]/carousel
+ *
+ * @since 1.12.0
+ * @status stable
+ *
+ * @slot - The carousel item's content.
+ *
+ *
+ */
+@customElement('sd-carousel-item')
+export default class SdCarouselItem extends SolidElement {
+  static isCarouselItem(node: Node) {
+    return node instanceof Element && node.getAttribute('aria-roledescription') === 'slide';
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.setAttribute('role', 'group');
+  }
+
+  render() {
+    return html` <slot></slot> `;
+  }
+
+  static styles = [
+    SolidElement.styles,
+    css`
+      ${componentStyles}
+      :host {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        width: 100%;
+        height: 100%;
+        aspect-ratio: var(--aspect-ratio);
+        scroll-snap-align: start;
+        scroll-snap-stop: always;
+      }
+
+      ::slotted(img) {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    `
+  ];
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'sd-carousel-item': SdCarouselItem;
+  }
+}
--- packages/components/src/styles/interactive/interactive.declaration.ts
+++ packages/components/src/styles/interactive/interactive.declaration.ts
@@ -11,6 +11,10 @@ export default {
       description: 'Makes an element look disabled.'
     },
     {
+      name: 'sd-interactive--inverted',
+      description: 'Inverts the colors of an element.'
+    },
+    {
       name: 'sd-interactive--reset',
       description: 'Resets the default browser styles of e. g. a button.'
     }
