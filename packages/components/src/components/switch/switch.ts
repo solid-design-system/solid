e4df8289a03b42de33b3a7f2c67b0f8783b7e6c3
Daniel Hargesheimer <daniel.hargesheimer@kleerly.de>
2023-11-17 11:51:52 +0100

    chore: bump component version

--- packages/components/src/components/switch/switch.ts
+++ packages/components/src/components/switch/switch.ts
@@ -14,7 +14,7 @@ import type { SolidFormControl } from '../../internal/solid-element';
  * @summary Switches allow the user to toggle an option on or off.
  * @documentation https://solid.union-investment.com/[storybook-link]/switch
  * @status stable
- * @since 1.24.0
+ * @since 1.26.0
  *
  *
  * @slot - The switch's label.
