92ba521823453a995a2c8c1eea9576250dedb790
Vahid Menu Nesro <vahid.nesro@virtual-identity.com>
2023-11-22 11:07:19 +0100

    fix: revert file move

--- packages/components/.storybook/addons/theme-generator/preview.ts
+++ packages/components/.storybook/addons/theme-generator/preview.ts
@@ -1,6 +1,6 @@
 import type { Renderer, ProjectAnnotations } from '@storybook/types';
-import { PARAM_KEY } from './constants';
-import { withGlobals } from './theme-generator/withGlobals';
+import { PARAM_KEY } from '../constants';
+import { withGlobals } from './withGlobals';
 
 const preview: ProjectAnnotations<Renderer> = {
   decorators: [withGlobals],
