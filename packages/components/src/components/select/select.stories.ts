135b475bbd4ce12b23eabea224f4bab548a0df4b
Alec Budd <alec.budd@prodyna.com>
2023-11-27 12:25:49 +0100

    feat: multiple stories added

--- packages/components/src/components/select/select.stories.ts
+++ packages/components/src/components/select/select.stories.ts
@@ -29,7 +29,7 @@ export default {
 
 export const Default = {
   render: (args: any) => {
-    return html`<div class="h-[240px]">${generateTemplate({ args })}</div>`;
+    return html`<div class="h-[220px] w-[420px]">${generateTemplate({ args })}</div>`;
   }
 };
 
@@ -44,7 +44,7 @@ export const Label = {
     }
   },
   render: (args: any) => {
-    return html`<div class="h-[240px]">
+    return html`<div class="h-[260px] w-[420px]">
       ${generateTemplate({
         constants: [
           {
@@ -71,7 +71,7 @@ export const HelpText = {
     }
   },
   render: (args: any) => {
-    return html`<div class="h-[240px]">
+    return html`<div class="h-[260px] w-[420px]">
       ${generateTemplate({
         constants: [
           {
@@ -108,7 +108,7 @@ export const Placeholder = {
     }
   },
   render: (args: any) => {
-    return html`<div class="h-[240px]">
+    return html`<div class="h-[220px] w-[420px]">
       ${generateTemplate({
         constants: [
           {
@@ -134,7 +134,7 @@ export const Clearable = {
     }
   },
   render: (args: any) => {
-    return html`<div class="h-[240px]">
+    return html`<div class="h-[220px] w-[420px]">
       ${generateTemplate({
         constants: [
           {
@@ -160,7 +160,7 @@ export const Disabled = {
     }
   },
   render: (args: any) => {
-    return html`<div>
+    return html`<div class="w-[420px]">
       ${generateTemplate({
         constants: [
           {
@@ -181,6 +181,52 @@ export const Disabled = {
 };
 
 /**
+ * To allow multiple options to be selected, use the `multiple` attribute. It’s a good practice to use `clearable` when this option is enabled. To use the checkbox with tags variant, set the `checklist` variant to `true`.  To set multiple values at once, set value to a space-delimited list of values.
+ */
+
+export const Multiple = {
+  parameters: {
+    controls: {
+      include: []
+    }
+  },
+  render: (args: any) => {
+    return html`<div class="h-[340px]">
+      ${generateTemplate({
+        options: {
+          classes: 'w-full [&>tbody>tr>td]:w-[50%]'
+        },
+        axis: {
+          x: {
+            type: 'attribute',
+            name: 'checklist'
+          }
+        },
+        constants: [
+          {
+            type: 'attribute',
+            name: 'clearable',
+            value: true
+          },
+          {
+            type: 'attribute',
+            name: 'multiple',
+            value: true
+          },
+          {
+            type: 'slot',
+            name: 'default',
+            value:
+              '<sd-option value="option-1">Option 1</sd-option><sd-option value="option-2">Option 2</sd-option><sd-option value="option-3">Option 3</sd-option>'
+          }
+        ],
+        args: null
+      })}
+    </div>`;
+  }
+};
+
+/**
  * Use the `size` attribute to change a select’s size. Note that `size` does not apply to listbox options.
  */
 
@@ -193,6 +239,9 @@ export const Sizes = {
   render: (args: any) => {
     return html`<div class="h-[340px]">
       ${generateTemplate({
+        options: {
+          classes: 'w-full'
+        },
         axis: {
           x: {
             type: 'attribute',
@@ -224,8 +273,11 @@ export const Placement = {
     }
   },
   render: (args: any) => {
-    return html`<div class="h-[340px]">
+    return html`<div class="h-[290px]">
       ${generateTemplate({
+        options: {
+          classes: 'w-full'
+        },
         axis: {
           x: {
             type: 'attribute',
@@ -257,7 +309,7 @@ export const GroupingOptions = {
     }
   },
   render: (args: any) => {
-    return html`<div class="h-[290px]">
+    return html`<div class="h-[290px] w-[420px]">
       ${generateTemplate({
         constants: [
           {
@@ -272,6 +324,133 @@ export const GroupingOptions = {
     </div>`;
   }
 };
+
+/**
+ * Shows available slots. The `label` and `help-text` slots will overwrite their corresponding attributes.
+ */
+
+export const Slots = {
+  parameters: {
+    controls: {
+      exclude: ['default', 'label', 'prefix', 'clear-icon', 'expand-icon', 'help-text']
+    }
+  },
+  render: (args: any) => {
+    return html`
+      ${['default', 'label', 'prefix', 'clear-icon', 'expand-icon', 'help-text'].map(slot =>
+        generateTemplate({
+          axis: {
+            x: {
+              type: 'slot',
+              name: slot,
+              title: 'slot=...',
+              values: [
+                {
+                  value:
+                    slot === 'default'
+                      ? `<div class="slot slot--border slot--background h-8 w-full"></div>`
+                      : `<div slot='${slot}' class="slot slot--border slot--background h-6 ${
+                          slot === 'label' || slot === 'help-text' ? 'w-18' : 'w-6'
+                        }"></div>`,
+                  title: slot
+                }
+              ]
+            }
+          },
+          constants: [
+            { type: 'template', name: 'width', value: '<div style="width: 300px">%TEMPLATE%</div>' },
+            { type: 'attribute', name: 'clearable', value: true },
+            { type: 'attribute', name: 'label', value: 'Label' },
+            { type: 'attribute', name: 'help-text', value: 'help-text' },
+            {
+              type: 'slot',
+              name: 'left',
+              value: '<sd-icon slot="prefix" library="global-resources" name="system/picture"></sd-icon>'
+            },
+            {
+              type: 'slot',
+              name: 'default',
+              value: '<sd-option value="option-1">Option 1</sd-option><sd-option value="option-2">Option 2</sd-option>'
+            }
+          ],
+          args: null
+        })
+      )}
+    `;
+  }
+};
+
+/**
+ * Use the `form-control`, `form-control-label`, `form-control-input`, `form-control-help-text`, `base`, `border`, `input`, `left`, `clear-button`, and `right` part selectors to customize the input.
+ */
+
+// export const Parts = {
+//   parameters: {
+//     controls: {
+//       exclude: ['label', 'left', 'right', 'clear-icon', 'help-text', 'clearable', 'value']
+//     }
+//   },
+//   render: (args: any) => {
+//     return generateTemplate({
+//       axis: {
+//         y: {
+//           type: 'template',
+//           name: 'sd-input::part(...){outline: solid 2px red}',
+//           values: [
+//             'form-control',
+//             'form-control-label',
+//             'form-control-input',
+//             'form-control-help-text',
+//             'base',
+//             'border',
+//             'input',
+//             'left',
+//             'clear-button',
+//             'right'
+//           ].map(part => {
+//             return {
+//               title: part,
+//               value: `<style>#part-${part} sd-input::part(${part}){outline: solid 2px red}</style><div id="part-${part}">%TEMPLATE%</div>`
+//             };
+//           })
+//         }
+//       },
+//       constants: [
+//         { type: 'attribute', name: 'clearable', value: true },
+//         { type: 'attribute', name: 'value', value: 'value' },
+//         { type: 'attribute', name: 'label', value: 'Label' },
+//         { type: 'attribute', name: 'help-text', value: 'help-text' },
+//         {
+//           type: 'slot',
+//           name: 'left',
+//           value: '<sd-icon slot="left" library="global-resources" name="system/picture"></sd-icon>'
+//         },
+//         {
+//           type: 'slot',
+//           name: 'right',
+//           value: '<sd-icon slot="right" library="global-resources" name="system/picture"></sd-icon>'
+//         }
+//       ],
+//       args
+//     });
+//   }
+// };
+
+/**
+ * `sd-input` is fully accessibile via keyboard.
+ */
+
+// export const Mouseless = {
+//   render: (args: any) => {
+//     return html`<div class="mouseless w-[250px]">${generateTemplate({ args })}</div>`;
+//   },
+
+//   play: async ({ canvasElement }: { canvasElement: HTMLUnknownElement }) => {
+//     const el = canvasElement.querySelector('.mouseless sd-input');
+//     await waitUntil(() => el?.shadowRoot?.querySelector('input'));
+//     el?.shadowRoot?.querySelector('input')!.focus();
+//   }
+// };
 
 /**
  * Dev: Temporary development story
@@ -300,32 +479,6 @@ export const Dev = {
         ],
         args
       })}
-    </div>`;
-  }
-};
-
-/**
- * Dev: Temporary development story
- */
-
-export const Dev2 = {
-  render: (args: any) => {
-    return html`<div class="max-w-[500px]">
-      <sd-select>
-        <sd-option value="option-1">Option 1</sd-option>
-        <sd-option value="option-2">Option 2</sd-option>
-        <sd-option value="option-3">Option 3</sd-option>
-        <sd-option value="option-4">Option 4</sd-option>
-        <sd-option value="option-5">Option 5</sd-option>
-      </sd-select>
-      <sd-select>
-        <sd-option value="option-1">Option 1</sd-option>
-        <sd-option value="option-2">Option 2</sd-option>
-        <sd-option value="option-3">Option 3</sd-option>
-        <sd-option value="option-4">Option 4</sd-option>
-        <sd-option value="option-5">Option 5</sd-option>
-      </sd-select>
-      <span class="text-2xl">Hello</span>
     </div>`;
   }
 };
