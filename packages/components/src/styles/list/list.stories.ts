797b0570760f3b3230591de1c348c084ee5a574b
Samuel Kohler <samuel.kohler@prodyna.com>
2023-12-11 16:01:21 +0100

    feat: adjust stories

--- packages/components/src/styles/list/list.stories.ts
+++ packages/components/src/styles/list/list.stories.ts
@@ -15,6 +15,9 @@ export default {
   component: 'sd-list',
   parameters: {
     ...parameters,
+    backgrounds: {
+      default: 'primary-100'
+    },
     design: {
       type: 'figma',
       url: 'https://www.figma.com/file/DDSyYvf2q99RhiyDjy03s5/List?type=design&node-id=971-4578&mode=design&t=2UZo6NW6ErMA2G5X-0'
@@ -29,6 +32,84 @@ export const Default = {
     return generateTemplate({
       options: {templateContent: '<ul class="%CLASSES%"><li>%SLOT%</li><li>%SLOT%</li><li>%SLOT%</li></ul>'},
       args
+    });
+  }
+};
+
+export const OrderedList = {
+  name: 'OrderedList',
+  parameters: {controls: {exclude: ['default']}},
+  render: (args: any) => {
+    return generateTemplate({
+      axis: {
+        x: {type: 'attribute', name: 'sd-list--inverted', values: ['', 'sd-list--inverted']},
+      },
+      args,
+      options: {
+        templateBackgrounds: {
+          alternate: 'x',
+          colors: ['rgb(var(--sd-color-primary-100, 236 240 249))', 'rgb(var(--sd-color-primary, 0 53 142))']
+        },
+        templateContent: '<ol class="%CLASSES%">\n' +
+          '    <li>%SLOT%</li>\n' +
+          '    <li>%SLOT%</li>\n' +
+          '    <li>%SLOT% </li>\n' +
+          '</ol>'
+      }
+    });
+  }
+};
+
+export const UnorderedList = {
+  name: 'UnorderedList',
+  parameters: {controls: {exclude: ['default']}},
+  render: (args: any) => {
+    return generateTemplate({
+      axis: {
+        x: {type: 'attribute', name: 'sd-list--inverted', values: ['', 'sd-list--inverted']},
+      },
+      args,
+      options: {
+        templateBackgrounds: {
+          alternate: 'x',
+          colors: ['rgb(var(--sd-color-primary-100, 236 240 249))', 'rgb(var(--sd-color-primary, 0 53 142))']
+        },
+        templateContent: '<ul class="%CLASSES%">\n' +
+          '    <li>%SLOT%</li>\n' +
+          '    <li>%SLOT%</li>\n' +
+          '    <li>%SLOT% </li>\n' +
+          '</ul>'
+      }
+    });
+  }
+};
+
+
+export const IconList = {
+  name: 'IconList',
+  parameters: {controls: {exclude: ['default']}},
+  render: (args: any) => {
+    return generateTemplate({
+      axis: {
+        x: {type: 'attribute', name: 'sd-list--inverted', values: ['', 'sd-list--inverted']},
+        y: {
+          type: 'attribute',
+          name: 'variants',
+          values: ['sd-list--icon', 'sd-list--icon sd-list--horizontal']
+        }
+      },
+      args,
+      options: {
+        templateBackgrounds: {
+          alternate: 'x',
+          colors: ['rgb(var(--sd-color-primary-100, 236 240 249))', 'rgb(var(--sd-color-primary, 0 53 142))']
+        },
+        templateContent: '<ul class="%CLASSES%">\n' +
+          '    <li><sd-icon name="content/picture" library="global-resources" color="primary"/>%SLOT%</li>\n' +
+          '    <li><sd-icon name="content/picture" library="global-resources" color="primary"/>%SLOT%</li>\n' +
+          '    <li><sd-icon name="content/picture" library="global-resources" color="primary"/>%SLOT% </li>\n' +
+          '</ul>'
+      }
     });
   }
 };
