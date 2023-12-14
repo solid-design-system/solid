871fd86f9ad34b6b5de457865910762ef4c2c84d
azraefendic <azra_efendic@live.com>
2023-12-14 09:24:36 +0100

    fix css in radio-button

--- packages/components/src/components/radio-button/radio-button.ts
+++ packages/components/src/components/radio-button/radio-button.ts
@@ -156,7 +156,7 @@ export default class SdRadioButton extends SolidElement {
       /* We use a hidden input so constraint validation errors work, since they don't appear to show when used with buttons. We can't actually hide it, though, otherwise the messages will be suppressed by the browser. */
       .hidden-input {
         all: unset;
-        @apply absolute inset-0 outline-dotted outline-1 outline-red-500 opacity-0 -z-10;
+        @apply absolute inset-0 outline-dotted outline-1 outline-error opacity-0 -z-10;
       }
 
       .lg-label {
