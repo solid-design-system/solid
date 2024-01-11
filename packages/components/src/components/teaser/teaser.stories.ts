1f9ee36f6b975835b075f70dbd5b73d900fb9983
Samuel Kohler <samuel.kohler@prodyna.com>
2024-01-11 08:10:02 +0100

    feat: update teaser stories

--- packages/components/src/components/teaser/teaser.stories.ts
+++ packages/components/src/components/teaser/teaser.stories.ts
@@ -463,13 +463,13 @@ export const SamplesNotClickable = {
  * ## Clickable teaser
  *
  * If the teaser itself should be clickable and there are no other links inside, then wrap the teaser with an anchor tag.
- * The button on the bottom of the teaser is not a link, it's just to make the interaction more obvious. According to
- * the HTML spec, it is not allowed to have an anchor tag inside another anchor.
+ * The button on the bottom of the teaser is **must not be a link**. According to the HTML spec, it is not allowed to
+ * have an anchor tag inside another anchor.
  *
  * ```html
  * <a href="#">
  *    <sd-teaser class="interactive">
- *      <sd-button>More</sd-button>
+ *      <sd-button>CTA</sd-button>
  *    </<sd-teaser>
  * </a>
  * ```
@@ -533,7 +533,7 @@ export const SamplesClickable = {
                 dolore magna aliqua.
               </p>
               <div class="flex-none">
-                <sd-button variant="primary" inverted>More</sd-button>
+                <sd-button variant="cta" inverted>Add to shopping cart</sd-button>
               </div>
             </div>
           </sd-teaser>
@@ -555,188 +555,12 @@ export const SamplesClickable = {
                 dolore magna aliqua.
               </p>
               <div class="flex-none">
-                <sd-button variant="primary">More</sd-button>
+                <sd-button variant="cta">Add to shopping cart</sd-button>
               </div>
             </div>
           </sd-teaser>
         </a>
       </div>
-    `;
-  }
-};
-
-/**
- * ## Clickable teaser with extra links
- *
- * If the teaser itself should be clickable and also provide external links inside, then add a `data-href` attribute
- * to the teaser and a `data-target` attribute. You can then use the following or similar JavaScript to handle the
- * click event.
- *
- * ```html
- * <sd-teaser class="interactive" data-href="#" data-target="_blank" tabindex="0" role="link">
- *   <sd-button href="#">Link</sd-button>
- * </<sd-teaser>
- * ```
- *
- * ```javascript
- * document.addEventListener('DOMContentLoaded', () => {
- *     function handleTeaserClick(e) {
- *         // Exclude all clickable elements inside the teaser
- *         if (e.target !== this && e.target.closest('sd-button, sd-link, a')) {
- *         return;
- *         }
- *
- *         const href = this.getAttribute('data-href');
- *         const target = this.getAttribute('data-target');
- *
- *         if (href) {
- *         const linkTarget = target === '_blank' ? '_blank' : '_self';
- *         window.open(href, linkTarget);
- *         }
- *     }
- *
- *     // Select all sd-teaser elements with a data-href attribute
- *     // and add click event listeners to these elements
- *     document.querySelectorAll('sd-teaser[data-href]').forEach(teaser => {
- *         teaser.addEventListener('click', handleTeaserClick);
- *     });
- * });
- * ```
- * To maintain the accessibility of the teaser, the `tabindex` attribute should be set and the `role` attribute
- * must be set to `link`. Ensure that the tab order is correct and the teaser can be opened with the `Enter` key.
- *
- * To further highlight the interactive state of the teaser, the opacity of the media part can be reduced on hover.
- *
- * ```css
- * sd-teaser.interactive:hover {
- *   &::part(media) {
- *     transition-property: opacity;
- *     transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
- *     transition-duration: 150ms;
- *     transition-duration: 300ms;
- *     opacity: 0.5;
- *   }
- * }
- * ```
- */
-
-export const SamplesClickableAndExtraLinks = {
-  name: 'Samples: Clickable teaser with extra links',
-  parameters: {
-    controls: {
-      disable: true
-    },
-    backgrounds: {
-      default: 'white'
-    }
-  },
-  render: () => {
-    return html`
-      <style>
-        #teaserWithContentPlaceholder::part(media) {
-          flex-grow: 1;
-        }
-
-        sd-teaser.interactive:hover {
-          &::part(media) {
-            transition-property: opacity;
-            transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-            transition-duration: 150ms;
-            transition-duration: 300ms;
-            opacity: 0.5;
-          }
-        }
-      </style>
-      <div class="flex justify-between gap-8">
-        <sd-teaser
-          variant="primary"
-          breakpoint="9999"
-          inset
-          class="flex-1 interactive hover:cursor-pointer"
-          data-href="#"
-          data-target="_blank"
-          tabindex="0"
-          role="link"
-        >
-          <div slot="media" class="relative">
-            <img class="aspect-video object-cover" src="./placeholders/generic.jpg" alt="A generic placeholder jpg" />
-          </div>
-          <div slot="meta" class="meta-info">
-            <span class="meta-info-item">01.12.2013</span>
-            <span class="meta-info-item">| Author name</span>
-          </div>
-          <h3 slot="headline">Clickable teaser with extra links</h3>
-          <div class="flex flex-col gap-5">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua.
-            </p>
-            <a href="https://www.finanzagenda.de/" target="_blank" class="sd-link">An anchor tag</a>
-            <sd-link href="https://www.finanzagenda.de/" target="_blank" class="sd-link" inverted>A SD-link</sd-link>
-            <div class="flex-none">
-              <sd-button variant="primary" href="https://www.finanzagenda.de/" target="_blank" inverted
-                >A SD-button</sd-button
-              >
-            </div>
-          </div>
-        </sd-teaser>
-
-        <sd-teaser
-          variant="white border-neutral-400"
-          breakpoint="9999"
-          inset
-          class="flex-1 interactive hover:cursor-pointer"
-          data-href="#"
-          data-target="_blank"
-          tabindex="0"
-          role="link"
-        >
-          <div slot="media" class="relative">
-            <img class="aspect-video object-cover" src="./placeholders/generic.jpg" alt="A generic placeholder jpg" />
-          </div>
-          <div slot="meta" class="meta-info">
-            <span class="meta-info-item">01.12.2013</span>
-            <span class="meta-info-item">| Author name</span>
-          </div>
-          <h3 slot="headline">Clickable teaser with extra links</h3>
-          <div class="flex flex-col gap-5">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua.
-            </p>
-            <a href="https://www.finanzagenda.de/" target="_blank" class="sd-link">An anchor tag</a>
-            <sd-link href="https://www.finanzagenda.de/" target="_blank" class="sd-link">A SD-link</sd-link>
-            <div class="flex-none">
-              <sd-button variant="primary" href="https://www.finanzagenda.de/" target="_blank">A SD-button</sd-button>
-            </div>
-          </div>
-        </sd-teaser>
-      </div>
-
-      <script>
-        document.addEventListener('DOMContentLoaded', () => {
-          function handleTeaserClick(e) {
-            // Exclude all clickable elements inside the teaser
-            if (e.target !== this && e.target.closest('sd-button, sd-link, a')) {
-              return;
-            }
-
-            const href = this.getAttribute('data-href');
-            const target = this.getAttribute('data-target');
-
-            if (href) {
-              const linkTarget = target === '_blank' ? '_blank' : '_self';
-              window.open(href, linkTarget);
-            }
-          }
-
-          // Select all sd-teaser elements with a data-href attribute
-          // and add click event listeners to these elements
-          document.querySelectorAll('sd-teaser[data-href]').forEach(teaser => {
-            teaser.addEventListener('click', handleTeaserClick);
-          });
-        });
-      </script>
     `;
   }
 };
