fa86f755b746fa0ae8f1a87a6688a68c020ffd09
Christoph Saile <christoph.saile@virtual-identity.com>
2024-01-24 15:49:40 +0100

    refactor: replace components colorCalculations script with script from theming package

--- packages/components/.storybook/addons/theme-generator/Panel.tsx
+++ packages/components/.storybook/addons/theme-generator/Panel.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { AddonPanel, Form } from '@storybook/components';
 import { PARAM_KEY, PANEL_DEFAULTS } from './constants';
 import { useGlobals } from '@storybook/manager-api';
-import { calculateColorsAsCss } from './colorCalculations';
+import { calculateColorsAsCss } from '@solid-design-system/theming/src/color-calculation';
 import theme from '../../../../tokens/src/create-theme.cjs';
 
 const { Textarea, Button } = Form;
