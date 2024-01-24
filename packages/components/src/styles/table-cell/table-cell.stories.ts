82a331dc98b2cad9856f1647d05d35af67d413be
Samuel Kohler <samuel.kohler@prodyna.com>
2024-01-24 08:09:42 +0100

    feat: clean up samples

--- packages/components/src/styles/table-cell/table-cell.stories.ts
+++ packages/components/src/styles/table-cell/table-cell.stories.ts
@@ -43,8 +43,167 @@ export const Default = {
  * The data generation and table sorting logic should not be used in production environments.
  *
  */
-
 export const Samples = {
+  parameters: {
+    controls: {
+      exclude: ['sd-table-cell--divider', 'sd-table-cell--bg-...']
+    }
+  },
+  render: (args: Record<string, any>) => {
+    // Initalize table data
+    const tableRowCount = 7;
+    const tableColumnCount = 5;
+    const headerData = Array.from({ length: tableColumnCount }, () => 'Header');
+    const tableData = Array.from({ length: tableRowCount }, () =>
+      Array.from({ length: tableColumnCount }, () => {
+        return args['default-slot'] ?? 'Lorem ipsum dolor sit amet.';
+      })
+    );
+
+    return html`
+      <style>
+        .story-wrapper {
+          display: inline-block;
+          max-width: 1200px;
+        }
+
+        .headline {
+          padding: 16px;
+          background: #e0e0e0;
+          text-align: left;
+          font-size: 14px;
+          font-weight: bold;
+          width: 100%;
+          box-sizing: border-box;
+        }
+
+        .disclaimer {
+          background-color: #f6f6f6;
+          padding: 16px;
+          text-align: left;
+          font-size: 14px;
+          color: #333;
+          width: 100%;
+          margin-bottom: 10px;
+          box-sizing: border-box;
+        }
+
+        .sample-table {
+          margin-top: 20px;
+          width: 100%;
+        }
+
+        .sd-table-cell.sortable {
+          padding: 0;
+        }
+
+        .sd-table-cell.sortable button {
+          display: inline-flex;
+          box-sizing: border-box;
+          height: 100%;
+          width: 100%;
+          margin: 0;
+          padding: 16px;
+          align-items: center;
+          justify-content: flex-start;
+          gap: 16px;
+          text-align: left;
+        }
+
+        div:not(:first-of-type).headline {
+          margin-top: 72px;
+        }
+      </style>
+      <div class="story-wrapper">
+        <div class="headline">Simple Table</div>
+        <table class="sd-table sample-table">
+          <thead>
+            ${(() => {
+              return html`<tr>
+                ${headerData.map(cellData => {
+                  return html`<th class="sd-table-cell sd-table-cell--bg-transparent">${cellData}</th>`;
+                })}
+              </tr>`;
+            })()}
+          </thead>
+          <tbody>
+            ${tableData.map(rowData => {
+              return html`<tr>
+                ${rowData.map(cellData => {
+                  return html`<td class="sd-table-cell sd-table-cell--bg-transparent">${cellData}</td>`;
+                })}
+              </tr>`;
+            })}
+          </tbody>
+        </table>
+        <div class="headline">Simple Table With Vertical Lines</div>
+        <table class="sd-table sample-table">
+          <thead>
+            ${(() => {
+              return html`<tr>
+                ${headerData.map((cellData, columnIndex) => {
+                  return html`<th
+                    class="sd-table-cell sd-table-cell--bg-transparent ${columnIndex + 1 < headerData.length
+                      ? 'sd-table-cell--divider'
+                      : ''}"
+                  >
+                    ${cellData}
+                  </th>`;
+                })}
+              </tr>`;
+            })()}
+          </thead>
+          <tbody>
+            ${tableData.map(rowData => {
+              return html`<tr>
+                ${rowData.map((cellData, columnIndex) => {
+                  return html`<td
+                    class="sd-table-cell sd-table-cell--bg-transparent ${columnIndex + 1 < headerData.length
+                      ? 'sd-table-cell--divider'
+                      : ''}"
+                  >
+                    ${cellData}
+                  </td>`;
+                })}
+              </tr>`;
+            })}
+          </tbody>
+        </table>
+        <div class="headline">Simple Table With Alternating Colors</div>
+        <table class="sd-table sample-table">
+          <thead>
+            ${(() => {
+              return html`<tr>
+                ${headerData.map(cellData => {
+                  return html`<th class="sd-table-cell sd-table-cell--bg-primary-100">${cellData}</th>`;
+                })}
+              </tr>`;
+            })()}
+          </thead>
+          <tbody>
+            ${tableData.map((rowData, rowIndex) => {
+              return html`<tr>
+                ${rowData.map(cellData => {
+                  return html`<td
+                    class="sd-table-cell ${rowIndex % 2 === 0
+                      ? 'sd-table-cell--bg-white'
+                      : 'sd-table-cell--bg-primary-100'}"
+                  >
+                    ${cellData}
+                  </td>`;
+                })}
+              </tr>`;
+            })}
+          </tbody>
+        </table>
+        <div class="headline">Multi Select Table</div>
+        <div class="disclaimer">This sample will be provided soon.</div>
+      </div>
+    `;
+  }
+};
+
+export const AdvancedSamples = {
   parameters: {
     controls: {
       exclude: ['sd-table-cell--divider', 'sd-table-cell--bg-...']
@@ -156,66 +315,6 @@ export const Samples = {
             </tbody>
           </table>
         </div>
-        <div class="headline">Simple Table With Vertical Lines</div>
-        <table class="sd-table sample-table">
-          <thead>
-            ${(() => {
-              return html`<tr>
-                ${headerData.map((cellData, columnIndex) => {
-                  return html`<th
-                    class="sd-table-cell sd-table-cell--bg-transparent ${columnIndex + 1 < headerData.length
-                      ? 'sd-table-cell--divider'
-                      : ''}"
-                  >
-                    ${cellData}
-                  </th>`;
-                })}
-              </tr>`;
-            })()}
-          </thead>
-          <tbody>
-            ${tableData.map(rowData => {
-              return html`<tr>
-                ${rowData.map((cellData, columnIndex) => {
-                  return html`<td
-                    class="sd-table-cell sd-table-cell--bg-transparent ${columnIndex + 1 < headerData.length
-                      ? 'sd-table-cell--divider'
-                      : ''}"
-                  >
-                    ${cellData}
-                  </td>`;
-                })}
-              </tr>`;
-            })}
-          </tbody>
-        </table>
-        <div class="headline">Simple Table With Alternating Colors</div>
-        <table class="sd-table sample-table">
-          <thead>
-            ${(() => {
-              return html`<tr>
-                ${headerData.map(cellData => {
-                  return html`<th class="sd-table-cell sd-table-cell--bg-primary-100">${cellData}</th>`;
-                })}
-              </tr>`;
-            })()}
-          </thead>
-          <tbody>
-            ${tableData.map((rowData, rowIndex) => {
-              return html`<tr>
-                ${rowData.map(cellData => {
-                  return html`<td
-                    class="sd-table-cell ${rowIndex % 2 === 0
-                      ? 'sd-table-cell--bg-white'
-                      : 'sd-table-cell--bg-primary-100'}"
-                  >
-                    ${cellData}
-                  </td>`;
-                })}
-              </tr>`;
-            })}
-          </tbody>
-        </table>
 
         <script>
           document.addEventListener('DOMContentLoaded', event => {
@@ -266,11 +365,6 @@ export const Samples = {
             })}
           </table>
         </div>
-        <div class="headline">Simple Table, First Column Fixed</div>
-        <div class="disclaimer">This sample will be provided soon.</div>
-
-        <div class="headline">Multi Select Table</div>
-        <div class="disclaimer">This sample will be provided soon.</div>
       </div>
     `;
   }
