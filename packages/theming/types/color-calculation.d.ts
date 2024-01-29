acea9ae549c3793c46f2917d869915fc942589e6
Christoph Saile <christoph.saile@virtual-identity.com>
2024-01-29 10:54:33 +0100

    chore: fix linter error

--- packages/theming/types/color-calculation.d.ts
+++ packages/theming/types/color-calculation.d.ts
@@ -1,2 +1,13 @@
-export function calculateColorsForType(type: any, theme: any, colors: any, useNormalizedLuminanceMap: any, useForcedShades: any): string;
-export function calculateColorsAsCss(colors: any, theme: any, useNormalizedLuminanceMap: boolean, useForcedShades: boolean): string;
+export function calculateColorsForType(
+  type: any,
+  theme: any,
+  colors: any,
+  useNormalizedLuminanceMap: any,
+  useForcedShades: any
+): string;
+export function calculateColorsAsCss(
+  colors: any,
+  theme: any,
+  useNormalizedLuminanceMap: boolean,
+  useForcedShades: boolean
+): string;
