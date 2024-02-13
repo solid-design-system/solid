0f6665cfee343c864beeac62ef0745a88ea70988
mariohamann <mario.hamann@virtual-identity.com>
2024-02-13 20:39:59 +0100

    setup storybook

--- packages/components/src/components/input/input.stories.ts
+++ packages/components/src/components/input/input.stories.ts
@@ -910,3 +910,144 @@ export const Samples = {
     `;
   }
 };
+
+export const AutoCompleteJs = {
+  name: 'autoComplete.js',
+  parameters: {
+    controls: {
+      exclude: ['autocomplete']
+    }
+  },
+  render: (args: any) => {
+    return html`
+      <script src="https://cdn.jsdelivr.net/npm/@tarekraafat/autocomplete.js@10.2.7/dist/autoComplete.min.js"></script>
+      <div class="flex flex-col gap-6">
+        <sd-input id="simple-example"><b slot="label">Simple example for autoComplete.js</b></sd-input>
+        <sd-input id="highlight-example"><b slot="label">Example with highlighting</b></sd-input>
+      </div>
+      <script>
+        const data = {
+          src: [
+            'PrivatFonds: Kontrolliert pro',
+            'PrivatFonds: Nachhaltig',
+            'UniAusschüttung A',
+            'UniAusschüttung -net- A',
+            'UniCommodities',
+            'UniDividendenAss A',
+            'UniDividendenAss -net- A',
+            'UniDynamicFonds: Europa A',
+            'UniDynamicFonds: Europa -net- A',
+            'UniDynamicFonds: Global A',
+            'UniDynamicFonds: Global -net- A',
+            'UniEuropa A',
+            'UniEuropa -net-',
+            'UniEuroRenta Corporates A',
+            'UniEuroRenta Real Zins A',
+            'UniEuroRenta Real Zins -net- A',
+            'UniFavorit: Aktien',
+            'UniFavorit: Aktien Europa A',
+            'UniFavorit: Aktien Europa -net- A',
+            'UniFavorit: Aktien -net-',
+            'UniGlobal',
+            'UniGlobal Dividende A',
+            'UniGlobal Dividende -net- A',
+            'UniGlobal II A',
+            'UniGlobal -net-',
+            'UniGlobal Vorsorge',
+            'UniIndustrie 4.0 A',
+            'UniIndustrie 4.0 -net- A',
+            'UniKlassikMix',
+            'UniMarktführer A',
+            'UniMarktführer -net- A',
+            'UniMultiAsset: Chance III',
+            'UniNachhaltig Aktien Deutschland',
+            'UniNachhaltig Aktien Deutschland -net-',
+            'UniNachhaltig Aktien Europa',
+            'UniNachhaltig Aktien Europa -net-',
+            'UniNachhaltig Aktien Global',
+            'UniNachhaltig Aktien Global -net-',
+            'UniNachhaltig Aktien Infrastruktur',
+            'UniNachhaltig Aktien Infrastruktur -net-',
+            'UniNordamerika',
+            'UniRak',
+            'UniRak Nachhaltig A',
+            'UniRak Nachhaltig Konservativ A',
+            'UniRak Nachhaltig Konservativ -net- A',
+            'UniRak Nachhaltig -net- A',
+            'UniRak -net-',
+            'UniReserve: Euro-Corporates',
+            'UniSector: BasicIndustries A',
+            'UniSector: BioPharma A',
+            'UniSector: HighTech A',
+            'UniSelection: Global I',
+            'UniStrategie: Ausgewogen',
+            'UniStrategie: Offensiv',
+            'UniStruktur',
+            'UniThemen Aktien A',
+            'UniThemen Aktien -net- A',
+            'UniThemen Defensiv A',
+            'UniValueFonds: Europa A',
+            'UniValueFonds: Europa -net- A',
+            'UniValueFonds: Global A',
+            'UniValueFonds: Global -net- A',
+            'UniZukunft Klima A',
+            'UniZukunft Klima -net- A',
+            'UniZukunft Welt A',
+            'UniZukunft Welt -net- A',
+            'Uni21.Jahrhundert -net-',
+            'BBBank Dynamik Union',
+            'BBBank Kontinuität Union',
+            'BBBank Wachstum Union',
+            'FVB-Aktienfonds Nachhaltig',
+            'Invest Global',
+            'LIGA-Pax-Aktien-Union',
+            'Nachhaltig Global Mittelhessen',
+            'Profi-Balance',
+            'SpardaRentenPlus A',
+            'Volksbank Bielefeld-Gütersloh NachhaltigkeitsInvest',
+            'VR Bank Rhein-Neckar Union Balance Invest',
+            'VR Sachsen Global Union',
+            'VR Westmünsterland Aktiv Nachhaltig',
+            'Werte Fonds Münsterland Klima'
+          ]
+        };
+        Promise.all([customElements.whenDefined('sd-input'), customElements.whenDefined('sd-popup')]).then(() => {
+          const input = document.querySelector('#simple-example').shadowRoot.querySelector('input');
+
+          const autoCompleteJS = new autoComplete({
+            // API Basic Configuration Object
+            placeHolder: 'Find funds...',
+            data,
+            selector: () => {
+              return input; // Any valid selector
+            },
+            resultsList: {
+              tag: 'sd-popup'
+            },
+            wrapper: false
+          });
+        });
+
+        Promise.all([customElements.whenDefined('sd-input'), customElements.whenDefined('sd-popup')]).then(() => {
+          const input = document.querySelector('#highlight-example').shadowRoot.querySelector('input');
+
+          const autoCompleteJS = new autoComplete({
+            // API Basic Configuration Object
+            placeHolder: 'Fonds finden...',
+            data,
+            selector: () => {
+              return input; // Any valid selector
+            },
+            resultsList: {
+              tag: 'sd-popup'
+            },
+            wrapper: false,
+            resultItem: {
+              highlight: true
+            }
+          });
+        });
+      </script>
+    `;
+  }
+};
