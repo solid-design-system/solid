a181ad017a1a835fd97b178cab89617c14e62c27
Samuel Kohler <samuel.kohler@prodyna.com>
2024-02-13 22:25:19 +0100

    feat: sd-copyright sd-media

--- packages/components/src/styles/copyright/copyright.stories.ts
+++ packages/components/src/styles/copyright/copyright.stories.ts
@@ -1,11 +1,10 @@
 import '../../solid-components';
 
-import { html } from 'lit-html';
-import { storybookDefaults, storybookHelpers } from '../../../scripts/storybook/helper';
+import { storybookDefaults, storybookHelpers, storybookTemplate } from '../../../scripts/storybook/helper';
 
 const { argTypes, parameters } = storybookDefaults('sd-copyright');
 const { overrideArgs } = storybookHelpers('sd-copyright');
-// const { generateTemplate } = storybookTemplate('sd-copyright');
+const { generateTemplate } = storybookTemplate('sd-copyright');
 
 /**
  *
@@ -14,24 +13,33 @@ export default {
   title: 'Styles/sd-copyright',
   component: 'sd-copyright',
   parameters: {
-    ...parameters,
-    design: {
-      type: 'figma',
-      url: 'https://www.figma.com/file/IUiRoK2jiW8ydM77uiY2RX/Chip?type=design&mode=design&t=cioeESUO1sJ6UIu8-0'
-    }
+    ...parameters
   },
-  args: overrideArgs({ type: 'slot', name: 'default', value: 'Lorem Ipsum' }),
+  args: overrideArgs(
+    {
+      type: 'slot',
+      name: 'default',
+      value: `<img src="./placeholders/generic.jpg" alt="A generic placeholder jpg" />`
+    },
+    {
+      '--copyright': '© 2024 Solid Design System'
+    }
+  ),
   argTypes
 };
 
 export const Default = {
-  render: () => {
-    return html`
-      <div class="max-w-xl p-4">
-        <div class="sd-copyright" style="--copyright: '© 2024 Solid Design System';">
-          <img src="./placeholders/generic.jpg" alt="A generic placeholder jpg" />
-        </div>
-      </div>
-    `;
+  parameters: {
+    controls: {
+      disable: true
+    }
+  },
+  render: (args: { [k: string]: any }) => {
+    return generateTemplate({
+      options: {
+        templateContent: `<div class="%CLASSES% max-w-xl" style="--copyright: '${args['--copyright']}';">%SLOT%</div>`
+      },
+      args
+    });
   }
 };
--- packages/components/src/styles/media/media.stories.ts
+++ packages/components/src/styles/media/media.stories.ts
@@ -1,11 +1,10 @@
 import '../../solid-components';
 
-import { html } from 'lit-html';
-import { storybookDefaults, storybookHelpers } from '../../../scripts/storybook/helper';
+import { storybookDefaults, storybookHelpers, storybookTemplate } from '../../../scripts/storybook/helper';
 
 const { argTypes, parameters } = storybookDefaults('sd-media');
 const { overrideArgs } = storybookHelpers('sd-media');
-// const { generateTemplate } = storybookTemplate('sd-media');
+const { generateTemplate } = storybookTemplate('sd-media');
 
 /**
  *
@@ -14,27 +13,47 @@ export default {
   title: 'Styles/sd-media',
   component: 'sd-media',
   parameters: {
-    ...parameters,
-    design: {
-      type: 'figma',
-      url: 'https://www.figma.com/file/IUiRoK2jiW8ydM77uiY2RX/Chip?type=design&mode=design&t=cioeESUO1sJ6UIu8-0'
-    }
+    ...parameters
   },
   args: overrideArgs({ type: 'slot', name: 'default', value: 'Lorem Ipsum' }),
   argTypes
 };
 
 export const Default = {
-  render: () => {
-    return html`
-      <div class="max-w-xl p-4">
-        <figure class="sd-media">
-          <div class="sd-copyright" style="--copyright: '© 2024 Solid Design System';">
-            <img src="./placeholders/generic.jpg" alt="A generic placeholder jpg" />
-          </div>
-          <figcaption>This is a media caption</figcaption>
-        </figure>
-      </div>
-    `;
+  render: (args: any) => {
+    return generateTemplate({
+      options: {
+        templateContent: `
+          <figure class="%CLASSES% max-w-xl p-4">
+              <img src="./placeholders/generic.jpg" alt="A generic placeholder jpg" />
+            <figcaption>%SLOT%</figcaption>
+          </figure>
+      `
+      },
+      args
+    });
+  }
+};
+
+export const MediaSample = {
+  parameters: {
+    controls: {
+      disable: true
+    }
+  },
+  render: (args: any) => {
+    return generateTemplate({
+      options: {
+        templateContent: `
+          <figure class="%CLASSES% max-w-xl p-4">
+            <div class="sd-copyright" style="--copyright: '© 2024 Solid Design System';">
+              <img src="./placeholders/generic.jpg" alt="A generic placeholder jpg" />
+            </div>
+            <figcaption>%SLOT%</figcaption>
+          </figure>
+      `
+      },
+      args
+    });
   }
 };
