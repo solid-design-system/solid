e363096a6665b4af2eeaa872c214e79add9480c1
Alec Budd <39494579+abudd1094@users.noreply.github.com>
2024-02-16 11:16:26 +0100

    feat: ✨ extend placeholder package with placeholder texts (#756)
    ## Description:
    closes #392 
    
    Adds a utility class SolidFaker with a singleton instance solidFaker
    which can be imported the same way as Faker JS.

---
+++ packages/placeholders/types/solid-faker.d.ts
@@ -0,0 +1,50 @@
+/**
+ * Minimal utility class for generating Lorem Ipsum random / pseudo-random text.
+ */
+export default class SolidFaker {
+  /**
+   * Constructor for SolidFaker class.
+   * @param {number} seedValue - Seed value for consistent randomness (optional).
+   */
+  constructor(seedValue: number);
+  /**
+   * Seed value for consistent randomness.
+   * @type {number|undefined}
+   */
+  seedValue: number | undefined;
+  /**
+   * Array containing Lorem Ipsum words.
+   * @type {string[]}
+   */
+  loremIpsumWords: string[];
+  /**
+   * Set a seed value for consistent randomness on each invocation.
+   * @param {number} seed - Seed value for consistent randomness.
+   */
+  seed(seed: number): void;
+  /**
+   * Generate a random index for selecting a word from loremIpsumWords array.
+   * @returns {number} - Random index.
+   */
+  getRandomIndex(): number;
+  /**
+   * Generate a random number of words.
+   * @param {number} numWords - Number of words to generate.
+   * @param {number} [capitalizeType=0] - Capitalization type:
+   *   0 - all lowercase, 1 - capitalize first word, 2 - capitalize all words.
+   * @returns {string} - Random words joined together.
+   */
+  words(numWords: number, capitalizeType?: number): string;
+  /**
+   * Generate a random number of sentences.
+   * @param {number} numSentences - Number of sentences to generate.
+   * @returns {string} - Random sentences joined together.
+   */
+  sentences(numSentences: number): string;
+  /**
+   * Generate a random number of paragraphs.
+   * @param {number} numParagraphs - Number of paragraphs to generate.
+   * @returns {string} - Random paragraphs joined together.
+   */
+  paragraphs(numParagraphs: number): string;
+}
