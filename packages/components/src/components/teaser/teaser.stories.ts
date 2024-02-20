33e07b50d630eba490f7de7cea63b5d2496b623b
Samuel Kohler <samuel.kohler@prodyna.com>
2024-02-20 11:46:31 +0100

    feat: add horizontal sample

--- packages/components/src/components/teaser/teaser.stories.ts
+++ packages/components/src/components/teaser/teaser.stories.ts
@@ -637,6 +637,64 @@ export const SamplesTeaserWithIcon = {
   }
 };
 
+export const SamplesTeaserWithIconHorizontal = {
+  name: 'Samples: Teaser with icon horizontal',
+  parameters: {
+    controls: {
+      disable: true
+    },
+    backgrounds: {
+      default: 'white'
+    }
+  },
+  render: () => {
+    return html`
+      <style>
+        #teaserWithIcon::part(media) {
+          display: flex;
+          flex-basis: 33.333333%;
+          justify-content: center;
+        }
+        #teaserWithIcon::part(content) {
+          flex-basis: 66.666667%;
+        }
+      </style>
+      <div class="flex justify-between gap-8">
+        <sd-teaser variant="primary" breakpoint="0" inset class="flex-1 h-[238px]" id="teaserWithIcon">
+          <div slot="media" class="flex flex-wrap content-center">
+            <sd-icon class="text-[96px]" name="content/picture" library="global-resources" color="white"> </sd-icon>
+          </div>
+          <h3 slot="headline">Not clickable teaser</h3>
+          <div class="flex flex-col gap-5">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua.
+            </p>
+            <div class="flex-none">
+              <sd-button href="#" target="_blank" variant="primary" inverted>Link</sd-button>
+            </div>
+          </div>
+        </sd-teaser>
+        <sd-teaser variant="primary-100" breakpoint="0" inset class="flex-1 h-[238px]" id="teaserWithIcon">
+          <div slot="media" class="flex flex-wrap content-center">
+            <sd-icon class="text-[96px]" name="content/picture" library="global-resources" color="primary"> </sd-icon>
+          </div>
+          <h3 slot="headline">Not clickable teaser</h3>
+          <div class="flex flex-col gap-5">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua.
+            </p>
+            <div class="flex-none">
+              <sd-button href="#" target="_blank" variant="primary">Link</sd-button>
+            </div>
+          </div>
+        </sd-teaser>
+      </div>
+    `;
+  }
+};
+
 export const SamplesCenterTeaserWithIcon = {
   name: 'Samples: Center aligned teaser with icon',
   parameters: {
