693af1fc8be7a2db0e2b795cf25aa86f707868ec
sam <76621625+saemik94@users.noreply.github.com>
2024-02-23 06:44:09 +0100

    feat: ✨ add sd-media + sd-copyright - CSS style (#755)
    Co-authored-by: Kohler, Samuel <samuel.kohler@prodyna.com>

---
+++ packages/components/src/styles/copyright/copyright.stories.ts
@@ -0,0 +1,45 @@
+import '../../solid-components';
+
+import { storybookDefaults, storybookHelpers, storybookTemplate } from '../../../scripts/storybook/helper';
+
+const { argTypes, parameters } = storybookDefaults('sd-copyright');
+const { overrideArgs } = storybookHelpers('sd-copyright');
+const { generateTemplate } = storybookTemplate('sd-copyright');
+
+/**
+ * Add a copyright at the bottom of a image.
+ */
+export default {
+  title: 'Styles/sd-copyright',
+  component: 'sd-copyright',
+  parameters: {
+    ...parameters
+  },
+  args: overrideArgs(
+    {
+      type: 'slot',
+      name: 'default',
+      value: `<img src="./placeholders/generic.jpg" alt="A generic placeholder jpg" class="aspect-video object-cover"/>`
+    },
+    {
+      '--copyright': '© 2024 Solid Design System'
+    }
+  ),
+  argTypes
+};
+
+export const Default = {
+  parameters: {
+    controls: {
+      disable: true
+    }
+  },
+  render: (args: { [k: string]: any }) => {
+    return generateTemplate({
+      options: {
+        templateContent: `<div class="%CLASSES% max-w-xl" style="--copyright: '${args['--copyright']}';">%SLOT%</div>`
+      },
+      args
+    });
+  }
+};
