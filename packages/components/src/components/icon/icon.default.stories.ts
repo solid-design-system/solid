e0c12c70d08bd5d5eb21cb4f434e87fe40e72319
mariohamann <mario.hamann@virtual-identity.com>
2024-03-20 11:43:11 +0100

    fix ts error

--- packages/components/src/components/icon/icon.default.stories.ts
+++ packages/components/src/components/icon/icon.default.stories.ts
@@ -1,5 +1,6 @@
 import '../../solid-components';
 import { storybookTemplate } from '../../../scripts/storybook/helper';
+// @ts-expect-error – dynamically loaded via Vite
 import iconsFromCdn from 'icons-from-cdn';
 
 const { generateTemplate } = storybookTemplate('sd-icon');
