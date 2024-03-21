b82d764b379c13401e34d4cceb6c44ae9817fbea
Vahid Nesro <63849626+Vahid1919@users.noreply.github.com>
2024-03-21 10:24:48 +0100

    feat: ✨ add sd-footnote - CSS style (#863)

---
+++ packages/components/src/styles/footnote/footnote.declaration.ts
@@ -0,0 +1,20 @@
+import type { Style } from '../../declaration';
+
+export default {
+  styleName: 'sd-footnote',
+  summary:
+    'A footnote contains additional information/sources related to the content and usually appears at the bottom of a page or below the content it refers to.',
+  status: 'stable',
+  since: '2.9.0',
+  attributes: [
+    {
+      name: 'sd-footnote--number',
+      description: 'This attribute allows content references to be displayed as numbered lists.'
+    },
+    {
+      name: 'sd-footnote--reset',
+      description:
+        'Resets the number of the footnote and starts counting from 1. This should be used at the beginning of a counting sequence and can be used again when starting a new context.'
+    }
+  ]
+} satisfies Style;
