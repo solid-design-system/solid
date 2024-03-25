37b7f905b1baa875f60f3957a0fba1809e87e187
Vahid <vahidmenu@gmail.com>
2024-03-25 12:04:07 +0100

    feat: add sd-step

---
+++ packages/components/src/components/step-group/step-group.stories.ts
@@ -0,0 +1,33 @@
+import '../../solid-components';
+import { html } from 'lit-html';
+import { storybookDefaults, storybookHelpers, storybookTemplate } from '../../../scripts/storybook/helper';
+import { withActions } from '@storybook/addon-actions/decorator';
+
+const { argTypes, parameters } = storybookDefaults('sd-step-group');
+const { overrideArgs } = storybookHelpers('sd-step-group');
+const { generateTemplate } = storybookTemplate('sd-step-group');
+
+export default {
+  title: 'Components/sd-step-group',
+  component: 'sd-step-group',
+  args: overrideArgs([
+    {
+      type: 'slot',
+      name: 'default',
+      value: `Hello World`
+    }
+  ]),
+  argTypes,
+  parameters: { ...parameters },
+  decorators: [withActions] as any
+};
+
+/**
+ * Default: This shows sd-step-group in its default state.
+ */
+
+export const Default = {
+  render: (args: any) => {
+    return html`${generateTemplate({ args })}`;
+  }
+};
---
+++ packages/components/src/components/step-group/step-group.ts
@@ -0,0 +1,33 @@
+import { css, html } from 'lit';
+import { customElement } from '../../internal/register-custom-element';
+import componentStyles from '../../styles/component.styles';
+import cx from 'classix';
+import SolidElement from '../../internal/solid-element';
+/**
+ * @summary Tabs are used inside [tab groups](/components/tab-group) to represent and activate [tab panels](/components/tab-panel).
+ * @documentation https://solid.union-investment.com/[storybook-link]/tab
+ * @status stable
+ * @since 2.6.0
+ *
+ *
+ *
+
+ */
+@customElement('sd-step-group')
+export default class SdStepGroup extends SolidElement {
+  connectedCallback() {
+    super.connectedCallback();
+  }
+
+  render() {
+    return html` <div part="base" class=${cx('border border-primary rounded-full')}></div> `;
+  }
+
+  static styles = [SolidElement.styles, componentStyles, css``];
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'sd-step-group': SdStepGroup;
+  }
+}
