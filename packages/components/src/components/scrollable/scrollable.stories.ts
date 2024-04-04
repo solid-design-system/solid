a9b5a55bebe73dd61af4c628afb4b0a30cd7bb37
azraefendic <azra_efendic@live.com>
2024-04-04 13:36:21 +0200

    scrollable stories

--- packages/components/src/components/scrollable/scrollable.stories.ts
+++ packages/components/src/components/scrollable/scrollable.stories.ts
@@ -1,9 +1,10 @@
 import '../../solid-components';
-import { storybookDefaults, storybookHelpers, storybookTemplate } from '../../../scripts/storybook/helper';
+import { html } from 'lit';
+import { storybookDefaults, storybookTemplate } from '../../../scripts/storybook/helper';
 import { withActions } from '@storybook/addon-actions/decorator';
 
-const { argTypes, parameters } = storybookDefaults('sd-scrollable');
-const { overrideArgs } = storybookHelpers('sd-scrollable');
+const { argTypes, args, parameters } = storybookDefaults('sd-scrollable');
+// const { overrideArgs } = storybookHelpers('sd-scrollable');
 const { generateTemplate } = storybookTemplate('sd-scrollable');
 
 export default {
@@ -16,17 +17,143 @@ export default {
       url: 'https://www.figma.com/file/frKFVz9UBKAtsvErLKTeGj/Scrollable?type=design&node-id=0-1&mode=design&t=OeLPPGif39ASuNmf-0'
     }
   },
-  args: overrideArgs([{ type: 'slot', name: 'default', value: 'This is a scrollable content' }]),
+  args,
   argTypes,
   decorators: [withActions] as any
 };
-
-/**
- * This shows the scrollable in its default state.
- */
+const longContent = `
+  <div style="width: 277px; height: 80px; line-height: 1.6;">
+    <p>This is a long scrollable content.</p>
+    <p>It contains multiple paragraphs and lines.</p>
+    <p>The content is intentionally long to trigger scrolling. You can scroll horizontally and vertically.</p>
+    <p>The scrollable component will display shadows and buttons based on the props.</p>
+    <p>Customize the content and attributes as needed.</p>
+  </div>
+`;
 
 export const Default = {
   render: (args: any) => {
-    return generateTemplate({ args });
+    return html`
+      <div style="width: 277px; height: 80px;">
+        ${generateTemplate({
+          args,
+          constants: [{ type: 'slot', name: 'default', value: longContent }]
+        })}
+      </div>
+    `;
+  }
+};
+
+export const ButtonWithGradient = {
+  parameters: { controls: { exclude: ['buttons'] } },
+  render: (args: any) => {
+    return html`
+      <div style="width: 277px; height: 80px;">
+        ${generateTemplate({
+          args,
+          constants: [
+            { type: 'slot', name: 'default', value: longContent },
+            { type: 'attribute', name: 'buttons', value: 'true' }
+          ]
+        })}
+      </div>
+    `;
+  }
+};
+
+export const Shadow = {
+  parameters: { controls: { exclude: ['shadow'] } },
+  render: (args: any) => {
+    return html`
+      <div style="width: 277px; height: 80px;">
+        ${generateTemplate({
+          args,
+          constants: [
+            { type: 'slot', name: 'default', value: longContent },
+            { type: 'attribute', name: 'shadow', value: 'true' }
+          ]
+        })}
+      </div>
+    `;
+  }
+};
+
+export const Scrollbar = {
+  parameters: { controls: { exclude: ['scrollbars'] } },
+  render: (args: any) => {
+    return html`
+      <div style="width: 277px; height: 80px;">
+        ${generateTemplate({
+          args,
+          constants: [
+            { type: 'slot', name: 'default', value: longContent },
+            { type: 'attribute', name: 'scrollbars', value: 'true' }
+          ]
+        })}
+      </div>
+    `;
+  }
+};
+
+// export const Inset = {
+//   parameters: { controls: { exclude: ['inset'] } },
+//   render: (args: any) => {
+//     return generateTemplate({
+//       axis: {
+//         x: { type: 'attribute', name: 'inset', values: [false, true] }
+//       },
+//       args,
+//       constants: [{ type: 'slot', name: 'default', value: longContent }]
+//     });
+//   }
+// };
+
+// export const CustomIcon = {
+//   parameters: { controls: { exclude: ['scrollbars'] } },
+//   render: (args: any) => {
+//     return html`
+//       <div style="width: 277px; height: 80px;">
+//         ${generateTemplate({
+//           args,
+//           constants: [
+//             { type: 'slot', name: 'default', value: longContent },
+//             { type: 'attribute', name: 'scrollbars', value: 'true' }
+//           ]
+//         })}
+//       </div>
+//     `;
+//   }
+// };
+
+export const Parts = {
+  parameters: {
+    controls: { exclude: ['base', 'content', 'button-left', 'button-right', 'button-top', 'button-bottom'] }
+  },
+  render: (args: any) => {
+    return generateTemplate({
+      axis: {
+        y: {
+          type: 'template',
+          name: 'sd-scrollable::part(...){outline: solid 2px red}',
+          values: ['base', 'content', 'button-left', 'button-right', 'button-top', 'button-bottom'].map(part => {
+            return {
+              title: part,
+              value: `<style>#part-${part} sd-scrollable::part(${part}){outline: solid 2px red;} #part-${part} .${part}{outline: solid 2px red;}</style><div id='part-${part}'>%TEMPLATE%</div>`
+            };
+          })
+        }
+      },
+      constants: [
+        { type: 'template', name: 'width', value: '<div style="width: 300px">%TEMPLATE%</div>' },
+        {
+          type: 'slot',
+          name: 'default',
+          value: longContent
+        },
+        { type: 'attribute', name: 'buttons', value: true },
+        { type: 'attribute', name: 'shadows', value: true }
+      ],
+      args
+    });
   }
 };
--- packages/components/src/components/scrollable/scrollable.ts
+++ packages/components/src/components/scrollable/scrollable.ts
@@ -1,7 +1,9 @@
-import { css, html } from 'lit';
+import { css, html, unsafeCSS } from 'lit';
 import { customElement } from '../../../src/internal/register-custom-element';
-import { property } from 'lit/decorators.js';
+import { property, state } from 'lit/decorators.js';
+import componentStyles from '../../styles/component.styles';
 import cx from 'classix';
+import InteractiveStyles from '../../styles/interactive/interactive.css?inline';
 import SolidElement from '../../internal/solid-element';
 
 /**
@@ -20,6 +22,10 @@ import SolidElement from '../../internal/solid-element';
  *
  * @csspart base - The scrollable's base wrapper.
  * @csspart content - The scrollable's main content.
+ * @csspart button-left - The scrollable's left scroll button.
+ * @csspart button-right - The scrollable's right scroll button.
+ * @csspart button-top - The scrollable's up scroll button.
+ * @csspart button-bottom - The scrollable's down scroll button.
  *
  * @cssproperty --gradient - Defines a custom color for the gradient.
  */
@@ -30,56 +36,345 @@ export default class SdScrollable extends SolidElement {
   @property({ type: String }) orientation: 'horizontal' | 'vertical' = 'horizontal';
 
   /** Activates scroll buttons */
-  @property({ type: Boolean }) buttons: boolean = false;
+  @property({ type: Boolean, reflect: true }) buttons = false;
 
   /** The amount in px to be scrolled when clicking the buttons. */
   @property({ type: Number }) scrollStep: number = 150;
 
-  /** IActivates browser scrollbars */
-  @property({ type: Boolean }) scrollbars: boolean = false;
+  /** Activates browser scrollbars */
+  @property({ type: Boolean, reflect: true }) scrollbars = false;
 
   /** Activates a shadow as optional visual scroll indicator */
-  @property({ type: Boolean }) shadow: boolean = false;
+  @property({ type: Boolean, reflect: true }) shadow = false;
 
-  /** Activates a gradient as optional visual scroll indicator */
-  @property({ type: Boolean }) gradient: boolean = false;
+  /* Adds inset padding*/
+  @property({ type: Boolean, reflect: true }) inset = false;
+
+  @state() private canScroll: Record<'left' | 'right' | 'up' | 'down', boolean> = {
+    left: false,
+    right: false,
+    up: false,
+    down: false
+  };
+
+  private resizeObserver: ResizeObserver;
+  private scrollContainer: HTMLElement | null = null;
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.resizeObserver = new ResizeObserver(() => {
+      this.updateScrollIndicatorVisibility();
+    });
+
+    this.updateComplete.then(() => {
+      this.scrollContainer = this.renderRoot.querySelector('.scroll-container');
+      if (this.scrollContainer) {
+        this.resizeObserver.observe(this.scrollContainer);
+      }
+    });
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    if (this.scrollContainer) {
+      this.resizeObserver.unobserve(this.scrollContainer);
+    }
+  }
+
+  updated(changedProperties: Map<string, unknown>) {
+    super.updated(changedProperties);
+    if (changedProperties.has('buttons') || changedProperties.has('shadow') || changedProperties.has('orientation')) {
+      this.updateScrollIndicatorVisibility();
+    }
+  }
+
+  private get container(): HTMLElement | null {
+    return this.renderRoot.querySelector('.scroll-container');
+  }
+
+  updateScrollIndicatorVisibility() {
+    const container = this.container;
+    if (container) {
+      const canScrollLeft = container.scrollLeft > 0;
+      const canScrollRight = container.scrollLeft + container.clientWidth < container.scrollWidth - 1;
+      const canScrollUp = container.scrollTop > 0;
+      const canScrollDown = container.scrollTop + container.clientHeight < container.scrollHeight - 1;
+
+      this.canScroll = {
+        left: canScrollLeft,
+        right: canScrollRight,
+        up: canScrollUp,
+        down: canScrollDown
+      };
+
+      if (canScrollLeft || canScrollUp) {
+        this.dispatchEvent(new CustomEvent('start'));
+      }
+      if (canScrollRight || canScrollDown) {
+        this.dispatchEvent(new CustomEvent('end'));
+      }
+    }
+  }
+
+  handleScroll(direction: 'left' | 'right' | 'up' | 'down') {
+    if (this.scrollContainer) {
+      const isHorizontal = this.orientation === 'horizontal';
+      const scrollAmount = direction === 'left' || direction === 'up' ? -this.scrollStep : this.scrollStep;
+      const scrollOptions: ScrollToOptions = {
+        behavior: 'smooth',
+        [isHorizontal ? 'left' : 'top']: scrollAmount
+      };
+      this.scrollContainer.scrollBy(scrollOptions);
+      this.dispatchEvent(new CustomEvent(`button-${direction}`));
+    }
+  }
+
+  renderIcon(direction: 'up' | 'down') {
+    const iconName = direction === 'up' ? 'chevron-down' : 'chevron-up';
+    const rotationClass = this.orientation === 'horizontal' ? 'rotate-[90deg]' : '';
+    return html`
+      <slot name="icon">
+        <sd-icon library="system" name=${iconName} class=${rotationClass}></sd-icon>
+      </slot>
+    `;
+  }
 
   render() {
+    const isHorizontal = this.orientation === 'horizontal';
+    const showButtonStart = this.buttons && (isHorizontal ? this.canScroll.left : this.canScroll.up);
+    const showButtonEnd = this.buttons && (isHorizontal ? this.canScroll.right : this.canScroll.down);
+    const showShadowStart = this.shadow && (isHorizontal ? this.canScroll.left : this.canScroll.up);
+    const showShadowEnd = this.shadow && (isHorizontal ? this.canScroll.right : this.canScroll.down);
+
     return html`
       <div
         part="base"
         class=${cx(
           'scroll-container',
-          this.orientation === 'horizontal' ? 'horizontal-scroll' : 'vertical-scroll',
-          this.scrollbars ? 'show-scrollbars' : 'hide-scrollbars'
+          isHorizontal ? 'horizontal-scroll' : 'vertical-scroll',
+          this.scrollbars ? 'show-scrollbars' : 'hide-scrollbars',
+          this.inset ? 'p-4' : ''
         )}
+        @scroll=${this.updateScrollIndicatorVisibility}
       >
         <slot></slot>
       </div>
-
-      ${this.buttons
+      ${showButtonStart
         ? html`
-            <button part="button" class="scroll-button" @click="${() => this.scroll({ top: 0 })}">
-              <slot name="button-icon">Start</slot>
-            </button>
-            Test
-            <button part="button" class="scroll-button" @click="${() => this.scroll({ top: 0 })}">
-              <slot name="button-icon">End</slot>
-            </button>
+            <div
+              part="button-start-container"
+              class=${cx('scroll-button-container', isHorizontal ? 'button-left' : 'button-up')}
+            >
+              <button
+                part="button-start"
+                class=${cx('scroll-button sd-interactive rounded-md flex')}
+                @click=${() => this.handleScroll(isHorizontal ? 'left' : 'up')}
+              >
+                ${this.renderIcon(isHorizontal ? 'up' : 'down')}
+              </button>
+            </div>
           `
-        : ''}
-      ${this.shadow ? html`<div part="shadow" class="scroll-shadow"></div>` : ''}
-      ${this.gradient ? html`<div part="gradient" class="scroll-gradient"></div>` : ''}
+        : null}
+      ${showButtonEnd
+        ? html`
+            <div
+              part="button-end-container"
+              class=${cx('scroll-button-container', isHorizontal ? 'button-right' : 'button-down')}
+            >
+              <button
+                part="button-end"
+                class=${cx('scroll-button sd-interactive rounded-md flex')}
+                @click=${() => this.handleScroll(isHorizontal ? 'right' : 'down')}
+              >
+                ${this.renderIcon(isHorizontal ? 'down' : 'up')}
+              </button>
+            </div>
+          `
+        : null}
+      ${showShadowStart
+        ? html`<div part="shadow-start" class=${cx('scroll-shadow', isHorizontal ? 'left' : 'up')}></div>`
+        : null}
+      ${showShadowEnd
+        ? html`<div part="shadow-end" class=${cx('scroll-shadow', isHorizontal ? 'right' : 'down')}></div>`
+        : null}
     `;
   }
 
-  static styles = css`
-    :host {
-      display: block;
-      position: relative;
-      overflow: hidden;
-    }
-  `;
+  static styles = [
+    SolidElement.styles,
+    unsafeCSS(InteractiveStyles),
+    componentStyles,
+    css`
+      :host {
+        display: block;
+        position: relative;
+        overflow: hidden;
+        --gradient: rgba(255, 255, 255, 0) 0%, #fff 80%, #fff 100%;
+      }
+
+      .scroll-container {
+        overflow-x: auto;
+        overflow-y: auto;
+        width: 100%;
+        height: 100%;
+      }
+
+      .horizontal-scroll {
+        overflow-x: scroll;
+        overflow-y: hidden;
+        white-space: nowrap;
+        display: flex;
+        align-items: center;
+      }
+
+      .vertical-scroll {
+        overflow-y: scroll;
+        overflow-x: hidden;
+        display: flex;
+        justify-items: center;
+      }
+
+      .hide-scrollbars {
+        scrollbar-width: none; /* Firefox */
+        -ms-overflow-style: none; /* IE 10+ */
+        &::-webkit-scrollbar {
+          width: 0;
+          height: 0;
+          background: transparent; /* Chrome/Safari/Webkit */
+        }
+      }
+
+      /* Default state: scrollbar is transparent */
+      .scroll-container::-webkit-scrollbar-thumb {
+        background: transparent;
+      }
+
+      /* On hover: apply custom styles to the scrollbar thumb */
+      .scroll-container:hover::-webkit-scrollbar-thumb {
+        background: var(--background-neutral-400, #c3c3c3);
+        border-radius: 2px;
+      }
+
+      /* Apply width and height to the scrollbar itself */
+      :host([orientation='vertical']) .scroll-container::-webkit-scrollbar {
+        width: 4px;
+      }
+
+      :host([orientation='horizontal']) .scroll-container::-webkit-scrollbar {
+        height: 4px;
+      }
+
+      /* Apply specific dimensions to the scrollbar thumb */
+      :host([orientation='vertical']) .scroll-container:hover::-webkit-scrollbar-thumb {
+        height: 24px;
+      }
+
+      :host([orientation='horizontal']) .scroll-container:hover::-webkit-scrollbar-thumb {
+        width: 24px;
+      }
+      .scroll-button-container {
+        position: absolute;
+        z-index: 1;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      .button-left {
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 48px;
+        background: linear-gradient(270deg, var(--gradient));
+      }
+
+      .button-right {
+        top: 0;
+        right: 0;
+        height: 100%;
+        width: 48px;
+        background: linear-gradient(90deg, var(--gradient));
+      }
+
+      .button-up {
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 48px; /* Adjust the height as needed */
+        background: linear-gradient(0deg, var(--gradient));
+      }
+
+      .button-down {
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 48px; /* Adjust the height as needed */
+        background: linear-gradient(180deg, var(--gradient));
+      }
+
+      .scroll-button {
+        position: relative;
+        padding: 0;
+        border: none;
+        background: none;
+        cursor: pointer;
+      }
+
+      .scroll-button[part='button-start'] {
+        justify-content: flex-start;
+      }
+
+      .scroll-button[part='button-end'] {
+        justify-content: flex-end;
+      }
+
+      .scroll-shadow {
+        position: absolute;
+        z-index: 1;
+        pointer-events: none;
+      }
+
+      .scroll-shadow.left {
+        top: 0;
+        left: 0;
+        width: 6px;
+        height: 100%;
+        background: linear-gradient(270deg, rgba(24, 24, 24, 0) 50%, rgba(24, 24, 24, 0.4) 100%);
+      }
+
+      .scroll-shadow.right {
+        top: 0;
+        right: 0;
+        width: 6px;
+        height: 100%;
+        background: linear-gradient(90deg, rgba(24, 24, 24, 0) 50%, rgba(24, 24, 24, 0.4) 100%);
+      }
+
+      .scroll-shadow.up {
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 6px;
+        background: linear-gradient(0deg, rgba(24, 24, 24, 0) 50%, rgba(24, 24, 24, 0.4) 100%);
+      }
+
+      .scroll-shadow.down {
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 6px;
+        background: linear-gradient(180deg, rgba(24, 24, 24, 0) 50%, rgba(24, 24, 24, 0.4) 100%);
+      }
+
+      .sd-icon--up {
+        align-self: flex-start;
+      }
+
+      .sd-icon--down {
+        align-self: flex-end;
+      }
+    `
+  ];
 }
 
 declare global {
