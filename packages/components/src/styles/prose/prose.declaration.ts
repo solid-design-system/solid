bacda3dcd02162988470dea2b762bd684b59d79d
mariohamann <mario.hamann@virtual-identity.com>
2024-04-10 10:06:58 +0200

    feat: init sd-prose

---
+++ packages/components/src/styles/prose/prose.declaration.ts
@@ -0,0 +1,9 @@
+import type { Style } from '../../declaration';
+
+export default {
+  styleName: 'sd-prose',
+  summary: 'Prose',
+  status: 'stable',
+  since: '1.13',
+  attributes: []
+} satisfies Style;
