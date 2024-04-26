f53f1853654d536fe8ed88e4c2d8054837d63f8a
Mario Hamann <mario.hamann@virtual-identity.com>
2024-04-26 12:31:09 +0200

    feat: ✨ sd-footnotes (#958)
    BREAKING CHANGE: The `sd-footnote` component has been deprecated and
    removed from the library.
    
    This PR shows adds `sd-footnotes`. As as a replacement for
    `sd-footnote`, it provides the possibility to use footnotes in
    combination with lists, which actually would be better in terms of DX
    and A11y.
    ---------
    
    Co-authored-by: Vahid <vahidmenu@gmail.com>

---
+++ packages/components/src/styles/footnotes/footnotes.declaration.ts
@@ -0,0 +1,10 @@
+import type { Style } from '../../declaration';
+
+export default {
+  styleName: 'sd-footnotes',
+  summary:
+    'A footnote contains additional information/sources related to the content and usually appears at the bottom of a page or below the content it refers to.',
+  status: 'stable',
+  since: '3.0.0',
+  attributes: []
+} satisfies Style;
