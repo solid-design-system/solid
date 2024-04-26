3290eb6bbd91ffe222f69e268a80f6770fb2f910
Vahid <vahidmenu@gmail.com>
2024-04-26 10:05:09 +0200

    fix: update version

--- packages/components/src/styles/footnotes/footnotes.declaration.ts
+++ packages/components/src/styles/footnotes/footnotes.declaration.ts
@@ -5,6 +5,6 @@ export default {
   summary:
     'A footnote contains additional information/sources related to the content and usually appears at the bottom of a page or below the content it refers to.',
   status: 'stable',
-  since: '2.13.0',
+  since: '3.0.0',
   attributes: []
 } satisfies Style;
