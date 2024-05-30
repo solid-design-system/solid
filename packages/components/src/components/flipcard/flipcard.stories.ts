81b1bc1a532ec2b401a5574da674f75b6cb5c603
Vahid <vahidmenu@gmail.com>
2024-05-30 05:23:29 +0200

    feat: add flipcard and include stories

--- packages/components/src/components/flipcard/flipcard.stories.ts
+++ packages/components/src/components/flipcard/flipcard.stories.ts
@@ -1,11 +1,26 @@
 import '../../solid-components';
 import { html } from 'lit';
 import { storybookDefaults, storybookHelpers, storybookTemplate } from '../../../scripts/storybook/helper';
+import { userEvent } from '@storybook/testing-library';
+import { waitUntil } from '@open-wc/testing-helpers';
 import { withActions } from '@storybook/addon-actions/decorator';
 
 const { argTypes, parameters } = storybookDefaults('sd-flipcard');
 const { generateTemplate } = storybookTemplate('sd-flipcard');
 const { overrideArgs } = storybookHelpers('sd-flipcard');
+
+// <div slot="front"><h4
+// class='sd-headline sd-headline--inline sd-headline--size-lg sd-headline--inverted'
+// >
+//   <sd-icon name="content/picture" library="global-resources"></sd-icon>
+//   Nisi eu excepteur anim esse
+// </h4>
+
+// <p class='sd-paragraph text-left sd-paragraph--inverted'>
+//   Lorem ipsum dolor sit amet per niente da faremmasds nonnummy dolore lorem ipsum dolor sit amet consectuer
+// </p>
+
+// <sd-link size="inherit" href="#" inverted>Link</sd-link></div>
 
 export default {
   title: 'Components/sd-flipcard',
@@ -14,34 +29,22 @@ export default {
     {
       type: 'slot',
       name: 'front',
-      value: `  <div slot="front"><h4
-      class='sd-headline sd-headline--inline sd-headline--size-lg sd-headline--inverted'
-      >
-        <sd-icon name="content/picture" library="global-resources"></sd-icon>
-        Nisi eu excepteur anim esse
-      </h4>
-  
-      <p class='sd-paragraph text-left sd-paragraph--inverted'>
-        Lorem ipsum dolor sit amet per niente da faremmasds nonnummy dolore lorem ipsum dolor sit amet consectuer
-      </p>
-  
-      <sd-link size="inherit" href="#" inverted>Link</sd-link></div> `
+      value: `<p slot="front">Lorem ipsum dolor sit amet per niente da faremmasds nonnummy dolore lorem ipsum dolor sit amet consectuer</p>`
     },
     {
       type: 'slot',
       name: 'back',
-      value: `   <div slot="back"><h4
-      class='sd-headline sd-headline--inline sd-headline--size-lg sd-headline--inverted'
-    >
-      <sd-icon name="content/picture" library="global-resources"></sd-icon>
-      Nisi eu excepteur anim esse
-    </h4>
-
-    <p class='sd-paragraph  text-left sd-paragraph--inverted'>
-      Lorem ipsum dolor sit amet per niente da faremmasds nonnummy dolore lorem ipsum dolor sit amet consectuer
-    </p>
-
-    <sd-link size="inherit" href="#" inverted>Link</sd-link></div>`
+      value: `<p slot="back">Lorem ipsum dolor sit amet per niente da faremmasds nonnummy dolore lorem ipsum dolor sit amet consectuer</p>`
+    },
+    {
+      type: 'slot',
+      name: 'mediaFront',
+      value: `<img slot='mediaFront' class="object-cover h-full w-full" src='./placeholders/images/generic.jpg' alt='Generic'/>`
+    },
+    {
+      type: 'slot',
+      name: 'mediaBack',
+      value: `<img slot='mediaBack' class="object-cover h-full w-full" src='./placeholders/images/generic.jpg' alt='Generic'/>`
     }
   ]),
 
@@ -51,11 +54,146 @@ export default {
   decorators: [withActions] as any
 };
 /**
- * This shows sd-divider in its default state.
+ * This shows sd-flipcard in its default state.
  */
 
 export const Default = {
   render: (args: any) => {
-    return html`<div style="width:350px">${generateTemplate({ args })}</div>`;
+    return html`<div style="width:300px">${generateTemplate({ args })}</div>`;
+  }
+};
+
+/**
+ * The sd-flipcard can be displayed in 3:4 or 16:9 aspect ratio.
+ */
+
+export const Ratio = {
+  parameters: { controls: { exclude: ['ratio'] } },
+  render: (args: any) =>
+    generateTemplate({
+      axis: {
+        x: {
+          type: 'attribute',
+          name: 'ratio'
+        }
+      },
+      args
+    }),
+  decorators: [
+    (story: any) =>
+      html`<style>
+          td.template {
+            width: 50%;
+          }
+        </style>
+        ${story()}`
+  ] as unknown
+};
+
+/**
+ * The sd-flipcard can be displayed in several ways using the `frontVariant` and `backVariant` attributes. This example shows the usage `frontVariant` attribute.
+ */
+
+export const Variants = {
+  parameters: { controls: { exclude: ['frontVariant'] } },
+  render: (args: any) =>
+    generateTemplate({
+      axis: {
+        y: {
+          type: 'attribute',
+          name: 'frontVariant'
+        }
+      },
+      args,
+      constants: [
+        {
+          type: 'template',
+          name: 'style',
+          value: '<div style="margin-bottom: 40px; width: 300px">%TEMPLATE%</div>'
+        }
+      ]
+    })
+};
+
+export const Parts = {
+  parameters: {
+    controls: {
+      exclude: [
+        'base',
+        'front',
+        'back',
+        'frontSlotContainer',
+        'backSlotContainer',
+        'mediaFront',
+        'mediaBack',
+        'frontSecondaryGradient',
+        'backSecondaryGradient'
+      ]
+    }
+  },
+  render: (args: any) => {
+    return generateTemplate({
+      axis: {
+        y: {
+          type: 'template',
+          name: 'sd-flipcard::part(...){outline: solid 2px red}',
+          values: [
+            'base',
+            'front',
+            'back',
+            'frontSlotContainer',
+            'backSlotContainer',
+            'mediaFront',
+            'mediaBack',
+            'frontSecondaryGradient',
+            'backSecondaryGradient'
+          ].map(part => {
+            return {
+              title: part,
+              value: `<style>#part-${part} sd-flipcard::part(${part}){outline: solid 2px red; outline-offset: -2px;}</style><div id="part-${part}">%TEMPLATE%</div>`
+            };
+          })
+        }
+      },
+      args,
+      constants: [
+        {
+          type: 'template',
+          name: 'style',
+          value: '<div style="margin-bottom: 40px; width: 300px">%TEMPLATE%</div>'
+        },
+        {
+          type: 'attribute',
+          name: 'frontVariant',
+          value: 'gradient-dark-top'
+        },
+        {
+          type: 'attribute',
+          name: 'darkVariant',
+          value: 'gradient-dark-bottom'
+        }
+      ]
+    });
+  }
+};
+
+/**
+ * `sd-flipcard` is fully accessibile via keyboard.
+ */
+
+export const Mouseless = {
+  render: (args: any) => {
+    return html`<div class="mouseless" style="width:300px">${generateTemplate({ args })}</div>`;
+  },
+
+  play: async ({ canvasElement }: { canvasElement: HTMLUnknownElement }) => {
+    const el = canvasElement.querySelector('.mouseless sd-flipcard');
+
+    await waitUntil(() => el?.shadowRoot?.querySelector('.flip-card__side--front'));
+
+    // We have to catch the event as otherwise Storybook will break
+    await userEvent.type(el!.shadowRoot!.querySelector('.flip-card__side--front')!, '{return}', {
+      pointerEventsCheck: 0
+    });
   }
 };
--- packages/components/src/components/flipcard/flipcard.ts
+++ packages/components/src/components/flipcard/flipcard.ts
@@ -1,6 +1,6 @@
 import { css, html } from 'lit';
 import { customElement } from '../../internal/register-custom-element';
-import { property } from 'lit/decorators.js';
+import { property, query } from 'lit/decorators.js';
 import componentStyles from '../../styles/component.styles';
 import cx from 'classix';
 import SolidElement from '../../internal/solid-element';
@@ -11,19 +11,34 @@ import SolidElement from '../../internal/solid-element';
  * @status stable
  * @since 3.5.0
  *
+ * @event sd-flip-front - Emmited when the front face of the flipcard is clicked.
+ * @event sd-flip-back - Emmited when the back face of the flipcard is clicked.
+ *
  * @slot front - The front face of the flipcard.
  * @slot back - The back face of the flipcard.
  * @slot mediaFront - An optional media slot which can be as a background. Dependent from gradient variant.
  * @slot mediaBack - An optional media slot which can be as a background. Dependent from gradient variant.
  *
  * @cssparts base - The component's base wrapper.
+ * @cssparts front - The container that wraps the front-side of the flipcard.
+ * @cssparts back - The container that wraps the back-side of the flipcard.
+ * @cssparts frontSlotContainer - The container that wraps the front slot.
+ * @cssparts backSlotContainer - The container that wraps the back slot.
+ * @cssparts mediaFront - The container that wraps the mediaFront slot.
+ * @cssparts mediaBack - The container that wraps the mediaBack slot.
+ * @cssparts frontSecondaryGradient - The container that wraps the secondary gradient of the front side.
+ * @cssparts backSecondaryGradient - The container that wraps the secondary gradient of the back side.
  *
  * @cssproperties --name - Description of the flipcard.
  */
 @customElement('sd-flipcard')
 export default class SdFlipcard extends SolidElement {
+  @query('[part="back"]') back: HTMLElement;
+  @query('[part="front"]') front: HTMLElement;
+
   /** Determines the variant of the front face of the flipcard. */
-  @property({ type: String, reflect: true }) frontVariant:
+  @property({ type: String, reflect: true })
+  frontVariant:
     | 'primary'
     | 'primary-100'
     | 'gradient-light-top'
@@ -38,7 +53,7 @@ export default class SdFlipcard extends SolidElement {
     | 'gradient-light-top'
     | 'gradient-light-bottom'
     | 'gradient-dark-top'
-    | 'gradient-dark-bottom' = 'primary';
+    | 'gradient-dark-bottom' = 'primary-100';
 
   /** Determines the ratio for the layout of the flipcard. */
   @property({ type: String, reflect: true }) ratio: '3:4' | '16:9' = '3:4';
@@ -47,45 +62,165 @@ export default class SdFlipcard extends SolidElement {
     super.connectedCallback();
   }
 
+  private flipFront() {
+    this.front.classList.add('clicked--front');
+    this.back.classList.add('clicked--back');
+    this.emit('sd-flip-front');
+  }
+
+  private flipBack() {
+    this.front.classList.remove('clicked--front');
+    this.back.classList.remove('clicked--back');
+    this.emit('sd-flip-back');
+  }
+
+  private handleFrontKeydown(event: KeyboardEvent) {
+    if (event.code === 'Enter' || event.code === 'Space') {
+      this.flipFront();
+    }
+  }
+
+  private handleBackKeydown(event: KeyboardEvent) {
+    if (event.code === 'Enter' || event.code === 'Space') {
+      this.flipBack();
+    }
+  }
+
   render() {
     return html`
       <div part="base" class=${cx('flip-card', this.ratio === '3:4' ? 'aspect-3/4' : 'aspect-video')}>
         <div
           part="front"
+          tabindex="0"
           class=${cx(
             'flip-card__side flip-card__side--front hover',
-            'flex flex-col gap-4 py-4 px-6',
-            'absolute top-0 left-0 w-full h-full',
-            {
-              primary: 'bg-primary',
-              'primary-100': 'bg-primary-100',
-              'gradient-light-top': '',
-              'gradient-light-bottom': '',
-              'gradient-dark-top': '',
-              'gradient-dark-bottom': ''
-            }[this.frontVariant]
+            'flex focus-visible:focus-outline',
+            'absolute top-0 left-0 w-full h-full justify-end text-left',
+            this.frontVariant === 'primary' && 'bg-primary',
+            this.frontVariant === 'primary-100' && 'bg-primary-100',
+            this.frontVariant === 'gradient-dark-bottom' || this.frontVariant === 'gradient-light-bottom'
+              ? 'flex-col-reverse'
+              : 'flex-col'
           )}
+          @click=${this.flipFront}
+          @keydown=${this.handleFrontKeydown}
         >
-          <slot name="front"></slot>
+          <div
+            part="mediaFront"
+            class=${cx(
+              'absolute h-full w-full -z-20',
+              (this.frontVariant === 'primary' || this.frontVariant === 'primary-100') && 'hidden'
+            )}
+          >
+            <slot name="mediaFront"></slot>
+          </div>
+
+          <div
+            part="frontSlotContainer"
+            class=${cx(
+              'py-4 px-6 flex mt-auto',
+              {
+                primary: 'text-white',
+                'primary-100': 'text-black',
+                'gradient-light-top': 'text-black',
+                'gradient-light-bottom': 'text-black',
+                'gradient-dark-top': 'text-white',
+                'gradient-dark-bottom': 'text-white'
+              }[this.frontVariant],
+              {
+                primary: '',
+                'primary-100': '',
+                'gradient-light-top': 'bg-gradient-to-b from-white/75 to-white/60',
+                'gradient-light-bottom': 'bg-gradient-to-t from-white/75 to-white/60',
+                'gradient-dark-top': 'bg-gradient-to-b from-primary-800/75 to-primary-800/60',
+                'gradient-dark-bottom': 'bg-gradient-to-t from-primary-800/75 to-primary-800/60'
+              }[this.frontVariant]
+            )}
+          >
+            <slot name="front"></slot>
+          </div>
+
+          <div
+            part="frontSecondaryGradient"
+            class=${cx(
+              'flex-1',
+              {
+                primary: '',
+                'primary-100': '',
+                'gradient-light-top': 'bg-gradient-to-b from-white/60 to-40%',
+                'gradient-light-bottom': 'bg-gradient-to-t from-white/60 to-40%',
+                'gradient-dark-top': 'bg-gradient-to-b from-primary-800/60 to-40%',
+                'gradient-dark-bottom': 'bg-gradient-to-t  from-primary-800/60 to-40%'
+              }[this.frontVariant]
+            )}
+          ></div>
         </div>
 
         <div
           part="back"
+          tabindex="0"
           class=${cx(
             'flip-card__side flip-card__side--back hover',
-            'flex flex-col gap-4 py-4 px-6',
-            'absolute top-0 left-0 w-full h-full',
-            {
-              primary: 'bg-primary',
-              'primary-100': 'bg-primary-100',
-              'gradient-light-top': '',
-              'gradient-light-bottom': '',
-              'gradient-dark-top': '',
-              'gradient-dark-bottom': ''
-            }[this.backVariant]
+            'flex focus-visible:focus-outline',
+            'absolute top-0 left-0 w-full h-full justify-end text-left',
+            this.backVariant === 'primary' && 'bg-primary',
+            this.backVariant === 'primary-100' && 'bg-primary-100',
+            this.backVariant === 'gradient-dark-bottom' || this.backVariant === 'gradient-light-bottom'
+              ? 'flex-col-reverse'
+              : 'flex-col'
           )}
+          @click=${this.flipBack}
+          @keydown=${this.handleBackKeydown}
         >
-          <slot name="back"></slot>
+          <div
+            part="mediaBack"
+            class=${cx(
+              'absolute h-full w-full -z-20',
+              (this.backVariant === 'primary' || this.backVariant === 'primary-100') && 'hidden'
+            )}
+          >
+            <slot name="mediaBack"></slot>
+          </div>
+
+          <div
+            part="backSlotContainer"
+            class=${cx(
+              'py-4 px-6 flex mt-auto',
+              {
+                primary: 'text-white',
+                'primary-100': 'text-black',
+                'gradient-light-top': 'text-black',
+                'gradient-light-bottom': 'text-black',
+                'gradient-dark-top': 'text-white',
+                'gradient-dark-bottom': 'text-white'
+              }[this.backVariant],
+              {
+                primary: '',
+                'primary-100': '',
+                'gradient-light-top': 'bg-gradient-to-b from-white/75 to-white/60',
+                'gradient-light-bottom': 'bg-gradient-to-t from-white/75 to-white/60',
+                'gradient-dark-top': 'bg-gradient-to-b from-primary-800/75 to-primary-800/60',
+                'gradient-dark-bottom': 'bg-gradient-to-t from-primary-800/75 to-primary-800/60'
+              }[this.backVariant]
+            )}
+          >
+            <slot name="back"></slot>
+          </div>
+
+          <div
+            part="backSecondaryGradient"
+            class=${cx(
+              'flex-1',
+              {
+                primary: '',
+                'primary-100': '',
+                'gradient-light-top': 'bg-gradient-to-b from-white/60 to-40%',
+                'gradient-light-bottom': 'bg-gradient-to-t from-white/60 to-40%',
+                'gradient-dark-top': 'bg-gradient-to-b from-primary-800/60 to-40%',
+                'gradient-dark-bottom': 'bg-gradient-to-t  from-primary-800/60 to-40%'
+              }[this.backVariant]
+            )}
+          ></div>
         </div>
       </div>
     `;
@@ -100,6 +235,7 @@ export default class SdFlipcard extends SolidElement {
     css`
       :host {
         display: block;
+        --name: '';
       }
 
       .flip-card {
@@ -107,31 +243,33 @@ export default class SdFlipcard extends SolidElement {
         perspective: 100rem;
       }
       .flip-card__side {
-        transition: all var(--flip-card-transition-duration, 800ms) var(--flip-card-transition-timing-function, ease);
+        transition: transform 1000ms ease;
         backface-visibility: hidden;
-        border-radius: var(--flip-card-border-radius, 4px);
         overflow: hidden;
-        box-shadow: var(--flip-card-box-shadow, 0 15px 40px rgba(0, 0, 0, 0.15));
+        box-shadow: 0 15px 40px rgba(0, 0, 0, 0.15);
       }
       .flip-card__side--front {
       }
       .flip-card__side--back {
         transform: rotateY(180deg);
       }
-      .flip-card:hover .flip-card__side--front.hover {
-        transform: rotateY(-180deg);
-      }
-      .flip-card:hover .flip-card__side--back.hover {
-        transform: rotateY(0);
+
+      @media (hover: hover) and (pointer: fine) {
+        .flip-card:hover .flip-card__side--front.hover {
+          transform: rotateY(-180deg);
+        }
+        .flip-card:hover .flip-card__side--back.hover {
+          transform: rotateY(0);
+        }
       }
 
-      /* .clicked--front {
+      .clicked--front {
         transform: rotateY(-180deg);
       }
 
       .clicked--back {
         transform: rotateY(0);
-      } */
+      }
     `
   ];
 }
