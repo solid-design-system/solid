8f8ff34ddd89f0f46546a64424dcd4a4cac07321
Vahid <vahidmenu@gmail.com>
2024-06-28 08:34:55 +0200

    feat: align this.open with <details> open attributw

--- packages/components/src/components/quickfact/quickfact.ts
+++ packages/components/src/components/quickfact/quickfact.ts
@@ -1,8 +1,7 @@
 import '../icon/icon';
 import { css, html, unsafeCSS } from 'lit';
 import { customElement } from '../../internal/register-custom-element';
-import { property, query } from 'lit/decorators.js';
-import { watch } from 'src/internal/watch';
+import { property, query, state } from 'lit/decorators.js';
 import componentStyles from '../../styles/component.styles';
 import cx from 'classix';
 import DisplayStyles from '../../styles/display/display.css?inline';
@@ -28,30 +27,23 @@ import SolidElement from '../../internal/solid-element';
 export default class SdQuickfact extends SolidElement {
   @query('[part~="base"]') base: HTMLBodyElement;
 
+  @query('slot:not([name])') defaultSlot: HTMLSlotElement;
+
   /**
    * Indicates whether or not the quickfact is open. You can toggle this attribute to show and hide content inside the quickfact, or you can use the `show()` and `hide()` methods and this attribute will reflect the quickfact's open state.
    */
   @property({ type: Boolean, reflect: true }) open = false;
 
-  @property({ type: Boolean, reflect: true }) active = true;
-
-  // @state()
-  // protected _active = false;
-
   connectedCallback() {
     super.connectedCallback();
-
-    console.log('connectedCallback is called');
-
-    this.base.addEventListener('toggle', () => {
-      console.log('toggle event is called');
-      console.log(this.base.getAttribute('open'));
-    });
   }
 
   /**
-   * name
+   * Boolean keeping track of the autoplay pause/play button
+   * @internal
    */
+  @state() defaultSlotIsFilled = false;
+
   public show() {
     this.open = true;
     this.base.setAttribute('open', '');
@@ -59,18 +51,20 @@ export default class SdQuickfact extends SolidElement {
 
   public hide() {
     this.open = false;
-    this.base.setAttribute('open', 'false');
-  }
-
-  @watch('base', { waitUntilFirstUpdate: true })
-  baseChanged() {
-    console.log('baseChanged is called');
-    this.open = this.base.hasAttribute('open');
+    this.base.removeAttribute('open');
   }
 
   render() {
     return html`
-      <details part="base" class="flex items-center" ?open=${this.open}>
+      <details
+        part="base"
+        class="flex items-center"
+        ?open=${this.open}
+        @toggle=${(e: ToggleEvent) => {
+          // Update the 'open' property based on the 'open' attribute of the <details> element
+          this.open = e.newState === 'open' ? true : false;
+        }}
+      >
         <summary class="flex flex-row sm:flex-col gap-4 items-center text-center">
           <slot name="icon"
             ><sd-icon class="h-12 w-12 sm:h-24 sm:w-24" name="content/image" color="primary"></sd-icon
@@ -81,32 +75,27 @@ export default class SdQuickfact extends SolidElement {
             <div class="text-base font-normal leading-normal text-black sm:text-xl">Con sectetur adipiscing elit</div>
           </div>
 
-          ${this.active
-            ? html`<button
-                part="icon-button"
-                class=${cx('!rounded-sm sd-interactive sd-interactive--reset')}
-                @click=${() => {
-                  console.log('clicked and open value: ', this.open);
-
-                  if (this.open) {
-                    this.hide();
-                  } else {
-                    this.show();
-                  }
-                  console.log('new open value: ', this.open);
-                }}
-              >
-                <slot name="next-icon">
-                  <sd-icon
-                    class=${cx('h-6 w-6 grid place-items-center')}
-                    library="system"
-                    name="${this.open ? 'chevron-up' : 'chevron-down'}"
-                  ></sd-icon>
-                </slot>
-              </button>`
-            : ''}
+          <button
+            part="icon-button"
+            class=${(cx('h-6 w-6 grid place-items-center'), !this.defaultSlotIsFilled && 'hidden')}
+            @click=${() => {
+              this.open = !this.open;
+            }}
+          >
+            <slot name="next-icon">
+              <sd-icon
+                class=${cx('h-6 w-6 grid place-items-center transition-transform', this.open && 'rotate-180')}
+                library="system"
+                name="chevron-down"
+              ></sd-icon>
+            </slot>
+          </button>
         </summary>
-        <slot></slot>
+        <slot
+          @slotchange=${() => {
+            this.defaultSlotIsFilled = this.defaultSlot.assignedElements().length > 0;
+          }}
+        ></slot>
       </details>
     `;
   }
