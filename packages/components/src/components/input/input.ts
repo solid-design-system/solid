cf625a0fd880a24eaa8869ad65d82fec8add4afa
JasminaIlic <jasmina.ilic@prodyna.com>
2024-07-26 09:04:57 +0200

    feat: 🎸 adding min and max posibility for date type of input (#1215)
    <!-- ## Title: Please consider adding the [skip chromatic] flag to the
    PR title in case you dont need chromatic testing your changes. -->
    ## Description:
    input type - date : min and max properties (of type type of string) was
    added so that type of input:Date can also use those properties.
    
    ## Definition of Reviewable:
    <!-- *PR notes: Irrelevant elements should be removed.* -->
    - [ ] Documentation is created/updated
    - [ ] Migration Guide is created/updated
    - [ ] E2E tests (features, a11y, bug fixes) are created/updated
    - [x] Stories (features, a11y) are created/updated
    - [ ] relevant tickets are linked

--- packages/components/src/components/input/input.ts
+++ packages/components/src/components/input/input.ts
@@ -156,10 +156,10 @@ export default class SdInput extends SolidElement implements SolidFormControl {
   @property({ type: Number }) maxlength: number;
 
   /** The input's minimum value. Only applies to date and number input types. */
-  @property({ type: Number }) min: number;
+  @property() min: number | string;
 
   /** The input's maximum value. Only applies to date and number input types. */
-  @property({ type: Number }) max: number;
+  @property() max: number | string;
 
   /**
    * By default, form controls are associated with the nearest containing `<form>` element. This attribute allows you
