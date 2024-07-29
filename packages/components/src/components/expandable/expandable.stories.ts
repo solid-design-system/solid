18f1b429478a3a028da8031405bcbf5143f39f65
Vahid <vahidmenu@gmail.com>
2024-07-29 14:32:36 +0200

    docs: formatting

--- packages/components/src/components/expandable/expandable.stories.ts
+++ packages/components/src/components/expandable/expandable.stories.ts
@@ -137,7 +137,7 @@ export const Mouseless = {
 
   play: async ({ canvasElement }: { canvasElement: HTMLUnknownElement }) => {
     const el = canvasElement.querySelector('.mouseless sd-expandable');
-    await waitUntil(() => el?.shadowRoot?.querySelector('button'));
+    await waitUntil(() => el!.shadowRoot!.querySelector('button'));
     await userEvent.type(el!.shadowRoot!.querySelector('button')!, '{space}', { pointerEventsCheck: 0 });
   }
 };
