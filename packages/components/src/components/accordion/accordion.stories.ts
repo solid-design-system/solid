ab311a5e8375ff3fd160a92c84149137679168e6
Sergio Fonseca <sergio.fonseca@virtual-identity.com>
2024-08-07 10:26:17 +0100

    feat: address design feedback

--- packages/components/src/components/accordion/accordion.stories.ts
+++ packages/components/src/components/accordion/accordion.stories.ts
@@ -66,15 +66,17 @@ export const Open = {
 export const Summary = {
   name: 'Summary',
   render: () => html`
-    <sd-accordion>
-      <div slot="summary">
-        <h4>Shareholder structure</h4>
-      </div>
-      <p>
-        Union Investment is the dedicated asset manager within the German cooperative financial network. All
-        shareholders are also part of the network, which ensures a stable and long-term ownership structure. This allows
-        us to maintain a sustainable business model and develop strong, long-term client relationships.
-      </p>
-    </sd-accordion>
+    <div class="grid grid-cols-2 gap-12">
+      <sd-accordion>
+        <div slot="summary">
+          <h4>I don't have a telephone number with a German area code. Can I still register?</h4>
+        </div>
+        <p>
+          Union Investment is the dedicated asset manager within the German cooperative financial network. All
+          shareholders are also part of the network, which ensures a stable and long-term ownership structure. This
+          allows us to maintain a sustainable business model and develop strong, long-term client relationships.
+        </p>
+      </sd-accordion>
+    </div>
   `
 };
