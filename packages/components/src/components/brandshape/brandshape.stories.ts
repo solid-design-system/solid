c98f9643d9e9f6e1803c5d18933b3fa7801db101
Sergio Fonseca <sergio.fonseca@virtual-identity.com>
2024-08-07 16:16:52 +0100

    feat: address pr comments

--- packages/components/src/components/brandshape/brandshape.stories.ts
+++ packages/components/src/components/brandshape/brandshape.stories.ts
@@ -12,8 +12,6 @@ const { generateTemplate } = storybookTemplate('sd-brandshape');
  *
  * Creates a container with brand character and visually emphasizes the content.
  *
- *  * **Related templates**:
- * - [Brand](?path=/docs/templates)
  */
 
 export default {
@@ -61,37 +59,31 @@ export const Default = {
 export const Variant = {
   name: 'Variant',
   render: () => html`
-    <div class="mb-5">
+    <div class="space-y-5">
       <sd-brandshape variant="primary">
         <p class="sd-headline sd-headline--lg text-white">Primary Variant</p>
       </sd-brandshape>
-    </div>
 
-    <div class="mb-5">
       <sd-brandshape variant="neutral-100">
         <p class="sd-headline sd-headline--lg">Neutral-100 Variant</p>
       </sd-brandshape>
-    </div>
 
-    <div class="bg-primary">
-      <sd-brandshape variant="white">
-        <p class="sd-headline sd-headline--lg">White Variant</p>
-      </sd-brandshape>
-    </div>
+      <div class="bg-primary">
+        <sd-brandshape variant="white">
+          <p class="sd-headline sd-headline--lg">White Variant</p>
+        </sd-brandshape>
+      </div>
 
-    <div>
       <sd-brandshape variant="border-primary">
         <p class="sd-headline sd-headline--lg">Border Primary Variant</p>
       </sd-brandshape>
-    </div>
 
-    <div class="bg-primary">
-      <sd-brandshape variant="border-white">
-        <p class="sd-headline sd-headline--lg text-white">Border White Variant</p>
-      </sd-brandshape>
-    </div>
+      <div class="bg-primary">
+        <sd-brandshape variant="border-white">
+          <p class="sd-headline sd-headline--lg text-white">Border White Variant</p>
+        </sd-brandshape>
+      </div>
 
-    <div>
       <sd-brandshape variant="image">
         <p class="sd-headline sd-headline--lg">Image Variant</p>
         <img slot="image" style="transform:translateY(-30%);" src="./placeholders/images/generic.jpg" alt="Generic" />
@@ -101,30 +93,24 @@ export const Variant = {
 };
 
 /**
- * By default, all shapes are displayed but it's possible to display only specific shapes by utilizing the `shapes` attribute.
+ * Use the `shapes` attribute to only display specific parts of the brandshape.
  */
 export const Shapes = {
   name: 'Shapes',
   render: () => html`
-    <div class="mb-5">
+    <div class="space-y-5">
       <sd-brandshape shapes='["top", "middle", "bottom"]'>
         <p class="text-white">All Shapes</p>
       </sd-brandshape>
-    </div>
 
-    <div class="mb-5">
       <sd-brandshape shapes='["top"]'>
         <p class="text-white">Top Shape</p>
       </sd-brandshape>
-    </div>
 
-    <div class="mb-5">
       <sd-brandshape shapes='["top", "middle"]'>
         <p class="text-white">Top and Middle Shape</p>
       </sd-brandshape>
-    </div>
 
-    <div class="mb-5">
       <sd-brandshape shapes='["middle", "bottom"]'>
         <p class="text-white">Middle and Bottom Shape</p>
       </sd-brandshape>
