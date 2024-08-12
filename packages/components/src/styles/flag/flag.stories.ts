c16fc32e250046c407b8398747f6fef0fb5c7f92
auroraVasconcelos <aurora.vasconcelos@virtual-identity.com>
2024-08-12 23:46:32 +0100

    feat: fix order and background color from neutral-200 to neutral-100

--- packages/components/src/styles/flag/flag.stories.ts
+++ packages/components/src/styles/flag/flag.stories.ts
@@ -1,10 +1,9 @@
 import '../../solid-components';
 import { html } from 'lit';
-import { storybookDefaults, storybookHelpers, storybookTemplate } from '../../../scripts/storybook/helper';
+import { storybookDefaults, storybookHelpers } from '../../../scripts/storybook/helper';
 
 const { argTypes, parameters } = storybookDefaults('sd-flag');
 const { overrideArgs } = storybookHelpers('sd-flag');
-const { generateTemplate } = storybookTemplate('sd-flag');
 
 /**
  * A small, non-interactive label the represents a category.
@@ -16,9 +15,6 @@ export default {
   component: 'sd-flag',
   parameters: {
     ...parameters,
-    backgrounds: {
-      default: 'neutral-200'
-    },
     design: {
       type: 'figma',
       url: 'https://www.figma.com/file/vQ57OHDm26QE1gtZKtfb6L/Flag?type=design&node-id=804-717&mode=design&t=bdhcOj9ub57hEPZl-0'
@@ -29,29 +25,24 @@ export default {
 };
 
 export const Default = {
-  render: (args: any) => {
-    return generateTemplate({
-      options: { templateContent: '<span class="%CLASSES%">%SLOT%</span>' },
-      args
-    });
-  }
+  render: () => html` <div class="sd-flag">Default</div> `
 };
 
 /**
  * Offers 4 color variants for alternate experiences:
- * - neutral-200 (default)
- * - neutral-300
- * - neutral-500
- * - white
+ * - neutral-200 (default): use the class `sd-flag--neutral-200`
+ * - neutral-300: use the class `sd-flag--neutral-300`
+ * - neutral-500: use the class `sd-flag--neutral-500`
+ * - white: use the class `sd-flag--white`
  */
 
 export const Variants = {
   name: 'Variants',
   render: () =>
-    html` <div class="flex gap-4">
+    html` <div class="flex gap-4 bg-neutral-100 p-8">
       <div class="sd-flag sd-flag--neutral-200">Default</div>
-      <div class="sd-flag sd-flag--neutral-500">Neutral 500</div>
       <div class="sd-flag sd-flag--neutral-300">Neutral 300</div>
+      <div class="sd-flag sd-flag--neutral-500">Neutral 500</div>
       <div class="sd-flag sd-flag--white">White</div>
     </div>`
 };
