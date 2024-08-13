13052f402031ca2d230d7de3bdc3fee53ee5fbe9
Sergio Fonseca <sergio.fonseca@virtual-identity.com>
2024-08-13 18:36:24 +0100

    feat: rewrite sd-tooltip documentation

--- packages/components/src/components/tooltip/tooltip.stories.ts
+++ packages/components/src/components/tooltip/tooltip.stories.ts
@@ -1,16 +1,18 @@
 import '../../solid-components';
 import { html } from 'lit';
 import { storybookDefaults, storybookHelpers, storybookTemplate } from '../../../scripts/storybook/helper';
-import { userEvent } from '@storybook/test';
-import { waitUntil } from '@open-wc/testing-helpers';
 import { withActions } from '@storybook/addon-actions/decorator';
 
 const { argTypes, parameters } = storybookDefaults('sd-tooltip');
 const { overrideArgs } = storybookHelpers('sd-tooltip');
 const { generateTemplate } = storybookTemplate('sd-tooltip');
 
+/**
+ * Used to display additional information based on a specific action.
+ */
 export default {
   title: 'Components/sd-tooltip',
+  tags: ['!dev'],
   component: 'sd-tooltip',
   args: overrideArgs([
     {
@@ -54,8 +56,8 @@ export default {
 /**
  * Default: This shows sd-tooltip in its default state.
  */
-
 export const Default = {
+  name: 'Default',
   render: (args: any) => {
     return html`<div class="template-position template-height">
       ${generateTemplate({
@@ -65,198 +67,31 @@ export const Default = {
   }
 };
 
-export const Placement = {
-  parameters: { controls: { exclude: 'placement' } },
-  render: (args: any) => {
-    return html`<div class="placement-story">
-      ${['top', 'bottom'].map(value =>
-        generateTemplate({
-          axis: {
-            x: {
-              type: 'attribute',
-              name: 'placement',
-              values: [value, `${value}-start`, `${value}-end`]
-            }
-          },
-          args,
-          constants: [
-            {
-              type: 'template',
-              name: 'placement',
-              value: `<div class="template-height template-position">%TEMPLATE%</div>`
-            }
-          ]
-        })
-      )}
-    </div> `;
-  }
-};
-
+/**
+ * Use the `size` attribute to set the size.
+ */
 export const Size = {
-  parameters: { controls: { exclude: 'size' } },
-  render: (args: any) =>
-    generateTemplate({
-      axis: {
-        x: {
-          type: 'attribute',
-          name: 'size'
-        }
-      },
-      args,
-      constants: [
-        { type: 'template', name: 'width', value: '<div class="template-position">%TEMPLATE%</div>' },
-        {
-          type: 'attribute',
-          name: 'placement',
-          value: 'bottom'
-        }
-      ]
-    })
-};
-
-export const Disabled = {
-  parameters: { controls: { exclude: 'disabled' } },
-  render: (args: any) =>
-    generateTemplate({
-      axis: {
-        x: {
-          type: 'attribute',
-          name: 'disabled'
-        }
-      },
-      args,
-      constants: [
-        { type: 'template', name: 'width', value: '<div class="template-position">%TEMPLATE%</div>' },
-        {
-          type: 'attribute',
-          name: 'placement',
-          value: 'bottom'
-        }
-      ]
-    })
+  name: 'Size',
+  render: () => html`
+    <div class="flex gap-12">
+      <sd-tooltip content="Lorem ipsum" placement="top" size="lg" trigger="click focus"></sd-tooltip>
+      <sd-tooltip content="Lorem ipsum" placement="top" size="sm" trigger="click focus"></sd-tooltip>
+    </div>
+  `
 };
 
 /**
- * In case of having the content longer than the space available, the tooltip can be set to scrollable by overriding the `pointer-events` property to auto. In this case the padding-right should also be adjusted via `sd-tooltip::part(body)`. This behavior is heavily discouraged, but it is available in case of need.
- *
+ * Use the `placement` attribute to set the placement.
  */
-export const LongContent = {
-  parameters: {
-    controls: { exclude: 'content' }
-  },
-  render: () => {
-    return html` <div>
-      <style>
-        sd-tooltip::part(body) {
-          padding-right: 8px;
-        }
-        .long-content {
-          padding-right: 4px;
-        }
-        .long-content::-webkit-scrollbar {
-          width: 4px;
-        }
-
-        .long-content::-webkit-scrollbar-thumb {
-          background-color: transparent; /* make scrollbar transparent */
-          border-radius: 4px;
-        }
-
-        .long-content:hover::-webkit-scrollbar-thumb {
-          background-color: rgba(81, 81, 81, 0.9);
-        }
-      </style>
-      ${generateTemplate({
-        axis: {
-          x: {
-            type: 'slot',
-            name: 'content',
-            values: [
-              {
-                value: `<div class='long-content' slot='content' style='pointer-events:auto; width: 100px;'>Lorem ipsum sic semper dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultrices aliquam, nunc nisl aliquet nunc, quis aliquam nisl nisl quis nisl. Nulla euismod, nisl quis ultrices aliquam, nunc nisl aliquet nunc, quis aliquam nisl nisl quis nisl. Lorem ipsum sic semper dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultrices aliquam, nunc nisl aliquet nunc, quis aliquam nisl nisl quis nisl. Nulla euismod, nisl quis ultrices aliquam, nunc nisl aliqu et nunc, quis ali qu am nisl nisl quis nisl.</div>`,
-                title: 'long with fixed width'
-              }
-            ]
-          }
-        },
-        args: overrideArgs([
-          {
-            type: 'attribute',
-            name: 'open',
-            value: true
-          },
-          {
-            type: 'attribute',
-            name: 'placement',
-            value: 'bottom'
-          }
-        ]),
-        constants: [
-          {
-            type: 'template',
-            name: 'width',
-            value: '<div class="template-position">%TEMPLATE%</div>'
-          }
-        ]
-      })}
-    </div>`;
-  }
+export const Placement = {
+  name: 'Placement',
+  render: () => html` <sd-tooltip content="Lorem ipsum" placement="top" size="lg" trigger="click focus"></sd-tooltip> `
 };
 
-export const Slots = {
-  parameters: {
-    controls: { exclude: ['default', 'content'] }
-  },
-  render: (args: any) => {
-    return generateTemplate({
-      axis: {
-        x: {
-          type: 'slot',
-          name: 'default',
-          title: 'slot=...',
-          values: [
-            {
-              value: `<div class="slot slot--border slot--background slot--text" style="--slot-content: ''; --slot-height: 24px; --slot-width: 24px;"></div>`,
-              title: 'default'
-            },
-            {
-              value: `<div class="slot slot--border slot--background slot--text" slot='content' style="--slot-content: ''; --slot-height: 22px; --slot-width: 82px;"></div>`,
-              title: 'content'
-            }
-          ]
-        }
-      },
-      args,
-      constants: [
-        {
-          type: 'template',
-          name: 'width',
-          value: '<div class="template-position">%TEMPLATE%</div>'
-        },
-        {
-          type: 'attribute',
-          name: 'placement',
-          value: 'bottom'
-        }
-      ]
-    });
-  }
-};
-
-export const Mouseless = {
-  parameters: {
-    controls: {
-      exclude: ['open']
-    }
-  },
-  render: (args: any) => {
-    return html`<div class="mouseless template-position template-height">${generateTemplate({ args })}</div>`;
-  },
-  play: async ({ canvasElement }: { canvasElement: HTMLUnknownElement }) => {
-    const el = canvasElement.querySelector('.mouseless sd-tooltip');
-    await waitUntil(() => el?.shadowRoot?.querySelector('#tooltip'));
-    await waitUntil(() => el?.shadowRoot?.querySelector('sd-icon'));
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    await userEvent.type(el!.shadowRoot!.querySelector('sd-icon')!, '{return}', { pointerEventsCheck: 0 });
-  }
+/**
+ * Use the `disabled` attribute to disable the tooltip.
+ */
+export const Disabled = {
+  name: 'Disabled',
+  render: () => html` <sd-tooltip content="Lorem ipsum" placement="top" size="lg" disabled></sd-tooltip> `
 };
