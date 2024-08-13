6b012735fc4cbb3a9a08c7afc9ab22fed2415a09
Sergio Fonseca <sergio.fonseca@virtual-identity.com>
2024-08-13 16:24:21 +0100

    feat: rewrite tab documentation

--- packages/components/src/components/tab/tab.stories.ts
+++ packages/components/src/components/tab/tab.stories.ts
@@ -7,8 +7,13 @@ const { argTypes, parameters } = storybookDefaults('sd-tab');
 const { overrideArgs } = storybookHelpers('sd-tab');
 const { generateTemplate } = storybookTemplate('sd-tab');
 
+/**
+ * Used inside `sd-tab-group` to represent and activate tab panels.
+ */
+
 export default {
   title: 'Components/sd-tab',
+  tags: ['!dev'],
   component: 'sd-tab',
   args: overrideArgs([
     {
@@ -33,129 +38,61 @@ export const Default = {
 };
 
 /**
- * Use the `active` attribute to toggle the active state. Styling to indicate the active state is applied in the `sd-tab-group` component.
- */
-
-export const Active = {
-  parameters: { controls: { exclude: ['active'] } },
-  render: (args: any) => {
-    return generateTemplate({
-      axis: {
-        y: { type: 'attribute', name: 'active', values: [false, true] }
-      },
-      args
-    });
-  }
-};
-
-/**
- * Use the variant attribute to alternate between the `default` and `container` styles.
+ * Use the `variant` attribute to display different styles.
  */
 
 export const Variant = {
-  parameters: { controls: { exclude: ['variant'] } },
-  render: (args: any) => {
-    return generateTemplate({
-      axis: {
-        y: { type: 'attribute', name: 'variant' },
-        x: { type: 'attribute', name: 'active', values: [false, true] }
-      },
-      args,
-      constants: { type: 'attribute', name: 'active', value: true }
-    });
-  }
+  name: 'Variant',
+  render: () => html`
+    <div class="flex gap-12">
+      <sd-tab variant="default">Default</sd-tab>
+      <sd-tab variant="container">Container</sd-tab>
+    </div>
+  `
 };
 
 /**
- * Use the `disabled` attribute to toggle the disabled state.
+ * Use the `active` attribute to toggle the active state.
  */
 
+export const Active = {
+  name: 'Active',
+  render: () => html`
+    <div class="flex gap-12">
+      <sd-tab active>Default</sd-tab>
+      <sd-tab variant="container" active>Container</sd-tab>
+    </div>
+  `
+};
+
+/**
+ * Use the `disabled` attribute to disable a tab.
+ */
 export const Disabled = {
-  parameters: { controls: { exclude: ['disabled'] } },
-  render: (args: any) => {
-    return generateTemplate({
-      axis: {
-        y: { type: 'attribute', name: 'disabled' }
-      },
-      args
-    });
-  }
+  name: 'Disabled',
+  render: () => html`
+    <div class="flex gap-12">
+      <sd-tab disabled>Default</sd-tab>
+    </div>
+  `
 };
 
-export const Parts = {
-  parameters: {
-    controls: {
-      exclude: ['base', 'active-tab-indicator', 'bottom-border']
-    }
-  },
-  render: (args: any) => {
-    return html`
-      ${['base', 'active-tab-indicator', 'hover-bottom-border'].map(part =>
-        generateTemplate({
-          axis: {
-            x: {
-              type: 'template',
-              name: 'sd-tab::part(...){outline: solid 2px red}',
-              values: [
-                {
-                  title: part,
-                  value: `<style>#part-${part} sd-tab::part(${part}){outline: solid 2px red; ${
-                    part === 'tabs' && 'outline-offset:-2px;'
-                  }}</style><div id="part-${part}">%TEMPLATE%</div>`
-                }
-              ]
-            },
-            y: { type: 'attribute', name: 'active', values: [false, true] }
-          },
-          constants: [
-            {
-              type: 'template',
-              name: 'width',
-              value: `
-                <div style="width: 600px; position: relative;">%TEMPLATE%
-                </div>
-              `
-            }
-          ],
-          args
-        })
-      )}
-    `;
-  }
-};
 /**
- * Use the `left` slot to optionally include an element (eg. icon) positioned to the left of the label.
+ * Use the `left` slot include an element (eg. icon) positioned to the left of the label.
  */
+export const WithIcon = {
+  name: 'With Icon',
+  render: () => html`
+    <div class="flex gap-12">
+      <sd-tab>
+        <sd-icon slot="left" name="system/picture" library="global-resources"></sd-icon>
+        Tab
+      </sd-tab>
 
-export const Sample = {
-  parameters: { controls: { exclude: ['active', 'variant'] } },
-  name: 'Sample: Icon',
-  render: (args: any) => {
-    return generateTemplate({
-      axis: {
-        y: { type: 'attribute', name: 'variant' },
-        x: {
-          type: 'slot',
-          name: 'left',
-          values: [
-            {
-              title: 'sd-icon',
-              value: `
-              <sd-icon slot="left" name="system/picture" library="global-resources"></sd-icon>`
-            },
-            {
-              title: 'sd-icon + sd-badge',
-              value: `
-              <div class="relative">
-            <sd-icon slot="left" name="system/picture" library="global-resources"></sd-icon>
-            <sd-badge class="absolute -top-0.5 -right-0.5" tabindex="-1" size="sm"></sd-badge>
-         </div>`
-            }
-          ]
-        }
-      },
-      args,
-      constants: { type: 'attribute', name: 'active', value: true }
-    });
-  }
+      <sd-tab variant="container" active>
+        <sd-icon slot="left" name="system/picture" library="global-resources"></sd-icon>
+        Tab
+      </sd-tab>
+    </div>
+  `
 };
