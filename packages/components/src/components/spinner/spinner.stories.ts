edb0301db1478af4a99767271c791ddf9de938e5
auroraVasconcelos <aurora.vasconcelos@virtual-identity.com>
2024-08-14 09:48:51 +0100

    chore: sd-spinner docs

--- packages/components/src/components/spinner/spinner.stories.ts
+++ packages/components/src/components/spinner/spinner.stories.ts
@@ -1,75 +1,82 @@
 import '../../solid-components';
 import { html } from 'lit-html';
-import { storybookDefaults, storybookTemplate } from '../../../scripts/storybook/helper';
+import { storybookDefaults } from '../../../scripts/storybook/helper';
 import { withActions } from '@storybook/addon-actions/decorator';
 
 const { argTypes, args, parameters } = storybookDefaults('sd-spinner');
-const { generateTemplate } = storybookTemplate('sd-spinner');
+
+/**
+ * A visual indicator with a looping animation that shows loading is in process in the background.
+ */
 
 export default {
   title: 'Components/sd-spinner',
   component: 'sd-spinner',
+  tags: ['!dev'],
   args,
   argTypes,
   parameters: { ...parameters },
   decorators: [withActions] as any
 };
 
-/**
- * Default: This shows sd-spinner in its default state.
- */
-
 export const Default = {
-  render: (args: any) => {
-    return generateTemplate({ args });
-  }
+  name: 'Default',
+  render: () => html`<sd-spinner color="currentColor"></sd-spinner>`
 };
 
 /**
- * Use the `color` attribute to change the color of the spinner and correspond (currentColor) with the parents color.
+ * Offers 4 `color` variants for alternate experiences:
+ * - `primary` (default): is the default color on light backgrounds
+ * - `neutral-500`: is only used for disabled buttons
+ * - `neutral-600`: is only used for inverted disabled buttons
+ * - `white`: is used on primary backgrounds and for inverted buttons
+ * - `currentColor`: is used to inherit the color from the parent element
  */
 
-export const Color = {
+export const Variants = {
+  name: 'Variants',
   parameters: { controls: { exclude: 'color' } },
-  render: (args: any) => {
-    return generateTemplate({
-      axis: {
-        x: { type: 'attribute', name: 'color' }
-      },
-      options: {
-        templateBackgrounds: {
-          alternate: 'x',
-          colors: ['', 'rgb(var(--sd-color-primary, 0 53 142))', '#F6F6F6']
-        }
-      },
-      args
-    });
-  }
+  render: () =>
+    html` <div class="flex flex-row items-center gap-12">
+      <div class="p-4">
+        <sd-spinner color="primary"></sd-spinner>
+      </div>
+      <div class="p-4 text-neutral-500">
+        <sd-spinner color="currentColor"></sd-spinner>
+      </div>
+      <div class="p-4 text-neutral-600">
+        <sd-spinner color="currentColor"></sd-spinner>
+      </div>
+      <div class="bg-primary p-4">
+        <sd-spinner color="white"></sd-spinner>
+      </div>
+      <div class="bg-neutral-100 p-4">
+        <sd-spinner color="currentColor"></sd-spinner>
+      </div>
+    </div>`
 };
 
 /**
- * Use the font-size in css to scale the spinner.
+ * Spinner has no size properties. It can scale proportionally.
+ *
+ * Use the `font-size` in css to scale the spinner.
  */
 
-export const Sizing = {
-  parameters: { controls: { exclude: ['color'] } },
-  render: (args: any) => {
-    return html`
-      ${generateTemplate({
-        axis: {
-          x: {
-            type: 'template',
-            name: 'individual sizing',
-            values: [
-              { value: '<div style="font-size: inherit">%TEMPLATE%</div>', title: 'font-size: inherit' },
-              { value: '<div style="font-size: 1rem">%TEMPLATE%</div>', title: 'font-size: 1rem' },
-              { value: '<div style="font-size: 2rem">%TEMPLATE%</div>', title: 'font-size: 2rem' },
-              { value: '<div style="font-size: 4rem">%TEMPLATE%</div>', title: 'font-size: 4rem' }
-            ]
-          }
-        },
-        args
-      })}
-    `;
-  }
+export const Size = {
+  name: 'Size',
+  render: () =>
+    html`<div class="flex flex-row gap-12">
+      <div style="font-size: inherit">
+        <sd-spinner color="currentColor"></sd-spinner>
+      </div>
+      <div style="font-size: 1rem">
+        <sd-spinner color="currentColor"></sd-spinner>
+      </div>
+      <div style="font-size: 2rem">
+        <sd-spinner color="currentColor"></sd-spinner>
+      </div>
+      <div style="font-size: 4rem">
+        <sd-spinner color="currentColor"></sd-spinner>
+      </div>
+    </div>`
 };
