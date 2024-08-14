21742809433798b4015e8aca63a8b092937efd3a
auroraVasconcelos <aurora.vasconcelos@virtual-identity.com>
2024-08-14 17:08:34 +0100

    chore: update sd-switch docs

--- packages/components/src/components/switch/switch.stories.ts
+++ packages/components/src/components/switch/switch.stories.ts
@@ -1,13 +1,16 @@
 import '../../solid-components';
 import { html } from 'lit-html';
-import { storybookDefaults, storybookHelpers, storybookTemplate } from '../../../scripts/storybook/helper';
-import { userEvent } from '@storybook/test';
-import { waitUntil } from '@open-wc/testing-helpers';
+import { storybookDefaults, storybookHelpers } from '../../../scripts/storybook/helper';
+
 const { argTypes, parameters } = storybookDefaults('sd-switch');
-const { generateTemplate } = storybookTemplate('sd-switch');
 const { overrideArgs } = storybookHelpers('sd-switch');
 
+/**
+ * Gives control over a feature or option that can be turned on or off
+ */
+
 export default {
+  tags: ['!dev'],
   title: 'Components/sd-switch',
   component: 'sd-switch',
   args: overrideArgs([{ type: 'slot', name: 'default', value: 'Default Slot' }]),
@@ -21,151 +24,43 @@ export default {
   }
 };
 
-/**
- * Default: This shows sd-switch in its default state.
- */
-
 export const Default = {
-  render: (args: any) => {
-    return generateTemplate({ args });
-  }
+  name: 'Default',
+  render: () => html`<sd-switch>Default Slot</sd-switch>`
 };
 
 /**
- * Use the disabled attribute to disable an input switch. Clicks will be suppressed until the disabled state is removed
- */
-
-export const Disabled = {
-  parameters: { controls: { exclude: ['disabled', 'default'] } },
-  render: (args: any) => {
-    return generateTemplate({
-      axis: {
-        x: {
-          type: 'attribute',
-          name: 'disabled',
-          values: [false, true]
-        }
-      },
-      constants: { type: 'attribute', name: 'disabled', value: true },
-      args
-    });
-  }
-};
-
-/**
- * Use the `required` attribute to mark the element as required. This can be used for form validation purposes.
- */
-
-export const Required = {
-  parameters: { controls: { exclude: ['required'] } },
-  render: (args: any) => {
-    return generateTemplate({
-      axis: {
-        y: { type: 'attribute', name: 'required' }
-      },
-      args
-    });
-  }
-};
-
-/**
- * Use the checked attribute to activate the switch.
+ * Use the `checked` attribute to activate the switch
  */
 
 export const Checked = {
-  parameters: { controls: { exclude: ['checked'] } },
-  render: (args: any) => {
-    return generateTemplate({
-      axis: {
-        x: {
-          type: 'attribute',
-          name: 'disabled',
-          values: [false, true]
-        }
-      },
-      constants: { type: 'attribute', name: 'checked', value: true },
-      args
-    });
-  }
+  render: () => html`<sd-switch disabled="" checked="">Default Slot</sd-switch>`
 };
 
 /**
- * Test invalid state inside a form.
+ * Use the `required `attribute to mark the element as required. This can be used for form validation purposes
+ * */
+
+export const Required = {
+  render: () => html`<sd-switch required="">Default Slot</sd-switch>`
+};
+
+/**
+ * Use the `disabled `attribute to disable an input checkbox. Clicks will be suppressed until the disabled state is removed
+ */
+
+export const Disabled = {
+  render: () => html`<sd-switch disabled="">Default Slot</sd-switch>`
+};
+
+/**
+ * The component gets `invalid` attribute when the form is invalid
  */
 
 export const Invalid = {
-  parameters: { controls: { exclude: ['required'] } },
-  render: (args: any) => {
-    return html`
-      <form>
-        ${generateTemplate({
-          args,
-          constants: [{ type: 'attribute', name: 'required', value: true }]
-        })}
-        <sd-button style="margin-top: 16px" type="submit">Submit</sd-button>
-      </form>
-    `;
-  },
-  play: async ({ canvasElement }: { canvasElement: HTMLUnknownElement }) => {
-    const el = canvasElement.querySelector('sd-button');
-    await waitUntil(() => el?.shadowRoot?.querySelector('button'));
-    await userEvent.type(el!.shadowRoot!.querySelector('button')!, '{return}', { pointerEventsCheck: 0 });
-  }
-};
-
-/**
- * Use the `base`, `control--unchecked`, `control--checked`, `checked` and `label` part selectors to customize the switch.
- */
-
-export const Parts = {
-  parameters: {
-    controls: {
-      exclude: [
-        'base',
-        'control',
-        'control--unchecked',
-        'control--checked',
-        'label',
-        'title',
-        'name',
-        'value',
-        'size',
-        'disabled',
-        'checked',
-        'form',
-        'thumb',
-        'required',
-        'default'
-      ]
-    }
-  },
-  render: (args: any) => {
-    return generateTemplate({
-      axis: {
-        y: {
-          type: 'template',
-          name: 'sd-switch::part(...){outline: solid 2px red}',
-          values: ['base', 'control', 'control--unchecked', 'control--checked', 'thumb', 'label'].map(part => {
-            return {
-              title: part,
-              value: `
-                <style>
-                    #part-${part} sd-switch::part(${part}){outline: solid 2px red};
-                    .hidden {display: none}
-                </style>
-                
-                <div id="part-${part}">${
-                  part.includes('control--checked')
-                    ? '<sd-switch checked>Default Slot</sd-switch>'
-                    : '<sd-switch>Default Slot</sd-switch>'
-                }</div>
-                <div class="hidden">%TEMPLATE%</div>
-              `
-            };
-          })
-        }
-      },
-      args
-    });
-  }
+  render: () =>
+    html` <form>
+      <sd-switch required="">Default Slot</sd-switch>
+      <sd-button style="margin-top: 16px" type="submit">Submit</sd-button>
+    </form>`
 };
