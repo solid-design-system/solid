d4e338dd9134e2b3a62483f701675144ecdcbcd1
auroraVasconcelos <aurora.vasconcelos@virtual-identity.com>
2024-08-19 17:47:17 +0100

    chore: revert default to interactive, remove name from stories and clean up description

--- packages/components/src/styles/chip/chip.stories.ts
+++ packages/components/src/styles/chip/chip.stories.ts
@@ -1,9 +1,10 @@
 import '../../solid-components';
 import { html } from 'lit';
-import { storybookDefaults, storybookHelpers } from '../../../scripts/storybook/helper';
+import { storybookDefaults, storybookHelpers, storybookTemplate } from '../../../scripts/storybook/helper';
 
 const { argTypes } = storybookDefaults('sd-chip');
 const { overrideArgs } = storybookHelpers('sd-chip');
+const { generateTemplate } = storybookTemplate('sd-chip');
 
 /**
  * A small, non-interactive label that represents a status, property or meta-data.
@@ -23,21 +24,21 @@ export default {
 };
 
 export const Default = {
-  name: 'Default',
-  render: () =>
-    html` <div class="flex gap-12 bg-neutral-100 p-8">
-      <div class="sd-chip sd-chip--primary-200">primary-200</div>
-    </div>`
+  render: (args: any) => {
+    return generateTemplate({
+      options: { templateContent: '<span class="%CLASSES%">%SLOT%</span>' },
+      args
+    });
+  }
 };
 /**
  * Use the `classes` variant for alternative appearances:
- * - `--primary-200` (default): use the class `sd-chip--primary-200`
- * - `--primary-300`: use the class `sd-chip--primary-300`
- * - `--primary-500`: use the class `sd-chip--primary-500`
- * - `--white`: use the class `sd-chip--white`
+ * - `--primary-200` (default)
+ * - `--primary-300`
+ * - `--primary-500`
+ * - `--white`
  */
 export const Variants = {
-  name: 'Variants',
   render: () =>
     html` <div class="flex gap-12 bg-neutral-100 p-8">
       <div class="sd-chip sd-chip--primary-200">primary-200</div>
