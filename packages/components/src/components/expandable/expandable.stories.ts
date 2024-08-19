19462dd93af339f5be8a3204fca3254c1a188a94
auroraVasconcelos <aurora.vasconcelos@virtual-identity.com>
2024-08-19 13:29:18 +0100

    chore: clean up names

--- packages/components/src/components/expandable/expandable.stories.ts
+++ packages/components/src/components/expandable/expandable.stories.ts
@@ -24,7 +24,6 @@ export default {
 };
 
 export const Default = {
-  name: 'Default',
   render: (args: any) => {
     return generateTemplate({
       args
@@ -41,22 +40,46 @@ export const Open = {
     return html`<div class="flex flex-col gap-12">
       <sd-expandable class="sd-prose sd-prose--full-width">
         <p>
-          Between 50 and 60 per cent of global economic output depends on functioning ecosystems and the associated
-          natural services. This emphasises the high economic importance of the topic and the need to address the
-          associated risks and take them into account in the investment process. In the biodiversity guideline, we
-          describe the relevance of the topic and Union Investment's approach as a long-term investor.
+          For us, diversity equals strength. By taking the views and experiences of a wide range of people into account,
+          we provide room for creative solutions and ensure that we are equipped for the future. A diverse and inclusive
+          working environment is important to us. We benefit from the perspectives of different genders, age groups,
+          educational levels and backgrounds, thus guaranteeing that we are successful together.
+        </p>
+        <p>
+          As a member of the cooperative financial network, diversity and corporate citizenship are an integral part of
+          our corporate culture. Our approach is based around interacting with each other, our customers and our
+          partners on an equal footing. Our actions are defined by values such as respect, professionalism and
+          collaboration. In the true spirit of the cooperative principles, we channel a wide range of strengths in order
+          to be stronger together.
+        </p>
+        <p>
+          By signing the Diversity Charter in 2013, Union Investment underlined how highly it values diversity within
+          the company. We want to attract and retain the best candidates, irrespective of cultural or social background,
+          ethnicity, gender, sexual orientation, disability, religion or age. To do this, we have created a working
+          environment that is free of prejudice, where respect is paramount and people can achieve their full potential.
+          This is the policy that underpins the actions of our managers and employees.
         </p>
       </sd-expandable>
       <sd-expandable open class="sd-prose sd-prose--full-width">
         <p>
-          Between 50 and 60 per cent of global economic output depends on functioning ecosystems and the associated
-          natural services. This emphasises the high economic importance of the topic and the need to address the
-          associated risks and take them into account in the investment process. In the biodiversity guideline, we
-          describe the relevance of the topic and Union Investment's approach as a long-term investor. Between 50 and 60
-          per cent of global economic output depends on functioning ecosystems and the associated natural services. This
-          emphasises the high economic importance of the topic and the need to address the associated risks and take
-          them into account in the investment process. In the biodiversity guideline, we describe the relevance of the
-          topic and Union Investment's approach as a long-term investor.
+          For us, diversity equals strength. By taking the views and experiences of a wide range of people into account,
+          we provide room for creative solutions and ensure that we are equipped for the future. A diverse and inclusive
+          working environment is important to us. We benefit from the perspectives of different genders, age groups,
+          educational levels and backgrounds, thus guaranteeing that we are successful together.
+        </p>
+        <p>
+          As a member of the cooperative financial network, diversity and corporate citizenship are an integral part of
+          our corporate culture. Our approach is based around interacting with each other, our customers and our
+          partners on an equal footing. Our actions are defined by values such as respect, professionalism and
+          collaboration. In the true spirit of the cooperative principles, we channel a wide range of strengths in order
+          to be stronger together.
+        </p>
+        <p>
+          By signing the Diversity Charter in 2013, Union Investment underlined how highly it values diversity within
+          the company. We want to attract and retain the best candidates, irrespective of cultural or social background,
+          ethnicity, gender, sexual orientation, disability, religion or age. To do this, we have created a working
+          environment that is free of prejudice, where respect is paramount and people can achieve their full potential.
+          This is the policy that underpins the actions of our managers and employees.
         </p>
       </sd-expandable>
     </div>`;
@@ -71,10 +94,24 @@ export const Inverted = {
     return html`<div class="bg-primary p-8">
       <sd-expandable inverted class="sd-prose sd-prose--full-width sd-prose--inverted">
         <p>
-          Between 50 and 60 per cent of global economic output depends on functioning ecosystems and the associated
-          natural services. This emphasises the high economic importance of the topic and the need to address the
-          associated risks and take them into account in the investment process. In the biodiversity guideline, we
-          describe the relevance of the topic and Union Investment's approach as a long-term investor.
+          For us, diversity equals strength. By taking the views and experiences of a wide range of people into account,
+          we provide room for creative solutions and ensure that we are equipped for the future. A diverse and inclusive
+          working environment is important to us. We benefit from the perspectives of different genders, age groups,
+          educational levels and backgrounds, thus guaranteeing that we are successful together.
+        </p>
+        <p>
+          As a member of the cooperative financial network, diversity and corporate citizenship are an integral part of
+          our corporate culture. Our approach is based around interacting with each other, our customers and our
+          partners on an equal footing. Our actions are defined by values such as respect, professionalism and
+          collaboration. In the true spirit of the cooperative principles, we channel a wide range of strengths in order
+          to be stronger together.
+        </p>
+        <p>
+          By signing the Diversity Charter in 2013, Union Investment underlined how highly it values diversity within
+          the company. We want to attract and retain the best candidates, irrespective of cultural or social background,
+          ethnicity, gender, sexual orientation, disability, religion or age. To do this, we have created a working
+          environment that is free of prejudice, where respect is paramount and people can achieve their full potential.
+          This is the policy that underpins the actions of our managers and employees.
         </p>
       </sd-expandable>
     </div>`;
