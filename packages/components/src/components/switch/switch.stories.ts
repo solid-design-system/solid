7a02cfb6594df0a325ca74fff7c1d91ae83ce7b6
auroraVasconcelos <aurora.vasconcelos@virtual-identity.com>
2024-08-20 12:23:31 +0100

    chore: align with design

--- packages/components/src/components/switch/switch.stories.ts
+++ packages/components/src/components/switch/switch.stories.ts
@@ -7,14 +7,14 @@ const { overrideArgs } = storybookHelpers('sd-switch');
 const { generateTemplate } = storybookTemplate('sd-switch');
 
 /**
- * Gives control over a feature or option that can be turned on or off
+ * Gives control over a feature or option that can be turned on or off.
  */
 
 export default {
   tags: ['!dev'],
   title: 'Components/sd-switch',
   component: 'sd-switch',
-  args: overrideArgs([{ type: 'slot', name: 'default', value: 'Default Slot' }]),
+  args: overrideArgs([{ type: 'slot', name: 'default', value: 'Switch' }]),
   argTypes,
   parameters: {
     ...parameters,
@@ -32,27 +32,27 @@ export const Default = {
 };
 
 /**
- * Use the `checked` attribute to activate the switch
+ * Use the `checked` attribute to activate the switch.
  */
 
 export const Checked = {
-  render: () => html`<sd-switch checked>Default Slot</sd-switch>`
+  render: () => html`<sd-switch checked>Checked</sd-switch>`
 };
 
 /**
- * Use the `required `attribute to mark the element as required. This can be used for form validation purposes
- * */
-
-export const Required = {
-  render: () => html`<sd-switch required>Default Slot</sd-switch>`
-};
-
-/**
- * Use the `disabled `attribute to disable an input checkbox. Clicks will be suppressed until the disabled state is removed
+ * Use the `disabled` attribute to disable an input switch. Clicks will be suppressed until the disabled state is removed.
  */
 
 export const Disabled = {
-  render: () => html`<sd-switch disabled>Default Slot</sd-switch>`
+  render: () => html`<sd-switch disabled>Disabled</sd-switch>`
+};
+
+/**
+ * Use the `required` attribute to mark the element as required. This can be used for form validation purposes.
+ * */
+
+export const Required = {
+  render: () => html`<sd-switch required>Required</sd-switch>`
 };
 
 /**
@@ -62,7 +62,7 @@ export const Disabled = {
 export const Invalid = {
   render: () =>
     html` <form>
-      <sd-switch required>Default Slot</sd-switch>
+      <sd-switch required>Invalid</sd-switch>
       <sd-button style="margin-top: 16px" type="submit">Submit</sd-button>
     </form>`
 };
