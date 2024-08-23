06999203cd71c81010426c9cd11b000d53252268
auroraVasconcelos <aurora.vasconcelos@virtual-identity.com>
2024-08-23 17:33:06 +0100

    wip: align with design

--- packages/components/src/components/input/input.stories.ts
+++ packages/components/src/components/input/input.stories.ts
@@ -1,62 +1,30 @@
 import '../../solid-components';
 import { html } from 'lit-html';
 import { storybookDefaults, storybookHelpers, storybookTemplate } from '../../../scripts/storybook/helper';
-import { userEvent } from '@storybook/test';
 import { waitUntil } from '@open-wc/testing-helpers';
-import { withActions } from '@storybook/addon-actions/decorator';
 import type SdInput from './input';
 
 const { argTypes, args, parameters } = storybookDefaults('sd-input');
 const { generateTemplate } = storybookTemplate('sd-input');
 const { overrideArgs } = storybookHelpers('sd-input');
 
+/**
+ * Allows users to enter text. It can be displayed in several ways, depending on the type attribute.
+ */
+
 export default {
   tags: ['!dev'],
   title: 'Components/sd-input',
   component: 'sd-input',
   args,
-  argTypes: {
-    ...argTypes,
-    'type-attr': {
-      name: 'type',
-      table: {
-        category: 'attributes',
-        defaultValue: 'text'
-      },
-      description:
-        'The type of input. Works the same as a native `<input>` element, but only a subset of types are supported. ',
-      control: 'select',
-      options: ['date', 'datetime-local', 'email', 'number', 'password', 'search', 'tel', 'text', 'time', 'url', 'text']
-    },
-    'min-attr': {
-      name: 'min',
-      table: {
-        category: 'attributes',
-        defaultValue: ''
-      },
-      description:
-        "The input's minimum value. Only applies to date and number input types. String format for date  is yyyy-mm-dd",
-      control: 'text'
-    },
-    'max-attr': {
-      name: 'max',
-      table: {
-        category: 'attributes',
-        defaultValue: ''
-      },
-      description:
-        "The input's maximum value. Only applies to date and number input types. String format for date  is yyyy-mm-dd",
-      control: 'text'
-    }
-  },
+  argTypes,
   parameters: {
     ...parameters,
     design: {
       type: 'figma',
       url: 'https://www.figma.com/file/xSIeTnyfW2T21Uw5JgdZOg/Input?node-id=0%3A1&mode=dev'
     }
-  },
-  decorators: [withActions] as any
+  }
 };
 
 /**
@@ -70,58 +38,110 @@ export const Default = {
 };
 
 /**
- * Use the `label` attribute to give the input an accessible label. For labels that contain HTML, use the `label` slot instead.
+ * Use the `size` attribute to change the size.
+ */
+
+export const Size = {
+  render: () => html`
+    <div class="grid grid-cols-2 gap-12 content-end">
+      <sd-input type="text" size="lg" label="Label"> </sd-input>
+      <sd-input type="text" size="md" label="Label"> </sd-input>
+      <sd-input type="text" size="sm" label="Label"> </sd-input>
+    </div>
+  `
+};
+
+/**
+ * Use the `label` attribute to give the input an accessible label.
+ *
+ * For labels that contain HTML, use the `label` slot instead.
  */
 
 export const Labels = {
-  args: overrideArgs([{ type: 'attribute', name: 'label', value: 'Label' }]),
-  render: (args: any) => {
-    return html`
+  render: () => html`
+    <div class="flex flex-row gap-12 items-baseline">
       <div class="w-[250px]">
-        ${generateTemplate({
-          args
-        })}
+        <sd-input type="text" size="lg" label="Label" spellcheck=""></sd-input>
       </div>
-    `;
-  }
+      <div class="w-[250px]">
+        <sd-input type="text" size="lg" spellcheck="">
+          <div slot="label" class="text-lg">Label</div>
+        </sd-input>
+      </div>
+    </div>
+  `
 };
 
 /**
- * Add descriptive help text to an input with the `help-text` attribute. For help texts that contain HTML, use the `help-text` slot instead.
+ *Use the `placeholder` attribute to add a placeholder.
+ */
+export const placeholder = {
+  render: () => html`
+    <div class="w-[250px]">
+      <sd-input type="text" size="lg" placeholder="Placeholder example" spellcheck=""></sd-input>
+    </div>
+  `
+};
+
+/**
+ * Use the `disabled` attribute to disable the input.
+ */
+
+export const Disabled = {
+  render: () =>
+    html`<div class="w-[250px]">
+      <sd-input type="text" label="Label" placeholder="Input text disabled" disabled></sd-input>
+    </div>`
+};
+
+/**
+ * Use the `read-only` attribute to render an input as read only.
+ */
+export const ReadOnly = {
+  render: () =>
+    html`<div class="w-[250px]">
+      <sd-input type="text" label="Label" placeholder="Read only example" readonly></sd-input>
+    </div>`
+};
+
+/**
+ * Use the `help-text` attribute to add a descriptive “help text”. For help texts that contain HTML, use the `help-text` slot instead.
  */
 
 export const HelpText = {
-  name: 'Help Text',
-  args: overrideArgs([
-    { type: 'attribute', name: 'label', value: 'Nickname' },
-    { type: 'attribute', name: 'help-text', value: 'How would you like to be called?' }
-  ]),
-  render: (args: any) => {
+  render: () => {
     return html`
-      <div class="w-[250px]">
-        ${generateTemplate({
-          args
-        })}
+      <div class="flex flex-rows gap-12">
+        <sd-input type="text" size="lg" label="Label" help-text="Help text"></sd-input>
+        <sd-input type="text" size="lg" label="Label">
+          <div slot="help-text" class="text-lg">Help text</div>
+        </sd-input>
       </div>
     `;
   }
 };
 
 /**
- * Use the `placeholder` attribute to add a placeholder.
+ * Use the `required` attribute to mark the element as required. This can be used for form validation purposes.
  */
 
-export const Placeholders = {
-  args: overrideArgs([{ type: 'attribute', name: 'placeholder', value: 'Type something' }]),
-  render: (args: any) => {
-    return html`
-      <div class="w-[250px]">
-        ${generateTemplate({
-          args
-        })}
-      </div>
-    `;
-  }
+export const Required = {
+  render: () =>
+    html`<div class="w-[250px]">
+      <sd-input type="text" label="Label" required></sd-input>
+    </div>`
+};
+
+/**
+ * The component gets `valid` state when the input is valid.
+ * Use the `style-on-valid` attribute to automatically indicate and show a valid state.
+ */
+
+export const Valid = {
+  render: () =>
+    html`<div class="w-[250px]">
+      <sd-input type="text" label="Label" style-on-valid></sd-input>
+    </div>`
 };
 
 /**
@@ -165,134 +185,6 @@ export const TogglePassword = {
         })}
       </div>
     `;
-  }
-};
-
-/**
- * Use the disabled attribute to disable an input. All interaction is disabled and no events will be fired.
- */
-
-export const Disabled = {
-  parameters: {
-    controls: {
-      exclude: ['disabled']
-    }
-  },
-  args: overrideArgs([
-    { type: 'attribute', name: 'value', value: 'value' },
-    { type: 'attribute', name: 'label', value: 'Label' },
-    { type: 'attribute', name: 'help-text', value: 'help-text' }
-  ]),
-  render: (args: any) => {
-    return html`
-      <div class="w-[250px]">
-        ${generateTemplate({
-          constants: [{ type: 'attribute', name: 'disabled', value: true }],
-          args
-        })}
-      </div>
-    `;
-  }
-};
-
-/**
- * Use the readonly attribute to render an input as readonly.  Interaction is enabled, but the input cannot be edited.  Events will be fired.
- */
-
-export const Readonly = {
-  parameters: {
-    controls: {
-      exclude: ['readonly']
-    }
-  },
-  args: overrideArgs([
-    { type: 'attribute', name: 'value', value: 'value' },
-    { type: 'attribute', name: 'label', value: 'Label' },
-    { type: 'attribute', name: 'help-text', value: 'help-text' }
-  ]),
-  render: (args: any) => {
-    return html`
-      <div class="w-[250px]">
-        ${generateTemplate({
-          constants: [{ type: 'attribute', name: 'readonly', value: true }],
-          args
-        })}
-      </div>
-    `;
-  }
-};
-
-/**
- * This shows sd-input in its various sizes.
- */
-
-export const Sizes = {
-  parameters: {
-    controls: {
-      exclude: ['size']
-    }
-  },
-  args: overrideArgs([
-    { type: 'attribute', name: 'value', value: 'value' },
-    { type: 'attribute', name: 'label', value: 'Label' },
-    { type: 'attribute', name: 'help-text', value: 'help-text' },
-    { type: 'attribute', name: 'clearable', value: true },
-    {
-      type: 'slot',
-      name: 'right',
-      value: '<sd-icon slot="right" library="global-resources" name="system/picture"></sd-icon>'
-    }
-  ]),
-  render: (args: any) => {
-    return generateTemplate({
-      axis: {
-        y: { type: 'attribute', name: 'size' }
-      },
-      args
-    });
-  }
-};
-
-/**
- * Per default the input will indicate an error state when the input is invalid. Use the `style-on-valid` attribute to indicate a valid state as well.
- */
-
-export const StyleOnValid = {
-  parameters: {
-    controls: {
-      exclude: ['style-on-valid']
-    }
-  },
-  args: overrideArgs([
-    { type: 'attribute', name: 'value', value: 'valu' },
-    { type: 'attribute', name: 'label', value: 'Label' },
-    { type: 'attribute', name: 'help-text', value: 'help-text' },
-    { type: 'attribute', name: 'clearable', value: true },
-    {
-      type: 'slot',
-      name: 'right',
-      value: '<sd-icon slot="right" library="global-resources" name="system/picture"></sd-icon>'
-    }
-  ]),
-  render: (args: any) => {
-    return generateTemplate({
-      axis: {
-        y: { type: 'attribute', name: 'style-on-valid' }
-      },
-      args
-    });
-  },
-
-  play: async ({ canvasElement }: { canvasElement: HTMLUnknownElement }) => {
-    const els = canvasElement.querySelectorAll('sd-input');
-
-    for (const el of els) {
-      await waitUntil(() => el?.shadowRoot?.querySelector('input'));
-      await userEvent.type(el.shadowRoot!.querySelector('input')!, 'e');
-    }
-
-    // tab to next element to loose focus
-    await userEvent.tab();
   }
 };
 
