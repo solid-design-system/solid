8e61f30e26a25526d791925c8c2972b6f895a829
Sergio Fonseca <sergio.fonseca@virtual-identity.com>
2024-08-28 19:27:30 +0100

    feat: address pr comments

--- packages/components/src/components/notification/notification.stories.ts
+++ packages/components/src/components/notification/notification.stories.ts
@@ -12,7 +12,7 @@ const { overrideArgs } = storybookHelpers('sd-notification');
  * Used to communicate important information or status to the user. Notifications can be displayed inline or as a toast / toast stack.
  *
  *  **Related templates**:
- * - [Toast Notification](?path=/docs/templates-toast-notification--docs)
+ * - [Notifications](?path=/docs/templates-notifications--docs)
  */
 
 export default {
@@ -60,6 +60,11 @@ export const Default = {
 
 /**
  * Use the `variant` attribute to change the theme of the notification.
+ *
+ * - `info`: suitable for notifications, conveying neutral information about an action.
+ * - `success`: imply a successful or positive outcome of an action.
+ * - `error`: indicate a destructive and irreversible outcome of an action.
+ * - `warning`: alert for possible issues or significant changes that must be considered.
  */
 
 export const Variants = {
@@ -140,4 +145,137 @@ export const DurationIndicator = {
       });
     </script>
   `
+};
+
+/**
+ * Display a toast notification at the top-right of the screen by using the `toast` method. Click on the **Show code** button to see the JavaScript code responsible for generating the toast notification. The default position is `top-right`.
+ */
+export const ToastNotification = {
+  name: 'Toast Notification',
+  render: (_args: Record<string, any>) => {
+    return html`
+      <div class="flex gap-2">
+        <sd-button
+          variant="secondary"
+          data-notification-type="info"
+          data-notification-position="top-right"
+          class="w-24"
+        >
+          Info
+        </sd-button>
+        <sd-button
+          variant="secondary"
+          data-notification-type="success"
+          data-notification-position="top-right"
+          class="w-24"
+        >
+          Success
+        </sd-button>
+        <sd-button
+          variant="secondary"
+          data-notification-type="warning"
+          data-notification-position="top-right"
+          class="w-24"
+          >Warning</sd-button
+        >
+        <sd-button
+          variant="secondary"
+          data-notification-type="error"
+          data-notification-position="top-right"
+          class="w-24"
+          >Error</sd-button
+        >
+      </div>
+      <script>
+        var buttons = document.querySelectorAll('[data-notification-position="top-right"]');
+
+        function notify(variant = 'info') {
+          const notification = Object.assign(document.createElement('sd-notification'), {
+            closable: true,
+            variant: variant,
+            toastStack: 'top-right',
+            duration: 5000,
+            innerHTML: 'Lorem ipsum dolor sit amet.'
+          });
+
+          document.body.append(notification);
+          return notification.toast();
+        }
+
+        buttons.forEach(button => {
+          button.addEventListener('click', () => {
+            notify(button.getAttribute('data-notification-type'));
+          });
+        });
+      </script>
+    `;
+  }
+};
+
+/**
+ * Display a toast notification positioned at the bottom-center of the screen by setting the `toastStack` attribute to `bottom-center`. Click on the **Show code** button to see the JavaScript code responsible for generating the toast notification.
+ */
+export const ToastBottomCenter = {
+  name: 'Toast Notification (Bottom Center)',
+  render: (_args: Record<string, any>) => {
+    return html`
+      <div class="flex gap-2">
+        <sd-button
+          variant="secondary"
+          data-notification-type="info"
+          data-notification-position="bottom-center"
+          class="w-24"
+        >
+          Info
+        </sd-button>
+        <sd-button
+          variant="secondary"
+          data-notification-type="success"
+          data-notification-position="bottom-center"
+          class="w-24"
+        >
+          Success
+        </sd-button>
+        <sd-button
+          variant="secondary"
+          data-notification-type="warning"
+          data-notification-position="bottom-center"
+          class="w-24"
+          >Warning</sd-button
+        >
+        <sd-button
+          variant="secondary"
+          data-notification-type="error"
+          data-notification-position="bottom-center"
+          class="w-24"
+          >Error</sd-button
+        >
+      </div>
+
+      <script>
+        var buttons = document.querySelectorAll('[data-notification-position="bottom-center"]');
+
+        function notifyBottomCenter(variant = 'info') {
+          const notification = Object.assign(document.createElement('sd-notification'), {
+            closable: true,
+            variant: variant,
+            toastStack: 'bottom-center',
+            duration: Infinity,
+            innerHTML: 'Lorem ipsum dolor sit amet.'
+          });
+
+          notification.style.width = '250px';
+
+          document.body.append(notification);
+          return notification.toast();
+        }
+
+        buttons.forEach(button => {
+          button.addEventListener('click', () => {
+            notifyBottomCenter(button.getAttribute('data-notification-type'));
+          });
+        });
+      </script>
+    `;
+  }
 };
