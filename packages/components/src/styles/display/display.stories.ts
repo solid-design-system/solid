59bb22b5c683431496615d7c7ae95defd17ae9ba
auroraVasconcelos <aurora.vasconcelos@virtual-identity.com>
2024-09-05 17:04:50 +0100

    chore: add new template

--- packages/components/src/styles/display/display.stories.ts
+++ packages/components/src/styles/display/display.stories.ts
@@ -8,6 +8,9 @@ const { generateTemplate } = storybookTemplate('sd-display');
 
 /**
  * Used to provide larger text sizes that are not used as headlines.
+ *
+ * **Related templates**:
+ * - [Headline, Display and Leadtext with Mark](?path=/docs/templates-headline-display-and-leadtext-with-mark--docs)
  */
 
 export default {
@@ -36,7 +39,7 @@ export const Default = {
 
 /**
  * Use `sd-display` modifiers for alternative appearances.
- * - `sd-display--size-default`
+ * - Extra large is the default display size
  * - `sd-display--size-3xl`
  * - `sd-display--size-4xl`
  */
@@ -46,7 +49,7 @@ export const Size = {
     <div class="flex flex-col gap-6">
       <div class="sd-display sd-display--size-4xl">Lorem ipsum</div>
       <div class="sd-display sd-display--size-3xl">Lorem ipsum</div>
-      <div class="sd-display sd-display--size-default">Lorem ipsum</div>
+      <div class="sd-display">Lorem ipsum</div>
     </div>
   `
 };
