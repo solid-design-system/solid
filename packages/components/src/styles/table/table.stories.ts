99ed9023fc09495a7d35e6c4dea707eb51606b36
Sergio Fonseca <sergio.fonseca@virtual-identity.com>
2024-09-06 11:48:01 +0100

    feat: rewrite sd-table documentation

--- packages/components/src/styles/table/table.stories.ts
+++ packages/components/src/styles/table/table.stories.ts
@@ -1,4 +1,5 @@
 import '../../solid-components';
+import { html } from 'lit-html';
 import { storybookDefaults, storybookHelpers, storybookTemplate } from '../../../scripts/storybook/helper';
 
 const { argTypes, parameters } = storybookDefaults('sd-table');
@@ -6,24 +7,47 @@ const { overrideArgs } = storybookHelpers('sd-table');
 const { generateTemplate } = storybookTemplate('sd-table');
 
 /**
- * The `sd-table` component resets the styles of a table to predefined values. To style table cells use the `sd-table-cell` component.
+ * Used to reset the styles of a table to predefined values. To style table cells use the `sd-table-cell` component.
+ *
+ * **Related components**:
+ * - [sd-table-cell](?path=/docs/styles-sd-table-cell--docs)
  */
 
 export default {
   title: 'Styles/sd-table',
+  tags: ['!dev'],
   component: 'sd-table',
   parameters: {
     ...parameters
   },
-  args: overrideArgs({ type: 'slot', name: 'default', value: 'Lorem ipsum dolor sit amet.' }),
+  args: overrideArgs({
+    type: 'slot',
+    name: 'default',
+    value: 'Lorem ipsum dolor sit amet.'
+  }),
   argTypes
 };
 
 export const Default = {
   render: (args: any) => {
     return generateTemplate({
-      options: { templateContent: '<table class="%CLASSES%"><tr><td>%SLOT%</td></tr></table>' },
+      options: { templateContent: '<table class="%CLASSES% w-full"><tr><td>%SLOT%</td></tr></table>' },
       args
     });
   }
+};
+
+/**
+ * Use `default` slot to add content to the table.
+ */
+export const Slot = {
+  render: () => html`
+    <table class="sd-table w-full">
+      <tbody>
+        <tr>
+          <td>Lorem ipsum dolor sit amet.</td>
+        </tr>
+      </tbody>
+    </table>
+  `
 };
