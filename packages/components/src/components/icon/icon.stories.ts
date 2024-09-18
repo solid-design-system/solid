61da923f6cb52a20f83ed02c88a32c94b930372f
mariohamann <mario.hamann@virtual-identity.com>
2024-09-18 09:27:32 +0200

    update icon docs

--- packages/components/src/components/icon/icon.stories.ts
+++ packages/components/src/components/icon/icon.stories.ts
@@ -1,5 +1,5 @@
 import '../../solid-components';
-import { icons } from './library.system';
+import { html } from 'lit-html';
 import { storybookDefaults, storybookHelpers, storybookTemplate } from '../../../scripts/storybook/helper';
 import { withActions } from '@storybook/addon-actions/decorator';
 
@@ -8,26 +8,123 @@ const { overrideArgs } = storybookHelpers('sd-icon');
 const { generateTemplate } = storybookTemplate('sd-icon');
 
 /**
- * ## Disclaimer
- * The `sd-icon` component does not contain any icons itself. It only provides a technical way to implement and use icons from a specfic library (see below).
- * All icons displayed here, are provided by the brand departement and requests towards the icons itself need to be addressed accordingly.
- * In order not to depend on any external library, the SDS-Library includes a small stack of "built-in" icons (e.g. close, chevron etc), which are maintained by the SDS team.
+ * **Used to display icons.**
  *
- * ## Colors
- * Per default icons inherit their color from the current text color. Thus, you can set the color property on the <sd-icon> element or an ancestor to change the color.
+ * Icons can be used to indicate an action or to represent content.
  *
- * ## Sizes
- * Icons are sized relative to the current font size. To change their size, set the font-size property on the icon itself or on a parent element as shown below.
+ */
+
+export default {
+  title: 'Components/sd-icon',
+  component: 'sd-icon',
+  args: overrideArgs([{ name: 'name', type: 'attribute', value: 'union-investment/content/image' }], args),
+  argTypes,
+  parameters: { ...parameters },
+  decorators: [withActions] as any
+};
+
+export const Default = {
+  render: (args: any) => {
+    return generateTemplate({ args });
+  }
+};
+
+/**
+ * Use the `name` attribute to change the icon. Checkout these lists of Union Investment's icons for available names:
+ * - [Content Icons](?path=/story/components-sd-icon-default--library-default-content)
+ * - [System Icons](?path=/story/components-sd-icon-default--library-default-system)
+ */
+
+export const Name = {
+  render: () => {
+    return html`
+      <div class="flex flex-col gap-12">
+        <div class="flex gap-12">
+          <sd-icon name="union-investment/content/image" label="Simplified picture frame"></sd-icon>
+          <sd-icon name="union-investment/system/image" label="Very simplified picture frame"></sd-icon>
+        </div>
+        <div class="flex gap-12">
+          <sd-icon name="union-investment/content/alarm" label="Simplified alarm bell"></sd-icon>
+          <sd-icon name="union-investment/system/alarm" label="Very simplified alarm bell"></sd-icon>
+        </div>
+      </div>
+    `;
+  }
+};
+
+/**
+ * Use the `label` attribute to announce non-decorative icons to assistive devices.
+ */
+
+export const Label = {
+  render: () => {
+    return html`
+      <div class="flex gap-2">
+        <sd-button variant="tertiary" size="sm"
+          ><sd-icon name="union-investment/system/arrow-left" label="Back"></sd-icon
+        ></sd-button>
+        <sd-button variant="tertiary" size="sm"
+          ><sd-icon name="union-investment/system/arrow-right" label="Next"></sd-icon
+        ></sd-button>
+      </div>
+    `;
+  }
+};
+
+/**
+ * Use the `color` attribute to change an icon’s size
  *
- * ## Libraries
- * You can register additional icons to use with the <sd-icon> component through icon libraries. Icon files can exist locally or on a CORS-enabled endpoint (e.g. a CDN). There is no limit to how many icon libraries you can register and there is no cost associated with registering them, as individual icons are only requested when they're used.
- * Solid will ship with two built-in icon libraries, default and system. The default icon library contains all of the icons by Union Investment's Design System. The system icon library contains only a small subset of icons that are used internally by Solid components.
+ * - `currentColor` (default): Inherits the current text color, which makes it easy to style icons via CSS.
+ * - `primary`: Sets the color to the primary color.
+ * - `white`: Sets the color to white.
+ */
+
+export const Color = {
+  render: () => {
+    return html`
+      <div class="flex gap-6">
+        <sd-icon class="p-2" name="union-investment/content/image" color="currentColor"></sd-icon>
+        <sd-icon class="p-2" name="union-investment/content/image" color="primary"></sd-icon>
+        <div class="p-2 bg-primary">
+          <sd-icon name="union-investment/content/image" color="white"></sd-icon>
+        </div>
+      </div>
+    `;
+  }
+};
+
+/**
+ * Icons are sized relative to the current font size. To change their size, set the `font-size` property on the icon itself or on a parent element.
+ */
+
+export const Size = {
+  render: () => {
+    return html`
+      <div class="flex gap-6">
+        <sd-icon class="text-sm" name="union-investment/content/image" label="Small picture frame"></sd-icon>
+        <sd-icon name="union-investment/content/image" label="Picture frame which inherits the size"></sd-icon>
+        <sd-icon class="text-xl" name="union-investment/content/image" label="Extra large picture frame"></sd-icon>
+        <sd-icon
+          style="font-size: 36px"
+          name="union-investment/content/image"
+          label="Even larger picture frame"
+        ></sd-icon>
+      </div>
+    `;
+  }
+};
+
+/**
+ * You can register additional icons to use with the `<sd-icon>` component through icon libraries. Icon files can exist locally or on a CORS-enabled endpoint (e.g. a CDN). There is no limit to how many icon libraries you can register and there is no cost associated with registering them, as individual icons are only requested when they're used.
+ * Solid ships with two built-in icon libraries, `default` and `system`:
+ * - `default`: The `default` icon library refers to the official CDN by Union Investment which is fed by Celum. It is provided by the brand departement, therefore requests towards the icons itself need to be addressed accordingly.
+ * - `system`: They icons are an integrated library of the Solid Components to ensure they're always available. They are a subset of Union Investment's official icons. As names and visuals may change over time, system icons should NOT be used directly.
  *
  * To register an additional icon library, use the `registerIconLibrary()` function that's exported from `utilities/icon-library.js`. At a minimum, you must provide a name and a resolver function. The resolver function translates an icon name to a URL where the corresponding SVG file exists. Refer to the examples below to better understand how it works.
  *
  * If necessary, a mutator function can be used to mutate the SVG element before rendering. This is necessary for some libraries due to the many possible ways SVGs are crafted. For example, icons should ideally inherit the current text color via currentColor, so you may need to apply fill="currentColor or stroke="currentColor" to the SVG element using this function.
  *
- * Here's an example that registers an icon library located in the /assets/icons directory.
+ * Here's an example that registers an icon library located in the `/assets/icons` directory.
  *
  * ```html
  * <script type="module">
@@ -77,244 +174,83 @@ const { generateTemplate } = storybookTemplate('sd-icon');
  * ```
  *
  * If an icon is used before registration occurs, it will be empty initially but shown when registered.
- * Check out the examples below or the [Shoelace Docs](https://shoelace.style/components/icon?id=icon-libraries)
- * to see how to handle this.
+ *
+ * The following examples demonstrate how to register icons from Union Investment's Global Resources CDN. Checkout [Shoelace's Docs](https://shoelace.style/components/icon?id=icon-libraries)
+ * to examples of popular, open source icon libraries.
  */
 
-export default {
-  title: 'Components/sd-icon',
-  component: 'sd-icon',
-  args: overrideArgs([{ name: 'name', type: 'attribute', value: 'union-investment/content/image' }], args),
-  argTypes,
-  parameters: { ...parameters },
-  decorators: [withActions] as any
-};
+export const IconLibraries = {
+  render: () => {
+    return html`
+      <sd-icon library="global-resources" name="system/picture"> </sd-icon>
 
-/**
- * Default: This shows the sd-icon in its default state.
- */
+      <script type="module">
+        // ESM:
+        // import { registerIconLibrary } from '@solid-design-system/components/unversioned/icon/library';
 
-export const Default = {
-  render: (args: any) => {
-    return generateTemplate({ args });
+        // UMD:
+        // const { registerIconLibrary } = window['SolidComponents'];
+
+        // preview-ignore:start
+        // Note on this script:
+        // - It's actual registration happens in preview.js, here it is replicated for the code preview.
+        // - To prevent errors, the registration is wrapped in a hidden conditional statement that is always false.
+        if (false) {
+          // preview-ignore:end
+          registerIconLibrary('global-resources', {
+            resolver: name => {
+              // split path and name
+              let path = name.split('/');
+              let iconName = path.pop();
+
+              // "system" and "system/colored" should both resolve to "system/colored", same for "content"
+              if (path.length === 1) {
+                path.push('colored');
+              }
+
+              return (
+                'https://global-resources.fe.union-investment.de/latest/scripts/services/svg/icons/' +
+                path.join('/') +
+                '/' +
+                iconName +
+                '.svg'
+              );
+            },
+
+            // We need currentColor as the main color for the icons
+            mutator: svg => {
+              console.log(svg, 'hello');
+              const recoloredElements = {};
+              recoloredElements['currentColorFills'] = svg.querySelectorAll('[fill="#00358e"], [fill="#fff"]');
+              recoloredElements['currentColorStrokes'] = svg.querySelectorAll('[stroke="#00358e"], [stroke="#fff"]');
+              recoloredElements['greenFills'] = svg.querySelectorAll('[fill="#43b02a"]');
+              recoloredElements['greenStrokes'] = svg.querySelectorAll('[stroke="#43b02a"]');
+
+              recoloredElements.currentColorFills.forEach(filledElement => {
+                filledElement.setAttribute('fill', 'currentColor');
+              });
+
+              recoloredElements.currentColorStrokes.forEach(strokedElement => {
+                strokedElement.setAttribute('stroke', 'currentColor');
+              });
+
+              recoloredElements.greenFills.forEach(filledElement => {
+                filledElement.setAttribute('fill', 'rgb(var(--sd-color-accent, 45 157 0) / var(--tw-bg-opacity, 1))');
+              });
+
+              recoloredElements.greenStrokes.forEach(strokedElement => {
+                strokedElement.setAttribute(
+                  'stroke',
+                  'rgb(var(--sd-color-accent, 45 157 0) / var(--tw-bg-opacity, 1))'
+                );
+              });
+              return svg;
+            }
+          });
+          // preview-ignore:start
+        }
+        // preview-ignore:end
+      </script>
+    `;
   }
-};
-
-/**
- * Default icons refer to the official CDN by Union Investment which is fed by Celum.
- * They are automatically altered to support theming.
- *
- * Use the `name` attribute to select the correct icon, e. g. `union-investment/content/{id}` to select the icon with the technicalId `{id}` in the `union-investment` folder.
- * As `content` and `system` icons are the most common ones, you can omit the `union-investment` part and just use `content/{id}` or `system/{id}`.
- */
-
-export const LibraryDefault = {
-  name: 'Library: default',
-  parameters: {
-    controls: { exclude: ['name', 'library'] },
-    chromatic: { disableSnapshot: true }
-  },
-  render: (args: any) =>
-    generateTemplate({
-      axis: {
-        x: {
-          type: 'attribute',
-          name: 'color'
-        },
-        y: {
-          type: 'attribute',
-          name: 'name',
-          values: ['content/image', 'union-investment/content/image', 'system/image', 'union-investment/system/image']
-        }
-      },
-      constants: [{ type: 'attribute', name: 'library', value: '' }],
-      options: {
-        templateBackgrounds: { alternate: 'x', colors: ['white', 'white', 'rgb(var(--sd-color-primary, 0 53 142))'] }
-      },
-      args
-    })
-}; /**
- * System icons are an integrated library of the Solid Components to ensure they're always available.
- * They are a subset of Union Investment's official icons. As these may change over time, system icons should NOT be used directly.
- *
- * The story below shows all available icons.
- */
-
-export const LibrarySystem = {
-  name: 'Library: system',
-  parameters: { controls: { exclude: ['name', 'library'] } },
-  render: (args: any) =>
-    generateTemplate({
-      axis: {
-        x: {
-          type: 'attribute',
-          name: 'color'
-        },
-        y: {
-          type: 'attribute',
-          name: 'name',
-          values: Object.keys(icons)
-        }
-      },
-      constants: [
-        { type: 'attribute', name: 'library', value: 'system' },
-        { type: 'attribute', name: 'name', value: 'check' }
-      ],
-      options: {
-        templateBackgrounds: { alternate: 'x', colors: ['white', 'white', 'rgb(var(--sd-color-primary, 0 53 142))'] }
-      },
-      args
-    })
-};
-
-/**
- * The following resolver allows it to fetch data from the global-resources CDN. It points to the latest branch.
- *
- * ```js
- *  import { registerIconLibrary } from '@solid-design-system/components/unversioned/icon/library';
- *
- *  registerIconLibrary('global-resources', {
- *   resolver: name => {
- *     // split path and name
- *     let path = name.split('/');
- *     let iconName = path.pop();
- *
- *     // "system" and "system/colored" should both resolve to "system/colored", same for "content"
- *     if (path.length === 1) {
- *       path.push('colored');
- *     }
- *
- *     return `https://global-resources.fe.union-investment.de/latest/scripts/services/svg/icons/${path.join(
- *       '/'
- *     )}/${iconName}.svg`;
- *   },
- *
- *   // We need currentColor as the main color for the icons
- *   mutator: svg => {
- *     const recoloredElements = {};
- *     recoloredElements['currentColorFills'] = svg.querySelectorAll('[fill="rgb(var(--sd-color-primary, 0 53 142))"], [fill="#fff"]');
- *     recoloredElements['currentColorStrokes'] = svg.querySelectorAll('[stroke="rgb(var(--sd-color-primary, 0 53 142))"], [stroke="#fff"]');
- *     recoloredElements['greenFills'] = svg.querySelectorAll('[fill="#43b02a"]');
- *     recoloredElements['greenStrokes'] = svg.querySelectorAll('[stroke="#43b02a"]');
- *
- *     recoloredElements.currentColorFills.forEach(filledElement => {
- *       filledElement.setAttribute('fill', 'currentColor');
- *     });
- *
- *     recoloredElements.currentColorStrokes.forEach(strokedElement => {
- *       strokedElement.setAttribute('stroke', 'currentColor');
- *     });
- *
- *     recoloredElements.greenFills.forEach(filledElement => {
- *       filledElement.setAttribute('fill', 'rgb(var(--sd-color-accent, 45 157 0) / var(--tw-bg-opacity, 1))');
- *     });
- *
- *     recoloredElements.greenStrokes.forEach(strokedElement => {
- *       strokedElement.setAttribute('stroke', 'rgb(var(--sd-color-accent, 45 157 0) / var(--tw-bg-opacity, 1))');
- *     });
- *     return svg;
- *   }
- * });
- * ```
- *
- * You can now use the component like this:
- *
- * ```html
- * <sd-icon name="system/alarm" library="global-resources"></sd-icon>
- * <sd-icon name="content/alarm" library="global-resources"></sd-icon>
- * ```
- */
-
-export const ExampleGlobalResources = {
-  name: 'Example: global-resources',
-  parameters: {
-    controls: { exclude: ['name', 'library'] }
-  },
-  render: (args: any) =>
-    generateTemplate({
-      axis: {
-        x: {
-          type: 'attribute',
-          name: 'name',
-          values: ['system/picture', 'content/picture']
-        },
-        y: {
-          type: 'attribute',
-          name: 'color'
-        }
-      },
-      constants: [{ type: 'attribute', name: 'library', value: 'global-resources' }],
-      options: {
-        templateBackgrounds: { alternate: 'y', colors: ['white', 'white', 'rgb(var(--sd-color-primary, 0 53 142))'] }
-      },
-      args
-    })
-};
-
-/**
- * The following resolver allows it to fetch data from the global-resources CDN, but overrides given paths and names.
- * This is especially useful if you want to override icons which are baked into components.
- *
- * ```js
- *  import { registerIconLibrary } from '@solid-design-system/components/unversioned/icon/library';
- *
- * registerIconLibrary('global-resources-overriden', {
- *   resolver: name => {
- *     // split path and name
- *     let path = name.split('/');
- *     let iconName = path.pop();
- *
- *     // "system" and "system/colored" should both resolve to "system/colored", same for "content"
- *     if (path.length === 1) {
- *       path.push('colored');
- *     }
- *
- *     // Override icon names which are baked into components
- *     if (path[0] === 'system') {
- *       iconName =
- *         {
- *           alarm: 'wecker'
- *         }[iconName] || iconName;
- *     } else if (path[0] === 'content') {
- *       iconName =
- *         {
- *           letter: 'korrespondenz'
- *         }[iconName] || iconName;
- *     }
- *
- *     return `https://global-resources.fe.union-investment.de/latest/scripts/services/svg/attrax-icons/${path.join(
- *       '/'
- *     )}/${iconName}.svg`;
- *   },
- *   // We need currentColor as the main color for the icons
- *   mutator: svg => svg.setAttribute('fill', 'currentColor')
- * });
- *
- * ```
- */
-
-export const ExampleGlobalResourcesOverriden = {
-  name: 'Example: global-resources (overriden)',
-  parameters: {
-    controls: { exclude: ['name', 'library'] }
-  },
-  render: (args: any) =>
-    generateTemplate({
-      axis: {
-        x: {
-          type: 'attribute',
-          name: 'name',
-          values: ['system/picture', 'content/picture']
-        },
-        y: {
-          type: 'attribute',
-          name: 'color'
-        }
-      },
-      constants: [{ type: 'attribute', name: 'library', value: 'global-resources-overriden' }],
-      options: {
-        templateBackgrounds: { alternate: 'y', colors: ['white', 'white', 'rgb(var(--sd-color-primary, 0 53 142))'] }
-      },
-      args
-    })
 };
