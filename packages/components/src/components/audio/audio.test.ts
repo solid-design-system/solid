84a5040c6449be34fed92332cc08cb3e9cea4197
Sergio Fonseca <sergio.fonseca@virtual-identity.com>
2024-09-24 17:27:30 +0100

    feat: update tests

--- packages/components/src/components/audio/audio.test.ts
+++ packages/components/src/components/audio/audio.test.ts
@@ -1,4 +1,4 @@
-import { expect, fixture, html, waitUntil } from '@open-wc/testing';
+import { expect, fixture, html } from '@open-wc/testing';
 import sinon from 'sinon';
 import type SdAudio from './audio';
 
@@ -9,7 +9,7 @@ describe('<sd-audio>', () => {
     expect(el).to.exist;
   });
 
-  it('should emit sd-playback-start and sd-playback-end events when play and pause buttons are clicked', async () => {
+  it('should emit sd-playback-start event when play buttons is clicked', async () => {
     const el = await fixture<SdAudio>(html`
       <sd-audio>
         <audio slot="default" src="test-audio.mp3"></audio>
@@ -18,42 +18,36 @@ describe('<sd-audio>', () => {
 
     await el.updateComplete;
 
-    const playButton = el.shadowRoot!.querySelector<HTMLButtonElement>('[part="play-button"]')!;
-    const slotElement = el.shadowRoot!.querySelector<HTMLSlotElement>('slot[name="default"]')!;
-    const assignedElements = slotElement.assignedElements();
-    const audioElement = assignedElements.length > 0 ? (assignedElements[0] as HTMLAudioElement) : null;
+    const playButton = el.shadowRoot!.querySelector('[part="play-button"]')!;
+    const spy = sinon.spy();
+    el.addEventListener('sd-playback-start', spy);
 
-    expect(audioElement).to.exist;
+    (playButton as HTMLElement).click();
 
-    const playbackStartSpy = sinon.spy();
-    const playbackEndSpy = sinon.spy();
-    el.addEventListener('sd-playback-start', playbackStartSpy);
-    el.addEventListener('sd-playback-end', playbackEndSpy);
-
-    // Mock play and pause methods
-    const mockPlay = sinon.stub(audioElement!, 'play').callsFake(async () => {
-      el.isPlaying = true;
-      audioElement!.dispatchEvent(new Event('play'));
-      return Promise.resolve();
-    });
-    const mockPause = sinon.stub(audioElement!, 'pause').callsFake(() => {
-      el.isPlaying = false;
-      audioElement!.dispatchEvent(new Event('pause'));
-    });
-
-    playButton.click();
-    await waitUntil(() => playbackStartSpy.calledOnce, 'sd-playback-start should be emitted', { timeout: 2000 });
-    expect(playbackStartSpy.calledOnce).to.be.true;
-
-    audioElement!.dispatchEvent(new Event('ended'));
-    await waitUntil(() => playbackEndSpy.calledOnce, 'sd-playback-end should be emitted', { timeout: 2000 });
-    expect(playbackEndSpy.calledOnce).to.be.true;
-
-    mockPlay.restore();
-    mockPause.restore();
+    expect(spy.calledOnce).to.be.true;
   });
 
-  it('should update the current time as audio plays', async () => {
+  // it('should update the current time as audio plays', async () => {
+  //   const el = await fixture<SdAudio>(html`
+  //     <sd-audio>
+  //       <audio slot="default" src="test-audio.mp3"></audio>
+  //     </sd-audio>
+  //   `);
+
+  //   await el.updateComplete;
+
+  //   const audio = el.querySelector('audio')!;
+  //   audio.currentTime = 0;
+
+  //   const playButton = el.shadowRoot!.querySelector('[part="play-button"]')!;
+  //   (playButton as HTMLElement).click();
+
+  //   await waitUntil(() => audio.currentTime > 0);
+
+  //   expect(audio.currentTime).to.be.above(0);
+  // });
+
+  it('should emit sd-playback-mute event when mute button is clicked', async () => {
     const el = await fixture<SdAudio>(html`
       <sd-audio>
         <audio slot="default" src="test-audio.mp3"></audio>
@@ -62,21 +56,40 @@ describe('<sd-audio>', () => {
 
     await el.updateComplete;
 
-    const slotElement = el.shadowRoot!.querySelector<HTMLSlotElement>('slot[name="default"]')!;
-    const assignedElements = slotElement.assignedElements();
-    const audioElement = assignedElements.length > 0 ? (assignedElements[0] as HTMLAudioElement) : null;
+    const muteButton = el.shadowRoot!.querySelector('[part="volume"]')!;
+    const spy = sinon.spy();
+    el.addEventListener('sd-playback-mute', spy);
 
-    expect(audioElement).to.exist;
+    (muteButton as HTMLElement).click();
 
-    sinon.stub(audioElement!, 'currentTime').get(() => 50);
-
-    // dispatch a timeupdate event to simulate the audio playing
-    audioElement!.dispatchEvent(new Event('timeupdate'));
-
-    expect(el.currentTime).to.equal('0:50');
+    expect(spy.calledOnce).to.be.true;
   });
 
-  it('should emit sd-transcript-click event when transcript icon is clicked', async () => {
+  it('should hide the timestamps when the attribute is set', async () => {
+    const el = await fixture<SdAudio>(html`
+      <sd-audio hide-timestamps>
+        <audio slot="default" src="test-audio.mp3"></audio>
+      </sd-audio>
+    `);
+
+    await el.updateComplete;
+
+    expect(el.shadowRoot!.querySelector('[part="timestamps"]')).to.be.null;
+  });
+
+  it('should render the canvas when the animated attribute is set', async () => {
+    const el = await fixture<SdAudio>(html`
+      <sd-audio animated>
+        <audio slot="default" src="test-audio.mp3"></audio>
+      </sd-audio>
+    `);
+
+    await el.updateComplete;
+
+    expect(el.shadowRoot!.querySelector('canvas')).to.exist;
+  });
+
+  it('should emit sd-transcript-click event when transcript button is clicked', async () => {
     const el = await fixture<SdAudio>(html`
       <sd-audio>
         <audio src="test-audio.mp3"></audio>
@@ -86,46 +99,12 @@ describe('<sd-audio>', () => {
 
     await el.updateComplete;
 
-    const transcriptIcon = el.shadowRoot!.querySelector('sd-icon[name="transcript"]')!;
+    const transcriptButton = el.shadowRoot!.querySelector('[name="transcript"]')!;
     const spy = sinon.spy();
     el.addEventListener('sd-transcript-click', spy);
 
-    (transcriptIcon as HTMLElement).click();
+    (transcriptButton as HTMLElement).click();
 
     expect(spy.calledOnce).to.be.true;
-  });
-
-  it('should handle volume slider changes', async () => {
-    const el = await fixture<SdAudio>(html`
-      <sd-audio>
-        <audio slot="default" src="test-audio.mp3"></audio>
-      </sd-audio>
-    `);
-
-    await el.updateComplete;
-
-    const slotElement = el.shadowRoot!.querySelector<HTMLSlotElement>('slot[name="default"]')!;
-    const assignedElements = slotElement.assignedElements();
-    const audioElement = assignedElements.length > 0 ? (assignedElements[0] as HTMLAudioElement) : null;
-
-    expect(audioElement).to.exist;
-
-    if (!audioElement) {
-      throw new Error('Audio element is not assigned');
-    }
-
-    const volumeSlider = el.shadowRoot!.querySelector<HTMLInputElement>('.audio-player__volume-slider')!;
-
-    expect(volumeSlider).to.exist;
-
-    audioElement.volume = 1;
-
-    volumeSlider.value = '50';
-    volumeSlider.dispatchEvent(new Event('click'));
-
-    // set a delay to allow the event to propagate
-    await new Promise(resolve => setTimeout(resolve, 0));
-
-    expect(audioElement.volume).to.equal(0.5);
   });
 });
