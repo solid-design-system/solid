aa62f93741682402bd8b6a05598545f28607274c
auroraVasconcelos <aurora.vasconcelos@virtual-identity.com>
2024-10-11 17:07:20 +0100

    chore: align quote with design

--- packages/components/src/templates/quote.stories.ts
+++ packages/components/src/templates/quote.stories.ts
@@ -21,58 +21,58 @@ export const Size = {
   render: () =>
     html` <p class="sd-display mb-6">size 4xl</p>
       <div class="text-left text-black mb-12">
-        <q class="text-3xl lg:text-4xl leading-tight"
-          >The environment is becoming increasingly challenging. Despite all the crises, the opportunities are greater
-          than the risks in the long term – this applies in particular to the growth areas of sustainability and
-          retirement pensions.</q
-        >
+        <q class="text-3xl lg:text-4xl leading-tight">
+          The Solid Design System by Union Investment provides a comprehensive toolkit for building consistent and
+          high-quality user interfaces. It includes well-documented components, design tokens, and accessibility
+          features, ensuring seamless integration across platforms.
+        </q>
         <div class="gap-6 flex items-center">
           <img
             class="w-16 h-16 rounded-full overflow-hidden object-cover"
-            src="./placeholders/images/family.jpg"
-            alt="Hans Joachim Reinke"
+            src="./placeholders/images/collaboration.jpg"
+            alt="Two men in business attire sitting on a sofa in a modern office, smiling and looking at a laptop screen."
           />
           <div>
-            <p class="sd-paragraph font-bold mb-1">Hans Joachim Reinke</p>
-            <p class="sd-paragraph sd-paragraph--size-sm">Chairman of the Board</p>
+            <p class="sd-paragraph font-bold mb-1">John Doe</p>
+            <p class="sd-paragraph sd-paragraph--size-sm">Team Member of SDS</p>
           </div>
         </div>
       </div>
       <p class="sd-display mb-6">size 3xl</p>
       <div class="text-left text-black mb-12">
-        <q class="block mb-4 text-2xl lg:text-3xl leading-tight"
-          >The environment is becoming increasingly challenging. Despite all the crises, the opportunities are greater
-          than the risks in the long term – this applies in particular to the growth areas of sustainability and
-          retirement pensions.</q
-        >
+        <q class="block mb-4 text-2xl lg:text-3xl leading-tight">
+          The Solid Design System by Union Investment provides a comprehensive toolkit for building consistent and
+          high-quality user interfaces. It includes well-documented components, design tokens, and accessibility
+          features, ensuring seamless integration across platforms.
+        </q>
         <div class="gap-6 flex items-center">
           <img
             class="w-16 h-16 rounded-full overflow-hidden object-cover"
-            src="./placeholders/images/family.jpg"
-            alt="Hans Joachim Reinke"
+            src="./placeholders/images/collaboration.jpg"
+            alt="Two men in business attire sitting on a sofa in a modern office, smiling and looking at a laptop screen."
           />
           <div>
-            <p class="sd-paragraph font-bold mb-1">Hans Joachim Reinke</p>
-            <p class="sd-paragraph sd-paragraph--size-sm">Chairman of the Board</p>
+            <p class="sd-paragraph font-bold mb-1">John Doe</p>
+            <p class="sd-paragraph sd-paragraph--size-sm">Team Member of SDS</p>
           </div>
         </div>
       </div>
       <p class="sd-display mb-6">size xl</p>
       <div class="text-left text-black">
-        <q class="block mb-4 text-xl leading-tight"
-          >The environment is becoming increasingly challenging. Despite all the crises, the opportunities are greater
-          than the risks in the long term – this applies in particular to the growth areas of sustainability and
-          retirement pensions.</q
-        >
+        <q class="block mb-4 text-xl leading-tight">
+          The Solid Design System by Union Investment provides a comprehensive toolkit for building consistent and
+          high-quality user interfaces. It includes well-documented components, design tokens, and accessibility
+          features, ensuring seamless integration across platforms.
+        </q>
         <div class="gap-4 flex items-center">
           <img
             class="w-16 h-16 rounded-full overflow-hidden object-cover"
-            src="./placeholders/images/family.jpg"
-            alt="Hans Joachim Reinke"
+            src="./placeholders/images/collaboration.jpg"
+            alt="Two men in business attire sitting on a sofa in a modern office, smiling and looking at a laptop screen."
           />
           <div>
-            <p class="sd-paragraph font-bold mb-1">Hans Joachim Reinke</p>
-            <p class="sd-paragraph sd-paragraph--size-sm">Chairman of the Board</p>
+            <p class="sd-paragraph font-bold mb-1">John Doe</p>
+            <p class="sd-paragraph sd-paragraph--size-sm">Team Member of SDS</p>
           </div>
         </div>
       </div>`
@@ -83,20 +83,20 @@ export const Inverted = {
   render: () =>
     html` <div class="flex items-end gap-5 p-5 bg-primary">
       <div class="text-left text-white">
-        <q class="block mb-6 text-3xl lg:text-4xl leading-tight"
-          >The environment is becoming increasingly challenging. Despite all the crises, the opportunities are greater
-          than the risks in the long term – this applies in particular to the growth areas of sustainability and
-          retirement pensions.</q
-        >
+        <q class="block mb-6 text-3xl lg:text-4xl leading-tight">
+          The Solid Design System by Union Investment provides a comprehensive toolkit for building consistent and
+          high-quality user interfaces. It includes well-documented components, design tokens, and accessibility
+          features, ensuring seamless integration across platforms.
+        </q>
         <div class="gap-6 flex items-center">
           <img
             class="w-16 h-16 rounded-full overflow-hidden object-cover"
-            src="./placeholders/images/family.jpg"
-            alt="Hans Joachim Reinke"
+            src="./placeholders/images/collaboration.jpg"
+            alt="Two men in business attire sitting on a sofa in a modern office, smiling and looking at a laptop screen."
           />
           <div>
-            <p class="sd-paragraph sd-paragraph--inverted font-bold mb-1">Hans Joachim Reinke</p>
-            <p class="sd-paragraph sd-paragraph--size-sm sd-paragraph--inverted">Chairman of the Board</p>
+            <p class="sd-paragraph sd-paragraph--inverted font-bold mb-1">John Doe</p>
+            <p class="sd-paragraph sd-paragraph--inverted sd-paragraph--size-sm">Team Member of SDS</p>
           </div>
         </div>
       </div>
@@ -107,14 +107,14 @@ export const QuoteWithoutProfileImage = {
   name: 'Quote without Profile Image',
   render: () =>
     html` <div class="text-left text-black">
-      <q class="block mb-6 text-3xl lg:text-4xl leading-tight"
-        >The environment is becoming increasingly challenging. Despite all the crises, the opportunities are greater
-        than the risks in the long term – this applies in particular to the growth areas of sustainability and
-        retirement pensions.</q
-      >
+      <q class="block mb-6 text-3xl lg:text-4xl leading-tight">
+        The Solid Design System by Union Investment provides a comprehensive toolkit for building consistent and
+        high-quality user interfaces. It includes well-documented components, design tokens, and accessibility features,
+        ensuring seamless integration across platforms.
+      </q>
       <div>
-        <p class="sd-paragraph font-bold mb-1">Hans Joachim Reinke</p>
-        <p class="sd-paragraph sd-paragraph--size-sm">Chairman of the Board</p>
+        <p class="sd-paragraph font-bold mb-1">John Doe</p>
+        <p class="sd-paragraph sd-paragraph--size-sm">Team Member of SDS</p>
       </div>
     </div>`
 };
