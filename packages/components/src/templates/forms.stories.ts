e4413e779ac1ef601cca1eb47e6d5f0cb55fa4b1
auroraVasconcelos <aurora.vasconcelos@virtual-identity.com>
2024-10-15 15:27:55 +0100

    fix: PR comments

--- packages/components/src/templates/forms.stories.ts
+++ packages/components/src/templates/forms.stories.ts
@@ -30,19 +30,18 @@ export const loginForm = {
     <section class="sd-container">
       <form id="loginForm">
         <h3 class="sd-headline sd-headline--size-3xl mb-8">Login for UnionFondsOnline users</h3>
-        <sd-input type="number" label="Access number" class="mb-4" autocomplete></sd-input>
+        <sd-input type="number" inputmode="numeric" label="Access number" class="mb-4"></sd-input>
         <sd-input
           type="password"
           label="Password"
-          password-toggle
-          spellcheck
           class="mb-4"
           autocomplete="current-password"
+          password-toggle
         ></sd-input>
         <sd-checkbox>Stay logged in on this computer</sd-checkbox>
         <div class="justify-between md:items-center md:flex-row mt-8 flex flex-col">
           <sd-link href="javascript:void(0)" class="mb-4 md:mb-0">Forgot password</sd-link>
-          <sd-button id="loginForm__submitButton">
+          <sd-button id="loginForm__submitButton" type="submit">
             Login
             <sd-icon name="system/lock-locked" slot="icon-left"></sd-icon>
           </sd-button>
@@ -56,37 +55,27 @@ export const contactForm = {
   render: () => html`
     <form class="sd-prose">
       <h3 class="sd-headline sd-headline--size-4xl">Contact</h3>
-      <sd-radio-group
-        id="inquiry-radio-group"
-        orientation="horizontal"
-        name="radio-group"
-        value="general_inquiry"
-        class="my-8"
-      >
-        <sd-radio name="inquiry-type" value="general_inquiry" onclick="toggleRegardingInput()">
-          General inquiry
-        </sd-radio>
-        <sd-radio name="inquiry-type" value="regarding" onclick="toggleRegardingInput()">Regarding</sd-radio>
+      <sd-radio-group id="inquiry-radio-group" orientation="horizontal" name="radio-group" value="general-inquiry">
+        <sd-radio name="inquiry-type" value="general-inquiry">General inquiry</sd-radio>
+        <sd-radio name="inquiry-type" value="regarding">Regarding</sd-radio>
       </sd-radio-group>
-      <div id="regarding-input" class="hidden">
-        <sd-input type="text" spellcheck></sd-input>
-      </div>
+      <sd-input type="text" inputmode="text" id="regarding-input" class="hidden" spellcheck></sd-input>
       <div class="flex flex-col gap-6">
-        <sd-select label="Salutation" placeholder="Please Select" max-options-visible="3">
+        <sd-select label="Salutation" placeholder="Please Select">
           <sd-option value="ms">Ms.</sd-option>
           <sd-option value="mrs">Mrs.</sd-option>
           <sd-option value="miss">Miss</sd-option>
           <sd-option value="mx">Mx.</sd-option>
         </sd-select>
         <div class="gap-6 flex flex-col md:flex-row">
-          <sd-input type="text" label="First name" spellcheck></sd-input>
-          <sd-input type="text" label="Last name" spellcheck required></sd-input>
+          <sd-input type="text" inputmode="text" label="First name" autocomplete="given-name"></sd-input>
+          <sd-input type="text" inputmode="text" label="Last name" required autocomplete="family-name"></sd-input>
         </div>
-        <sd-input type="email" label="Email" spellcheck required></sd-input>
+        <sd-input type="email" inputmode="email" label="Email" required autocomplete="email"></sd-input>
         <sd-textarea label="News" rows="4" spellcheck></sd-textarea>
         <sd-checkbox-group>
           <sd-checkbox value="privacy_policy" required>
-            I accept the <sd-link href="#">Privacy Policy</sd-link>.
+            I accept the <sd-link href="javascript:void(0)">Privacy Policy</sd-link>.
           </sd-checkbox>
           <sd-checkbox value="marketing_emails">I would like to receive marketing emails.</sd-checkbox>
         </sd-checkbox-group>
@@ -100,17 +89,14 @@ export const contactForm = {
     <script>
       const radioGroup = document.getElementById('inquiry-radio-group');
       const regardingInput = document.getElementById('regarding-input');
-      console.log(radioGroup, regardingInput);
-      function toggleRegardingInput() {
-        var selectedValue = radioGroup.getAttribute('value');
-        console.log(selectedValue);
-
-        if (selectedValue === 'regarding') {
-          setTimeout(() => regardingInput.classList.remove('hidden'), 1);
+      radioGroup.addEventListener('sd-change', event => {
+        const value = event.target.value;
+        if (value === 'regarding') {
+          regardingInput.classList.remove('hidden');
         } else {
-          setTimeout(() => regardingInput.classList.add('hidden'), 1);
+          regardingInput.classList.add('hidden');
         }
-      }
+      });
     </script>
   `
 };
