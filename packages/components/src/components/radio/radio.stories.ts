b7f3828e30174eec5ba461873fc2f51b8290faec
Vahid <vahidmenu@gmail.com>
2024-10-17 15:46:57 +0200

    docs: complete docs for medium size

--- packages/components/src/components/radio/radio.stories.ts
+++ packages/components/src/components/radio/radio.stories.ts
@@ -37,16 +37,24 @@ export const Default = {
 };
 
 /**
- * Use the `size` attribute to change the size of the input radio. This attribute affects the font-size within the element, while the element itself remains the same size:
+ * Use the `size` attribute to change the size of the input radio (`md` equals `lg` but added for consistency reason on input elements):
  *
  * - `lg` (default)
+ * - `md`
  * - `sm`
+ *
+ * This attribute affects the font-size within the element, while the element itself remains the same size.
  */
 export const Size = {
   name: 'Size',
   render: () => html`
     <div class="flex gap-12">
       <sd-radio-group size="lg" value="1">
+        <sd-radio value="1">Radio 1</sd-radio>
+        <sd-radio value="2">Radio 2</sd-radio>
+        <sd-radio value="3">Radio 3</sd-radio>
+      </sd-radio-group>
+      <sd-radio-group size="md" value="1">
         <sd-radio value="1">Radio 1</sd-radio>
         <sd-radio value="2">Radio 2</sd-radio>
         <sd-radio value="3">Radio 3</sd-radio>
