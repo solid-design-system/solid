f9d53a0cd914f8b92f5f2fc01e59447bf8739cc0
auroraVasconcelos <aurora.vasconcelos@virtual-identity.com>
2024-10-18 11:01:18 +0100

    chore: add notifications status and copyright

--- packages/components/src/templates/tab-group.stories.ts
+++ packages/components/src/templates/tab-group.stories.ts
@@ -38,14 +38,14 @@ export const TabCentered = {
       <sd-tab-group activation id="centered">
         <sd-tab slot="nav" panel="tab-1">Gender</sd-tab>
         <sd-tab-panel name="tab-1">
-          <figure class="sd-media pb-4">
+          <figure class="sd-media sd-copyright" style="--copyright: '© 2024 Solid Design System';">
             <img
               src="./placeholders/images/collaboration.jpg"
               alt="Two men in business attire sitting on a sofa in a modern office, smiling and looking at a laptop screen."
               class="aspect-video object-cover"
             />
           </figure>
-          <p class="sd-paragraph">
+          <p class="sd-paragraph mt-4">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh justo ullamcorper odio tempor molestie
             phasellus dui vel id. Velit in sed non orci pellentesque vivamus nunc. At non tortor, sit neque tristique.
             Facilisis commodo integer hendrerit tortor.
@@ -53,14 +53,14 @@ export const TabCentered = {
         </sd-tab-panel>
         <sd-tab slot="nav" panel="tab-2">Age diversity</sd-tab>
         <sd-tab-panel name="tab-2">
-          <figure class="sd-media pb-4">
+          <figure class="sd-media sd-copyright" style="--copyright: '© 2024 Solid Design System';">
             <img
               src="./placeholders/images/family.jpg"
               alt="A man with glasses and a beard holding two young children in his arms, smiling outdoors with trees in the background."
               class="aspect-video object-cover"
             />
           </figure>
-          <p class="sd-paragraph">
+          <p class="sd-paragraph mt-4">
             Vivamus mattis mauris nec vulputate facilisis. Nulla facilisi. Pellentesque consectetur mi eget tristique
             vestibulum. Nunc porta urna sit amet magna congue feugiat. Quisque commodo arcu ex, eget mollis magna porta
             eu. Aliquam at turpis dolor. Quisque ut neque laoreet, cursus tellus non, venenatis elit.
@@ -68,14 +68,14 @@ export const TabCentered = {
         </sd-tab-panel>
         <sd-tab slot="nav" panel="tab-3">Education</sd-tab>
         <sd-tab-panel name="tab-3">
-          <figure class="sd-media pb-4">
+          <figure class="sd-media sd-copyright" style="--copyright: '© 2024 Solid Design System';">
             <img
               src="./placeholders/images/workspace.jpg"
               alt="Close-up of a pair of glasses, a pen, and an open notebook with notes written on it, on a desk next to a laptop."
               class="aspect-video object-cover"
             />
           </figure>
-          <p class="sd-paragraph">
+          <p class="sd-paragraph mt-4">
             Sed dignissim ipsum vel ultricies interdum. Quisque posuere felis sit amet fringilla tristique. Donec velit
             ex, viverra id lorem hendrerit, congue gravida justo. Fusce ultricies tellus arcu, nec suscipit metus
             sagittis sed. Vestibulum lacinia enim eu tellus auctor, quis pulvinar felis varius.
@@ -83,14 +83,14 @@ export const TabCentered = {
         </sd-tab-panel>
         <sd-tab slot="nav" panel="tab-4">Background</sd-tab>
         <sd-tab-panel name="tab-4">
-          <figure class="sd-media pb-4">
+          <figure class="sd-media sd-copyright" style="--copyright: '© 2024 Solid Design System';">
             <img
               src="./placeholders/images/collaboration.jpg"
               alt="Two men in business attire sitting on a sofa in a modern office, smiling and looking at a laptop screen."
               class="aspect-video object-cover"
             />
           </figure>
-          <p class="sd-paragraph">
+          <p class="sd-paragraph mt-4">
             Maecenas aliquet nibh nec tempor ultrices. Donec mattis sapien sed elit tincidunt, quis feugiat risus
             facilisis. Cras rutrum venenatis dui, in sollicitudin nulla vestibulum nec. Maecenas sed imperdiet sapien,
             non lacinia ex. Etiam rhoncus ullamcorper sem, eu euismod ipsum volutpat non.
@@ -144,7 +144,7 @@ export const TabWithIconBadge = {
       </sd-tab-panel>
       <sd-tab slot="nav" panel="tab-2">
         <sd-icon slot="left" name="system/folder"></sd-icon>
-        <sd-badge size="sm" class="mb-3 mr-2" variant="error"></sd-badge>
+        <sd-badge size="sm" class="mb-3 mr-2" variant="error">You have notifications</sd-badge>
         Files
       </sd-tab>
       <sd-tab-panel name="tab-2">
