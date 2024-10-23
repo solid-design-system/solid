c5648ff24aa84170014d670a1d6bb3a1bd0b5e98
Vahid <vahidmenu@gmail.com>
2024-10-23 09:23:20 +0200

    docs: update tooltip templates

--- packages/components/src/templates/tooltop.stories.ts
+++ packages/components/src/templates/tooltop.stories.ts
@@ -16,10 +16,10 @@ export default {
 export const InputWithTooltip = {
   name: 'Input with Tooltip',
   render: () => html`
-    <sd-input class="w-[400px]">
+    <sd-input class="w-[400px] py-6">
       <div slot="label" class="flex items-center gap-1">
         <label>Liquid Assets</label>
-        <sd-tooltip size="sm" content="Lorem ipsum" trigger="hover focus"></sd-tooltip>
+        <sd-tooltip size="sm" content="Lorem ipsum"></sd-tooltip>
       </div>
     </sd-input>
   `
@@ -27,21 +27,40 @@ export const InputWithTooltip = {
 
 export const SelectWithTooltip = {
   render: () => html`
-    <sd-select
-      class="w-[400px] h-[300px]"
-      size="lg"
-      placement="bottom"
-      label="Access Role"
-      placeholder="Please Select"
-      value=""
-    >
-      <div slot="label" class="flex items-center">
-        <label class="text-black font-bold mr-2">Access Role</label>
-        <sd-tooltip content="Lorem ipsum sic semper" placement="top" trigger="hover" size="sm"></sd-tooltip>
+    <sd-select class="w-[400px] py-6" size="lg" placement="top" placeholder="Please Select" value="">
+      <div slot="label" class="flex flex-row items-center gap-1">
+        Label Slot <sd-tooltip content="Lorem ipsum sic semper" size="sm" hoist></sd-tooltip>
       </div>
+
       <sd-option value="option-1">Viewer</sd-option>
       <sd-option value="option-2">Editor</sd-option>
       <sd-option value="option-3">Owner</sd-option>
     </sd-select>
+  `
+};
+
+export const TextAreaWithTooltip = {
+  render: () => html`
+    <sd-textarea
+      class="w-full py-4"
+      value="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod te."
+    >
+      <div slot="label" class="flex items-center gap-1">
+        Description <sd-tooltip content="Lorem ipsum sic semper" size="sm"></sd-tooltip>
+      </div>
+    </sd-textarea>
+  `
+};
+
+export const TooltipWithBoldedText = {
+  render: () => html`
+    <div class="h-40 flex items-center pl-20">
+      <sd-tooltip
+        ><span slot="content" class="sd-prose sd-prose--inverted"
+          ><h5>Headline</h5>
+          Lorem ipsum sic semper</span
+        ></sd-tooltip
+      >
+    </div>
   `
 };
