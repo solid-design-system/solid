3bb3bb95835945eb8d2ca6bccf03d748488d017c
Vahid <vahidmenu@gmail.com>
2024-10-29 09:36:29 +0100

    feat: add tooltip slot to input elements

--- packages/components/src/templates/tooltop.stories.ts
+++ packages/components/src/templates/tooltop.stories.ts
@@ -1,11 +1,6 @@
 import '../solid-components';
 import { html } from 'lit-html';
 
-// Hides duplicate story
-/**
- * ```
- * ```
- */
 export default {
   tags: ['!dev'],
   title: 'Templates/Tooltip',
@@ -22,10 +17,8 @@ export const InputWithTooltip = {
   name: 'Input with Tooltip',
   render: () => html`
     <sd-input class="w-[400px] py-6">
-      <div slot="label" class="flex items-center gap-1">
-        <label>Liquid Assets</label>
-        <sd-tooltip size="sm" content="Lorem ipsum"></sd-tooltip>
-      </div>
+      <label slot="label">Liquid Assets</label>
+      <sd-tooltip slot="tooltip" size="sm" content="Lorem ipsum"></sd-tooltip>
     </sd-input>
   `
 };
@@ -33,9 +26,8 @@ export const InputWithTooltip = {
 export const SelectWithTooltip = {
   render: () => html`
     <sd-select class="w-[400px] py-6" size="lg" placement="top" placeholder="Please Select" value="">
-      <div slot="label" class="flex flex-row items-center gap-1">
-        Label Slot <sd-tooltip content="Lorem ipsum sic semper" size="sm" hoist></sd-tooltip>
-      </div>
+      <div slot="label">Label Slot</div>
+      <sd-tooltip slot="tooltip" content="Lorem ipsum sic semper" size="sm" hoist></sd-tooltip>
 
       <sd-option value="option-1">Viewer</sd-option>
       <sd-option value="option-2">Editor</sd-option>
@@ -50,9 +42,8 @@ export const TextAreaWithTooltip = {
       class="w-full py-4"
       value="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod te."
     >
-      <div slot="label" class="flex items-center gap-1">
-        Description <sd-tooltip content="Lorem ipsum sic semper" size="sm"></sd-tooltip>
-      </div>
+      <div slot="label">Description</div>
+      <sd-tooltip slot="tooltip" content="Lorem ipsum sic semper" size="sm"></sd-tooltip>
     </sd-textarea>
   `
 };
