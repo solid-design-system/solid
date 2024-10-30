b0140c3e108ccd19c9334827412498b67e255ef3
Sergio Fonseca <sergio.fonseca@virtual-identity.com>
2024-10-30 18:27:53 +0000

    fix: content is not center aligned in safari due to leading-none class

--- packages/components/src/components/badge/badge.ts
+++ packages/components/src/components/badge/badge.ts
@@ -33,7 +33,7 @@ export default class SdBadge extends SolidElement {
         part="base"
         aria-labelledby="content"
         class=${cx(
-          'inline-flex items-center justify-center gap-x-[1px] text-center font-bold leading-none whitespace-nowrap border rounded-full select-none cursor-[inherit]',
+          'inline-flex items-center justify-center gap-x-[1px] text-center font-bold whitespace-nowrap border rounded-full select-none cursor-[inherit]',
           {
             /* variants */
             default: !this.inverted
