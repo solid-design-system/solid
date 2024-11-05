165756409ae5e7d58b2ca146463e2fe7e55027d1
Sergio Fonseca <sergio.fonseca@virtual-identity.com>
2024-11-05 16:47:35 +0000

    Revert "feat: stepper announces when value changes"
    This reverts commit 5cc342b37a1a56ab49c7712e0766c354f55c1e3e.

--- packages/components/src/templates/input.stories.ts
+++ packages/components/src/templates/input.stories.ts
@@ -16,13 +16,11 @@ export default {
 
 /**
  * ### Input with Currency Stepper
- *
- * __Accessibility Hint:__ Use the `aria-live="polite"` attribute to announce value changes.
  */
 export const InputWithCurrencyStepper = {
   render: () =>
     html`<div class="w-[250px]">
-      <sd-input label="Currency Stepper" id="stepperSampleInput" type="number" min="0" aria-live="polite"
+      <sd-input label="Currency Stepper" id="stepperSampleInput" type="number" min="0"
         ><span slot="right" class="text-sm inline-flex items-center"
           ><span class="text-neutral-700">EUR</span>
           <button
