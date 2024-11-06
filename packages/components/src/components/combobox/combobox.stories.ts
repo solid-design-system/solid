7fe926da7a611b90c8259c130a754871d4c9bd35
Daniel Hargesheimer <hargi@me.com>
2024-11-06 13:18:48 +0100

    doc: change label text

--- packages/components/src/components/combobox/combobox.stories.ts
+++ packages/components/src/components/combobox/combobox.stories.ts
@@ -253,7 +253,7 @@ export const Focus = {
   },
   render: () => html`
     <div class="h-[260px] w-[400px]">
-      <sd-combobox label="Preferred Color">
+      <sd-combobox label="Label">
         <sd-option value="option-1">Option 1</sd-option>
         <sd-option value="option-2">Option 2</sd-option>
         <sd-option value="option-3">Option 3</sd-option>
