f3c8e4768e10264c666a2d98c5110a8f0446996b
Sergio Fonseca <sergio.fonseca@virtual-identity.com>
2024-11-06 09:58:18 +0000

    chore: update vite config

--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -1,9 +1,9 @@
-import customElementConfig from './custom-elements-manifest.config';
+import customElementConfig from '../components/custom-elements-manifest.config';
 import VitePluginCreateEmptyCemIfNotExisting from './scripts/vite-plugin-create-empty-cem-if-not-existing';
 import VitePluginCustomElementsManifest from 'vite-plugin-cem';
 import VitePluginFetchIconsFromCdn from './scripts/vite-plugin-fetch-icons-from-cdn';
 import VitePluginGetPlaywrightVersion from './scripts/vite-plugin-get-playwright-version';
-import VitePluginGetTailwindTheme from '../components/scripts/vite-plugin-get-tailwind-theme';
+import VitePluginGetTailwindTheme from './scripts/vite-plugin-get-tailwind-theme';
 import VitePluginLitTailwind from '../components/scripts/vite-plugin-lit-tailwind';
 import type { defineConfig } from 'vite';
 
@@ -22,16 +22,13 @@ export default (() => {
       VitePluginCreateEmptyCemIfNotExisting(),
       VitePluginCustomElementsManifest({
         ...customElementConfig,
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
         plugins: customElementConfig.plugins.filter(
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
           plugin => ['solid-custom-tags', 'remove-html-members'].includes(plugin.name)
           // eslint-disable-next-line @typescript-eslint/no-explicit-any
         ) as any[]
       })
     ]
-    /**
-     * Build step for browsers (includes lit etc.)
-     * - umd: Combined js file, faster to load, but not tree-shakeable, recommended
-     * - es: Chunked js files, usable in browser, makes it possible to pick single components
-     */
   };
 }) as unknown as typeof defineConfig;
