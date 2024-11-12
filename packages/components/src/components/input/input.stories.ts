618475420b993fe8baa1b9bf04272e017b561bcc
Sergio Fonseca <sergio.fonseca@virtual-identity.com>
2024-11-12 12:48:17 +0000

    chore: remove deprecated icon library

--- packages/components/src/components/input/input.stories.ts
+++ packages/components/src/components/input/input.stories.ts
@@ -151,11 +151,11 @@ export const Icon = {
   render: () =>
     html`<div class="flex flex-rows gap-12">
       <sd-input label="Label" spellcheck>
-        <sd-icon label="landscape" library="global-resources" name="system/picture" slot="left"></sd-icon>
+        <sd-icon label="landscape" name="system/image" slot="left"></sd-icon>
       </sd-input>
 
       <sd-input label="Label" spellcheck>
-        <sd-icon label="landscape" library="global-resources" name="system/picture" slot="right"></sd-icon>
+        <sd-icon label="landscape" name="system/image" slot="right"></sd-icon>
       </sd-input>
     </div>`
 };
