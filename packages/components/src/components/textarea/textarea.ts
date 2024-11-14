ce37a9cab6e7ebccabc057bf49eaf4f92fb3cc7a
Paulo Vareiro <paulo.vareiro@virtual-identity.com>
2024-11-14 09:29:56 +0000

    fix: sd-textarea aria-invalid value

--- packages/components/src/components/textarea/textarea.ts
+++ packages/components/src/components/textarea/textarea.ts
@@ -307,7 +307,6 @@ export default class SdTextarea extends SolidElement implements SolidFormControl
     // States
     const hasLabel = this.label ? true : !!slots['label'];
     const hasHelpText = this.helpText ? true : !!slots['helpText'];
-    const isInvalid = !this.checkValidity();
 
     // Hierarchy of textarea states:
     const textareaState = this.disabled
@@ -406,7 +405,7 @@ export default class SdTextarea extends SolidElement implements SolidFormControl
               enterkeyhint=${ifDefined(this.enterkeyhint)}
               inputmode=${ifDefined(this.inputmode)}
               aria-describedby="help-text invalid-message"
-              ?aria-invalid=${isInvalid}
+              aria-invalid=${this.showInvalidStyle}
               @change=${this.handleChange}
               @input=${this.handleInput}
               @invalid=${this.handleInvalid}
