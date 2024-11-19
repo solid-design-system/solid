ea5aae8e8780d7d98486cae409432803d87d1962
Paulo Vareiro <paulo.vareiro@virtual-identity.com>
2024-11-19 11:05:01 +0000

    chore: remove unecessary unselected sd-tag

--- packages/docs/src/stories/components/tag.stories.ts
+++ packages/docs/src/stories/components/tag.stories.ts
@@ -59,7 +59,6 @@ export const Selected = {
   render: () => html`
     <div id="tags-selected" class="flex gap-12">
       <sd-tag selected toggleable>Selected</sd-tag>
-      <sd-tag toggleable>Unselected</sd-tag>
     </div>
 
     <script type="module">
