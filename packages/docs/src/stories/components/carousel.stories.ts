49c05221625b8aa4f02ac77f62d332b82ce88f50
Vahid <vahidmenu@gmail.com>
2024-11-19 11:02:35 +0100

    fix: remove unneccesary div

--- packages/docs/src/stories/components/carousel.stories.ts
+++ packages/docs/src/stories/components/carousel.stories.ts
@@ -76,32 +76,29 @@ export const Default = {
 export const Variant = {
   render: () => html`
     <div class="flex gap-12">
-      <div>
-        <div>
-          <sd-carousel variant="number">
-            <sd-carousel-item>
-              <div class="slot slot--border slot--text h-16">Default slot 1</div>
-            </sd-carousel-item>
-            <sd-carousel-item>
-              <div class="slot slot--border slot--text h-16">Default slot 2</div>
-            </sd-carousel-item>
-            <sd-carousel-item>
-              <div class="slot slot--border slot--text h-16">Default slot 3</div>
-            </sd-carousel-item>
-          </sd-carousel>
-        </div>
-        <sd-carousel variant="dot">
-          <sd-carousel-item>
-            <div class="slot slot--border slot--text h-16">Default slot 1</div>
-          </sd-carousel-item>
-          <sd-carousel-item>
-            <div class="slot slot--border slot--text h-16">Default slot 2</div>
-          </sd-carousel-item>
-          <sd-carousel-item>
-            <div class="slot slot--border slot--text h-16">Default slot 3</div>
-          </sd-carousel-item>
-        </sd-carousel>
-      </div>
+      <sd-carousel variant="number">
+        <sd-carousel-item>
+          <div class="slot slot--border slot--text h-16">Default slot 1</div>
+        </sd-carousel-item>
+        <sd-carousel-item>
+          <div class="slot slot--border slot--text h-16">Default slot 2</div>
+        </sd-carousel-item>
+        <sd-carousel-item>
+          <div class="slot slot--border slot--text h-16">Default slot 3</div>
+        </sd-carousel-item>
+      </sd-carousel>
+
+      <sd-carousel variant="dot">
+        <sd-carousel-item>
+          <div class="slot slot--border slot--text h-16">Default slot 1</div>
+        </sd-carousel-item>
+        <sd-carousel-item>
+          <div class="slot slot--border slot--text h-16">Default slot 2</div>
+        </sd-carousel-item>
+        <sd-carousel-item>
+          <div class="slot slot--border slot--text h-16">Default slot 3</div>
+        </sd-carousel-item>
+      </sd-carousel>
     </div>
   `
 };
