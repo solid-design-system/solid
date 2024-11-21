40112992528748b8463c4b535f3230a936040a67
Sergio Fonseca <sergio.fonseca@virtual-identity.com>
2024-11-21 10:51:41 +0000

    docs: add accessibility hints

--- packages/docs/src/stories/components/teaser-media.stories.ts
+++ packages/docs/src/stories/components/teaser-media.stories.ts
@@ -145,6 +145,12 @@ export const Variant = {
  * - Use the `default` slot to display main information and/or action elements below the headline.
  * - Use the `media` slot to add an image to the teaser.
  * - Use the `headline` slot to display titles. It should always contain a <h*> element.
+ *
+ * **Accessibility Hint:**
+ *
+ * - Please make sure to use semantically correct headline tags for the headline slot to provide accessible content.
+ * - The heading in a teaser should be the first item in the DOM. A heading introduces a new thematic region and separates the following content from the previous region.
+ * - If the image in a teaser doesn't contribute to the information delivered to the user, don't describe the image in the alt attribute and leave it empty.
  */
 export const DefaultMediaAndHeadlineSlot = {
   name: 'Default, Media and Headline Slot',
@@ -184,7 +190,7 @@ export const MetaSlot = {
 /**
  * Use the `expandable` slot to add content that only shows up on hover.
  *
- * **Accessibility Hint:** Expandable slot content should not contain any important content and should not have any actions, since it is not shown on mobile devices and is only accessible for mouse users.
+ * **Accessibility Hint:** Expandable teaser parts are only accessible to mouse users. The expandable content cannot be accessed via touch, keyboard, or screen readers. Therefore, please avoid adding important content or action elements in these areas.
  */
 
 export const ExpandableSlot = {
