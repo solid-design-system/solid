cf752b7bf268487024914de5319835bbb60916ef
Sergio Fonseca <sergio.fonseca@virtual-identity.com>
2024-12-16 15:43:17 +0000

    chore: update template and screenshot tests

--- packages/docs/src/stories/templates/interactive.stories.ts
+++ packages/docs/src/stories/templates/interactive.stories.ts
@@ -24,15 +24,15 @@ export default {
 export const Examples = {
   render: () => html`
     <div class="flex flex-col gap-12">
-      <button class="sd-interactive sd-interactive--reset">Text</button>
-      <button class="sd-interactive sd-interactive--reset">
+      <button class="sd-interactive sd-interactive--reset" title="Action name">Text</button>
+      <button class="sd-interactive sd-interactive--reset" title="Action name">
         <sd-icon name="system/image" label="Icon only button"></sd-icon>
       </button>
-      <button class="sd-interactive sd-interactive--reset flex flex-row items-center gap-2">
+      <button class="sd-interactive sd-interactive--reset flex flex-row items-center gap-2" title="Action name">
         <sd-icon name="system/image"></sd-icon>
         <span>Text</span>
       </button>
-      <button class="sd-interactive sd-interactive--reset flex flex-row items-center gap-2">
+      <button class="sd-interactive sd-interactive--reset flex flex-row items-center gap-2" title="Action name">
         <span>Text</span>
         <sd-icon name="system/image"></sd-icon>
       </button>
