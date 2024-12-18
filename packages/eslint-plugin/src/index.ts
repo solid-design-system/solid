3345802d228d36f3d3bf0d77553b2597b67b8609
Paulo Vareiro <paulo.vareiro@virtual-identity.com>
2024-12-18 18:37:45 +0000

    chore: update name

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -2,7 +2,7 @@ import SdButtonIconLabelRequired from './rules/sd-button-icon-label-required';
 
 const plugin = {
   meta: {
-    name: '@solid-design-system/eslint',
+    name: '@solid-design-system/eslint-plugin',
     version: '1.0.0'
   },
   rules: {
