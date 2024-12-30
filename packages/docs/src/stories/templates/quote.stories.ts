8dbc63fc00fe006a890f80dd0ed7f28415eace79
Paulo Vareiro <paulo.vareiro@virtual-identity.com>
2024-12-30 11:36:49 +0000

    fix: replace q element by blockquote

--- packages/docs/src/stories/templates/quote.stories.ts
+++ packages/docs/src/stories/templates/quote.stories.ts
@@ -1,6 +1,10 @@
 import '../../../../components/src/solid-components';
 import { html } from 'lit-html';
 
+/**
+ * **Accessibility Information:**
+ * Use the `<blockquote>` HTML element for block quotes and the `<q>` HTML element for short inline quotes.
+ */
 export default {
   tags: ['!dev'],
   title: 'Templates/Quote',
@@ -17,11 +21,11 @@ export const Size = {
   render: () =>
     html` <p class="sd-display mb-6">size 4xl</p>
       <div class="text-left text-black mb-12">
-        <q class="text-3xl lg:text-4xl leading-tight">
+        <blockquote class="text-3xl lg:text-4xl leading-tight">
           The Solid Design System by Union Investment provides a comprehensive toolkit for building consistent and
           high-quality user interfaces. It includes well-documented components, design tokens, and accessibility
           features, ensuring seamless integration across platforms.
-        </q>
+        </blockquote>
         <div class="gap-6 flex items-center">
           <img
             class="w-16 h-16 rounded-full overflow-hidden object-cover"
@@ -36,11 +40,11 @@ export const Size = {
       </div>
       <p class="sd-display mb-6">size 3xl</p>
       <div class="text-left text-black mb-12">
-        <q class="block mb-4 text-2xl lg:text-3xl leading-tight">
+        <blockquote class="block mb-4 text-2xl lg:text-3xl leading-tight">
           The Solid Design System by Union Investment provides a comprehensive toolkit for building consistent and
           high-quality user interfaces. It includes well-documented components, design tokens, and accessibility
           features, ensuring seamless integration across platforms.
-        </q>
+        </blockquote>
         <div class="gap-6 flex items-center">
           <img
             class="w-16 h-16 rounded-full overflow-hidden object-cover"
@@ -55,11 +59,11 @@ export const Size = {
       </div>
       <p class="sd-display mb-6">size xl</p>
       <div class="text-left text-black">
-        <q class="block mb-4 text-xl leading-tight">
+        <blockquote class="block mb-4 text-xl leading-tight">
           The Solid Design System by Union Investment provides a comprehensive toolkit for building consistent and
           high-quality user interfaces. It includes well-documented components, design tokens, and accessibility
           features, ensuring seamless integration across platforms.
-        </q>
+        </blockquote>
         <div class="gap-4 flex items-center">
           <img
             class="w-16 h-16 rounded-full overflow-hidden object-cover"
@@ -79,11 +83,11 @@ export const Inverted = {
   render: () =>
     html` <div class="flex items-end gap-5 p-5 bg-primary">
       <div class="text-left text-white">
-        <q class="block mb-6 text-3xl lg:text-4xl leading-tight">
+        <blockquote class="block mb-6 text-3xl lg:text-4xl leading-tight">
           The Solid Design System by Union Investment provides a comprehensive toolkit for building consistent and
           high-quality user interfaces. It includes well-documented components, design tokens, and accessibility
           features, ensuring seamless integration across platforms.
-        </q>
+        </blockquote>
         <div class="gap-6 flex items-center">
           <img
             class="w-16 h-16 rounded-full overflow-hidden object-cover"
@@ -103,11 +107,11 @@ export const QuoteWithoutProfileImage = {
   name: 'Quote without Profile Image',
   render: () =>
     html` <div class="text-left text-black">
-      <q class="block mb-6 text-3xl lg:text-4xl leading-tight">
+      <blockquote class="block mb-6 text-3xl lg:text-4xl leading-tight">
         The Solid Design System by Union Investment provides a comprehensive toolkit for building consistent and
         high-quality user interfaces. It includes well-documented components, design tokens, and accessibility features,
         ensuring seamless integration across platforms.
-      </q>
+      </blockquote>
       <div>
         <p class="sd-paragraph font-bold mb-1">John Doe</p>
         <p class="sd-paragraph sd-paragraph--size-sm">Team Member of SDS</p>
