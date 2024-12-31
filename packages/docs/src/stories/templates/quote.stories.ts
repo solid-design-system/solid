0f5fb43b42745421cd4b15c36d5fe4e6a656f4a8
Paulo Vareiro <paulo.vareiro@virtual-identity.com>
2024-12-31 10:14:09 +0000

    feat: implement profile image placeholder

--- packages/docs/src/stories/templates/quote.stories.ts
+++ packages/docs/src/stories/templates/quote.stories.ts
@@ -29,7 +29,7 @@ export const Size = {
         <div class="gap-6 flex items-center">
           <img
             class="w-16 h-16 rounded-full overflow-hidden object-cover"
-            src="./placeholders/images/collaboration.jpg"
+            src="./placeholders/images/profile.png"
             alt=""
           />
           <div>
@@ -48,7 +48,7 @@ export const Size = {
         <div class="gap-6 flex items-center">
           <img
             class="w-16 h-16 rounded-full overflow-hidden object-cover"
-            src="./placeholders/images/collaboration.jpg"
+            src="./placeholders/images/profile.png"
             alt=""
           />
           <div>
@@ -67,7 +67,7 @@ export const Size = {
         <div class="gap-4 flex items-center">
           <img
             class="w-16 h-16 rounded-full overflow-hidden object-cover"
-            src="./placeholders/images/collaboration.jpg"
+            src="./placeholders/images/profile.png"
             alt=""
           />
           <div>
@@ -91,7 +91,7 @@ export const Inverted = {
         <div class="gap-6 flex items-center">
           <img
             class="w-16 h-16 rounded-full overflow-hidden object-cover"
-            src="./placeholders/images/collaboration.jpg"
+            src="./placeholders/images/profile.png"
             alt=""
           />
           <div>
