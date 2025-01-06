32aac0f1bd5f5bd2c0ad4ca545c1fd3ac4bd5913
Paulo Vareiro <paulo.vareiro@virtual-identity.com>
2025-01-06 13:04:05 +0000

    fix: remove unecessary role button on accordion summary

--- packages/components/src/components/accordion/accordion.ts
+++ packages/components/src/components/accordion/accordion.ts
@@ -182,7 +182,6 @@ export default class SdAccordion extends SolidElement {
           class=${cx(
             'flex text-base gap-4 font-bold items-center cursor-pointer select-none px-4 py-3 focus-visible:focus-outline text-primary hover:bg-neutral-200 relative group'
           )}
-          role="button"
           aria-expanded=${this.open ? 'true' : 'false'}
           aria-controls="content"
           tabindex="0"
