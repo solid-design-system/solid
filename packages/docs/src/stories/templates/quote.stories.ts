542607b8c812901bf85d3cc98b0ff5bad8a10ac4
Paulo Vareiro <paulo.vareiro@virtual-identity.com>
2025-01-09 09:42:40 +0000

    fix: remove unecessary css

--- packages/docs/src/stories/templates/quote.stories.ts
+++ packages/docs/src/stories/templates/quote.stories.ts
@@ -21,10 +21,10 @@ export const Size = {
   render: () =>
     html` <p class="sd-display mb-6">size 4xl</p>
       <div class="text-left text-black mb-12">
-        <blockquote class="text-3xl lg:text-4xl leading-tight">
-          The Solid Design System by Union Investment provides a comprehensive toolkit for building consistent and
+        <blockquote class="mb-4 text-3xl lg:text-4xl leading-tight">
+          “The Solid Design System by Union Investment provides a comprehensive toolkit for building consistent and
           high-quality user interfaces. It includes well-documented components, design tokens, and accessibility
-          features, ensuring seamless integration across platforms.
+          features, ensuring seamless integration across platforms.”
         </blockquote>
         <div class="gap-6 flex items-center">
           <img
@@ -41,9 +41,9 @@ export const Size = {
       <p class="sd-display mb-6">size 3xl</p>
       <div class="text-left text-black mb-12">
         <blockquote class="block mb-4 text-2xl lg:text-3xl leading-tight">
-          The Solid Design System by Union Investment provides a comprehensive toolkit for building consistent and
+          “The Solid Design System by Union Investment provides a comprehensive toolkit for building consistent and
           high-quality user interfaces. It includes well-documented components, design tokens, and accessibility
-          features, ensuring seamless integration across platforms.
+          features, ensuring seamless integration across platforms.”
         </blockquote>
         <div class="gap-6 flex items-center">
           <img
@@ -60,9 +60,9 @@ export const Size = {
       <p class="sd-display mb-6">size xl</p>
       <div class="text-left text-black">
         <blockquote class="block mb-4 text-xl leading-tight">
-          The Solid Design System by Union Investment provides a comprehensive toolkit for building consistent and
+          “The Solid Design System by Union Investment provides a comprehensive toolkit for building consistent and
           high-quality user interfaces. It includes well-documented components, design tokens, and accessibility
-          features, ensuring seamless integration across platforms.
+          features, ensuring seamless integration across platforms.”
         </blockquote>
         <div class="gap-4 flex items-center">
           <img
@@ -83,10 +83,10 @@ export const Inverted = {
   render: () =>
     html` <div class="flex items-end gap-5 p-5 bg-primary">
       <div class="text-left text-white">
-        <blockquote class="block mb-6 text-3xl lg:text-4xl leading-tight">
-          The Solid Design System by Union Investment provides a comprehensive toolkit for building consistent and
+        <blockquote class="block mb-4 text-3xl lg:text-4xl leading-tight">
+          “The Solid Design System by Union Investment provides a comprehensive toolkit for building consistent and
           high-quality user interfaces. It includes well-documented components, design tokens, and accessibility
-          features, ensuring seamless integration across platforms.
+          features, ensuring seamless integration across platforms.”
         </blockquote>
         <div class="gap-6 flex items-center">
           <img
@@ -108,9 +108,9 @@ export const QuoteWithoutProfileImage = {
   render: () =>
     html` <div class="text-left text-black">
       <blockquote class="block mb-6 text-3xl lg:text-4xl leading-tight">
-        The Solid Design System by Union Investment provides a comprehensive toolkit for building consistent and
+        “The Solid Design System by Union Investment provides a comprehensive toolkit for building consistent and
         high-quality user interfaces. It includes well-documented components, design tokens, and accessibility features,
-        ensuring seamless integration across platforms.
+        ensuring seamless integration across platforms.”
       </blockquote>
       <div>
         <p class="sd-paragraph font-bold mb-1">Jane Doe</p>
