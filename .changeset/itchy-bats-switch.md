eb48355c15c1daa59ee65b69897ed499b888d51b
Paulo Vareiro <paulo.vareiro@virtual-identity.com>
2025-01-27 15:45:16 +0000

    fix: sd-textarea placeholder disabled color

---
+++ .changeset/itchy-bats-switch.md
@@ -0,0 +1,5 @@
+---
+'@solid-design-system/components': patch
+---
+
+Change sd-textarea placeholder color when disabled
--- packages/components/src/components/textarea/textarea.ts
+++ packages/components/src/components/textarea/textarea.ts
@@ -382,8 +382,8 @@ export default class SdTextarea extends SolidElement implements SolidFormControl
               part="textarea"
               id="input"
               class=${cx(
-                'flex-grow focus:outline-none bg-transparent placeholder-neutral-700 resize-none',
-                this.disabled && 'cursor-not-allowed',
+                'flex-grow focus:outline-none bg-transparent resize-none',
+                this.disabled ? 'cursor-not-allowed placeholder-neutral-500' : 'placeholder-neutral-700',
                 textSize
               )}
               title=${this.title /* An empty title prevents browser validation tooltips from appearing on hover */}
