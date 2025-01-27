f300c373ec40e7ae5fa13bbb023a56acda623d3f
Paulo Vareiro <paulo.vareiro@virtual-identity.com>
2025-01-27 12:28:54 +0000

    feat: implement test.watch

--- packages/docs/scripts/test.js
+++ packages/docs/scripts/test.js
@@ -3,6 +3,12 @@ import concurrently from 'concurrently';
 
 const PORT = 6999;
 
+const ci = process.env.CI;
+const watch = process.argv.includes('--watch');
+
+const workers = ci ? '--maxWorkers=1' : '';
+const testCommand = `test-storybook ${workers} --testTimeout=40000 --url http://127.0.0.1:${PORT}`;
+
 function checkPort(port) {
   const output = spawnSync(`lsof -i tcp:${port} | awk '{print $2}' |grep --invert PID`, { shell: true });
   if (output.error) {
@@ -18,7 +24,13 @@ function build() {
 }
 
 function test() {
-  return execSync(`test-storybook --maxWorkers=1 --testTimeout=40000 --url http://127.0.0.1:${PORT}`, {
+  return execSync(testCommand, {
+    stdio: 'inherit'
+  });
+}
+
+function testWatch() {
+  return execSync(`${testCommand} --watch`, {
     stdio: 'inherit'
   });
 }
@@ -33,7 +45,7 @@ async function previewAndTest() {
           prefixColor: 'magenta'
         },
         {
-          command: `wait-on -v -t 5s tcp:127.0.0.1:${PORT} && pnpm test.only`,
+          command: `wait-on -v -t 5s tcp:127.0.0.1:${PORT} && ${testCommand}`,
           name: 'Testing',
           prefixColor: 'blue'
         }
@@ -53,8 +65,20 @@ async function previewAndTest() {
   );
 }
 
-const isPortBusy = checkPort(PORT);
-if (isPortBusy) {
+if (watch) {
+  const isPortBusy = checkPort(PORT);
+
+  if (!isPortBusy) {
+    console.log(`Storybook is not running. Please run storybook on a separate terminal by running: pnpm dev`);
+    process.exit(1);
+  }
+
+  testWatch();
+  process.exit(0);
+}
+
+const isPreviewPortBusy = checkPort(PORT);
+if (isPreviewPortBusy) {
   console.log(`Preview in ${PORT} is already running. Running tests only...`);
   test();
 } else {
