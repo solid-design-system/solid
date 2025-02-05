0953a40610dffed5d64e374b69db802e0a1ac05a
Paulo Vareiro <paulo.vareiro@virtual-identity.com>
2025-02-05 17:09:18 +0000

    chore: skip link name

--- packages/docs/src/stories/styles/footnotes.stories.ts
+++ packages/docs/src/stories/styles/footnotes.stories.ts
@@ -15,7 +15,7 @@ const { generateTemplate } = storybookTemplate('sd-footnotes');
 
 export default {
   title: 'Styles/sd-footnotes',
-  tags: ['!dev'],
+  tags: ['!dev', 'skip-a11y-[link-name]'],
   component: 'sd-footnotes',
   parameters: {
     ...parameters,
