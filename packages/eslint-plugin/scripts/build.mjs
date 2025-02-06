7c0e6e4e85b0b86c086bdb7b014dec3981326a4f
Paulo Vareiro <paulo.vareiro@virtual-identity.com>
2025-02-06 15:01:52 +0000

    feat: improve dist folder

--- packages/eslint-plugin/scripts/build.mjs
+++ packages/eslint-plugin/scripts/build.mjs
@@ -1,13 +1,18 @@
 import * as esbuild from 'esbuild';
+import { globby } from 'globby';
 
-esbuild
-  .build({
-    entryPoints: ['src/**/*.ts'],
-    outdir: 'dist',
-    bundle: false,
-    minify: false,
-    platform: 'node',
-    target: 'es2018'
-  })
-  .then(() => console.log('✅ Build successful!'))
-  .catch(error => console.log('❌', error.message));
+(async () => {
+  const entryPoints = await globby([`src/**/*.ts`, '!src/**/*.spec.ts', '!src/**/*.d.ts']);
+
+  esbuild
+    .build({
+      entryPoints,
+      outdir: 'dist',
+      bundle: false,
+      minify: false,
+      platform: 'node',
+      target: 'es2018'
+    })
+    .then(() => console.log('✅ Build successful!'))
+    .catch(error => console.log('❌', error.message));
+})();
