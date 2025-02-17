5407bc0680c7e09926fea85755fbb964fe8e59d1
Sergio Fonseca <sergio.fonseca@virtual-identity.com>
2025-02-17 13:17:42 +0000

    feat: add example with tooltip

--- packages/docs/src/stories/components/checkbox-group.stories.ts
+++ packages/docs/src/stories/components/checkbox-group.stories.ts
@@ -112,6 +112,14 @@ export const Label = {
         <sd-checkbox value="2">Checkbox 2</sd-checkbox>
         <sd-checkbox value="3">Checkbox 3</sd-checkbox>
       </sd-checkbox-group>
+
+      <sd-checkbox-group orientation="vertical">
+        <sd-tooltip slot="tooltip" content="Checkbox Group" trigger="click" size="sm"></sd-tooltip>
+        <label slot="label">Group Label</label>
+        <sd-checkbox value="1">Checkbox 1</sd-checkbox>
+        <sd-checkbox value="2">Checkbox 2</sd-checkbox>
+        <sd-checkbox value="3">Checkbox 3</sd-checkbox>
+      </sd-checkbox-group>
     </div>
   `
 };
