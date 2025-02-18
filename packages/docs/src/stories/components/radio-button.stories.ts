38fc676d0b247bf8ffdbecc8dbe1f042272da353
Sérgio Fonseca <42741644+smfonseca@users.noreply.github.com>
2025-02-18 10:40:10 +0000

    Update packages/docs/src/stories/components/radio-button.stories.ts
    Co-authored-by: Aurora Vasconcelos <aurora.vasconcelos@virtual-identity.com>

--- packages/docs/src/stories/components/radio-button.stories.ts
+++ packages/docs/src/stories/components/radio-button.stories.ts
@@ -209,7 +209,7 @@ export const Disabled = {
 };
 
 /**
- * Use the `visually-disabled` attribute to style the component as if it was disabled and enable aria-disabled to allow it to be reachable by screen readers.
+ * Use the `visually-disabled` attribute to style the component as if it was disabled and enable `aria-disabled` to allow it to be reachable by screen readers.
  *
  * __Hint:__ When using this attribute, make sure to provide ways to inform the user why the element is disabled and how to enable it. This can be done by using the `help-text` attribute or wrapping the element in a sd-tooltip.
  *
