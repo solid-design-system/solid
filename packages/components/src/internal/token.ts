521e3c4132dc373b09c2d3b76f78d81d6d45b366
Paulo Vareiro <paulo.vareiro@virtual-identity.com>
2025-02-21 15:40:12 +0000

    chore: simplify types

--- packages/components/src/internal/token.ts
+++ packages/components/src/internal/token.ts
@@ -6,7 +6,7 @@ const processors: Record<string, (value: string) => string | number> = {
   duration: (value: string): number => parseFloat(value.replace('s', '')) * 1000
 };
 
-export const token = <T extends keyof typeof TOKENS>(name: string): ReturnType<(typeof processors)[T]> | null => {
+export const token = (name: string): string | number | null => {
   const el = document.createElement('div');
   el.className = name;
   el.style.position = 'absolute';
@@ -20,5 +20,5 @@ export const token = <T extends keyof typeof TOKENS>(name: string): ReturnType<(
   const value = window.getComputedStyle(el).getPropertyValue(property);
   document.body.removeChild(el);
 
-  return processors[entry as T]?.(value) ?? value;
+  return processors[entry]?.(value) ?? value;
 };
