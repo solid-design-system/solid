8cb6bcd0b540bc7dd7ac36069729eac39a6ee96c
Paulo Vareiro <paulo.vareiro@virtual-identity.com>
2025-03-03 11:41:42 +0000

    feat: increase show size to 7

--- packages/docs/src/stories/styles/pagination.stories.ts
+++ packages/docs/src/stories/styles/pagination.stories.ts
@@ -60,6 +60,8 @@ export const Default = {
                   <li><a href="/?page=3">3</a></li>
                   <li><a href="/?page=4">4</a></li>
                   <li><a href="/?page=5">5</a></li>
+                  <li><a href="/?page=6">6</a></li>
+                  <li><a href="/?page=7">7</a></li>
                   <li><a href="/?page=2"><sd-icon name="system/chevron-right" label="Next"></a></li>
                 </ul>
               </nav>`
