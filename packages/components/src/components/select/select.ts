4761ea3cb2b78bafd284f21c67ab92608ed5bdc3
Sergio Fonseca <sergio.fonseca@virtual-identity.com>
2025-03-03 15:53:23 +0000

    fix: adjust placeholder color when disabled or visually-disabled

--- packages/components/src/components/select/select.ts
+++ packages/components/src/components/select/select.ts
@@ -1157,6 +1157,11 @@ export default class SdSelect extends SolidElement implements SolidFormControl {
         content: ' *';
       }
 
+      :host([visually-disabled]) ::placeholder,
+      :host([disabled]) ::placeholder {
+        @apply text-neutral-500;
+      }
+
       [part='listbox'] {
         max-height: var(--auto-size-available-height, auto);
       }
