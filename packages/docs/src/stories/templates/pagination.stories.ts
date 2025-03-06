e75216baf70376f3b0c5b4334959ef6e846a8722
Paulo Vareiro <paulo.vareiro@virtual-identity.com>
2025-03-06 16:07:29 +0000

    chore: remove unecessary data-page attribute

--- packages/docs/src/stories/templates/pagination.stories.ts
+++ packages/docs/src/stories/templates/pagination.stories.ts
@@ -16,29 +16,17 @@ export default {
 export const Default = {
   render: () =>
     html`
-      <nav id="pagination" class="sd-pagination" aria-label="Default pagination">
+      <nav id="pagination" class="sd-pagination" aria-label="Pagination">
         <ul>
           <li><a aria-hidden="true"><sd-icon name="system/chevron-left" label="Go to previous page"></a></li>
-          <li><a href="/?page=1" data-page="1" aria-current="page" aria-label="Page 1">1</a></li>
-          <li><a href="/?page=2" data-page="2" aria-label="Go to page 2">2</a></li>
-          <li><a href="/?page=3" data-page="3" aria-label="Go to page 3">3</a></li>
-          <li><a href="/?page=4" data-page="4" aria-label="Go to page 4">4</a></li>
-          <li><a href="/?page=5" data-page="5" aria-label="Go to page 5">5</a></li>
-          <li><a href="/?page=6" data-page="6" aria-label="Go to page 6">6</a></li>
-          <li><a href="/?page=7" data-page="7" aria-label="Go to page 7">7</a></li>
-          <li><a href="/?page=8" data-page="8" aria-label="Go to page 8">8</a></li>
-          <li><a href="/?page=9" data-page="9" aria-label="Go to page 9">9</a></li>
-          <li><a href="/?page=10" data-page="10" aria-label="Go to page 10">10</a></li>
-          <li><a href="/?page=11" data-page="11" aria-label="Go to page 11">11</a></li>
-          <li><a href="/?page=12" data-page="12" aria-label="Go to page 12">12</a></li>
-          <li><a href="/?page=13" data-page="13" aria-label="Go to page 13">13</a></li>
-          <li><a href="/?page=14" data-page="14" aria-label="Go to page 14">14</a></li>
-          <li><a href="/?page=15" data-page="15" aria-label="Go to page 15">15</a></li>
-          <li><a href="/?page=16" data-page="16" aria-label="Go to page 16">16</a></li>
-          <li><a href="/?page=17" data-page="17" aria-label="Go to page 17">17</a></li>
-          <li><a href="/?page=18" data-page="18" aria-label="Go to page 18">18</a></li>
-          <li><a href="/?page=19" data-page="19" aria-label="Go to page 19">19</a></li>
-          <li><a href="/?page=20" data-page="20" aria-label="Go to page 20">20</a></li>
+          <li><a href="/?page=1" aria-current="page">1</a></li>
+          <li><a href="/?page=2">2</a></li>
+          <li><a href="/?page=3">3</a></li>
+          <li><a href="/?page=4">4</a></li>
+          <li><a href="/?page=5">5</a></li>
+          <li><a href="/?page=6">6</a></li>
+          <li><a href="/?page=7">7</a></li>
+          <li><a href="/?page=8">8</a></li>
           <li><a href="/?page=2"><sd-icon name="system/chevron-right" label="Go to next page"></a></li>
         </ul>
         <p id="pagination-live" aria-live="polite"></p>
@@ -48,7 +36,7 @@ export const Default = {
         const live = pagination.querySelector('#pagination-live');
         const state = { current: 1 };
 
-        const pages = pagination.querySelectorAll('[data-page]');
+        const pages = pagination.querySelectorAll('ul > li a:not(:has(sd-icon))');
         const previous = pagination.querySelector('ul > li:first-child a:has(sd-icon)');
         const next = pagination.querySelector('ul > li:last-child a:has(sd-icon)');
 
@@ -56,11 +44,9 @@ export const Default = {
           const current = pages[state.current - 1];
           pages.forEach((page) => {
             page.removeAttribute('aria-current');
-            page.setAttribute('aria-label', 'Go to page ' + page.dataset.page)
           });
 
           current.setAttribute('aria-current', 'page');
-          current.setAttribute('aria-label', 'Page ' + pages[state.current - 1].dataset.page);
           live.innerHTML = 'Current page: ' + state.current;
 
           const isFirstPage = state.current <= 1;
@@ -86,7 +72,7 @@ export const Default = {
         const handlePageClick = (event) => {
           event.preventDefault();
 
-          const page = parseInt(event.target.dataset.page, 10);
+          const page = parseInt(event.target.innerHTML, 10);
           if (page === state.current) return;
           
           state.current = page;
