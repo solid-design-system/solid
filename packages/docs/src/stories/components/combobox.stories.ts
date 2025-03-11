ce8dd1d931efbf1aa9ab4e8cd4947fbe3361539d
Sergio Fonseca <sergio.fonseca@virtual-identity.com>
2025-03-11 16:40:57 +0000

    fix: update label text

--- packages/docs/src/stories/components/combobox.stories.ts
+++ packages/docs/src/stories/components/combobox.stories.ts
@@ -341,7 +341,7 @@ export const Clearable = {
 export const Icons = {
   render: () => html`
     <div class="w-[400px] h-[400px]">
-      <sd-combobox size="lg" clearable label="Large">
+      <sd-combobox size="lg" clearable label="Label">
         <sd-icon slot="left" name="system/image" aria-hidden="true" color="currentColor"></sd-icon>
         ${createColorOptionsHtml()}
         <button slot="right" aria-label="Search">
