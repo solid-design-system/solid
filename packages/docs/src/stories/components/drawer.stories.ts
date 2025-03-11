12d0a8484a6fb05a3502801b2e3ac6e77a069aac
Aurora Vasconcelos <aurora.vasconcelos@virtual-identity.com>
2025-03-11 14:14:40 +0000

    Update packages/docs/src/stories/components/drawer.stories.ts
    Co-authored-by: Sérgio Fonseca <42741644+smfonseca@users.noreply.github.com>

--- packages/docs/src/stories/components/drawer.stories.ts
+++ packages/docs/src/stories/components/drawer.stories.ts
@@ -36,7 +36,7 @@ export default {
     },
     { type: 'attribute', name: 'open', value: true },
     { type: 'attribute', name: 'contained', value: true },
-    { type: 'attribute', name: 'label', value: 'Aria Label' }
+    { type: 'attribute', name: 'label', value: 'Label' }
   ]),
   argTypes,
   parameters: {
