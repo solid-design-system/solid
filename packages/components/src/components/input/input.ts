e247c6bc9ca4e3a896a0b3e59585429572d324c3
Sérgio Fonseca <sergio.fonseca@virtual-identity.com>
2025-03-31 17:24:27 +0100

    fix: password toggle button is focusable and add aria-pressed attribute

--- packages/components/src/components/input/input.ts
+++ packages/components/src/components/input/input.ts
@@ -643,28 +643,20 @@ export default class SdInput extends SolidElement implements SolidFormControl {
                   <button
                     aria-label=${this.localize.term(this.passwordVisible ? 'hidePassword' : 'showPassword')}
                     part="password-toggle-button"
-                    class="flex items-center"
+                    class=${cx('flex items-center sd-interactive', iconMarginLeft)}
                     type="button"
                     @click=${this.handlePasswordToggle}
-                    tabindex="-1"
+                    aria-pressed=${this.passwordVisible ? 'true' : 'false'}
                   >
                     ${this.passwordVisible
                       ? html`
                           <slot name="show-password-icon"
-                            ><sd-icon
-                              class=${cx(iconColor, iconMarginLeft, iconSize)}
-                              library="system"
-                              name="eye"
-                            ></sd-icon
+                            ><sd-icon class=${cx(iconColor, iconSize)} library="system" name="eye"></sd-icon
                           ></slot>
                         `
                       : html`
                           <slot name="hide-password-icon"
-                            ><sd-icon
-                              class=${cx(iconColor, iconMarginLeft, iconSize)}
-                              library="system"
-                              name="eye-crossed-out"
-                            ></sd-icon
+                            ><sd-icon class=${cx(iconColor, iconSize)} library="system" name="eye-crossed-out"></sd-icon
                           ></slot>
                         `}
                   </button>
