b8b58a8c279633f7dacddeff83e9aebc45f783ae
Paulo Vareiro <paulo.vareiro@virtual-identity.com>
2025-04-02 15:49:28 +0100

    docs: add information about pre initializing toast stacks

--- packages/docs/src/stories/components/notification.stories.ts
+++ packages/docs/src/stories/components/notification.stories.ts
@@ -166,6 +166,24 @@ export const DurationIndicator = {
  * - `top-right` (default)
  * - `bottom-center`
  *
+ * **Important:** Some screen readers may occasionally ignore live regions that are added to a page after it has already loaded.
+ * Therefore, to make sure the toast stack regions are already present on page load, please make sure to render the following:
+ *
+ * ```html
+ * <div
+ *   role="region"
+ *   id="sd-toast-stack--top-right"
+ *   class="sd-toast-stack sd-toast-stack--top-right"
+ *   aria-label="Top right notifications"
+ *  ></div>
+ *  <div
+ *   role="region"
+ *   id="sd-toast-stack--bottom-center"
+ *   class="sd-toast-stack sd-toast-stack--bottom-center"
+ *   aria-label="Bottom center notifications"
+ *  ></div>
+ * ```
+ *
  * __Hints:__
  * - It requires the use of the `toast` method to work. Click on the `Show code` button to see the JavaScript code responsible for generating the toast notification.
  * - Click on one of the buttons below to see the corresponding toast notification.
