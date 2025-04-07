af1fcb43bbaa1858ccaece94e9f1758599282db7
Paulo Vareiro <paulo.vareiro@virtual-identity.com>
2025-04-07 14:42:08 +0100

    fix: reopen closable notification after close

--- packages/docs/src/stories/components/notification.stories.ts
+++ packages/docs/src/stories/components/notification.stories.ts
@@ -110,9 +110,9 @@ export const Closable = {
     html` <sd-notification id="closable-example" variant="info" open closable>Lorem ipsum dolor sit</sd-notification>
       <script>
         var closableNotification = document.querySelector('#closable-example');
-        closableNotification.addEventListener('click', () => {
+        closableNotification.addEventListener('sd-after-hide', () => {
           setTimeout(() => {
-            notification.open = true;
+            closableNotification.open = true;
           }, 3000);
         });
       </script>`
