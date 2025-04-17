264120f7196dc4c60dc1e74e3fa44a12d80bf159
Paulo Vareiro <paulo.vareiro@virtual-identity.com>
2025-04-17 12:03:59 +0100

    chore: remove extra aria-label

--- packages/components/src/components/drawer/drawer.ts
+++ packages/components/src/components/drawer/drawer.ts
@@ -357,12 +357,7 @@ export default class SdDrawer extends SolidElement {
                 class="absolute top-2 right-2"
                 ><sd-icon label=${this.localize.term('close')} name="close" library="system"></sd-icon
               ></sd-button>`}
-          <div
-            part="body"
-            class="flex-auto block px-4 focus-visible:focus-outline !-outline-offset-2"
-            aria-label=${this.label}
-            tabindex="0"
-          >
+          <div part="body" class="flex-auto block px-4 focus-visible:focus-outline !-outline-offset-2" tabindex="0">
             <slot></slot>
           </div>
           <footer part="footer" class=${cx(this.hasSlotController.test('footer') ? 'text-left p-4' : 'hidden')}>
