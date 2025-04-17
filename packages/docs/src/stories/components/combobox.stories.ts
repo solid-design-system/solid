0db506448fd816c8ff675a0368d10d5399090be7
Sérgio Fonseca <sergio.fonseca@virtual-identity.com>
2025-04-17 18:03:21 +0100

    chore: add new search story

--- packages/docs/src/stories/components/combobox.stories.ts
+++ packages/docs/src/stories/components/combobox.stories.ts
@@ -337,7 +337,6 @@ export const Clearable = {
  *  Show the search icon in either the left slot or right icon slot instead of the chevron icon (don’t show two icons on the right-hand side).
  *
  *  __Accessibility hint__: The label can be omitted for search input fields if a button (e.g., aria-label="Search") with a search icon is present.
- *
  */
 export const Icons = {
   render: () => html`
@@ -348,6 +347,23 @@ export const Icons = {
         <button slot="right" aria-label="Search" class="sd-interactive flex">
           <sd-icon library="system" name="magnifying-glass" aria-hidden="true" color="currentColor"></sd-icon>
         </button>
+      </sd-combobox>
+    </div>
+  `
+};
+
+/**
+ * Use the `type="search"` to control the type of input the browser renders.
+ */
+export const Search = {
+  render: () => html`
+    <div class="w-[400px] h-[500px]">
+      <sd-combobox size="lg" label="Label" placement="bottom" type="search" value="">
+        <sd-option value="option-1">Option 1</sd-option>
+        <sd-option value="option-2">Option 2</sd-option>
+        <sd-option value="option-3">Option 3</sd-option>
+        <sd-option value="option-4">Option 4</sd-option>
+        <sd-option value="option-5">Option 5</sd-option>
       </sd-combobox>
     </div>
   `
