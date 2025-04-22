abcdfeed967405946519e245bf9eb239835e1d18
Sérgio Fonseca <42741644+smfonseca@users.noreply.github.com>
2025-04-22 16:42:23 +0100

    feat: status badge template (#2156)
    <!-- ## Title: Please consider adding the [skip chromatic] flag to the
    PR title in case you dont need chromatic testing your changes. -->
    ## Description:
    Closes [#1852
    ](https://github.com/solid-design-system/solid/issues/1852)
    
    ## Definition of Reviewable:
    <!-- *PR notes: Irrelevant elements should be removed.* -->
    - [ ] Documentation is created/updated
    - [ ] Migration Guide is created/updated
    - [ ] E2E tests (features, a11y, bug fixes) are created/updated
    <!-- *If this PR includes a bug fix, an E2E test is necessary to verify
    the change. If the fix is purely visual, ensuring it is captured within
    our chromatic screenshot tests is sufficient.* -->
    - [ ] Stories (features, a11y) are created/updated
    - [ ] relevant tickets are linked

--- packages/docs/src/stories/templates/status-badge.stories.ts
+++ packages/docs/src/stories/templates/status-badge.stories.ts
@@ -14,11 +14,11 @@ export default {
 };
 
 /**
- * ### Status Badge with Custom Icon
  *
  * The icons used in this component, should be exclusively from the `sd-status-assets` [icon library](?path=/story/components-sd-icon-default--status-library).
  */
 export const StatusBadgeWithCustomIcon = {
+  name: 'Status Badge with Custom Icon',
   render: () => html`
     <div class="grid col-span-12 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-12">
       <div class="sd-status-badge sd-status-badge--success">
@@ -85,5 +85,166 @@ export const StatusBadgeWithCustomIcon = {
         Unknown
       </div>
     </div>
+  `
+};
+
+export const StatusBadgeWithChip = {
+  name: 'Status Badge with Chip',
+  render: () => html`
+    <style>
+      .data-wrapper {
+        container-type: inline-size;
+      }
+
+      .info-wrapper {
+        gap: 0.25rem;
+      }
+
+      .info-wrapper,
+      .inner-wrapper {
+        grid-template-columns: repeat(1, minmax(0, 1fr));
+      }
+
+      @container (min-width: 700px) {
+        .info-wrapper {
+          grid-template-columns: repeat(2, minmax(0, 1fr));
+          gap: 1rem;
+        }
+
+        .inner-wrapper {
+          grid-template-columns: auto 1fr;
+        }
+      }
+    </style>
+
+    <div class="flex flex-col gap-8 contacts">
+      <div>
+        <div class="flex justify-between">
+          <div>
+            <div class="flex flex-col lg:flex-row">
+              <h2 class="sd-headline">Max Mustermann</h2>
+              <div class="sd-status-badge sd-status-badge--success my-4 lg:ml-4">
+                <sd-icon name="status-check" library="sd-status-assets"></sd-icon>
+                Available
+              </div>
+            </div>
+            <span class="sd-chip sd-chip--primary-200">Authorized for telephone information</span>
+          </div>
+
+          <sd-button class="edit-button" variant="secondary">
+            Edit
+            <sd-icon name="system/pen" slot="icon-left"></sd-icon>
+          </sd-button>
+        </div>
+
+        <div class="sd-container sd-container--variant-neutral-100 mt-8 data-wrapper">
+          <p class="sd-headline sd-headline--size-xl">Master data</p>
+
+          <div class="grid mt-4 info-wrapper">
+            <div class="grid gap-x-4 gap-y-1 inner-wrapper">
+              <div class="sd-paragraph">Intermediary</div>
+              <div class="sd-paragraph font-bold">VR-Bank Musterstadt eG</div>
+
+              <div class="sd-paragraph">Geno ID</div>
+              <div class="sd-paragraph font-bold">YC12345</div>
+
+              <div class="sd-paragraph">Branch number</div>
+              <div class="sd-paragraph font-bold flex items-center">1234 – GST Musterstadt</div>
+
+              <div class="sd-paragraph">Consultant number</div>
+              <div class="sd-paragraph font-bold inline-flex items-center">1234 4678 A – Service-Kunden</div>
+            </div>
+
+            <div class="grid gap-x-4 gap-y-1 self-start inner-wrapper">
+              <div class="sd-paragraph">E-Mail</div>
+              <div class="sd-paragraph font-bold">max.mustermann@union-investment.de</div>
+
+              <div class="sd-paragraph">Telephone</div>
+              <div class="sd-paragraph font-bold">+49 (0) 00 123 456 78</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div class="flex justify-between">
+          <div>
+            <div class="flex flex-col lg:flex-row">
+              <h2 class="sd-headline">Jane Doe</h2>
+              <div class="sd-status-badge sd-status-badge--warning my-4 lg:ml-4">
+                <sd-icon name="status-exclamation" library="sd-status-assets"></sd-icon>
+                Be right back
+              </div>
+            </div>
+            <span class="sd-chip sd-chip--primary-200">Not authorized for telephone information</span>
+          </div>
+
+          <sd-button class="edit-button" variant="secondary">
+            Edit
+            <sd-icon name="system/pen" slot="icon-left"></sd-icon>
+          </sd-button>
+        </div>
+
+        <div class="sd-container sd-container--variant-neutral-100 mt-8 data-wrapper">
+          <p class="sd-headline sd-headline--size-xl">Master data</p>
+
+          <div class="grid mt-4 info-wrapper">
+            <div class="grid gap-x-4 gap-y-1 inner-wrapper">
+              <div class="sd-paragraph">Intermediary</div>
+              <div class="sd-paragraph font-bold">VR-Bank Gotham City eG</div>
+
+              <div class="sd-paragraph">Geno ID</div>
+              <div class="sd-paragraph font-bold">YC67891</div>
+
+              <div class="sd-paragraph">Branch number</div>
+              <div class="sd-paragraph font-bold flex items-center">5678 – GST Gotham City</div>
+
+              <div class="sd-paragraph">Consultant number</div>
+              <div class="sd-paragraph font-bold inline-flex items-center">1234 4678 B – Service-Kunden</div>
+            </div>
+
+            <div class="grid gap-x-4 gap-y-1 self-start inner-wrapper">
+              <div class="sd-paragraph">E-Mail</div>
+              <div class="sd-paragraph font-bold">jane.doe@union-investment.de</div>
+
+              <div class="sd-paragraph">Telephone</div>
+              <div class="sd-paragraph font-bold">+49 (0) 00 987 565 32</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script type="module">
+      const editButtons = document.querySelectorAll('.contacts .edit-button');
+
+      const debounce = (func, timeout = 10) => {
+        let timer;
+        return (...args) => {
+          clearTimeout(timer);
+          timer = setTimeout(() => {
+            func.apply(this, args);
+          }, timeout);
+        };
+      };
+
+      editButtons.forEach(button => {
+        button.addEventListener('click', () => {
+          alert('Edit button clicked!');
+        });
+      });
+
+      const updateButtonSizes = size => {
+        editButtons.forEach(button => {
+          button.setAttribute('size', size);
+        });
+      };
+
+      const handleResize = debounce(() => {
+        const size = window.innerWidth < 640 ? 'sm' : 'lg';
+        updateButtonSizes(size);
+      }, 250);
+
+      window.addEventListener('resize', handleResize);
+    </script>
   `
 };
