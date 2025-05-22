bb1df77c10a547ee7e1419c0fc51338032658bd7
Sérgio Fonseca <sergio.fonseca@virtual-identity.com>
2025-05-22 12:08:23 +0100

    feat: add motion

--- packages/components/src/components/switch/switch.ts
+++ packages/components/src/components/switch/switch.ts
@@ -196,7 +196,7 @@ export default class SdSwitch extends SolidElement implements SolidFormControl {
           class=${cx(
             `relative flex flex-initial items-center justify-center border rounded-full h-4 w-8 transition-colors ease duration-100
             peer-focus-visible:outline peer-focus-visible:outline-2 peer-focus-visible:outline-offset-2
-            peer-focus-visible:outline-primary`,
+            peer-focus-visible:outline-primary transition-colors ease-in-out duration-medium hover:duration-fast`,
             this.disabled && this.checked
               ? 'border-neutral-500 bg-neutral-500'
               : this.disabled
@@ -212,7 +212,7 @@ export default class SdSwitch extends SolidElement implements SolidFormControl {
             id="thumb"
             part="thumb"
             class=${cx(
-              'w-2.5 h-2.5 rounded-full transition-transform ease-in-out duration-200',
+              'w-2.5 h-2.5 rounded-full transition[transform,colors] ease-in-out duration-medium',
               this.disabled && this.checked
                 ? 'bg-white translate-x-2'
                 : this.disabled
