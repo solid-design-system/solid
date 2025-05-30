1e354be8b12b9fdfc2c36faea01586422f33e9ce
Mojsoski, Blagoja (UIS) <blagoja.mojsoski@union-investment.de>
2025-05-30 10:38:21 +0200

    feat: Add motion design for  `sd-accordion` .

--- packages/components/src/components/accordion/accordion.ts
+++ packages/components/src/components/accordion/accordion.ts
@@ -186,7 +186,7 @@ export default class SdAccordion extends SolidElement {
           part="header"
           id="header"
           class=${cx(
-            'flex text-base gap-4 font-bold items-center cursor-pointer select-none px-4 py-3 focus-visible:focus-outline text-primary hover:bg-neutral-200 relative group'
+            'flex text-base gap-4 font-bold items-center cursor-pointer select-none px-4 py-3 focus-visible:focus-outline text-primary relative group transition-colors ease-in-out duration-medium hover:duration-fast hover:bg-neutral-200 border-neutral-400'
           )}
           aria-expanded=${this.open ? 'true' : 'false'}
           aria-controls="content"
@@ -242,7 +242,7 @@ setDefaultAnimation('accordion.show', {
     { height: '0', opacity: '0' },
     { height: 'auto', opacity: '1' }
   ],
-  options: { duration: 300, easing: 'ease' }
+  options: { duration: 'var(--sd-duration-medium, 300)', easing: 'ease-in-out' }
 });
 
 setDefaultAnimation('accordion.hide', {
@@ -250,7 +250,7 @@ setDefaultAnimation('accordion.hide', {
     { height: 'auto', opacity: '1' },
     { height: '0', opacity: '0' }
   ],
-  options: { duration: 300, easing: 'ease' }
+  options: { duration: 'var(--sd-duration-medium, 300)', easing: 'ease-in-out' }
 });
 
 declare global {
