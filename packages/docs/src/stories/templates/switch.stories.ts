a462d871d701ffeb0910fe45ff877bcc783322f8
auroraVasconcelos <aurora.vasconcelos@virtual-identity.com>
2025-06-26 16:56:31 +0100

    feat: update changes

--- packages/docs/src/stories/templates/switch.stories.ts
+++ packages/docs/src/stories/templates/switch.stories.ts
@@ -19,28 +19,28 @@ export default {
 export const Default = {
   name: 'Switch with Tooltip',
   render: () => html`
-    <div class="grid grid-cols-3 grid-rows-2 gap-4">
-      <sd-select style label="Label" placeholder="Please Select" max-options-visible="3">
-        <sd-option value="option-1">Option 1</sd-option>
-        <sd-option value="option-2">Option 2</sd-option>
-        <sd-option value="option-3">Option 3</sd-option>
+    <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
+      <sd-select style label="Investment product type" placeholder="Please Select" max-options-visible="2">
+        <sd-option value="option-1">Equity fund</sd-option>
+        <sd-option value="option-2">Bond fund</sd-option>
       </sd-select>
-      <sd-select style label="Label" placeholder="Please Select" max-options-visible="3">
-        <sd-option value="option-1">Option 1</sd-option>
-        <sd-option value="option-2">Option 2</sd-option>
-        <sd-option value="option-3">Option 3</sd-option>
+      <sd-select style label="Regional focus" placeholder="Please Select" max-options-visible="3">
+        <sd-option value="option-1">Global</sd-option>
+        <sd-option value="option-2">Europe</sd-option>
       </sd-select>
-      <sd-select style label="Label" placeholder="Please Select" max-options-visible="3">
-        <sd-option value="option-1">Option 1</sd-option>
-        <sd-option value="option-2">Option 2</sd-option>
-        <sd-option value="option-3">Option 3</sd-option>
+      <sd-select style label="Recommended investment horizon" placeholder="Please Select" max-options-visible="3">
+        <sd-option value="option-1">Short-term (1-3 years)</sd-option>
+        <sd-option value="option-2">Medium-term (3-5 years)</sd-option>
+        <sd-option value="option-3">Long-term (5+ years)</sd-option>
       </sd-select>
-      <sd-switch
-        >With sustainability strategy (47) <sd-tooltip slot="tooltip" size="sm" content="Lorem ipsum"></sd-tooltip
-      ></sd-switch>
-      <sd-switch
-        >Eligible for savings plan (162) <sd-tooltip slot="tooltip" size="sm" content="Lorem ipsum"></sd-tooltip
-      ></sd-switch>
+      <sd-switch>
+        With sustainability strategy (47)
+        <sd-tooltip slot="tooltip" size="sm" content="Lorem ipsum"></sd-tooltip>
+      </sd-switch>
+      <sd-switch>
+        Eligible for savings plan (162)
+        <sd-tooltip slot="tooltip" size="sm" content="Lorem ipsum"></sd-tooltip>
+      </sd-switch>
     </div>
   `
 };
