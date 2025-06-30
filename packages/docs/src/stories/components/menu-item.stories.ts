38157987da1cebd14e6ffd962ef9829b111c93aa
Sérgio Fonseca <42741644+smfonseca@users.noreply.github.com>
2025-06-30 11:11:02 +0100

    Update packages/docs/src/stories/components/menu-item.stories.ts
    Co-authored-by: Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>

--- packages/docs/src/stories/components/menu-item.stories.ts
+++ packages/docs/src/stories/components/menu-item.stories.ts
@@ -8,7 +8,7 @@ const { overrideArgs } = storybookHelpers('sd-menu-item');
 const { generateTemplate } = storybookTemplate('sd-menu-item');
 
 export default {
-  tags: ['!dev', 'skip-a11y-[aria-required-parent]'],
+  tags: ['!dev', 'autodocs', 'skip-a11y-[aria-required-parent]'],
   title: 'Components/sd-menu-item',
   component: 'sd-menu-item',
   args: overrideArgs({ type: 'slot', name: 'default', value: 'Menu item 1' }),
