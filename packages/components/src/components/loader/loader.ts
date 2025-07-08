85ac69483a7dfc50a3486ecf80c15f30d87a7848
Sérgio Fonseca <sergio.fonseca@virtual-identity.com>
2025-07-08 11:19:48 +0100

    chore: update version

--- packages/components/src/components/loader/loader.ts
+++ packages/components/src/components/loader/loader.ts
@@ -9,7 +9,7 @@ import SolidElement from '../../internal/solid-element';
  * @summary Loaders are used to show the progress of an indeterminate operation.
  * @documentation https://solid.union-investment.com/[storybook-link]/loader
  * @status stable
- * @since 5.10
+ * @since 5.11
  */
 @customElement('sd-loader')
 export default class SdLoader extends SolidElement {
