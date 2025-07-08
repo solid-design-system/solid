0956144826c79c4651133e001dcf4a05faabdc50
Sérgio Fonseca <sergio.fonseca@virtual-identity.com>
2025-07-08 12:57:03 +0100

    fix: sync overview links style with figma

--- packages/docs/src/Overview.jsx
+++ packages/docs/src/Overview.jsx
@@ -6,21 +6,23 @@ export const OverviewFormatter = ({ children, story }) => {
   const defaultStoryCanvas = () => <Canvas of={story} />;
   const documentationLinks = ({ children, ...props }) => (
     <div className="flex flex-col items-start gap-8 mt-8 mb-8 md:flex-row md:items-center">
-      {Object.entries(JSON.parse(props.links || '{}')).map(([name, link]) => (
-        <div key={name} className="flex flex-row items-center gap-4">
-          <sd-link href={link}>
-            {name
-              .split('-')
-              .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-              .join(' ')}
-          </sd-link>
+      {Object.entries(JSON.parse(props.links || '{}')).map(([name, link]) => {
+        const linkName = name
+          .split('-')
+          .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(' ');
 
-          <div className={`m-0 sd-status-badge ${link ? `sd-status-badge--success` : `sd-status-badge--warning`}`}>
-            <sd-icon name={link ? 'status-check' : 'status-exclamation'} library="sd-status-assets"></sd-icon>
-            {link ? 'Available' : 'Currently unavailable'}
+        return (
+          <div key={name} className="flex flex-row items-center gap-4">
+            {link ? <sd-link href={link}>{linkName}</sd-link> : <p>{linkName}</p>}
+
+            <div className={`m-0 sd-status-badge ${link ? `sd-status-badge--success` : `sd-status-badge--warning`}`}>
+              <sd-icon name={link ? 'status-check' : 'status-exclamation'} library="sd-status-assets"></sd-icon>
+              {link ? 'Available' : 'Currently unavailable'}
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 
