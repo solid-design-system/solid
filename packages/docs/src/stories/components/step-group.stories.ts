a5b72a75ed9042f4e9cc99df01d74185b9396c20
Blagoja Mojsoski <55854229+balco0110@users.noreply.github.com>
2025-07-24 16:44:41 +0200

    chore: adjusted the step group docs according to figma

--- packages/docs/src/stories/components/step-group.stories.ts
+++ packages/docs/src/stories/components/step-group.stories.ts
@@ -195,15 +195,15 @@ export const NotInteractive = {
   render: () => html`
     <sd-step-group size="lg" orientation="horizontal" active-step="0" not-interactive>
       <sd-step size="lg" orientation="horizontal">
-        <div slot="label">Label</div>
+        <div slot="label">Step 1</div>
       </sd-step>
 
       <sd-step size="lg" orientation="horizontal" current>
-        <span slot="label">Label</span>
+        <span slot="label">Step 2</span>
       </sd-step>
 
       <sd-step size="lg" orientation="horizontal" disabled>
-        <span slot="label">Label</span>
+        <span slot="label">Step 3</span>
       </sd-step>
     </sd-step-group>
   `
@@ -218,17 +218,17 @@ export const Icon = {
     <sd-step-group size="lg" orientation="horizontal" active-step="0" not-interactive>
       <sd-step size="lg" orientation="horizontal" not-interactive>
         <sd-icon slot="circle-content" name="content/image" class="h-12 w-12"></sd-icon>
-        <div slot="label">Label</div>
+        <div slot="label">Step 1</div>
       </sd-step>
 
       <sd-step size="lg" orientation="horizontal" not-interactive>
         <sd-icon slot="circle-content" name="content/image" class="h-12 w-12"></sd-icon>
-        <div slot="label">Label</div>
+        <div slot="label">Step 2</div>
       </sd-step>
 
       <sd-step size="lg" orientation="horizontal" not-interactive>
         <sd-icon slot="circle-content" name="content/image" class="h-12 w-12"></sd-icon>
-        <div slot="label">Label</div>
+        <div slot="label">Step 3</div>
       </sd-step>
     </sd-step-group>
   `
