60565d8477e31e6ebe5a21d69e4b3ad0f0ee065f
Sérgio Fonseca <sergio.fonseca@virtual-identity.com>
2025-10-27 10:47:47 +0000

    chore: add locale aware screenshot test

--- packages/docs/src/stories/components/datepicker.test.stories.ts
+++ packages/docs/src/stories/components/datepicker.test.stories.ts
@@ -136,6 +136,16 @@ export const Mouseless = {
   }
 };
 
+export const LocaleAware = {
+  name: 'Locale Aware',
+  render: () => {
+    return html`<div class="flex flex-col gap-4">
+      <sd-datepicker lang="de"></sd-datepicker>
+      <sd-datepicker lang="de" range rangeStart="2025.10.02" rangeEnd="2025.10.12"></sd-datepicker>
+    </div>`;
+  }
+};
+
 export const Combination = generateScreenshotStory([
   Default,
   Size,
@@ -143,5 +153,7 @@ export const Combination = generateScreenshotStory([
   Placeholder,
   Disabled,
   DisabledWeekends,
-  DisabledDates
+  DisabledDates,
+  Mouseless,
+  LocaleAware
 ]);
