f4674304e2008919d6c847761747826e18fcd1ba
Paulo Vareiro <paulo.vareiro@virtual-identity.com>
2025-10-28 17:08:54 +0000

    feat: generate font-family

--- packages/tokens/scripts/tailwind/token-processors/typography.js
+++ packages/tokens/scripts/tailwind/token-processors/typography.js
@@ -6,38 +6,48 @@ import { BaseTokenProcessor } from './base.js';
 export class TypographyTokenProcessor extends BaseTokenProcessor {
   constructor(options = {}) {
     super(options);
+
+    this.tokensMap = {
+      'font-family': 'font',
+      'font-size': null
+    };
   }
 
   canProcess(token) {
-    return token.type === 'fontSize' || token.type === 'fontWeight';
+    return token.type === 'fontSize' || token.type === 'fontWeight' || token.path.includes('font-family');
   }
 
   process(token) {
     const processed = this.processTokenPath(token);
     const value = this.getTokenValue(token);
 
-    if (['font-size'].includes(processed.path[0])) {
+    if (['utilities', 'components'].includes(processed.path[0])) {
       processed.path = processed.path.slice(1);
     }
 
+    const mapped = this.tokensMap[processed.path[0]];
+    if (mapped !== undefined) {
+      processed.path[0] = mapped;
+      processed.path = processed.path.filter(Boolean);
+    }
+
     const name = processed.path.join('-');
-    const variable = {
+
+    const cssvariables = [];
+    cssvariables.push({
       type: 'spacing',
       name: `--${name}`,
       value: this.cssvar(name),
       variant: 'default'
-    };
+    });
 
-    const core =
-      processed.variant !== 'default'
-        ? {
-            ...variable,
-            name: this.cssprefix(name),
-            value,
-            variant: processed.variant
-          }
-        : undefined;
+    cssvariables.push({
+      type: 'spacing',
+      name: this.cssprefix(name),
+      value: token.type === 'string' ? `'${value}'` : value,
+      variant: processed.variant
+    });
 
-    return [variable, core];
+    return cssvariables;
   }
 }
