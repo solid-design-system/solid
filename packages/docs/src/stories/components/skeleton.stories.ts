904abc94ad90c699c0f3636d4cf571923137dcbc
Mojsoski, Blagoja (UIS) <blagoja.mojsoski@union-investment.de>
2025-11-11 15:43:18 +0100

    chore: resolved the comments regarding the variants in docs and headline size in the overview

--- packages/docs/src/stories/components/skeleton.stories.ts
+++ packages/docs/src/stories/components/skeleton.stories.ts
@@ -40,3 +40,23 @@ export const Default = {
     `;
   }
 };
+
+/**
+ * Use the `variant` attribute to set the skeleton’s variant:
+ *
+ * - rectangular: Variant’s height can be adjusted according to needs
+ * - circular: Variant’s width and height can be adjusted according to needs
+ */
+
+export const Variants = {
+  render: () => {
+    return html`
+      <div class="h-full">
+        <div class="flex flex-row gap-4 w-full">
+          <sd-skeleton class="w-[154px] h-4"></sd-skeleton>
+          <sd-skeleton variant="circular" class="h-4 w-4"></sd-skeleton>
+        </div>
+      </div>
+    `;
+  }
+};
