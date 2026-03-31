80fa0693fea2956922d000c83b29be9d191eb961
Sérgio Fonseca <42741644+smfonseca@users.noreply.github.com>
2026-03-31 21:29:49 +0100

    feat: brandshape transparent variants (#2874)
    <!-- ## Title: Please consider adding the [skip chromatic] flag to the
    PR title in case you dont need chromatic testing your changes. -->
    ## Description:
    Closes #1553 
    
    ## Definition of Reviewable:
    <!-- *PR notes: Irrelevant elements should be removed.* -->
    - [ ] Documentation is created/updated
    - [ ] Migration Guide is created/updated
    <!-- *PR notes: If this PR includes a BREAKING CHANGE, a migration guide
    is needed to explain how users can migrate between versions. * -->
    - [ ] E2E tests (features, a11y, bug fixes) are created/updated
    <!-- *If this PR includes a bug fix, an E2E test is necessary to verify
    the change. If the fix is purely visual, ensuring it is captured within
    our chromatic screenshot tests is sufficient.* -->
    - [ ] Stories (features, a11y) are created/updated
    - [ ] relevant tickets are linked

--- packages/docs/src/stories/templates/brandshape.stories.ts
+++ packages/docs/src/stories/templates/brandshape.stories.ts
@@ -31,3 +31,57 @@ export const Default = {
       </p>
     </sd-brandshape>`
 };
+
+export const Transparent = {
+  name: 'Brandshape with transparent variant',
+  render: () =>
+    html` <style>
+        .brandshape-wrapper {
+          background: url('./placeholders/images/skyline.jpg');
+          background-size: cover;
+          background-position: center;
+          padding: 3rem;
+        }
+      </style>
+      <div class="brandshape-wrapper">
+        <sd-brandshape variant="primary|80">
+          <h4 class="sd-headline sd-headline--3xl mb-4 text-white">
+            Exclusion criteria: Ensure minimum standards, avoid controversies
+          </h4>
+          <p class="sd-paragraph text-white">
+            In this approach, single or multiple criteria are defined that exclude investment in certain companies,
+            industries or countries. The individual criteria can be determined individually within the SIRIS platform,
+            such as the exclusion of companies that generate more than 5 per cent of their turnover from gambling. The
+            exclusion criteria are additionally reviewed in a two-stage research process.
+          </p>
+          <sd-button variant="cta" class="mt-4">Find out more</sd-button>
+        </sd-brandshape>
+      </div>`
+};
+
+export const Copyright = {
+  name: 'Brandshape with Copyright',
+  render: () =>
+    html` <style>
+        sd-brandshape::part(content) {
+          height: 200px;
+        }
+
+        .sd-copyright::after {
+          bottom: 60px;
+          padding-right: 8px;
+        }
+      </style>
+      <sd-brandshape
+        variant="image"
+        class="sd-copyright sd-copyright--orientation-vertical"
+        style="--copyright: '© Union Investment 2025';"
+      >
+        <img
+          slot="image"
+          src="./placeholders/images/skyline.jpg"
+          alt="A vibrant city skyline at dusk, symbolizing economic growth and investment opportunities in urban hubs"
+          class="object-bottom"
+        />
+      </sd-brandshape>`
+};
