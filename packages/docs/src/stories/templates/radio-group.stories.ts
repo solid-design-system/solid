a4b037cafa6ba84103954e2f6d72570f3ca41539
Blagoja Mojsoski <55854229+balco0110@users.noreply.github.com>
2026-04-21 15:50:22 +0200

    feat: added realistic radio-group template (#2911)
    <!-- ## Title: Please consider adding the [skip chromatic] flag to the
    PR title in case you dont need chromatic testing your changes. -->
    ## Description:
    <!-- *PR notes: Please describe the changes in this PR.* -->
    Closes: https://github.com/solid-design-system/solid/issues/2149
    ## Definition of Reviewable:
    <!-- *PR notes: Irrelevant elements should be removed.* -->
    - [x] Documentation is created/updated
    <!-- *PR notes: If this PR includes a BREAKING CHANGE, a migration guide
    is needed to explain how users can migrate between versions. * -->
    - [x] E2E tests (features, a11y, bug fixes) are created/updated
    <!-- *If this PR includes a bug fix, an E2E test is necessary to verify
    the change. If the fix is purely visual, ensuring it is captured within
    our chromatic screenshot tests is sufficient.* -->
    - [x] Stories (features, a11y) are created/updated
    - [x] relevant tickets are linked
    
    ---------
    
    Co-authored-by: Sérgio Fonseca <42741644+smfonseca@users.noreply.github.com>

---
+++ packages/docs/src/stories/templates/radio-group.stories.ts
@@ -0,0 +1,64 @@
+import '../../../../components/src/solid-components';
+import { html } from 'lit-html';
+
+export default {
+  tags: ['!dev', 'autodocs'],
+  title: 'Templates/Radio Group',
+  parameters: {
+    chromatic: { disableSnapshot: true },
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/YDktJcseQIIQbsuCpoKS4V/Component-Docs?node-id=2213-9389&t=5PpAC3TA3kYF7ufX-0'
+    }
+  }
+};
+
+export const radioGroupWithHelpText = {
+  name: 'Radio Group with Help text',
+  render: () => html`
+    <sd-radio-group
+      name="greeting"
+      value="mr"
+      label="Greeting"
+      orientation="horizontal"
+      help-text="Please select your preferred salutation."
+      required
+    >
+      <sd-radio value="mr">Mr.</sd-radio>
+      <sd-radio value="ms">Ms.</sd-radio>
+      <sd-radio value="non-binary">Non-binary</sd-radio>
+    </sd-radio-group>
+  `
+};
+
+export const radioGroupWithErrorText = {
+  name: 'Radio Group with Error text',
+  render: () => html`
+    <sd-radio-group
+      id="radio-group-error"
+      name="role"
+      label="What is your role/function?"
+      orientation="vertical"
+      required
+    >
+      <sd-radio value="advisor-in-service">Advisor in service</sd-radio>
+      <sd-radio value="customer-advisor">Customer advisor</sd-radio>
+      <sd-radio value="securities-specialist">Securities specialist/Wealth advisor</sd-radio>
+      <sd-radio value="online-branch-employee">Online branch employee</sd-radio>
+      <sd-radio value="other">Other</sd-radio>
+    </sd-radio-group>
+
+    <script type="module">
+      await customElements.whenDefined('sd-radio-group');
+
+      const radioGroup = document.querySelector('#radio-group-error');
+      radioGroup.setCustomValidity('Select an option to proceed.');
+      radioGroup.reportValidity();
+
+      radioGroup.addEventListener('sd-change', () => {
+        radioGroup.setCustomValidity('');
+        radioGroup.reportValidity();
+      });
+    </script>
+  `
+};
