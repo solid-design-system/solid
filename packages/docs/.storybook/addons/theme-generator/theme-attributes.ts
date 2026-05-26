d61a5146f6380b5a5f52b0ee776aed60070066f9
martascorreia <marta.correia@virtual-identity.com>
2026-05-26 14:07:13 +0100

    feat: adding bbbank logos

--- packages/docs/.storybook/addons/theme-generator/theme-attributes.ts
+++ packages/docs/.storybook/addons/theme-generator/theme-attributes.ts
@@ -1,35 +1,36 @@
 export const themeAttributes = {
   'sd-theme-ui-light': {
-    logoLg: 'https://solid-design-system.fe.union-investment.de/docs/images/logo-unioninvestment-lg.svg',
-    logoSm: 'https://solid-design-system.fe.union-investment.de/docs/images/logo-unioninvestment-sm.svg',
+    logoLg: '/images/logo-unioninvestment-lg.svg',
+    logoSm: '/images/logo-unioninvestment-sm.svg',
     alt: 'Union Investment Homepage',
     css: 'ui-light/ui-light.css'
   },
 
   'sd-theme-ui-dark': {
-    logoLg: 'https://solid-design-system.fe.union-investment.de/docs/images/logo-unioninvestment-inverted-lg.svg',
-    logoSm: 'https://solid-design-system.fe.union-investment.de/docs/images/logo-unioninvestment-inverted-sm.svg',
+    logoLg: '/images/logo-unioninvestment-inverted-lg.svg',
+    logoSm: '/images/logo-unioninvestment-inverted-sm.svg',
     alt: 'Union Investment Homepage',
     css: 'ui-dark/ui-dark.css'
   },
 
   'sd-theme-vb': {
-    logoLg: 'https://solid-design-system.fe.union-investment.de/docs/images/logo-VB-lg.svg',
-    logoSm: 'https://solid-design-system.fe.union-investment.de/docs/images/logo-VB-sm.svg',
+    logoLg: '/images/logo-VB-lg.svg',
+    logoSm: '/images/logo-VB-sm.svg',
     alt: 'Volksbanken Homepage',
     css: 'vb/vb.css'
   },
 
   'sd-theme-kid': {
-    logoLg: 'https://solid-design-system.fe.union-investment.de/docs/images/logo-kidstarter-lg.svg',
-    logoSm: 'https://solid-design-system.fe.union-investment.de/docs/images/logo-kidstarter-sm.svg',
+    logoLg: '/images/logo-kidstarter-lg.svg',
+    logoSm: '/images/logo-kidstarter-sm.svg',
     alt: 'Kidstarter Homepage',
     css: 'kid/kid.css'
   },
 
   'sd-theme-bb': {
-    logoLg: 'https://solid-design-system.fe.union-investment.de/docs/images/logo-bbbank-lg.svg',
-    logoSm: 'https://solid-design-system.fe.union-investment.de/docs/images/logo-bbbank-sm.svg',
+    logoLg: '/images/logo-bbbank-lg.svg',
+    logoMd: '/images/logo-bbbank-md.svg',
+    logoSm: '/images/logo-bbbank-sm.svg',
     alt: 'BB Bank Homepage',
     css: 'bb/bb.css'
   }
