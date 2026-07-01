bdc3e846047e674487fbbab2c440694b6547bd52
auroraVasconcelos <aurorabsvasconcelos@gmail.com>
2026-07-01 18:06:48 +0100

    fix: alignments

--- packages/components/src/components/combobox/combobox.ts
+++ packages/components/src/components/combobox/combobox.ts
@@ -1285,7 +1285,11 @@ export default class SdCombobox extends SolidElement implements SolidFormControl
                   class=${cx(
                     'absolute left-4 z-20 pointer-events-none transition-all duration-200',
                     hasIconLeft ? floatingLabelHorizontalAlignmentWithIconLeft : 'left-4',
-                    !isFloatingLabelActive ? 'top-1/2 -translate-y-1/2 form-control-color-text' : 'top-1 text-xs',
+                    !isFloatingLabelActive
+                      ? 'top-1/2 -translate-y-1/2 form-control-color-text'
+                      : this.size === 'lg'
+                        ? 'top-2 text-xs'
+                        : 'top-1 text-xs',
                     isFloatingLabelActive && 'mt-1'
                   )}
                   for="input"
