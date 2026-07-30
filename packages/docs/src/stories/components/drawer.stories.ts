aeb06a239ccd54283d9e5b68447e28c912a696c4
Mojsoski, Blagoja (UIS) <mojsoski93@gmail.com>
2026-07-30 09:25:42 +0200

    chore: fixed the action buttons

--- packages/docs/src/stories/components/drawer.stories.ts
+++ packages/docs/src/stories/components/drawer.stories.ts
@@ -84,8 +84,8 @@ export const Open = {
         </sd-button>
         <div class="slot slot--border slot--text h-full">Default slot</div>
         <div slot="footer" class="flex flex-col w-full gap-4">
-          <sd-button variant="primary">Primary Action</sd-button>
-          <sd-button variant="secondary">Secondary Action</sd-button>
+          <sd-button variant="primary">Primary action</sd-button>
+          <sd-button variant="secondary">Secondary action</sd-button>
         </div>
       </sd-drawer>
     </div>
