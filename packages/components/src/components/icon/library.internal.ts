2fa2fc6ec1f8ed848f640e06296c6554038e39db
martascorreia <marta.correia@virtual-identity.com>
2026-08-18 09:27:31 +0100

    feat: renaming icons

--- packages/components/src/components/icon/library.internal.ts
+++ packages/components/src/components/icon/library.internal.ts
@@ -33,12 +33,12 @@ export const icons = {
       <path d="m7.6 13.3.6.8.3-.3.7-.6V18h1.3v-6H9.4zm7-1.4q-2.2 0-2.2 3.1 0 3 2.2 3c2.2 0 2.2-1 2.2-3q0-3.1-2.1-3.1m0 5.3q-1 0-1-2.3t1-2.2c1 0 1 .8 1 2.3q0 2.2-1 2.2"/>
     </svg>
   `,
-  'chevron-down': `
+  'chevron-bottom': `
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
       <path d="M20.3 6.3 12 15.5 3.7 6.3 3 6a1 1 0 0 0-.7 1.7l9 10a1 1 0 0 0 1.4 0l9-10q.3-.3.3-.7a1 1 0 0 0-1.7-.7"/>
     </svg>
   `,
-  'chevron-up': `
+  'chevron-top': `
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
       <path d="M12.7 6.3a1 1 0 0 0-1.4 0l-9 10-.3.7a1 1 0 0 0 1.7.7L12 8.5l8.3 9.2q.2.2.7.3a1 1 0 0 0 .7-1.7z"/>
     </svg>
@@ -59,11 +59,11 @@ export const icons = {
   'chevron-sm-left': `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
     <path fill="currentColor" d="M14.742 6.333a.997.997 0 0 0-1.41-.076l.001-.001-6 5a.997.997 0 0 0 0 1.486v.001l6 5a1 1 0 0 0 1.333-1.487l-5.173-4.257 5.173-4.257a.997.997 0 0 0 .076-1.409Z"/>
   </svg>`,
-  'chevrons-sm-left': `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+  'chevrons-small-left': `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
     <path fill="currentColor" d="M11.74 6.333a.997.997 0 0 0-1.41-.076l.001-.001-6 5a.997.997 0 0 0-.001 1.486l.001.001 6 5a1 1 0 0 0 1.333-1.487l-5.173-4.257 5.173-4.257a.997.997 0 0 0 .076-1.409Z"/>
     <path fill="currentColor" d="M18.74 6.333a.997.997 0 0 0-1.41-.076l.001-.001-6 5a.997.997 0 0 0-.001 1.486l.001.001 6 5a1 1 0 0 0 1.333-1.487l-5.173-4.257 5.173-4.257a.997.997 0 0 0 .076-1.409Z"/>
   </svg>`,
-  'chevrons-sm-right': `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+  'chevrons-small-right': `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
     <path fill="currentColor" d="M13.718 6.257a1 1 0 1 0-1.336 1.485h.002L17.557 12l-5.173 4.257a1 1 0 0 0 1.336 1.485h-.002l6-5a.997.997 0 0 0 .001-1.485l-.001-.001-6-5Z"/>
     <path fill="currentColor" d="M6.718 6.257a1 1 0 1 0-1.336 1.485h.002L10.557 12l-5.173 4.257a1 1 0 0 0 1.336 1.485h-.002l6-5a.997.997 0 0 0 .001-1.485l-.001-.001-6-5Z"/>
   </svg>`,
@@ -78,13 +78,13 @@ export const icons = {
       <path d="M21.7 2.3a1 1 0 0 0-1.4 0L12 10.6 3.7 2.3a1 1 0 0 0-1.4 1.4l8.3 8.3-8.3 8.3a1 1 0 0 0 0 1.4 1 1 0 0 0 1.4 0l8.3-8.3 8.3 8.3a1 1 0 0 0 1.4 0 1 1 0 0 0 0-1.4L13.4 12l8.3-8.3a1 1 0 0 0 0-1.4"/>
     </svg>
   `,
-  'closing-round': `
+  'close-circle': `
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
       <path d="M12 2a10 10 0 1 0 0 20 10 10 0 0 0 0-20m0 18a8 8 0 1 1 0-16 8 8 0 0 1 0 16"/>
       <path d="M16.4 7.6a1 1 0 0 0-1.4 0l-3 3-3-3A1 1 0 0 0 7.6 9l3 3-3 3A1 1 0 0 0 9 16.4l3-3 3 3a1 1 0 0 0 1.4-1.4l-3-3 3-3a1 1 0 0 0 0-1.4"/>
     </svg>
   `,
-  eye: `
+  'eye-open': `
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
       <path d="M12 19c-5.3 0-10-5.7-10-7s4.7-7 10-7 10 5.7 10 7-4.7 7-10 7m-7.9-7c.8 1.2 4 5 7.9 5s7.1-3.8 7.9-5c-.8-1.2-4-5-7.9-5s-7.1 3.8-7.9 5"/>
       <path d="M12 16.1A4 4 0 0 1 7.9 12 4 4 0 0 1 12 7.9a4 4 0 0 1 4.1 4.1 4 4 0 0 1-4.1 4.1m0-6.2a2.1 2.1 0 1 0 0 4.2 2.1 2.1 0 0 0 0-4.2"/>
@@ -102,16 +102,6 @@ export const icons = {
       <path d="M12 2a10 10 0 1 0 0 20 10 10 0 0 0 0-20m0 18a8 8 0 1 1 0-16 8 8 0 0 1 0 16"/>
       <path d="M13.3 7.8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m-.3 3.9a1 1 0 0 0-1-1h-1a1 1 0 0 0 0 2v2.6h2zM11 16.3v-1h-1a1 1 0 0 0 0 2h2a1 1 0 0 1-1-1m3-1h-1v1a1 1 0 0 1-1 1h2a1 1 0 0 0 0-2"/>
       <path d="M13 16.3v-1h-2v1a1 1 0 0 0 2 0"/>
-    </svg>
-  `,
-  'status-check': `
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 13">
-      <path d="M10 1.1 4.3 9.5 1.9 7a.6.6 0 0 0-1 .5L1 8l3 3 .5.1q.4 0 .5-.2l6-9a1 1 0 0 0-.2-1 1 1 0 0 0-.9.2"/>
-    </svg>
-  `,
-  'status-minus': `
-    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12">
-      <path d="M10 5.4H2a.6.6 0 0 0 0 1.2h8a.6.6 0 0 0 0-1.2"/>
     </svg>
   `,
   'minus-circle': `
@@ -137,7 +127,7 @@ export const icons = {
       <path d="M11 10.7V15a1 1 0 0 0 2 0v-4.3a1 1 0 0 0-2 0m2 7.3a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
     </svg>
   `,
-  start: `
+  play: `
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
       <path d="m57.4 29.8-37.3-24A2.7 2.7 0 0 0 16 8v48a2.7 2.7 0 0 0 2.7 2.7 3 3 0 0 0 1.4-.5l37.3-24a3 3 0 0 0 1.2-2.2q0-1.4-1.2-2.2M21.4 51V13L51 32z"/>
     </svg>
@@ -146,12 +136,6 @@ export const icons = {
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
       <path d="M12 2a10 10 0 1 0 0 20 10 10 0 0 0 0-20m0 18a8 8 0 1 1 0-16 8 8 0 0 1 0 16"/>
       <path d="M15.8 7.5a1 1 0 0 0-1.4.3L10.9 14 9 12.3a1 1 0 0 0-1.6.7q0 .4.2.7l2.7 2.7q.3.2.7.3h.1q.5-.1.8-.5l4.3-7.4.1-.5a1 1 0 0 0-.5-.8"/>
-    </svg>
-  `,
-  warning: `
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-      <path d="M12.9 2.6a1 1 0 0 0-1.8 0l-9 18-.1.4q0 1 1 1h18a1 1 0 0 0 .9-1.5zM4.6 20 12 5.2 19.4 20z"/>
-      <path d="M11 10.7V15a1 1 0 0 0 2 0v-4.3a1 1 0 0 0-2 0m2 7.3a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
     </svg>
   `,
   'exclamation-circle': `
