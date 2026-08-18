18fc276d9e49f84a93d3f5216e34bf8252f6b60c
martascorreia <marta.correia@virtual-identity.com>
2026-08-18 09:56:38 +0100

    fix: more icon renaming

--- packages/components/src/components/carousel/carousel.ts
+++ packages/components/src/components/carousel/carousel.ts
@@ -733,7 +733,7 @@ export default class SdCarousel extends SolidElement {
                 <sd-icon
                   class=${cx('h-6 w-6 rotate-90 grid place-items-center')}
                   library="_internal"
-                  name="${isLtr ? 'chevron-down' : 'chevron-up'}"
+                  name="${isLtr ? 'chevron-bottom' : 'chevron-top'}"
                 ></sd-icon>
               </slot>
             </button>
@@ -839,7 +839,7 @@ export default class SdCarousel extends SolidElement {
                 <sd-icon
                   class=${cx('h-6 w-6 rotate-90 grid place-items-center')}
                   library="_internal"
-                  name="${isLtr ? 'chevron-up' : 'chevron-down'}"
+                  name="${isLtr ? 'chevron-bottom' : 'chevron-top'}"
                 ></sd-icon>
               </slot>
             </button>
