628a906b0d5f239af15a8768ef6e6c29957ff59a
auroraVasconcelos <aurorabsvasconcelos@gmail.com>
2026-08-18 16:39:13 +0100

    fix: sd-chip template

--- packages/docs/src/stories/templates/chip.stories.ts
+++ packages/docs/src/stories/templates/chip.stories.ts
@@ -14,33 +14,98 @@ export default {
 };
 
 /**
- * ### Background Options
- *
  * Recommended combinations:
- * - `primary-200` on white background
- * - `primary-300` on primary-100 background
- * - `primary-500` on primary background
+ * - `primary-subtle` or `neutral-subtle` on white background
+ * - `primary-low` on primary-100 background
+ * - `neutral-low` on neutral-100 background
+ * - `primary-high` on primary background
  * - `white` on image background
  */
 export const BackgroundOptions = {
   render: () =>
-    html` <div class="grid grid-cols-2 grid-rows-2">
-      <div class="bg-white p-4 border border-neutral-200">
-        <span class="sd-chip sd-chip--primary-200">primary-200</span>
+    html`<div class="grid grid-cols-3 gap-px bg-neutral-400 border border-neutral-400">
+        <div class="bg-white py-6 px-8 flex flex-col gap-2 items-start min-h-32">
+          <span class="sd-chip">primary-subtle</span>
+          <span class="sd-chip sd-chip--color-neutral">neutral-subtle</span>
+        </div>
+        <div class="bg-primary-100 py-6 px-8 min-h-32">
+          <span class="sd-chip sd-chip--shade-low">primary-low</span>
+        </div>
+        <div class="bg-neutral-100 py-6 px-8 min-h-32">
+          <span class="sd-chip sd-chip--color-neutral sd-chip--shade-low">neutral-low</span>
+        </div>
+        <div class="bg-primary py-6 px-8">
+          <span class="sd-chip sd-chip--shade-high">primary-high</span>
+        </div>
+        <div class="relative col-span-2 py-6 px-8 min-h-32">
+          <img
+            class="absolute inset-0 w-full h-full object-cover"
+            src="./placeholders/images/coffeeshop.jpg"
+            alt="A group of people sitting in a coffee shop"
+          />
+          <div class="relative">
+            <span class="sd-chip sd-chip--color-white">white</span>
+          </div>
+        </div>
       </div>
-      <div class="bg-primary-100 p-4">
-        <span class=" sd-chip sd-chip--primary-300">primary-300</span>
+
+      <div class="grid grid-cols-3 gap-px bg-neutral-400 border border-neutral-400 mt-8">
+        <div class="bg-white py-6 flex flex-col gap-4 items-end min-h-32">
+          <span class="sd-chip sd-chip--shade-low sd-chip--size-lg sd-chip--sharp">primary-low</span>
+          <span class="sd-chip sd-chip--color-neutral sd-chip--shade-low sd-chip--size-lg sd-chip--sharp">
+            neutral-low
+          </span>
+        </div>
+        <div class="bg-primary-100 py-6 min-h-32 relative">
+          <span class="sd-chip sd-chip--shade-medium sd-chip--size-lg sd-chip--sharp absolute right-0"
+            >primary-medium</span
+          >
+        </div>
+        <div class="bg-neutral-100 py-6 min-h-32 relative">
+          <span
+            class="sd-chip sd-chip--color-neutral sd-chip--shade-medium sd-chip--size-lg sd-chip--sharp absolute right-0"
+            >neutral-medium</span
+          >
+        </div>
+        <div class="bg-primary py-6 relative">
+          <span class="sd-chip sd-chip--shade-high sd-chip--size-lg sd-chip--sharp absolute right-0">primary-high</span>
+        </div>
+        <div class="relative col-span-2 py-6 min-h-32">
+          <img
+            class="absolute inset-0 w-full h-full object-cover"
+            src="./placeholders/images/coffeeshop.jpg"
+            alt="A group of people sitting in a coffee shop"
+          />
+          <div class="relative">
+            <span class="sd-chip sd-chip--color-neutral sd-chip--size-lg sd-chip--sharp absolute right-0"
+              >neutral-subtle</span
+            >
+          </div>
+        </div>
+      </div>`
+};
+
+/**
+ * Recommended combinations:
+ * - `primary-medium` or `neutral-medium` on white background
+ * - `primary-medium` on primary-100 background
+ * - `neutral-medium` on neutral-100 background
+ */
+
+export const BackgroundOptionsOutlined = {
+  name: '',
+  render: () => html`
+    <div class="grid grid-cols-3 gap-px bg-neutral-400 border border-neutral-400 mt-4">
+      <div class="bg-white py-6 px-8 flex flex-col gap-2 items-start min-h-32">
+        <span class="sd-chip sd-chip--shade-medium sd-chip--outline">primary-medium</span>
+        <span class="sd-chip sd-chip--color-neutral sd-chip--shade-medium sd-chip--outline">neutral-medium</span>
       </div>
-      <div class="bg-primary p-4">
-        <span class="sd-chip sd-chip--primary-500">primary-500</span>
+      <div class="bg-primary-100 py-6 px-8 min-h-32">
+        <span class="sd-chip sd-chip--shade-medium sd-chip--outline">primary-medium</span>
       </div>
-      <div slot="media" class="relative">
-        <img
-          class="aspect-video object-cover"
-          src="./placeholders/images/coffeeshop.jpg"
-          alt="A group of people sitting in a coffee shop"
-        />
-        <span class="absolute top-3 left-4 sd-chip sd-chip--white">white</span>
+      <div class="bg-neutral-100 py-6 px-8 min-h-32">
+        <span class="sd-chip sd-chip--color-neutral sd-chip--shade-medium sd-chip--outline">neutral-medium</span>
       </div>
-    </div>`
+    </div>
+  `
 };
