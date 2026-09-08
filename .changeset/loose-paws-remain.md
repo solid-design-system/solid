9d01a424c27287e2157bfc3441f4979d166f0b31
rahulsuvarna18 <rahulsuvarna18@gmail.com>
2026-09-08 15:04:38 +0200

    feat: added the new story in the dropdown template

---
+++ .changeset/loose-paws-remain.md
@@ -0,0 +1,5 @@
+---
+'@solid-design-system/docs': minor
+---
+
+Added a new story in the dropdown template.
--- packages/docs/src/stories/templates/dropdown.stories.ts
+++ packages/docs/src/stories/templates/dropdown.stories.ts
@@ -42,8 +42,12 @@ export const Default = {
     </div>
 
     <script type="module">
-      await Promise.all([customElements.whenDefined('sd-navigation-item')]).then(() => {
+      await Promise.all([
+        customElements.whenDefined('sd-dropdown'),
+        customElements.whenDefined('sd-navigation-item')
+      ]).then(() => {
         const dropdown = document.getElementById('dropdown');
+        dropdown.containingElement = document.body;
         const navigationItems = dropdown.querySelectorAll('sd-navigation-item[href]');
 
         const handleNavigationItemClick = e => {
@@ -54,6 +58,47 @@ export const Default = {
         };
 
         navigationItems.forEach(item => item.addEventListener('click', handleNavigationItemClick));
+      });
+    </script>
+  `
+};
+
+/**
+ * Example of how to use a dropdown as a contextual action menu triggered by an icon button. A list of navigation items come with an icon on the left. Use it for making actions like Share, Download, and Delete quickly to scan and access.
+ */
+
+export const MenuWithIcons = {
+  name: 'Dropdown with Menu Items',
+  render: () => html`
+    <div class="min-h-[300px]">
+      <sd-dropdown id="dropdown-menu-items" distance="4" rounded open stay-open-on-select>
+        <sd-button variant="secondary" slot="trigger">
+          <sd-icon name="system/more-functions" label="Icon only"></sd-icon>
+        </sd-button>
+        <sd-menu>
+          <sd-button variant="secondary" slot="trigger">
+            <sd-icon name="system/more-functions" label="Icon only"></sd-icon>
+          </sd-button>
+          <sd-menu-item>
+            <sd-icon name="system/share" slot="icon-indent"></sd-icon>
+            Share
+          </sd-menu-item>
+          <sd-menu-item>
+            <sd-icon name="system/download" slot="icon-indent"></sd-icon>
+            Download
+          </sd-menu-item>
+          <sd-menu-item>
+            <sd-icon name="system/trash" slot="icon-indent"></sd-icon>
+            Delete
+          </sd-menu-item>
+        </sd-menu>
+      </sd-dropdown>
+    </div>
+
+    <script type="module">
+      customElements.whenDefined('sd-dropdown').then(() => {
+        const dropdown = document.getElementById('dropdown-menu-items');
+        dropdown.containingElement = document.body;
       });
     </script>
   `
